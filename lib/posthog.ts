f4f248860b413a1d31495bed6fccec1e7b5f8a69
Matthias Nannt <mail@matthiasnannt.com>
2022-08-17 22:57:53 +0200

    disable posthog autotracking, send specific events via backend (#62)

--- lib/posthog.ts
+++ lib/posthog.ts
@@ -1,48 +1,27 @@
-import { useRouter } from "next/router";
 import getConfig from "next/config";
-import posthog from "posthog-js";
-import { useEffect } from "react";
+import PostHog from "posthog-node";
 
-const { publicRuntimeConfig } = getConfig();
+const { serverRuntimeConfig } = getConfig();
 const enabled =
-  publicRuntimeConfig.posthogApiKey && publicRuntimeConfig.posthogApiHost;
+  serverRuntimeConfig.posthogApiKey && serverRuntimeConfig.posthogApiHost;
 
-export const usePosthog = () => {
-  const router = useRouter();
-
-  useEffect(() => {
-    if (enabled) {
-      // Init PostHog
-      posthog.init(publicRuntimeConfig.posthogApiKey, {
-        api_host: publicRuntimeConfig.posthogApiHost,
-        loaded: function (posthog) {
-          if (process.env.NODE_ENV === "development")
-            posthog.opt_out_capturing();
-        },
-      });
-    }
-    // Track page views
-    const handleRouteChange = () => {
-      if (enabled) {
-        posthog.capture("$pageview");
-      }
-    };
-    router.events.on("routeChangeComplete", handleRouteChange);
-
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
-};
-
-export const trackPosthogEvent = (eventName: string, attributes: object) => {
-  if (enabled) {
-    posthog.capture(eventName, attributes);
+export const caputurePosthogEvent = (userId, eventName, properties = {}) => {
+  if (!enabled) {
+    return;
   }
-};
+  const client = new PostHog(
+    serverRuntimeConfig.posthogApiKey,
+    { host: serverRuntimeConfig.posthogApiHost } // You can omit this line if using PostHog Cloud
+  );
 
-export const identifyPoshogUser = (userId: string) => {
-  if (enabled) {
-    posthog.identify(userId);
-  }
+  console.log("send event!!!!!");
+
+  client.capture({
+    distinctId: userId,
+    event: eventName,
+    properties,
+  });
+
+  // On program exit, call shutdown to stop pending pollers and flush any remaining events
+  client.shutdown();
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,15 @@
-import App, { AppProps } from "next/app";
-import { SessionProvider } from "next-auth/react";
-import { ToastContainer } from "react-toastify";
-import { usePosthog } from "../lib/posthog";
 import "highlight.js/styles/tokyo-night-dark.css";
-import "../styles/globals.css";
+import { SessionProvider } from "next-auth/react";
+import { AppProps } from "next/app";
+import { ToastContainer } from "react-toastify";
 import "../styles/editorjs.css";
+import "../styles/globals.css";
 import "../styles/toastify.css";
 
 function SnoopApp({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) {
-  usePosthog();
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
@@ -19,12 +17,5 @@ function SnoopApp({
     </SessionProvider>
   );
 }
-
-SnoopApp.getInitialProps = async (appContext) => {
-  // calls page's `getInitialProps` and fills `appProps.pageProps`
-  const appProps = await App.getInitialProps(appContext);
-
-  return { ...appProps };
-};
 
 export default SnoopApp;
