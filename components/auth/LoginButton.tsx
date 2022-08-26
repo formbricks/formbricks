c4c377babde3ee447db560a529ef0b31ef371947
Midka <me@midka.dev>
2022-08-26 18:40:45 +0300

    feat(styling): LoginButton now uses StandardButton

--- components/auth/LoginButton.tsx
+++ components/auth/LoginButton.tsx
@@ -1,4 +1,5 @@
 import { IconType } from 'react-icons';
+import StandardButton from '../StandardButton';
 
 type Props = {
   text?: string;
@@ -14,13 +15,15 @@ const LoginButton = ({
   className = 'bg-black-700 text-white',
 }: Props) => {
   return (
-    <button onClick={onClick} className="grow">
-      <div
-        className={`flex gap-4 text-sm items-center p-3 rounded-lg align-middle ${className}`}
-      >
-        {Icon && <Icon size={25} />} {text}
-      </div>
-    </button>
+    <StandardButton
+      onClick={onClick}
+      className="grow relative flex items-center gap-3 bg-black-700 justify-center w-full pl-2 pr-4 py-2 text-sm font-medium text-white border border-transparent rounded-md shadow-sm"
+      icon
+    >
+      <div className="grow-0 absolute left-2">{Icon && <Icon size={25} />}</div>
+      {/* // className={`flex  text-sm items-center p-3 rounded-lg align-middle ${className}`} */}
+      <p className="text-center m-auto w-100">{text}</p>
+    </StandardButton>
   );
 };
 
