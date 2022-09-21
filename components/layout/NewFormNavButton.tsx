10270001b55d3c5c5048e22376eb7fbb78acc86e
mbuhendwa <mkbuhendwa@gmail.com>
2022-09-21 12:17:38 +0100

    fix: bug on undefined user role when accessing pages

--- components/layout/NewFormNavButton.tsx
+++ components/layout/NewFormNavButton.tsx
@@ -23,7 +23,8 @@ export default function NewFormNavButton({}) {
       >
         <nav className="hidden sm:flex" aria-label="Breadcrumb">
           <ol className="flex items-center space-x-4">
-            {session.user.role!==UserRole.ADMIN ? <></> : (<li>
+            {/* TODO: maybe create a separate page for candidates and not mix both views */}
+            {session.user?.role!==UserRole.ADMIN ? <></> : (<li>
               <div className="inline-flex items-center px-6 py-2 text-sm font-medium leading-4 bg-transparent border border-transparent hover:text-white focus:outline-none">
                 <PlusIcon className="-ml-0.5 mr-2 h-4 w-4" aria-hidden="true" />
                 create form
