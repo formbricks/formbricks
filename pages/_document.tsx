5d02e5cb40aa88e0b4d3d7314fbc3199940be908
CR3Y745 <creytas@gmail.com>
2022-09-28 10:54:03 +0100

    feat: change snoopform favicon by kda's one

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,18 +8,18 @@ export default function Document() {
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/favicons/favicon-32x32.png"
+          href="/favicons/favicon-kda.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/favicons/favicon-16x16.png"
+          href="/favicons/favicon-kda.png"
         />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/favicons/apple-touch-icon.png"
+          href="/favicons/favicon-kda.png"
         />
         <link rel="manifest" href="/favicons/site.webmanifest" />
         <link
