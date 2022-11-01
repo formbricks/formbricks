3157e85da6dd0e4eaaf42465d1f3828211fa01f6
Matthias Nannt <mail@matthiasnannt.com>
2022-11-01 10:01:16 +0100

    remove unused packages

--- apps/formbricks-com/pages/_app.tsx
+++ apps/formbricks-com/pages/_app.tsx
@@ -1,11 +1,5 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { Poppins } from "@next/font/google";
-import clsx from "clsx";
-
-const poppins = Poppins({
-  weight: "400",
-});
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
