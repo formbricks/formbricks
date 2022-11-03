e7e2cd3dc07fca03ee9bc9176a2fea9ef3aa39cf
Matthias Nannt <mail@matthiasnannt.com>
2022-11-03 11:42:28 +0100

    add plausibe to formbricks-com

--- apps/formbricks-com/pages/_app.tsx
+++ apps/formbricks-com/pages/_app.tsx
@@ -1,6 +1,14 @@
 import type { AppProps } from "next/app";
+import PlausibleProvider from "next-plausible";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <PlausibleProvider
+      customDomain="https://plausible.formbricks.com"
+      domain="formbricks.com"
+      selfHosted={true}>
+      <Component {...pageProps} />
+    </PlausibleProvider>
+  );
 }
