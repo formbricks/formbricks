b0f20d43d5f3620c2fc8b03af20fddfda565369f
knugget <johannes@knugget.de>
2022-11-03 20:54:47 +0200

    missing subpages

--- apps/formbricks-com/pages/get-started.tsx
+++ apps/formbricks-com/pages/get-started.tsx
@@ -13,54 +13,54 @@ import {
 } from "@heroicons/react/24/outline";
 
 const GetStartedPage = () => (
-  <Layout meta={{ title: "React Form Builder Library by Formbricks" }}>
-    <HeroTitle HeadingPt1="How do you want to" HeadingTeal="run" HeadingPt2="Formbricks?" />
-    <div className="grid grid-cols-2 gap-8 px-16 mb-32">
-      <div className="px-10 py-6 rounded-lg bg-gradient-to-b from-blue-200 to-gray-50 dark:from-blue-300 dark:to-gray-100">
-        <CloudIcon className="flex-shrink-0 w-20 h-20 text-blue-500" />
-        <h2 className="text-4xl font-bold text-blue mt-7">Cloud</h2>
+  <Layout meta={{ title: "Get started with Formbricks Open-source Form Infrastructure" }}>
+    <HeroTitle headingPt1="How do you want to" headingTeal="run" headingPt2="Formbricks?" />
+    <div className="mb-32 grid grid-cols-2 gap-8 px-16">
+      <div className="rounded-lg bg-gradient-to-b from-blue-200 to-gray-50 px-10 py-6 dark:from-blue-300 dark:to-gray-100">
+        <CloudIcon className="h-20 w-20 flex-shrink-0 text-blue-500" />
+        <h2 className="text-blue mt-7 text-4xl font-bold">Cloud</h2>
         <p className="text-sm text-gray-500">Managed hosting by Formbricks core team</p>
-        <p className="font-semibold mt-7">
+        <p className="mt-7 font-semibold">
           <span className="font-bold text-black">Free</span> for 500 submissions/mo
         </p>
         <p className="text-sm text-gray-500">then $0.01/submission</p>
-        <div className="flex items-center py-1 mt-7">
-          <InboxArrowDownIcon className="flex-shrink-0 mr-3 text-blue-500 h- w-7" />
-          <p className="font-medium text-blue">Start receiving submissions right away</p>
+        <div className="mt-7 flex items-center py-1">
+          <InboxArrowDownIcon className="h- mr-3 w-7 flex-shrink-0 text-blue-500" />
+          <p className="text-blue font-medium">Start receiving submissions right away</p>
         </div>
         <div className="flex items-center py-1">
-          <ArrowPathIcon className="flex-shrink-0 mr-3 text-blue-500 h-7 w-7" />
-          <p className="font-medium text-blue">Automatic upgrades</p>
+          <ArrowPathIcon className="mr-3 h-7 w-7 flex-shrink-0 text-blue-500" />
+          <p className="text-blue font-medium">Automatic upgrades</p>
         </div>
         <div className="flex items-center py-1">
-          <PuzzlePieceIcon className="flex-shrink-0 mr-3 text-blue-500 h-7 w-7" />
-          <p className="font-medium text-blue">All enterprise features included</p>
+          <PuzzlePieceIcon className="mr-3 h-7 w-7 flex-shrink-0 text-blue-500" />
+          <p className="text-blue font-medium">All enterprise features included</p>
         </div>
-        <Button className="w-full font-bold text-center mt-7" variant="highlight">
+        <Button className="mt-7 w-full text-center font-bold" variant="highlight">
           Start FREE on Formbricks Cloud
         </Button>
       </div>
-      <div className="px-10 py-6 rounded-lg bg-gradient-to-b from-blue-200 to-gray-50 dark:from-blue-300 dark:to-gray-100">
-        <ServerStackIcon className="flex-shrink-0 w-20 h-20 text-blue-500" />
-        <h2 className="text-4xl font-bold text-blue mt-7">Self-host</h2>
+      <div className="rounded-lg bg-gradient-to-b from-blue-200 to-gray-50 px-10 py-6 dark:from-blue-300 dark:to-gray-100">
+        <ServerStackIcon className="h-20 w-20 flex-shrink-0 text-blue-500" />
+        <h2 className="text-blue mt-7 text-4xl font-bold">Self-host</h2>
         <p className="text-sm text-gray-500">Submission data never leaves your infrastructure</p>
-        <p className="font-semibold mt-7">
+        <p className="mt-7 font-semibold">
           <span className="font-bold text-black">Free</span> for 500 submissions/mo
         </p>
         <p className="text-sm text-gray-500">then $0.01/submission</p>
-        <div className="flex items-center py-1 mt-7">
-          <ShieldCheckIcon className="flex-shrink-0 mr-3 text-blue-500 h- w-7" />
-          <p className="font-medium text-blue">Easy deploy for most private cloud platforms</p>
+        <div className="mt-7 flex items-center py-1">
+          <ShieldCheckIcon className="h- mr-3 w-7 flex-shrink-0 text-blue-500" />
+          <p className="text-blue font-medium">Easy deploy for most private cloud platforms</p>
         </div>
         <div className="flex items-center py-1">
-          <CommandLineIcon className="flex-shrink-0 mr-3 text-blue-500 h-7 w-7" />
-          <p className="font-medium text-blue">Full access to production instance</p>
+          <CommandLineIcon className="mr-3 h-7 w-7 flex-shrink-0 text-blue-500" />
+          <p className="text-blue font-medium">Full access to production instance</p>
         </div>
         <div className="flex items-center py-1">
-          <BuildingLibraryIcon className="flex-shrink-0 mr-3 text-blue-500 h-7 w-7" />
-          <p className="font-medium text-blue">Full compliance with all data privacy regulation</p>
+          <BuildingLibraryIcon className="mr-3 h-7 w-7 flex-shrink-0 text-blue-500" />
+          <p className="text-blue font-medium">Full compliance with all data privacy regulation</p>
         </div>
-        <Button className="w-full font-bold text-center mt-7" variant="highlight">
+        <Button className="mt-7 w-full text-center font-bold" variant="highlight">
           Get started
         </Button>
       </div>
