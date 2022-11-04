ab13707090aa28af0f9492a5842bf0ad23496489
Matthias Nannt <mail@matthiasnannt.com>
2022-11-04 16:34:15 +0100

    update meta information

--- apps/formbricks-com/components/shared/MetaInformation.tsx
+++ apps/formbricks-com/components/shared/MetaInformation.tsx
@@ -6,10 +6,15 @@ interface Props {
 }
 
 export default function MetaInformation({ title, description }: Props) {
+  const pageTitle = `${title} - Formbricks - Open Source Form Infrastructure`;
   return (
     <Head>
-      <title>{title} - Formbricks - Open Source Form Infrastructure</title>
+      <title>{pageTitle}</title>
       <meta name="description" content={description} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={`https://${process.env.VERCEL_URL}/social-image.png`} />
+      <meta property="og:image:alt" content="Formbricks - Open Source Form Infrastructure" />
     </Head>
   );
 }
