ec695df017652203f7197e65772a9a59645b991d
Matthias Nannt <mail@matthiasnannt.com>
2022-11-28 18:51:11 +0100

    fix redirect to login not working - formbricks hq

--- apps/hq/src/app/app/page.tsx
+++ apps/hq/src/app/app/page.tsx
@@ -2,20 +2,27 @@
 
 import { useMemberships } from "@/lib/memberships";
 import { Button } from "@formbricks/ui";
+import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import LoadingSpinner from "../LoadingSpinner";
 
 export default function ProjectsPage() {
+  const { data: session, status } = useSession();
   const { memberships, isErrorMemberships } = useMemberships();
   const router = useRouter();
 
   useEffect(() => {
-    if (memberships) {
+    if (session && memberships && memberships.length > 0) {
       const teamId = memberships[0].teamId;
       router.push(`/app/teams/${teamId}/forms`);
     }
-  }, [memberships, router]);
+  }, [memberships, router, session]);
+
+  if (!session) {
+    router.push(`/auth/signin?callbackUrl=${encodeURIComponent(window.location.href)}`);
+    return <div></div>;
+  }
 
   if (isErrorMemberships) {
     return <div>Something went wrong...</div>;
