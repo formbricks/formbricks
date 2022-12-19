0dc79bd206e066d6ac586d7ec6e87bb99751f0f3
Matthias Nannt <mail@matthiasnannt.com>
2022-12-19 09:00:44 +0100

    add icons to ui package

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,1 +1,8 @@
+var fs = require("fs");
+
 export * from "./Button";
+
+// export all icons
+fs.readdirSync("./icons").forEach(function (file) {
+  if (file.indexOf(".js") > -1 && file != "index.js") exports[file.replace(".js", "")] = require("./" + file);
+});
