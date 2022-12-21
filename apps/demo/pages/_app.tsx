1f3200fb4f311286cacba74201e9699df20c28b2
Matthias Nannt <mail@matthiasnannt.com>
2022-12-21 16:35:25 +0100

    add feedback widget

--- apps/demo/pages/_app.tsx
+++ apps/demo/pages/_app.tsx
@@ -4,6 +4,8 @@ import "react-toastify/dist/ReactToastify.css";
 
 import type { AppProps } from "next/app";
 import { ToastContainer } from "react-toastify";
+import Head from "next/head";
+import Script from "next/script";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
