54ede8ec8d1125d008f83d56ca2731ccb9fd9689
Matthias Nannt <mail@matthiasnannt.com>
2023-01-03 11:43:58 +0100

    add style to feedback widget config

--- apps/demo/pages/feedback-widget.tsx
+++ apps/demo/pages/feedback-widget.tsx
@@ -11,7 +11,7 @@ export default function FeedbackWidget() {
   return (
     <>
       <Script src="https://unpkg.com/@formbricks/feedback@0.1.1" defer />
-      <Script id="feedbackfin-setup">{`
+      <Script id="feedback-setup">{`
       window.formbricks = {
       config: {
         hqUrl: "http://localhost:3000",
