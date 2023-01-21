16ad208f8ea1e4d1af57629921a0c46e6f2c4b6e
Johannes <72809645+jobenjada@users.noreply.github.com>
2023-01-21 11:18:01 -0600

    remove free plan from waitlist thank-you page

--- apps/formbricks-com/components/engine/ThankYouHeading.tsx
+++ apps/formbricks-com/components/engine/ThankYouHeading.tsx
@@ -3,12 +3,10 @@ import { SurveyElement } from "./engineTypes";
 export default function ThankYouHeading({ element }: { element: SurveyElement }) {
   return (
     <div className="text-center">
-      <h2 className="mt-3 text-xl font-bold text-slate-700 dark:text-slate-100 sm:text-2xl md:text-3xl">
-        We’re onboarding new users <span className="text-brand">regularly!</span>
+      <h2 className="mt-3 text-xl font-bold text-slate-700 dark:text-slate-100 sm:text-2xl ">
+        Thank you! We’re onboarding new users <span className="text-brand">regularly.</span>
       </h2>
-      <p className="mt-4 text-slate-500 dark:text-slate-300">
-        Thank you for signing up. We will be in touch shortly.
-      </p>
+      <p className="mt-4 text-slate-500 dark:text-slate-300">We will be in touch shortly.</p>
     </div>
   );
 }
