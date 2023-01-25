090ac903e81d7f722a6dcc218b032129cb9a259e
knugget <johannes@knugget.de>
2023-01-25 13:51:50 +0100

    fix slider button blocking UI

--- apps/demo/components/PmfButton.tsx
+++ apps/demo/components/PmfButton.tsx
@@ -40,38 +40,33 @@ export default function PmfButton() {
       document.removeEventListener("mousedown", handleClickOutside);
     };
   }, [feedbackRef, isOpen]);
-
+  // xs:translate-x-[21.2rem] translate-y-[21rem]
   return (
-    <>
+    <div
+      className={clsx(
+        "xs:flex-row xs:right-0 xs:top-1/2 xs:w-[24rem] xs:-translate-y-1/2 fixed bottom-0 z-50 h-fit w-full transition-all duration-500 ease-in-out",
+        isOpen ? "xs:-translate-x-0 translate-y-0" : "xs:translate-x-full xs:-mr-1 translate-y-full"
+      )}
+      onClick={(e) => {
+        e.stopPropagation();
+      }}
+      ref={feedbackRef}>
+      <button
+        className="xs:-rotate-90  xs:top-1/2 xs:-left-[5.8rem] xs:-translate-y-1/2 xs:-translate-x-0 xs:w-32 xs:p-4 absolute left-1/2 w-28 -translate-x-1/2 -translate-y-full rounded-t bg-cyan-600 p-3   font-medium text-white"
+        onClick={() => {
+          if (!isOpen) {
+            if (window) {
+              window.formbricks.init();
+              window.formbricks.reset();
+            }
+          }
+          setIsOpen(!isOpen);
+        }}>
+        {isOpen ? "Close" : "Feedback"}
+      </button>
       <div
-        className={clsx(
-          "xs:right-0 xs:top-1/2 xs:w-[26rem] xs:-translate-y-1/2 xs:h-[30rem] fixed bottom-0 z-50 h-96 w-full transition-transform duration-500 ease-in-out",
-          isOpen ? "xs:-translate-x-0 translate-y-0" : "xs:translate-x-[21.2rem] translate-y-[21rem]"
-        )}>
-        <div
-          className="xs:flex-row flex h-full flex-col"
-          onClick={(e) => {
-            e.stopPropagation();
-          }}
-          ref={feedbackRef}>
-          <button
-            className="xs:-rotate-90 xs:-mr-7 xs:my-auto z-30 mx-auto -mb-2 max-h-16 w-32 rounded bg-cyan-600 p-4 font-medium text-white"
-            onClick={() => {
-              if (!isOpen) {
-                if (window) {
-                  window.formbricks.init();
-                  window.formbricks.reset();
-                }
-              }
-              setIsOpen(!isOpen);
-            }}>
-            {isOpen ? "Close" : "Feedback"}
-          </button>
-          <div
-            className="z-40 h-full w-full overflow-hidden rounded-lg bg-[#f8fafc] shadow-lg"
-            id="formbricks"></div>
-        </div>
-      </div>
-    </>
+        className="xs:px-2 h-full overflow-hidden rounded-l-lg bg-[#f8fafc] shadow-lg"
+        id="formbricks"></div>
+    </div>
   );
 }
