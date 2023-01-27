13b0f4069502e7c6e76d510ebc7304a9b4aaf682
Sacre-MBIKU-KDA <sacre@kinshasadigital.com>
2023-01-27 13:00:53 +0100

    fix: remove dev migration files and come back to the prod version

--- prisma/migrations/20230119194229_snoop_form_feat_total_candidate_submit/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Form" ADD COLUMN     "isFinished" BOOLEAN NOT NULL DEFAULT false;
-
--- AlterTable
-ALTER TABLE "NoCodeForm" ADD COLUMN     "isFinished" BOOLEAN NOT NULL DEFAULT false;
