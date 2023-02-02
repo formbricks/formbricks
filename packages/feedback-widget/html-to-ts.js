e77cf5d1d186a42fb7dac26f33f43687e84c5f26
Gwenaël Gallon <gwenael.gallon@mac.com>
2023-02-02 15:03:29 +0100

    bump dependencies @formbricks/feedback-widget

--- packages/feedback-widget/html-to-ts.js
+++ packages/feedback-widget/html-to-ts.js
@@ -1,11 +1,11 @@
 import { readFileSync, writeFileSync } from "fs";
-import { minify } from "html-minifier";
+import { minify } from "html-minifier-terser";
 
 const html = readFileSync("./src/form.html").toString();
 
 writeFileSync(
   "./src/form-html.ts",
-  `export const formHTML = \`${minify(html, {
+  `export const formHTML = \`${ await minify(html, {
     collapseInlineTagWhitespace: true,
     collapseWhitespace: true,
   })}\`;`
