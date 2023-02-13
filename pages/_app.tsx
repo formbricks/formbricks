3b37db110a1d4f4a65b05836b61bc389c660416d
Sacre-MBIKU-KDA <sacre@kinshasadigital.com>
2023-02-13 16:27:57 +0100

    feay: remove google tag manager scripts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,18 +14,6 @@ function SnoopApp({
 }: AppProps) {
   return (
     <>
-      <Script
-        strategy='lazyOnload'
-        src='https://www.googletagmanager.com/gtag/js?id=G-FBKS1QLDSZ'
-      ></Script>
-      <Script strategy='lazyOnload'>
-        {` 
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', ${process.env.GOOGLE_TAG_MANAGER_ID});
-  `}
-      </Script>
       <SessionProvider session={session}>
         <Component {...pageProps} />
         <ToastContainer />
