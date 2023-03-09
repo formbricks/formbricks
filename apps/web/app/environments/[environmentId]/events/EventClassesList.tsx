a77679cddb05c3f5bd9c8b797b98eb90825e89a0
knugget <johannes@knugget.de>
2023-03-09 22:10:34 +0100

    event detail modal

--- apps/web/app/environments/[environmentId]/events/EventClassesList.tsx
+++ apps/web/app/environments/[environmentId]/events/EventClassesList.tsx
@@ -5,10 +5,13 @@ import Button from "@/components/ui/Button";
 import { useEventClasses } from "@/lib/eventClasses";
 import { timeSinceConditionally } from "@/lib/time";
 import { CodeBracketIcon, CursorArrowRaysIcon, SparklesIcon } from "@heroicons/react/20/solid";
-import Link from "next/link";
+import { useState } from "react";
+import EventDetailModal from "./EventDetailModal";
 
 export default function EventClassesList({ environmentId }) {
   const { eventClasses, isLoadingEventClasses, isErrorEventClasses } = useEventClasses(environmentId);
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   if (isLoadingEventClasses) {
     return <LoadingSpinner />;
@@ -17,13 +20,14 @@ export default function EventClassesList({ environmentId }) {
   if (isErrorEventClasses) {
     return <div>Error</div>;
   }
+
   return (
     <>
       <div className="mb-6 text-right">
         <Button variant="primary">Add event</Button>
       </div>
       <div className="rounded-lg border border-slate-200">
-        <div className="grid h-12 grid-cols-7 content-center rounded-lg bg-slate-100 text-left text-sm font-semibold text-gray-900">
+        <div className="grid h-12 grid-cols-7 content-center rounded-lg bg-slate-100 text-left text-sm font-semibold text-slate-900">
           <div className="col-span-4 pl-6 ">Name</div>
           <div className="text-center"># events</div>
           <div className="text-center">Created</div>
@@ -33,13 +37,18 @@ export default function EventClassesList({ environmentId }) {
         </div>
         <div className="grid-cols-7">
           {eventClasses.map((eventClass) => (
-            <Link href="/" className="w-full">
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                setIsModalOpen(true);
+              }}
+              className="w-full">
               <div
                 key={eventClass.id}
                 className="m-2 grid h-16  grid-cols-7 content-center rounded-lg hover:bg-slate-100">
                 <div className="col-span-4 flex items-center pl-6 text-sm">
                   <div className="flex items-center">
-                    <div className="h-5 w-5 flex-shrink-0 text-slate-300">
+                    <div className="text-slate-00 h-5 w-5 flex-shrink-0">
                       {eventClass.type === "code" ? (
                         <CodeBracketIcon />
                       ) : eventClass.type === "noCode" ? (
@@ -48,30 +57,36 @@ export default function EventClassesList({ environmentId }) {
                         <SparklesIcon />
                       ) : null}
                     </div>
-                    <div className="ml-4">
-                      <div className="font-medium text-gray-900">{eventClass.name}</div>
-                      <div className="text-gray-500">{eventClass.description}</div>
+                    <div className="ml-4 text-left">
+                      <div className="font-medium text-slate-900">{eventClass.name}</div>
+                      <div className="text-xs text-slate-400">{eventClass.description}</div>
                     </div>
                   </div>
                 </div>
-                <div className="my-auto whitespace-nowrap text-center text-sm text-gray-500">
+                <div className="my-auto whitespace-nowrap text-center text-sm text-slate-500">
                   {eventClass._count?.events}
                 </div>
-                <div className="my-auto whitespace-nowrap text-center text-sm text-gray-500">
+                <div className="my-auto whitespace-nowrap text-center text-sm text-slate-500">
                   {timeSinceConditionally(eventClass.createdAt)}
                 </div>
-                <div>
+                <div className="text-center">
                   {eventClass.type !== "automatic" && (
-                    <a href="#" className="text-brand-dark hover:text-brand">
+                    <button
+                      onClick={(e) => {
+                        e.preventDefault();
+                        setIsModalOpen(true);
+                      }}
+                      className="text-brand-dark hover:text-brand">
                       Edit<span className="sr-only">, {eventClass.name}</span>
-                    </a>
+                    </button>
                   )}
                 </div>
               </div>
-            </Link>
+            </button>
           ))}
         </div>
       </div>
+      <EventDetailModal open={isModalOpen} setOpen={setIsModalOpen} eventId="1" />
     </>
   );
 }
