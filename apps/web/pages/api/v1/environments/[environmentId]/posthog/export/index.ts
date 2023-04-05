764e16a1d8d190854877d3241bb8e50e7aff1331
Johannes <johannes@formbricks.com>
2023-04-05 17:49:59 +0200

    update posthog auth

--- apps/web/pages/api/v1/environments/[environmentId]/posthog/export/index.ts
+++ apps/web/pages/api/v1/environments/[environmentId]/posthog/export/index.ts
@@ -1,18 +1,12 @@
-import { getSessionOrUser, hasEnvironmentAccess } from "@/lib/api/apiHelper";
+import { hasEnvironmentAccess } from "@/lib/api/apiHelper";
 import { prisma } from "@formbricks/database";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
-  // Check Authentication
-  const user: any = await getSessionOrUser(req, res);
-  if (!user) {
-    return res.status(401).json({ message: "Not authenticated" });
-  }
-
   const environmentId = req.query?.environmentId?.toString();
 
-  const hasAccess = await hasEnvironmentAccess(user, environmentId);
-  if (hasAccess === false) {
+  const hasAccess = await hasEnvironmentAccess(req, res, environmentId);
+  if (!hasAccess) {
     return res.status(403).json({ message: "Not authorized" });
   }
 
