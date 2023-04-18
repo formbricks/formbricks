189db69310facc1fa0a9b84caf2f8b39e214ac81
moritzrengert <moritz@rengert.de>
2023-04-18 21:50:09 +0200

    fix: reflect changes from previous commit in js package

---
+++ packages/js/src/components/QuestionConditional.tsx
@@ -0,0 +1,42 @@
+import { h } from "preact";
+import type { Question } from "@formbricks/types/js";
+import OpenTextQuestion from "./OpenTextQuestion";
+import MultipleChoiceSingleQuestion from "./MultipleChoiceSingleQuestion";
+import MultipleChoiceMultiQuestion from "./MultipleChoiceMultiQuestion";
+
+interface QuestionConditionalProps {
+  question: Question;
+  onSubmit: (data: { [x: string]: any }) => void;
+  lastQuestion: boolean;
+  brandColor: string;
+}
+
+export default function QuestionConditional({
+  question,
+  onSubmit,
+  lastQuestion,
+  brandColor,
+}: QuestionConditionalProps) {
+  return question.type === "openText" ? (
+    <OpenTextQuestion
+      question={question}
+      onSubmit={onSubmit}
+      lastQuestion={lastQuestion}
+      brandColor={brandColor}
+    />
+  ) : question.type === "multipleChoiceSingle" ? (
+    <MultipleChoiceSingleQuestion
+      question={question}
+      onSubmit={onSubmit}
+      lastQuestion={lastQuestion}
+      brandColor={brandColor}
+    />
+  ) : question.type === "multipleChoiceMulti" ? (
+    <MultipleChoiceMultiQuestion
+      question={question}
+      onSubmit={onSubmit}
+      lastQuestion={lastQuestion}
+      brandColor={brandColor}
+    />
+  ) : null;
+}
