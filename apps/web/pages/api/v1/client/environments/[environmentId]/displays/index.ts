d889775c56189b6add961cab8e07348137aa2a0f
Matthias Nannt <mail@matthiasnannt.com>
2023-04-20 09:54:45 +0200

    add display track event to posthog

--- apps/web/pages/api/v1/client/environments/[environmentId]/displays/index.ts
+++ apps/web/pages/api/v1/client/environments/[environmentId]/displays/index.ts
@@ -1,3 +1,4 @@
+import { capturePosthogEvent } from "@formbricks/lib/posthogServer";
 import { prisma } from "@formbricks/database";
 import type { NextApiRequest, NextApiResponse } from "next";
 
@@ -20,6 +21,38 @@ export default async function handle(req: NextApiRequest, res: NextApiResponse) 
     if (!surveyId) {
       return res.status(400).json({ message: "Missing surveyId" });
     }
+
+    // get teamId from environment
+    const environment = await prisma.environment.findUnique({
+      where: {
+        id: environmentId,
+      },
+      select: {
+        product: {
+          select: {
+            team: {
+              select: {
+                id: true,
+                memberships: {
+                  select: {
+                    userId: true,
+                    role: true,
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    });
+
+    if (!environment) {
+      return res.status(404).json({ message: "Environment not found" });
+    }
+
+    const teamId = environment.product.team.id;
+    // find team owner
+    const teamOwnerId = environment.product.team.memberships.find((m) => m.role === "owner")?.userId;
 
     const createBody: any = {
       select: {
@@ -45,6 +78,14 @@ export default async function handle(req: NextApiRequest, res: NextApiResponse) 
 
     // create new display
     const displayData = await prisma.display.create(createBody);
+
+    if (teamOwnerId) {
+      await capturePosthogEvent(teamOwnerId, "response created", teamId, {
+        surveyId,
+      });
+    } else {
+      console.warn("Posthog capture not possible. No team owner found");
+    }
 
     return res.json(displayData);
   }
