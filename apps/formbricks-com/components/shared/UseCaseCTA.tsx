60b79abaa765bd8ccf6d8181b54a61dd2d541871
Johannes <johannes@formbricks.com>
2023-05-05 12:13:29 +0200

    update button text

--- apps/formbricks-com/components/shared/UseCaseCTA.tsx
+++ apps/formbricks-com/components/shared/UseCaseCTA.tsx
@@ -9,10 +9,10 @@ export default function UseCaseHeader({ href }: UseCaseCTAProps) {
   return (
     <div className="my-4 flex items-center space-x-2">
       <Button variant="secondary" href={href}>
-        In-depth manual
+        Step-by-step manual
       </Button>
       <Button variant="darkCTA" href="https://app.formbricks.com/auth/signup" EndIcon={ArrowRightIcon}>
-        Sign Up
+        Sign up (free)
       </Button>
     </div>
   );
