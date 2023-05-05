7c45bd84c902b59a4b92d4591c87a5f342cacaee
Matthias Nannt <mail@matthiasnannt.com>
2023-05-05 10:52:38 +0200

    fix migrations

--- packages/database/prisma/migrations/20230505085230_add_webhooks/migration.sql
+++ packages/database/prisma/migrations/20230505085230_add_webhooks/migration.sql
@@ -1,3 +1,15 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `tags` on the `Response` table. All the data in the column will be lost.
+
+*/
+-- CreateEnum
+CREATE TYPE "PipelineTriggers" AS ENUM ('responseCreated', 'responseUpdated', 'responseFinished');
+
+-- AlterTable
+ALTER TABLE "Response" DROP COLUMN "tags";
+
 -- CreateTable
 CREATE TABLE "Webhook" (
     "id" TEXT NOT NULL,
@@ -5,6 +17,7 @@ CREATE TABLE "Webhook" (
     "updated_at" TIMESTAMP(3) NOT NULL,
     "url" TEXT NOT NULL,
     "environmentId" TEXT NOT NULL,
+    "triggers" "PipelineTriggers"[],
 
     CONSTRAINT "Webhook_pkey" PRIMARY KEY ("id")
 );
