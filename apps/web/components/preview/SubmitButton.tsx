7663f5cc707b039f4f8fe63db2ca0d90ee119635
moritzrengert <moritz@rengert.de>
2023-05-16 01:37:17 +0200

    feat; add darker color to js package

--- apps/web/components/preview/SubmitButton.tsx
+++ apps/web/components/preview/SubmitButton.tsx
@@ -4,7 +4,7 @@ import { isLight } from "@/lib/utils";
 function SubmitButton({ question, lastQuestion, brandColor }) {
   return (
     <button
-      type="submit"
+      type="button"
       className={cn(
         "flex items-center rounded-md border border-transparent px-3 py-3 text-base font-medium leading-4 text-white shadow-sm hover:opacity-90 focus:outline-none focus:ring-2 focus:ring-slate-500 focus:ring-offset-2",
         isLight(brandColor) ? "text-black" : "text-white"
