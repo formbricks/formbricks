13b96e6d41d109636b4f942fe250dd5d34405625
Matthias Nannt <mail@matthiasnannt.com>
2023-05-15 16:33:47 +0200

    fix linting errors

--- apps/demo/pages/app/index.tsx
+++ apps/demo/pages/app/index.tsx
@@ -94,14 +94,14 @@ export default function AppPage({}) {
               onClick={() => {
                 formbricks.setAttribute("Plan", "Free");
               }}>
-              Set attribute "Free"
+              Set attribute &quot;Free&quot;
             </button>
             <button
               className="mr-2 flex max-w-xs items-center rounded-full bg-white text-sm  font-medium text-slate-700 focus:outline-none focus:ring-2 focus:ring-cyan-500 focus:ring-offset-2 lg:rounded-md lg:p-2 lg:hover:bg-slate-50"
               onClick={() => {
                 formbricks.setAttribute("Plan", "Paid");
               }}>
-              Set attribute "Paid"
+              Set attribute &quot;Paid&quot;
             </button>
 
             {/* Profile dropdown */}
