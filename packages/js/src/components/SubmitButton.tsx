26804160bda64cb38d3700e833846ea819ab24b5
moritzrengert <moritz@rengert.de>
2023-05-16 01:48:22 +0200

    fix: dark text inside js widget

--- packages/js/src/components/SubmitButton.tsx
+++ packages/js/src/components/SubmitButton.tsx
@@ -8,8 +8,8 @@ function SubmitButton({ question, lastQuestion, brandColor, onClick, type = "sub
     <button
       type={type}
       className={cn(
-        "fb-flex fb-items-center fb-rounded-md fb-border fb-border-transparent fb-px-3 fb-py-3 fb-text-base fb-font-medium fb-leading-4 fb-text-white fb-shadow-sm hover:fb-opacity-90 focus:fb-outline-none focus:fb-ring-2 focus:fb-ring-slate-500 focus:fb-ring-offset-2",
-        isLight(brandColor) ? "gb-text-black" : "fb-text-white"
+        "fb-flex fb-items-center fb-rounded-md fb-border fb-border-transparent fb-px-3 fb-py-3 fb-text-base fb-font-medium fb-leading-4 fb-shadow-sm hover:fb-opacity-90 focus:fb-outline-none focus:fb-ring-2 focus:fb-ring-slate-500 focus:fb-ring-offset-2",
+        isLight(brandColor) ? "fb-text-black" : "fb-text-white"
       )}
       style={{ backgroundColor: brandColor }}
       onClick={onClick}>
