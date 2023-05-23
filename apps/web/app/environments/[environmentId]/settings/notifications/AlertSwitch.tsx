f1596b59eafd0d70d4b197dc984e903e8b558c82
Matthias Nannt <mail@matthiasnannt.com>
2023-05-23 17:31:22 +0200

    remove optimistic update due to react issues

--- apps/web/app/environments/[environmentId]/settings/notifications/AlertSwitch.tsx
+++ apps/web/app/environments/[environmentId]/settings/notifications/AlertSwitch.tsx
@@ -4,7 +4,6 @@ import { Switch } from "@formbricks/ui";
 import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
 import { updateNotificationSettings } from "./actions";
-import { experimental_useOptimistic as useOptimistic } from "react";
 
 interface AlertSwitchProps {
   surveyId: string;
@@ -13,24 +12,18 @@ interface AlertSwitchProps {
 }
 
 export function AlertSwitch({ surveyId, userId, notificationSettings }: AlertSwitchProps) {
-  const [optimisticValue, setOptimisticValue] = useOptimistic(
-    notificationSettings[surveyId]?.responseFinished,
-    (_, newValue) => newValue
-  );
   const router = useRouter();
 
   return (
     <Switch
       id="every-submission"
       aria-label="toggle every submission"
-      checked={optimisticValue}
+      checked={notificationSettings[surveyId]["responseFinished"]}
       onCheckedChange={async () => {
-        // get new value
+        // update notificiation settings
         const updatedNotificationSettings = { ...notificationSettings };
         updatedNotificationSettings[surveyId]["responseFinished"] =
           !updatedNotificationSettings[surveyId]["responseFinished"];
-        // set optimistic value
-        setOptimisticValue(true);
         // update db
         await updateNotificationSettings(userId, notificationSettings);
         toast.success(`Every new submission is coming your way!`);
