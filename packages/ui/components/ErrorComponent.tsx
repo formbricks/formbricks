79a86050c30d11c40374400cd43e0e455911769d
Konrad Kalemba <konrad_kal@hotmail.com>
2023-05-23 09:22:04 +0200

    fix typo in ErrorComponent (#293)

--- packages/ui/components/ErrorComponent.tsx
+++ packages/ui/components/ErrorComponent.tsx
@@ -8,11 +8,9 @@ export const ErrorComponent: React.FC = ({}) => {
           <XCircleIcon className="h-12 w-12 text-red-400" aria-hidden="true" />
         </div>
         <div className="ml-3">
-          <h3 className="text-sm font-medium text-red-800">Error loading ressources</h3>
+          <h3 className="text-sm font-medium text-red-800">Error loading resources</h3>
           <div className="mt-2 text-sm text-red-700">
-            <p>
-              This ressource doesn&apos;t exist or you don&apos;t have the neccessary rights to access it.
-            </p>
+            <p>This resource doesn&apos;t exist or you don&apos;t have the neccessary rights to access it.</p>
           </div>
         </div>
       </div>
