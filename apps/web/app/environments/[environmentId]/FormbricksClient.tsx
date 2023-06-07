369da2a6a2fcc61190737d4d62d866a2a76d6a95
Matthias Nannt <mail@matthiasnannt.com>
2023-06-07 10:59:36 +0200

    move formbricks client to useEffect

--- apps/web/app/environments/[environmentId]/FormbricksClient.tsx
+++ apps/web/app/environments/[environmentId]/FormbricksClient.tsx
@@ -4,17 +4,22 @@ import { formbricksEnabled } from "@/lib/formbricks";
 import formbricks from "@formbricks/js";
 import { useEffect } from "react";
 
-if (typeof window !== "undefined" && formbricksEnabled) {
+/* if (typeof window !== "undefined" && formbricksEnabled) {
   formbricks.init({
     environmentId: process.env.NEXT_PUBLIC_FORMBRICKS_ENVIRONMENT_ID || "",
     apiHost: process.env.NEXT_PUBLIC_FORMBRICKS_API_HOST || "",
     logLevel: "debug",
   });
-}
+} */
 
 export default function FormbricksClient({ session }) {
   useEffect(() => {
     if (formbricksEnabled && session.user && formbricks) {
+      formbricks.init({
+        environmentId: process.env.NEXT_PUBLIC_FORMBRICKS_ENVIRONMENT_ID || "",
+        apiHost: process.env.NEXT_PUBLIC_FORMBRICKS_API_HOST || "",
+        logLevel: "debug",
+      });
       formbricks.setUserId(session.user.id);
       formbricks.setEmail(session.user.email);
       if (session.user.plan) {
