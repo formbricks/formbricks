48a92b1eaea66960fce5c6bc2ec9666c076e77cc
Francois Disubi <francois@kinshasadigital.com>
2023-06-12 11:08:55 +0100

    feat: add delete account action when user click on delete account button

--- apps/web/app/api/v1/users/me/route.ts
+++ apps/web/app/api/v1/users/me/route.ts
@@ -39,13 +39,93 @@ export async function PUT(request: NextRequest) {
 }
 
 export async function DELETE() {
-  const sessionUser = await getSessionUser();
+  try {
+    const sessionUser = await getSessionUser();
 
-  if (!sessionUser) {
-    return new Response("Not authenticated", {
-      status: 401,
+    if (!sessionUser) {
+      return new Response("Not authenticated", {
+        status: 401,
+      });
+    }
+
+    const memberships = await prisma.membership.findMany({
+      where: {
+        userId: sessionUser.id,
+      },
+      include: {
+        team: {
+          select: {
+            id: true,
+            name: true,
+            memberships: {
+              select: {
+                user: {
+                  select: {
+                    id: true,
+                    name: true,
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
     });
-  }
 
-  return NextResponse.json({ name: "fs" });
+    memberships.forEach(() => async (membership) => {
+      if (membership.role === "owner") {
+        if (membership.team.memberships.length > 1) {
+          const newOwerner = membership.team.memberships.find((team) => team.user.id !== sessionUser.id);
+
+          await prisma.membership.update({
+            where: {
+              teamId: membership.teamId,
+              userId: newOwerner.user.id,
+            },
+            data: {
+              role: "owner",
+            },
+          });
+
+          await prisma.membership.update({
+            where: {
+              teamId: membership.teamId,
+              userId: sessionUser.id,
+            },
+            data: {
+              role: "admin",
+            },
+          });
+        } else {
+          await prisma.membership.delete({
+            where: {
+              teamId: membership.teamId,
+              userId: sessionUser.id,
+            },
+          });
+        }
+      } else {
+        await prisma.membership.delete({
+          where: {
+            teamId: membership.teamId,
+            userId: sessionUser.id,
+          },
+        });
+      }
+    });
+
+    // TODO  Logout user before deleting account
+
+    // Delete user
+    await prisma.user.delete({
+      where: {
+        id: sessionUser.id,
+      },
+    });
+
+    return NextResponse.json({ name: memberships });
+  } catch (error) {
+    // TODO handle this error
+    console.log("error.message =>>>>>>>>>>>>>>>>>>>", error.message);
+  }
 }
