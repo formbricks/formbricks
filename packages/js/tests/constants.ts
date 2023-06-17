489a9f053e4aa71813e6ea028820684b2fcfb524
ShubhamPalriwala <spalriwalau@gmail.com>
2023-06-18 02:39:50 +0530

    test: formbricks set attributes

--- packages/js/tests/constants.ts
+++ packages/js/tests/constants.ts
@@ -1,4 +1,34 @@
+const generateUserId = () => {
+    const min = 1000;
+    const max = 9999;
+    const randomNum = Math.floor(Math.random() * (max - min + 1) + min);
+    return randomNum.toString();
+}
+
+const generateEmailId = () => {
+    const domain = "formbricks.test";
+    const randomString = Math.random().toString(36).substring(2);
+    const emailId = `${randomString}@${domain}`;
+    return emailId;
+};
+
+const generateRandomString = () => {
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    const maxLength = 8;
+
+    let randomString = "";
+    for (let i = 0; i < maxLength; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        randomString += characters.charAt(randomIndex);
+    }
+    return randomString;
+};
+
 export const constants = {
     environmentId: "take-this-from-gh-actions-env",
     apiHost: "https://app.formbricks.com",
+    initialUserId: generateUserId(),
+    initialUserEmail: generateEmailId(),
+    customAttributeKey: generateRandomString(),
+    customAttributeValue: generateRandomString(),
 } as const;
--- packages/js/tests/index.test.ts
+++ packages/js/tests/index.test.ts
@@ -3,6 +3,7 @@
  */
 import formbricks from "../src/index";
 import { constants } from "./constants"
+import { Attribute } from "./types";
 
 test("Test Jest", () => {
     expect(1 + 9).toBe(10);
@@ -25,3 +26,44 @@ test("Formbricks should Initialise", async () => {
         expect(jsonSavedConfig.apiHost).toEqual(constants.apiHost);
     }
 });
+
+test("Formbricks should get no current person", () => {
+    let currentState = formbricks.getPerson()
+    if (currentState != undefined) {
+        let currentState = formbricks.getPerson()
+        const currentStateAttributes: Array<Attribute> = currentState.attributes as Array<Attribute>;
+        expect(currentStateAttributes).toHaveLength(0)
+    } else {
+        expect(currentState).toBeUndefined()
+    }
+})
+
+test("Formbricks should set attributes", async () => {
+    formbricks.setUserId(constants.initialUserId)
+    formbricks.setEmail(constants.initialUserEmail)
+    await formbricks.setAttribute(constants.customAttributeKey, constants.customAttributeValue)
+
+    let currentState = formbricks.getPerson()
+    expect(currentState.environmentId).toEqual(constants.environmentId)
+
+    const currentStateAttributes: Array<Attribute> = currentState.attributes as Array<Attribute>;
+
+    let numberOfUserAttributes = currentStateAttributes.length
+    expect(numberOfUserAttributes).toEqual(3)
+
+    currentStateAttributes.forEach((attribute) => {
+        switch (attribute.attributeClass.name) {
+            case "userId":
+                expect(attribute.value).toEqual(constants.initialUserId)
+                break;
+            case "email":
+                expect(attribute.value).toEqual(constants.initialUserEmail)
+                break;
+            case constants.customAttributeKey:
+                expect(attribute.value).toEqual(constants.customAttributeValue)
+                break;
+            default:
+                expect(0).toEqual(1)
+        }
+    })
+})
