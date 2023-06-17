6a77500dfc117950dbb99d109ab8738eb23aa6e0
ShubhamPalriwala <spalriwalau@gmail.com>
2023-06-18 02:18:45 +0530

    test: formbricks init

---
+++ packages/js/tests/constants.ts
@@ -0,0 +1,4 @@
+export const constants = {
+    environmentId: "take-this-from-gh-actions-env",
+    apiHost: "https://app.formbricks.com",
+} as const;
--- packages/js/tests/index.test.ts
+++ packages/js/tests/index.test.ts
@@ -2,8 +2,26 @@
  * @jest-environment jsdom
  */
 import formbricks from "../src/index";
+import { constants } from "./constants"
 
-test('Test Jest', () => {
+test("Test Jest", () => {
     expect(1 + 9).toBe(10);
 });
 
+test("Formbricks should Initialise", async () => {
+    formbricks.init({
+        environmentId: constants.environmentId,
+        apiHost: constants.apiHost,
+        logLevel: "debug",
+    });
+    await new Promise((unused) => setTimeout(unused, 2000)); // Need to wait for the init() to fetch and log everything
+
+    const configFromBrowser = localStorage.getItem("formbricksConfig");
+    expect(configFromBrowser).toBeTruthy();
+
+    if (configFromBrowser) {
+        const jsonSavedConfig = JSON.parse(configFromBrowser);
+        expect(jsonSavedConfig.environmentId).toEqual(constants.environmentId);
+        expect(jsonSavedConfig.apiHost).toEqual(constants.apiHost);
+    }
+});
