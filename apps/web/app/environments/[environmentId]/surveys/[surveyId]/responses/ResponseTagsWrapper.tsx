c62bdb30613b78f76e8db538c6e48086f85b5acd
pandeymangg <anshuman.pandey9999@gmail.com>
2023-06-18 22:24:49 +0530

    feat: adds UI components for Combobox

--- apps/web/app/environments/[environmentId]/surveys/[surveyId]/responses/ResponseTagsWrapper.tsx
+++ apps/web/app/environments/[environmentId]/surveys/[surveyId]/responses/ResponseTagsWrapper.tsx
@@ -1,6 +1,19 @@
 import { useResponses } from "@/lib/responses/responses";
 import { useCreateTag } from "@/lib/tags/mutateTags";
-import { Button, Input } from "@formbricks/ui";
+import { cn } from "@formbricks/lib/cn";
+import {
+  Button,
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  Input,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@formbricks/ui";
+import { Check, ChevronsUpDown } from "lucide-react";
 import React from "react";
 import { useState } from "react";
 
@@ -14,6 +27,68 @@ interface IResponseTagsWrapperProps {
   surveyId: string;
   productId: string;
   responseId: string;
+}
+
+const frameworks = [
+  {
+    value: "next.js",
+    label: "Next.js",
+  },
+  {
+    value: "sveltekit",
+    label: "SvelteKit",
+  },
+  {
+    value: "nuxt.js",
+    label: "Nuxt.js",
+  },
+  {
+    value: "remix",
+    label: "Remix",
+  },
+  {
+    value: "astro",
+    label: "Astro",
+  },
+];
+
+export function ComboboxDemo() {
+  const [open, setOpen] = React.useState(false);
+  const [value, setValue] = React.useState("");
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        {/* <Button variant="darkCTA" role="combobox" aria-expanded={open} className="w-[200px] justify-between">
+          {value ? frameworks.find((framework) => framework.value === value)?.label : "Select framework..."}
+          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+        </Button> */}
+
+        <Button variant="darkCTA">+ Add Tag</Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[200px] p-0">
+        <Command>
+          <CommandInput placeholder="Search framework..." />
+          <CommandEmpty>No framework found.</CommandEmpty>
+          <CommandGroup>
+            {frameworks.map((framework) => (
+              <CommandItem
+                key={framework.value}
+                onSelect={(currentValue) => {
+                  setValue(currentValue === value ? "" : currentValue);
+                  setOpen(false);
+                }}>
+                <Check
+                  className={cn("mr-2 h-4 w-4", value === framework.value ? "opacity-100" : "opacity-0")}
+                />
+                {framework.label}
+              </CommandItem>
+            ))}
+          </CommandGroup>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
 }
 
 const ResponseTagsWrapper: React.FC<IResponseTagsWrapperProps> = ({
@@ -30,6 +105,7 @@ const ResponseTagsWrapper: React.FC<IResponseTagsWrapperProps> = ({
 
   return (
     <div className="flex items-center gap-3 p-6">
+      <ComboboxDemo />
       <div className="flex items-center gap-2">
         {data.map((tag) => (
           <div
