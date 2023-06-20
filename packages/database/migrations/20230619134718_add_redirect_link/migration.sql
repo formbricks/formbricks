0fd71e867826a54e04f8fd08eea262dd92078323
Dhruwang <dhruwangjariwala18@gmail.com>
2023-06-20 12:31:02 +0530

    resolved migration error

--- packages/database/migrations/20230619134718_add_redirect_link/migration.sql
+++ packages/database/migrations/20230619134718_add_redirect_link/migration.sql
@@ -1,11 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `redirectLink` on the `User` table. All the data in the column will be lost.
-
-*/
 -- AlterTable
 ALTER TABLE "Survey" ADD COLUMN     "redirectLink" TEXT;
 
--- AlterTable
-ALTER TABLE "User" DROP COLUMN "redirectLink";
