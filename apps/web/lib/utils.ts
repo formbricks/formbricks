1e9ce2822ccf44076d3a119575ad987efe08e453
joe-shajan <joeshajan1551@gmail.com>
2023-06-22 10:36:16 +0530

    fix: remove deepequal function

--- apps/web/lib/utils.ts
+++ apps/web/lib/utils.ts
@@ -62,12 +62,3 @@ export const toJson = (obj: any): Object | null => {
     return null;
   }
 };
-
-export const deepEqual = (obj1: any, obj2: any): boolean => {
-  return obj1 && obj2 && typeof obj1 === "object" && typeof obj2 === "object"
-    ? Object.keys(obj1).length === Object.keys(obj2).length &&
-        Object.keys(obj1).reduce(function (isEqual, key) {
-          return isEqual && deepEqual(obj1[key], obj2[key]);
-        }, true)
-    : obj1 === obj2;
-};
