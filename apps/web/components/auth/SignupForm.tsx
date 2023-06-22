7fef74d53ae10d94af1a0f13ee33de911ce07571
Dhruwang <dhruwangjariwala18@gmail.com>
2023-06-22 17:05:22 +0530

    removed cursor-pointer from XCircleIcon

--- apps/web/components/auth/SignupForm.tsx
+++ apps/web/components/auth/SignupForm.tsx
@@ -63,7 +63,7 @@ export const SignupForm = () => {
         <div className="absolute top-10 rounded-md bg-teal-50 p-4">
           <div className="flex">
             <div className="flex-shrink-0">
-              <XCircleIcon className="h-5 w-5 text-teal-400 cursor-pointer" aria-hidden="true" />
+              <XCircleIcon className="h-5 w-5 text-teal-400" aria-hidden="true" />
             </div>
             <div className="ml-3">
               <h3 className="text-sm font-medium text-teal-800">An error occurred when logging you in</h3>
