67423f3fb87b37f44501a3f84fb98e939b123269
Matthias Nannt <mail@matthiasnannt.com>
2023-06-26 13:35:38 +0200

    add missing migration

--- packages/types/v1/surveys.ts
+++ packages/types/v1/surveys.ts
@@ -197,7 +197,7 @@ export const ZSurvey = z.object({
   displayOption: z.enum(["displayOnce", "displayMultiple", "respondMultiple"]),
   autoClose: z.union([z.number(), z.null()]),
   triggers: z.array(ZEventClass),
-  redirectUrl: z.union([z.string(), z.null()]),
+  redirectUrl: z.string().url().optional(),
   recontactDays: z.union([z.number(), z.null()]),
   questions: ZSurveyQuestions,
   thankYouCard: ZSurveyThankYouCard,
