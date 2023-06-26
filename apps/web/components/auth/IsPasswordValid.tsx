4386d68796a9b925611364276003602fa5033013
Matthias Nannt <mail@matthiasnannt.com>
2023-06-26 14:40:40 +0200

    fix infinite state updates

--- apps/web/components/auth/IsPasswordValid.tsx
+++ apps/web/components/auth/IsPasswordValid.tsx
@@ -1,5 +1,5 @@
-import { CheckIcon } from '@heroicons/react/24/solid';
-import React, { useState, useEffect } from 'react';
+import { CheckIcon } from "@heroicons/react/24/solid";
+import React, { useState, useEffect } from "react";
 
 interface Validation {
   label: string;
@@ -8,52 +8,55 @@ interface Validation {
 
 const PASSWORD_REGEX = {
   UPPER_AND_LOWER: /^(?=.*[A-Z])(?=.*[a-z])/,
-  NUMBER: /\d/
+  NUMBER: /\d/,
 };
 
-export default function IsPasswordValid({ password, setIsValid }:
-  { password: string | null, setIsValid: (isValid: boolean) => void }) {
+const DEFAULT_VALIDATIONS = [
+  { label: "Mix of uppercase and lowercase", state: false },
+  { label: "Minimum 8 characters long", state: false },
+  { label: "Contain at least 1 number", state: false },
+];
 
-  const [validations, setValidations] = useState<Validation[]>([
-    { label: 'Mix of uppercase and lowercase', state: false },
-    { label: 'Minimum 8 characters long', state: false },
-    { label: 'Contain at least 1 number', state: false },
-  ]);
+export default function IsPasswordValid({
+  password,
+  setIsValid,
+}: {
+  password: string | null;
+  setIsValid: (isValid: boolean) => void;
+}) {
+  const [validations, setValidations] = useState<Validation[]>(DEFAULT_VALIDATIONS);
 
   useEffect(() => {
-    resetValidations(false);
+    let newValidations = [...DEFAULT_VALIDATIONS];
     if (password) {
-      checkValidation(0, PASSWORD_REGEX.UPPER_AND_LOWER.test(password));
-      checkValidation(1, password.length >= 8);
-      checkValidation(2, PASSWORD_REGEX.NUMBER.test(password));
+      newValidations = checkValidation(newValidations, 0, PASSWORD_REGEX.UPPER_AND_LOWER.test(password));
+      newValidations = checkValidation(newValidations, 1, password.length >= 8);
+      newValidations = checkValidation(newValidations, 2, PASSWORD_REGEX.NUMBER.test(password));
     }
-    setIsValid(validations.every((validation) => validation.state === true));
-  }, [password, validations]);
+    setIsValid(newValidations.every((validation) => validation.state === true));
+    setValidations(newValidations);
 
-  const resetValidations = (state: boolean) => {
-    setValidations((prevValidations) =>
-      prevValidations.map((validation) => ({ ...validation, state }))
-    );
-  };
-
-  const checkValidation = (index: number, state: boolean) => {
-    setValidations((prevValidations) => {
+    function checkValidation(prevValidations, index: number, state: boolean) {
       const updatedValidations = [...prevValidations];
       updatedValidations[index].state = state;
       return updatedValidations;
-    });
-  };
+    }
+  }, [password, setIsValid]);
 
   const renderIcon = (state: boolean) => {
     if (state === false) {
-      return <span className="w-5 h-5 flex items-center justify-center"><i className="w-2 h-2 rounded-full bg-slate-700 inline-block"></i></span>;
+      return (
+        <span className="flex h-5 w-5 items-center justify-center">
+          <i className="inline-block h-2 w-2 rounded-full bg-slate-700"></i>
+        </span>
+      );
     } else {
       return <CheckIcon className="h-5 w-5" />;
     }
   };
 
   return (
-    <div className="sm:text-sm my-2 text-slate-700 text-left">
+    <div className="my-2 text-left text-slate-700 sm:text-sm">
       <ul>
         {validations.map((validation, index) => (
           <li key={index}>
