c63cb99ff79d7422c73edfbd22b225fff1e61e84
Matthias Nannt <mail@matthiasnannt.com>
2023-06-28 13:39:08 +0200

    update migrations

--- packages/database/migrations/20230626091038_add_produt_placement/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "WidgetPlacement" AS ENUM ('bottomLeft', 'bottomRight', 'topLeft', 'topRight', 'center');
-
--- AlterTable
-ALTER TABLE "Product" ADD COLUMN     "placement" "WidgetPlacement" NOT NULL DEFAULT 'bottomRight';
--- packages/database/migrations/20230627201437_add_center_modal_props/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "Product" ADD COLUMN     "clickOutsideClose" BOOLEAN NOT NULL DEFAULT true,
-ADD COLUMN     "darkOverlay" BOOLEAN NOT NULL DEFAULT false;
