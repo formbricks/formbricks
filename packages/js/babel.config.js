4f836067c8b54494824df4cc386d0b7d1ada9b6e
Matthias Nannt <mail@matthiasnannt.com>
2023-06-30 13:01:34 +0200

    update babel config to fix errors in formbricks-js build

--- packages/js/babel.config.js
+++ packages/js/babel.config.js
@@ -1,8 +1,6 @@
-module.exports = {
-    presets: [['@babel/preset-env', { targets: { node: 'current' } }], '@babel/preset-typescript'],
-    "plugins": [
-        ["@babel/plugin-transform-react-jsx", {
-            "runtime": "automatic"
-        }]
-    ]
-}
+module.exports = (api) => {
+  return {
+    presets: [["@babel/preset-env", { targets: { node: "current" } }], "@babel/preset-typescript"],
+    plugins: [["@babel/plugin-transform-react-jsx", { runtime: api.env("test") ? "automatic" : "classic" }]],
+  };
+};
