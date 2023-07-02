fde340929f6bc00257e0e63ab78db6ddb3d0ebd5
Piyush Gupta <piyushguptaa2z123@gmail.com>
2023-07-02 16:10:35 +0530

    resolved migrations

--- packages/database/migrations/20230701165416_rename/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `closeDate` on the `Survey` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Survey" DROP COLUMN "closeDate",
-ADD COLUMN     "closeOnDate" TIMESTAMP(3);
