459830d976f4674ed0392c6dac3a8a37ebce8f16
Matthias Nannt <mail@matthiasnannt.com>
2023-07-13 14:54:48 +0200

    update shuffle method

--- packages/js/src/lib/utils.ts
+++ packages/js/src/lib/utils.ts
@@ -17,7 +17,7 @@ export function isLight(color) {
 }
 
 const shuffle = (array: any[]) => {
-  for (let i = array.length - 1; i > 0; i--) {
+  for (let i = 0; i < array.length; i++) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
   }
