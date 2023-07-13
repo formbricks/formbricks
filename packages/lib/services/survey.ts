4410b14d0c7775b349ece9839cffa336c3c0e1c4
Ankur Datta <ankurr2002@gmail.com>
2023-07-13 17:53:48 +0000

    [ADD] services

--- packages/lib/services/survey.ts
+++ packages/lib/services/survey.ts
@@ -2,7 +2,14 @@ import { prisma } from "@formbricks/database";
 import { z } from "zod";
 import { ValidationError } from "@formbricks/errors";
 import { DatabaseError, ResourceNotFoundError } from "@formbricks/errors";
-import { TSurvey, TSurveyWithAnalytics, ZSurvey, ZSurveyWithAnalytics } from "@formbricks/types/v1/surveys";
+import {
+  TSurvey,
+  TSurveyWithAnalytics,
+  ZSurvey,
+  ZSurveyWithAnalytics,
+  TSurveyWithResponseCount,
+  ZSurveyWithResponseCount,
+} from "@formbricks/types/v1/surveys";
 import { Prisma } from "@prisma/client";
 import "server-only";
 import { cache } from "react";
@@ -146,3 +153,53 @@ export const getSurveys = cache(async (environmentId: string): Promise<TSurvey[]
     throw new ValidationError("Data validation of survey failed");
   }
 });
+
+export const getSurveysWithResponseCount = cache(
+  async (environmentId: string): Promise<TSurveyWithResponseCount[]> => {
+    let surveysPrisma;
+    try {
+      surveysPrisma = await prisma.survey.findMany({
+        where: {
+          environment: {
+            id: environmentId,
+          },
+        },
+        include: {
+          _count: {
+            select: {
+              responses: true,
+            },
+          },
+        },
+      });
+    } catch (error) {
+      console.log(error);
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        throw new DatabaseError("Database operation failed");
+      }
+
+      throw error;
+    }
+
+    const surveys: TSurveyWithResponseCount[] = [];
+    for (const surveyPrisma of surveysPrisma) {
+      const transformedSurvey = {
+        ...surveyPrisma,
+        attributeFilters: [],
+        triggers: [],
+        responses: surveyPrisma._count.responses,
+      };
+      const survey = ZSurveyWithResponseCount.parse(transformedSurvey);
+      surveys.push(survey);
+    }
+
+    try {
+      return surveys;
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        console.error(JSON.stringify(error.errors, null, 2)); // log the detailed error information
+      }
+      throw new ValidationError("Data validation of survey failed");
+    }
+  }
+);
