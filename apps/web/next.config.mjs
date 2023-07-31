38d3de2165656e31a03793199faf6356cab2a002
Matti Nannt <mail@matthiasnannt.com>
2023-07-31 10:26:59 +0200

    Add sentry error reporting (#619)

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,3 +1,4 @@
+import { withSentryConfig } from "@sentry/nextjs";
 import "./env.mjs";
 import { createId } from "@paralleldrive/cuid2";
 
@@ -65,4 +66,39 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+const sentryOptions = {
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options
+
+  // Suppresses source map uploading logs during build
+  silent: true,
+
+  org: "formbricks",
+  project: "formbricks-cloud",
+};
+
+const sentryConfig = {
+  // For all available options, see:
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+  // Upload a larger set of source maps for prettier stack traces (increases build time)
+  widenClientFileUpload: true,
+
+  // Transpiles SDK to be compatible with IE11 (increases bundle size)
+  transpileClientSDK: true,
+
+  // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
+  tunnelRoute: "/monitoring",
+
+  // Hides source maps from generated client bundles
+  hideSourceMaps: true,
+
+  // Automatically tree-shake Sentry logger statements to reduce bundle size
+  disableLogger: true,
+};
+
+const exportConfig = process.env.NEXT_PUBLIC_SENTRY_DSN
+  ? withSentryConfig(nextConfig, sentryOptions, sentryConfig)
+  : nextConfig;
+
+export default exportConfig;
