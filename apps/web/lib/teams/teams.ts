cb2f180c8689add12dabe5ffc65f4553bd7ab98b
pandeymangg <anshuman.pandey9999@gmail.com>
2023-08-04 16:11:14 +0530

    fix: removes unused code

--- apps/web/lib/teams/teams.ts
+++ apps/web/lib/teams/teams.ts
@@ -1,6 +1,5 @@
 import useSWR from "swr";
 import { fetcher } from "@formbricks/lib/fetcher";
-import { INTERNAL_SECRET } from "@formbricks/lib/constants";
 
 export const useTeam = (environmentId: string) => {
   const { data, isLoading, error, mutate, isValidating } = useSWR(
@@ -15,20 +14,4 @@ export const useTeam = (environmentId: string) => {
     isValidatingTeam: isValidating,
     mutateTeam: mutate,
   };
-};
-
-export const addDemoProduct = async (teamId: string) => {
-  const response = await fetch(`/api/v1/teams/${teamId}/add_demo_product`, {
-    method: "POST",
-    headers: {
-      "x-api-key": INTERNAL_SECRET,
-    },
-  });
-
-  if (!response.ok) {
-    const error = new Error("An error occurred while adding the demo product to your team.");
-    throw error;
-  }
-
-  return response.json();
 };
