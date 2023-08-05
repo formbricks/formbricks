c22158515d288738915a39c1d9d57bdb38d66383
Piyush Gupta <piyushguptaa2z123@gmail.com>
2023-08-05 09:32:23 +0530

    feat: added delete team functionality

--- apps/web/app/(app)/environments/[environmentId]/settings/members/DeleteTeam.tsx
+++ apps/web/app/(app)/environments/[environmentId]/settings/members/DeleteTeam.tsx
@@ -1,61 +1,56 @@
 "use client";
 
+import toast from "react-hot-toast";
 import DeleteDialog from "@/components/shared/DeleteDialog";
 import LoadingSpinner from "@/components/shared/LoadingSpinner";
-import { useEnvironment } from "@/lib/environments/environments";
+import { useState, Dispatch, SetStateAction } from "react";
+import { useRouter } from "next/navigation";
 import { useMembers } from "@/lib/members";
-import { useProduct } from "@/lib/products/products";
 import { useProfile } from "@/lib/profile";
 import { truncate } from "@/lib/utils";
 import { Button, ErrorComponent, Input } from "@formbricks/ui";
-import { useTeamMutation } from "@/lib/teams/mutateTeams";
-import { useTeam } from "@/lib/teams/teams";
-import { useState, Dispatch, SetStateAction } from "react";
-import toast from "react-hot-toast";
+import { useTeam, deleteTeam } from "@/lib/teams/teams";
 import { useMemberships } from "@/lib/memberships";
 
 export default function DeleteTeam({ environmentId }) {
   const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState(false);
 
+  const router = useRouter();
   const { profile } = useProfile();
   const { memberships } = useMemberships();
-  const { team } = useMembers(environmentId);
-  const { product, isLoadingProduct, isErrorProduct } = useProduct(environmentId);
-  const { environment } = useEnvironment(environmentId);
-  const { team: teamData } = useTeam(environmentId);
-
-  console.log({ profile, team, product, environment, teamData, memberships });
+  const { team, isErrorTeam: isErrorTeamMembers } = useMembers(environmentId);
+  const { team: teamData, isLoadingTeam, isErrorTeam } = useTeam(environmentId);
 
   const availableTeams = memberships?.length;
   const role = team?.members?.filter((member) => member?.userId === profile?.id)[0]?.role;
-  const isUserAdminOrOwner = role === "admin" || role === "owner";
-  const isDeleteDisabled = availableTeams <= 1 || !isUserAdminOrOwner;
+  const isUserOwner = role === "owner";
+  const isDeleteDisabled = availableTeams <= 1 || !isUserOwner;
 
-  if (isLoadingProduct) {
+  if (isLoadingTeam) {
     return <LoadingSpinner />;
   }
-  if (isErrorProduct) {
+  if (isErrorTeam) {
     return <ErrorComponent />;
   }
 
-  //   const handleDeleteProduct = async () => {
-  //     if (environment?.availableProducts?.length <= 1) {
-  //       toast.error("Cannot delete product. Your team needs at least 1.");
-  //       setIsDeleteDialogOpen(false);
-  //       return;
-  //     }
-  //     const deleteProductRes = await deleteProduct(environmentId);
+  const handleDeleteTeam = async () => {
+    if (memberships?.length <= 1) {
+      toast.error("Cannot delete team. You need at least 1.");
+      setIsDeleteDialogOpen(false);
+      return;
+    }
+    const deleteTeamRes = await deleteTeam(environmentId);
 
-  //     if (deleteProductRes?.id?.length > 0) {
-  //       toast.success("Product deleted successfully.");
-  //       //   router.push("/");
-  //     } else if (deleteProductRes?.message?.length > 0) {
-  //       toast.error(deleteProductRes.message);
-  //       setIsDeleteDialogOpen(false);
-  //     } else {
-  //       toast.error("Error deleting product. Please try again.");
-  //     }
-  //   };
+    if (deleteTeamRes?.deletedTeam?.id?.length > 0) {
+      toast.success("Team deleted successfully.");
+      router.push("/");
+    } else if (deleteTeamRes?.message?.length > 0) {
+      toast.error(deleteTeamRes.message);
+      setIsDeleteDialogOpen(false);
+    } else {
+      toast.error("Error deleting team. Please try again.");
+    }
+  };
 
   return (
     <div>
@@ -77,12 +72,17 @@ export default function DeleteTeam({ environmentId }) {
       )}
       {isDeleteDisabled && (
         <p className="text-sm text-red-700">
-          {!isUserAdminOrOwner
-            ? "Only Admin or Owners can delete teams."
+          {!isUserOwner
+            ? "Only Owner can delete the team."
             : "This is your only team, it cannot be deleted. Create a new team first."}
         </p>
       )}
-      <DeleteTeamModal open={isDeleteDialogOpen} setOpen={setIsDeleteDialogOpen} teamData={teamData} />
+      <DeleteTeamModal
+        open={isDeleteDialogOpen}
+        setOpen={setIsDeleteDialogOpen}
+        teamData={teamData}
+        deleteTeam={handleDeleteTeam}
+      />
     </div>
   );
 }
@@ -91,28 +91,14 @@ interface DeleteTeamModalProps {
   open: boolean;
   setOpen: Dispatch<SetStateAction<boolean>>;
   teamData: { name: string; id: string; plan: string };
+  deleteTeam: () => void;
 }
 
-function DeleteTeamModal({ setOpen, open, teamData }: DeleteTeamModalProps) {
-  const [deleting, setDeleting] = useState(false);
+function DeleteTeamModal({ setOpen, open, teamData, deleteTeam }: DeleteTeamModalProps) {
   const [inputValue, setInputValue] = useState("");
 
   const handleInputChange = (e) => {
     setInputValue(e.target.value);
-  };
-
-  const deleteTeam = async () => {
-    try {
-      setDeleting(true);
-      //   await deleteProfile();
-      //   await signOut();
-      //   await formbricksLogout();
-    } catch (error) {
-      toast.error("Something went wrong");
-    } finally {
-      setDeleting(false);
-      setOpen(false);
-    }
   };
 
   return (
@@ -120,35 +106,26 @@ function DeleteTeamModal({ setOpen, open, teamData }: DeleteTeamModalProps) {
       open={open}
       setOpen={setOpen}
       deleteWhat="team"
-      onDelete={() => deleteTeam()}
+      onDelete={deleteTeam}
       text="Before you proceed with deleting this team, please be aware of the following consequences:"
-      isDeleting={deleting}
-      disabled={inputValue !== teamData.name}>
+      disabled={inputValue !== teamData?.name}>
       <div className="py-5">
         <ul className="list-disc pb-6 pl-6">
           <li>
             Permanent removal of all <b>products linked to this team</b>. This includes all surveys,
             responses, user actions and attributes associated with these products.
           </li>
-          <li>
-            If you are the owner of a team with other admins, the ownership of that team will be transferred
-            to another admin.
-          </li>
-          <li>
-            If you are the only member of a team or there is no other admin present, the team will be
-            irreversibly deleted along with all associated data.
-          </li>
           <li>This action cannot be undone. If it&apos;s gone, it&apos;s gone.</li>
         </ul>
         <form>
           <label htmlFor="deleteTeamConfirmation">
-            Please enter <b>{teamData.name}</b> in the following field to confirm the definitive deletion of
+            Please enter <b>{teamData?.name}</b> in the following field to confirm the definitive deletion of
             this team:
           </label>
           <Input
             value={inputValue}
             onChange={handleInputChange}
-            placeholder={teamData.name}
+            placeholder={teamData?.name}
             className="mt-5"
             type="text"
             id="deleteTeamConfirmation"
--- apps/web/pages/api/v1/environments/[environmentId]/team/index.ts
+++ apps/web/pages/api/v1/environments/[environmentId]/team/index.ts
@@ -1,8 +1,12 @@
-import { hasEnvironmentAccess } from "@/lib/api/apiHelper";
+import { getSessionUser, hasEnvironmentAccess } from "@/lib/api/apiHelper";
 import { prisma } from "@formbricks/database";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
+  const currentUser: any = await getSessionUser(req, res);
+  if (!currentUser) {
+    return res.status(401).json({ message: "Not authenticated" });
+  }
   const environmentId = req.query?.environmentId?.toString();
 
   if (!environmentId) {
@@ -46,6 +50,65 @@ export default async function handle(req: NextApiRequest, res: NextApiResponse) 
       return res.status(404).json({ message: "This team doesn't exist" });
     }
     return res.json(team);
+  }
+
+  // DELETE
+  else if (req.method === "DELETE") {
+    try {
+      const environment = await prisma.environment.findUnique({
+        where: {
+          id: environmentId,
+        },
+        select: {
+          product: {
+            select: {
+              teamId: true,
+            },
+          },
+        },
+      });
+      if (environment === null) {
+        return res.status(404).json({ message: "This environment doesn't exist" });
+      }
+      const team = await prisma.team.findUnique({
+        where: {
+          id: environment.product.teamId,
+        },
+        select: {
+          id: true,
+          name: true,
+          stripeCustomerId: true,
+          plan: true,
+        },
+      });
+
+      if (team === null) {
+        return res.status(404).json({ message: "This team doesn't exist" });
+      }
+
+      const membership = await prisma.membership.findUnique({
+        where: {
+          userId_teamId: {
+            userId: currentUser.id,
+            teamId: team.id,
+          },
+        },
+      });
+
+      if (membership?.role !== "owner") {
+        return res.status(403).json({ message: "You are not allowed to delete this team" });
+      }
+
+      const prismaRes = await prisma.team.delete({
+        where: {
+          id: team.id,
+        },
+      });
+
+      return res.status(200).json({ deletedTeam: prismaRes });
+    } catch (error) {
+      return res.status(500).json({ message: error.message });
+    }
   }
   // Unknown HTTP Method
   else {
