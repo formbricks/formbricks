fdb1aa2299da76ba708a97a139229d50b7203244
Shubham Palriwala <spalriwalau@gmail.com>
2023-08-06 13:23:37 +0530

    Rewrite Person Detail Page to Server Components (#609)
    * feat: migration /[personId] page to server side
    
    * feat: decouple components in person page
    
    * fix: ZDisplaysWithSurveyName now extends the ZDisplay type
    
    * feat: drop custom service and use existing service for survey and response
    
    * run pnpm format
    
    * shift data fetching to component level but still server side
    
    * rename event to action
    
    * move special person services to activity service
    
    * remove activityFeedItem type in ActivityFeed
    
    * simplify TResponseWithSurvey
    
    ---------
    
    Co-authored-by: Matthias Nannt <mail@matthiasnannt.com>

--- apps/web/app/(app)/environments/[environmentId]/people/[personId]/(responseSection)/ResponsesFeed.tsx
+++ apps/web/app/(app)/environments/[environmentId]/people/[personId]/(responseSection)/ResponsesFeed.tsx
@@ -1,26 +1,35 @@
+import { formatDistance } from "date-fns";
 import EmptySpaceFiller from "@/components/shared/EmptySpaceFiller";
 import SurveyStatusIndicator from "@/components/shared/SurveyStatusIndicator";
-import { timeSince } from "@formbricks/lib/time";
+import { TResponseWithSurvey } from "@formbricks/types/v1/responses";
 import Link from "next/link";
 
-export default function ResponseFeed({ person, sortByDate, environmentId }) {
+export default function ResponseFeed({
+  responses,
+  sortByDate,
+  environmentId,
+}: {
+  responses: TResponseWithSurvey[];
+  sortByDate: boolean;
+  environmentId: string;
+}) {
   return (
     <>
-      {person.responses.length === 0 ? (
+      {responses.length === 0 ? (
         <EmptySpaceFiller type="response" environmentId={environmentId} />
       ) : (
         <div>
-          {person.responses
+          {responses
             .slice()
             .sort((a, b) =>
               sortByDate
                 ? new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
                 : new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
             )
-            .map((response, responseIdx) => (
-              <li key={response.createdAt} className="list-none">
+            .map((response: TResponseWithSurvey, responseIdx) => (
+              <li key={response.id} className="list-none">
                 <div className="relative pb-8">
-                  {responseIdx !== person.responses.length - 1 ? (
+                  {responseIdx !== responses.length - 1 ? (
                     <span
                       className="absolute left-4 top-4 -ml-px h-full w-0.5 bg-slate-200"
                       aria-hidden="true"
@@ -31,8 +40,14 @@ export default function ResponseFeed({ person, sortByDate, environmentId }) {
                       <div className="px-4 py-5 sm:p-6">
                         <div className="flex w-full justify-between">
                           <div className="text-sm text-slate-400">
-                            <time className="text-slate-700" dateTime={timeSince(response.createdAt)}>
-                              {timeSince(response.createdAt)}
+                            <time
+                              className="text-slate-700"
+                              dateTime={formatDistance(response.createdAt, new Date(), {
+                                addSuffix: true,
+                              })}>
+                              {formatDistance(response.createdAt, new Date(), {
+                                addSuffix: true,
+                              })}
                             </time>
                           </div>
                           <div className="flex items-center justify-center space-x-2  rounded-full bg-slate-50 px-3 py-1 text-sm text-slate-600">
@@ -52,8 +67,8 @@ export default function ResponseFeed({ person, sortByDate, environmentId }) {
                             <div key={question.id}>
                               <p className="text-sm text-slate-500">{question.headline}</p>
                               <p className="ph-no-capture my-1 text-lg font-semibold text-slate-700">
-                                {response.data[question.id] instanceof Array
-                                  ? response.data[question.id].join(", ")
+                                {Array.isArray(response.data[question.id])
+                                  ? (response.data[question.id] as string[]).join(", ")
                                   : response.data[question.id]}
                               </p>
                             </div>
