71d797de0e7346d0201d81a2da3415a71368d089
Matthias Nannt <mail@matthiasnannt.com>
2023-08-07 15:31:09 +0200

    fix migrations

--- packages/database/migrations/20230724125938_added_highlight_border_color_to_product_model/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Product" ADD COLUMN     "highlightBorderColor" TEXT NOT NULL DEFAULT '#64748b';
--- packages/database/migrations/20230724142939_added_boolean_field_for_showing_highlight_border/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Product" ADD COLUMN     "showHighlightBorder" BOOLEAN NOT NULL DEFAULT false;
