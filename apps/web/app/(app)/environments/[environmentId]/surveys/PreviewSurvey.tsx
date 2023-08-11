77e081c9d1247502700ed802cb6ec81e92136696
Matthias Nannt <mail@matthiasnannt.com>
2023-08-11 10:46:55 +0200

    re-embed restart button, make phone preview more responsive

--- apps/web/app/(app)/environments/[environmentId]/surveys/PreviewSurvey.tsx
+++ apps/web/app/(app)/environments/[environmentId]/surveys/PreviewSurvey.tsx
@@ -10,6 +10,8 @@ import type { Logic, Question } from "@formbricks/types/questions";
 import { Survey } from "@formbricks/types/surveys";
 import type { TEnvironment } from "@formbricks/types/v1/environment";
 import type { TProduct } from "@formbricks/types/v1/product";
+import { Button } from "@formbricks/ui";
+import { ArrowPathRoundedSquareIcon } from "@heroicons/react/24/outline";
 import { ComputerDesktopIcon, DevicePhoneMobileIcon } from "@heroicons/react/24/solid";
 import { useEffect, useRef, useState } from "react";
 interface PreviewSurveyProps {
@@ -348,75 +350,79 @@ export default function PreviewSurvey({
 
   return (
     <div className="flex h-full w-full flex-col items-center justify-items-center">
-      <div className="flex h-[95%] max-h-[95%] w-5/6 items-center justify-center rounded-lg border border-slate-300 bg-slate-200">
+      <div className="relative flex h-[95%] max-h-[95%] w-5/6 items-center justify-center rounded-lg border border-slate-300 bg-slate-200">
         {previewMode === "mobile" && (
-          <div className="relative h-[90%] w-1/2 overflow-hidden rounded-[3rem] border-8 border-slate-500 bg-slate-400">
-            {/* below element is use to create notch for the mobile device mockup   */}
-            <div className="absolute left-1/2 right-1/2 top-0 z-20 h-4 w-1/2 -translate-x-1/2 transform rounded-b-md bg-slate-500"></div>
-            {previewType === "modal" ? (
-              <Modal
-                isOpen={isModalOpen}
-                placement={product.placement}
-                highlightBorderColor={product.highlightBorderColor}
-                previewMode="mobile">
-                {!countdownStop && autoClose !== null && autoClose > 0 && (
-                  <Progress progress={countdownProgress} brandColor={brandColor} />
-                )}
-                <PreviewModalContent
-                  activeQuestionId={activeQuestionId}
-                  lastActiveQuestionId={lastActiveQuestionId}
-                  questions={questions}
-                  brandColor={brandColor}
-                  thankYouCard={thankYouCard}
-                  gotoNextQuestion={gotoNextQuestion}
-                  showBackButton={showBackButton}
-                  goToPreviousQuestion={goToPreviousQuestion}
-                  storedResponseValue={storedResponseValue}
-                  showFormbricksSignature={showFormbricksSignature}
-                />
-                <Progress progress={progress} brandColor={brandColor} />
-              </Modal>
-            ) : (
-              <div
-                className="absolute top-0 z-10 flex h-full w-full flex-grow flex-col overflow-y-auto"
-                ref={ContentRef}>
-                <div className="flex w-full flex-grow flex-col items-center justify-center bg-white py-6">
-                  <div className="w-full max-w-md px-4">
-                    <QuestionRenderer
-                      activeQuestionId={activeQuestionId}
-                      lastActiveQuestionId={lastActiveQuestionId}
-                      questions={questions}
-                      brandColor={brandColor}
-                      thankYouCard={thankYouCard}
-                      gotoNextQuestion={gotoNextQuestion}
-                      showBackButton={showBackButton}
-                      goToPreviousQuestion={goToPreviousQuestion}
-                      storedResponseValue={storedResponseValue}
-                    />
+          <>
+            <div className="absolute right-0 top-0 m-2">
+              <ResetProgressButton resetQuestionProgress={resetQuestionProgress} />
+            </div>
+            <div className="relative h-[90%] max-h-[40rem] w-80 overflow-hidden rounded-[3rem] border-8 border-slate-500 bg-slate-400">
+              {/* below element is use to create notch for the mobile device mockup   */}
+              <div className="absolute left-1/2 right-1/2 top-0 z-20 h-4 w-1/2 -translate-x-1/2 transform rounded-b-md bg-slate-500"></div>
+              {previewType === "modal" ? (
+                <Modal
+                  isOpen={isModalOpen}
+                  placement={product.placement}
+                  highlightBorderColor={product.highlightBorderColor}
+                  previewMode="mobile">
+                  {!countdownStop && autoClose !== null && autoClose > 0 && (
+                    <Progress progress={countdownProgress} brandColor={brandColor} />
+                  )}
+                  <PreviewModalContent
+                    activeQuestionId={activeQuestionId}
+                    lastActiveQuestionId={lastActiveQuestionId}
+                    questions={questions}
+                    brandColor={brandColor}
+                    thankYouCard={thankYouCard}
+                    gotoNextQuestion={gotoNextQuestion}
+                    showBackButton={showBackButton}
+                    goToPreviousQuestion={goToPreviousQuestion}
+                    storedResponseValue={storedResponseValue}
+                    showFormbricksSignature={showFormbricksSignature}
+                  />
+                  <Progress progress={progress} brandColor={brandColor} />
+                </Modal>
+              ) : (
+                <div
+                  className="absolute top-0 z-10 flex h-full w-full flex-grow flex-col overflow-y-auto"
+                  ref={ContentRef}>
+                  <div className="flex w-full flex-grow flex-col items-center justify-center bg-white py-6">
+                    <div className="w-full max-w-md px-4">
+                      <QuestionRenderer
+                        activeQuestionId={activeQuestionId}
+                        lastActiveQuestionId={lastActiveQuestionId}
+                        questions={questions}
+                        brandColor={brandColor}
+                        thankYouCard={thankYouCard}
+                        gotoNextQuestion={gotoNextQuestion}
+                        showBackButton={showBackButton}
+                        goToPreviousQuestion={goToPreviousQuestion}
+                        storedResponseValue={storedResponseValue}
+                      />
+                    </div>
+                  </div>
+                  <div className="z-10 w-full rounded-b-lg bg-white">
+                    <div className="mx-auto max-w-md space-y-6 p-6 pt-4">
+                      <Progress progress={progress} brandColor={brandColor} />
+                      {showFormbricksSignature && <FormbricksSignature />}
+                    </div>
                   </div>
                 </div>
-                <div className="z-10 w-full rounded-b-lg bg-white">
-                  <div className="mx-auto max-w-md space-y-6 p-6 pt-4">
-                    <Progress progress={progress} brandColor={brandColor} />
-                    {showFormbricksSignature && <FormbricksSignature />}
-                  </div>
-                </div>
-              </div>
-            )}
-          </div>
+              )}
+            </div>
+          </>
         )}
         {previewMode === "desktop" && (
           <div className="flex h-full w-5/6 flex-1 flex-col">
-            <div className="flex h-8 items-center rounded-t-lg bg-slate-100">
+            <div className="flex h-8 w-full items-center rounded-t-lg bg-slate-100">
               <div className="ml-6 flex space-x-2">
                 <div className="h-3 w-3 rounded-full bg-red-500"></div>
                 <div className="h-3 w-3 rounded-full bg-amber-500"></div>
                 <div className="h-3 w-3 rounded-full bg-emerald-500"></div>
               </div>
-              <p>
-                <span className="ml-4 font-mono text-sm text-slate-400">
-                  {previewType === "modal" ? "Your web app" : "Preview"}
-                </span>
+              <p className="ml-4 flex w-full justify-between font-mono text-sm text-slate-400">
+                {previewType === "modal" ? "Your web app" : "Preview"}
+                <ResetProgressButton resetQuestionProgress={resetQuestionProgress} />
               </p>
             </div>
 
@@ -485,5 +491,17 @@ export default function PreviewSurvey({
         />
       </div>
     </div>
+  );
+}
+
+function ResetProgressButton({ resetQuestionProgress }) {
+  return (
+    <Button
+      variant="minimal"
+      className="py-0.2 bg-white px-2 text-sm text-slate-500"
+      onClick={resetQuestionProgress}>
+      Restart
+      <ArrowPathRoundedSquareIcon className="ml-2 h-4 w-4" />
+    </Button>
   );
 }
