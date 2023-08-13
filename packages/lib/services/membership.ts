e9510cb5a39f34193dc46849f76cab1872563e6b
pandeymangg <anshuman.pandey9999@gmail.com>
2023-08-13 15:37:10 +0530

    fix: fixes membership types

--- packages/lib/services/membership.ts
+++ packages/lib/services/membership.ts
@@ -1,8 +1,8 @@
 import { prisma } from "@formbricks/database";
-import { TMembership } from "@formbricks/types/v1/memberships";
+import { TMember, TMembership } from "@formbricks/types/v1/memberships";
 import { cache } from "react";
 
-export const getMembersByTeamId = cache(async (teamId: string): Promise<TMembership[]> => {
+export const getMembersByTeamId = cache(async (teamId: string): Promise<TMember[]> => {
   const membersData = await prisma.membership.findMany({
     where: { teamId },
     select: {
@@ -31,15 +31,19 @@ export const getMembersByTeamId = cache(async (teamId: string): Promise<TMembers
   return members;
 });
 
-export const getMembershipByUserId = cache(async (userId: string, teamId: string) => {
-  const membership = await prisma.membership.findUnique({
-    where: {
-      userId_teamId: {
-        userId,
-        teamId,
+export const getMembershipByUserId = cache(
+  async (userId: string, teamId: string): Promise<TMembership | null> => {
+    const membership = await prisma.membership.findUnique({
+      where: {
+        userId_teamId: {
+          userId,
+          teamId,
+        },
       },
-    },
-  });
+    });
 
-  return membership;
-});
+    if (!membership) return null;
+
+    return membership;
+  }
+);
--- packages/types/v1/memberships.ts
+++ packages/types/v1/memberships.ts
@@ -1,11 +1,22 @@
 import z from "zod";
 
+export const ZMembershipRole = z.enum(["owner", "admin", "editor", "developer", "viewer"]);
+
 export const ZMembership = z.object({
+  teamId: z.string(),
+  userId: z.string(),
+  accepted: z.boolean(),
+  role: ZMembershipRole,
+});
+
+export const ZMember = z.object({
   name: z.string().nullable(),
   email: z.string(),
   userId: z.string(),
   accepted: z.boolean(),
-  role: z.enum(["owner", "admin", "editor", "developer", "viewer"]),
+  role: ZMembershipRole,
 });
 
 export type TMembership = z.infer<typeof ZMembership>;
+export type TMember = z.infer<typeof ZMember>;
+export type TMembershipRole = z.infer<typeof ZMembershipRole>;
