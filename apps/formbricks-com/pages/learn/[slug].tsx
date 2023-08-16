a0ff35ed867607fb2a0e32ccc1d9694809f3951b
Johannes <johannes@formbricks.com>
2023-08-16 13:21:43 +0200

    new data model, faq data, new image host

--- apps/formbricks-com/pages/learn/[slug].tsx
+++ apps/formbricks-com/pages/learn/[slug].tsx
@@ -1,79 +1,86 @@
 import LayoutMdx from "@/components/shared/LayoutMdx";
+import { FAQPageJsonLd } from "next-seo";
+import Image from "next/image";
 import fetch from "node-fetch";
 import ReactMarkdown from "react-markdown";
 
-type ArticleAttributes = {
-  createdAt: string;
-  updatedAt: string;
-  publishedAt: string;
-  Article: {
-    id: number;
-    slug: string;
-    author: string;
-  };
-  Metadata?: {
-    metaTitle: string;
-    metaDescription: string;
-    metaSection: string;
-    tag1: string;
-    tag2: string;
-    tag3: string;
-  };
-};
-
-type Article = {
-  id: number;
-  attributes: ArticleAttributes;
-};
-
-type APIResponse = {
-  data: Article[];
-};
-
 export async function getStaticPaths() {
-  const response = await fetch("http://127.0.0.1:1337/api/learn-articles?populate=*");
-  const articles: APIResponse = await response.json();
+  const response = await fetch(
+    "http://127.0.0.1:1337/api/articles?populate[meta][populate]=*&filters[category][name][$eq]=learn"
+  );
+  const articles = await response.json();
 
   if (!response.ok) {
     throw new Error(`HTTP error! Status: ${response.status}`);
   }
 
   const paths = articles.data.map((article) => ({
-    params: { slug: article.attributes.Article.slug },
+    params: { slug: article.attributes.slug },
   }));
 
   return { paths, fallback: true };
 }
 
 export async function getStaticProps({ params }) {
-  const res = await fetch(`http://127.0.0.1:1337/api/learn-articles?populate=*`);
+  const res = await fetch(
+    `http://127.0.0.1:1337/api/articles?populate[meta][populate]=*&populate[faq][populate]=*&filters[slug][$eq]=${params.slug}`
+  );
   if (!res.ok) {
     throw new Error("Something went wrong");
   }
-  const resData: APIResponse = await res.json();
-  const articles = resData.data;
-  // find article by slug attribute
-  const article = articles.find((a) => a.attributes.Article.slug === params.slug);
+  const resData = await res.json();
+  const article = resData.data[0];
   return { props: { article } };
 }
 
-export default function ArticlePage({ article }) {
-  if (!article) return <div>Loading...</div>;
+export default function ArticlePage({ article = {} }) {
+  if (!article || !article.attributes) return <div>Loading...</div>;
 
-  const { Article, Metadata } = article.attributes;
+  // Use next/image to render images in markdown
+  const renderers = {
+    img: (image) => {
+      return <Image src={image.src} alt={image.alt} width={1000} height={500} />;
+    },
+  };
+
+  const {
+    attributes: {
+      author,
+      publishedAt,
+      text,
+      faq,
+      meta: {
+        title,
+        description,
+        section,
+        tags = [], // default empty array if tags are not provided
+      } = {}, // default empty object if meta is not provided
+    } = {}, // default empty object if attributes are not provided
+  } = article;
+
+  const metaTags = tags.map((tag) => tag.tag);
 
   const meta = {
-    title: Article.title,
-    description: Metadata.metaDescription,
-    publishedTime: article.attributes.publishedAt,
-    authors: [Article.author],
-    section: Metadata.metaSection,
-    tags: [Metadata.tag1, Metadata.tag2, Metadata.tag3, Metadata.tag4].filter(Boolean),
+    title,
+    description,
+    publishedTime: publishedAt,
+    authors: [author],
+    section,
+    tags: metaTags,
   };
+
+  // Convert the FAQ details into the desired format for FAQPageJsonLd
+  const faqEntities = faq.map(({ question, answer }) => ({
+    questionName: question,
+    acceptedAnswerText: answer,
+  }));
 
   return (
     <LayoutMdx meta={meta}>
-      <ReactMarkdown>{Article.content}</ReactMarkdown>
+      <>
+        <ReactMarkdown components={renderers}>{text}</ReactMarkdown>
+        <FAQPageJsonLd mainEntity={faqEntities} />
+      </>
     </LayoutMdx>
   );
 }
