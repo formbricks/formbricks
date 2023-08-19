692c56226d8a06bb46e8bdf0f325e8ee9192cbb1
joyal007 <joyalraphel00@gmail.com>
2023-08-19 19:08:58 +0530

    fixed button descendent on button error

--- apps/web/app/(app)/environments/[environmentId]/surveys/[surveyId]/SummaryHeader.tsx
+++ apps/web/app/(app)/environments/[environmentId]/surveys/[surveyId]/SummaryHeader.tsx
@@ -71,9 +71,9 @@ const SummaryHeader = ({ surveyId, environmentId, survey }: SummaryHeaderProps) 
       <div className="block sm:hidden">
         <DropdownMenu>
           <DropdownMenuTrigger>
-            <Button size="sm" variant="secondary" className="h-full w-full rounded-md p-2">
+            <div className="px-3 py-2 text-sm leading-4 font-mediumtext-slate-600 hover:text-slate-500 bg-slate-200 hover:bg-slate-100 dark:bg-slate-700 dark:text-slate-300 dark:hover:bg-slate-600 focus:outline-none focus:ring-2 focus:ring-offset-1  focus:bg-slate-300 focus:ring-neutral-500 h-full w-full rounded-md p-2">
               <EllipsisHorizontalIcon className="h-6" />
-            </Button>
+            </div>
           </DropdownMenuTrigger>
           <DropdownMenuContent align="end" className="p-2">
             {survey.type === "link" && (
