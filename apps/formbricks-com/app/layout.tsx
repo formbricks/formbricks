81652b445b4c2989c3f60186c82f84193cc52a8b
Matthias Nannt <mail@matthiasnannt.com>
2023-08-22 16:13:14 +0200

    fix font on mainpage

--- apps/formbricks-com/app/layout.tsx
+++ apps/formbricks-com/app/layout.tsx
@@ -1,13 +1,9 @@
-import '@/styles/tailwind.css'
+import "@/styles/globals.css";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
