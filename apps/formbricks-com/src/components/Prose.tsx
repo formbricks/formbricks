7ce8d75d911b6cc5d8885b92549f73f1bc3012c4
Matthias Nannt <mail@matthiasnannt.com>
2023-08-22 13:58:23 +0200

    add new template

---
+++ apps/formbricks-com/src/components/Prose.tsx
@@ -0,0 +1,19 @@
+import clsx from 'clsx'
+
+export function Prose<T extends React.ElementType = 'div'>({
+  as,
+  className,
+  ...props
+}: Omit<React.ComponentPropsWithoutRef<T>, 'as' | 'className'> & {
+  as?: T
+  className?: string
+}) {
+  let Component = as ?? 'div'
+
+  return (
+    <Component
+      className={clsx(className, 'prose dark:prose-invert')}
+      {...props}
+    />
+  )
+}
