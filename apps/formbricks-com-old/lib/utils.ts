8c229e468579a77165ac00a7ac9d5c379d349cc2
Matthias Nannt <mail@matthiasnannt.com>
2023-08-22 14:38:52 +0200

    remove old formbricks-com

--- apps/formbricks-com-old/lib/utils.ts
+++
@@ -1,8 +0,0 @@
-export function formatDate(dateString: string) {
-  return new Date(`${dateString}T00:00:00Z`).toLocaleDateString("en-US", {
-    day: "numeric",
-    month: "long",
-    year: "numeric",
-    timeZone: "UTC",
-  });
-}
--- apps/formbricks-com/next.config.mjs
+++ apps/formbricks-com/next.config.mjs
@@ -1,9 +1,10 @@
-import nextMDX from '@next/mdx'
+import nextMDX from "@next/mdx";
 
-import { recmaPlugins } from './src/mdx/recma.mjs'
-import { rehypePlugins } from './src/mdx/rehype.mjs'
-import { remarkPlugins } from './src/mdx/remark.mjs'
-import withSearch from './src/mdx/search.mjs'
+import { withPlausibleProxy } from "next-plausible";
+import { recmaPlugins } from "./src/mdx/recma.mjs";
+import { rehypePlugins } from "./src/mdx/rehype.mjs";
+import { remarkPlugins } from "./src/mdx/remark.mjs";
+import withSearch from "./src/mdx/search.mjs";
 
 const withMDX = nextMDX({
   options: {
@@ -11,11 +12,112 @@ const withMDX = nextMDX({
     rehypePlugins,
     recmaPlugins,
   },
-})
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
-}
+  pageExtensions: ["js", "jsx", "ts", "tsx", "mdx"],
+  transpilePackages: ["@formbricks/ui", "@formbricks/lib"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "seo-strapi-aws-s3.s3.eu-central-1.amazonaws.com",
+        port: "",
+      },
+    ],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/discord",
+        destination: "https://discord.gg/3YFcABF2Ts",
+        permanent: true,
+      },
+      {
+        source: "/roadmap",
+        destination: "https://github.com/orgs/formbricks/projects/1",
+        permanent: true,
+      },
+      {
+        source: "/github",
+        destination: "https://github.com/formbricks/formbricks",
+        permanent: true,
+      },
+      {
+        source: "/deal",
+        destination: "/concierge",
+        permanent: false,
+      },
+      {
+        source: "/privacy",
+        destination: "/privacy-policy",
+        permanent: true,
+      },
+      {
+        source: "/form-hq",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/docs/getting-started/nextjs",
+        destination: "/docs/getting-started/nextjs-app",
+        permanent: true,
+      },
+      {
+        source: "/docs/formbricks-hq/self-hosting",
+        destination: "/docs",
+        permanent: true,
+      },
+      {
+        source: "/docs/react-form-library/getting-started",
+        destination: "/docs",
+        permanent: true,
+      },
+      {
+        source: "/docs/react-form-library/work-with-components",
+        destination: "/docs",
+        permanent: true,
+      },
+      {
+        source: "/docs/react-form-library/introduction",
+        destination: "/docs",
+        permanent: true,
+      },
+      {
+        source: "/docs/formbricks-hq/schema",
+        destination: "/docs",
+        permanent: true,
+      },
+      {
+        source: "/docs/events/why",
+        destination: "/docs/actions/why",
+        permanent: true,
+      },
+      {
+        source: "/docs/events/code",
+        destination: "/docs/actions/code",
+        permanent: true,
+      },
+      {
+        source: "/docs/events/code",
+        destination: "/docs/actions/code",
+        permanent: true,
+      },
+      {
+        source: "/pmf",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/blog/v1-and-how-we-got-here",
+        destination: "/blog/experience-management-open-source",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-export default withSearch(withMDX(nextConfig))
+export default withPlausibleProxy({ customDomain: "https://plausible.formbricks.com" })(
+  withSearch(withMDX(nextConfig))
+);
