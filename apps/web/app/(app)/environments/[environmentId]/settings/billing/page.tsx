8e69f2fcdaf37199dcaa510a187e9c3b59e692e5
Matthias Nannt <mail@matthiasnannt.com>
2023-08-24 11:38:52 +0200

    add 404 page, throw 404 on billing page when !IS_FORMBRICKS_CLOUD

--- apps/web/app/(app)/environments/[environmentId]/settings/billing/page.tsx
+++ apps/web/app/(app)/environments/[environmentId]/settings/billing/page.tsx
@@ -1,14 +1,19 @@
 export const revalidate = REVALIDATION_INTERVAL;
 
-import { REVALIDATION_INTERVAL } from "@formbricks/lib/constants";
+import { IS_FORMBRICKS_CLOUD, REVALIDATION_INTERVAL } from "@formbricks/lib/constants";
 
 import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
+import { getTeamByEnvironmentId } from "@formbricks/lib/services/team";
 import { getServerSession } from "next-auth";
+import { notFound } from "next/navigation";
 import SettingsTitle from "../SettingsTitle";
 import PricingTable from "./PricingTable";
-import { getTeamByEnvironmentId } from "@formbricks/lib/services/team";
 
 export default async function ProfileSettingsPage({ params }) {
+  if (!IS_FORMBRICKS_CLOUD) {
+    notFound();
+  }
+
   const session = await getServerSession(authOptions);
   const team = await getTeamByEnvironmentId(params.environmentId);
 
@@ -22,12 +27,10 @@ export default async function ProfileSettingsPage({ params }) {
 
   return (
     <>
-      {team && session && (
-        <div>
-          <SettingsTitle title="Billing & Plan" />
-          <PricingTable team={team} />
-        </div>
-      )}
+      <div>
+        <SettingsTitle title="Billing & Plan" />
+        <PricingTable team={team} />
+      </div>
     </>
   );
 }
