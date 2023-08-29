9be129fe5a1a7e1e8c78b0dc66e706df340d1dd4
tykerr <tykerr301@gmail.com>
2023-08-30 01:45:44 +0700

    add db migration

---
+++ packages/database/migrations/20230829184522_add_single_use_id_to_survey/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[surveyId,suId]` on the table `Response` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- AlterTable
+ALTER TABLE "Response" ADD COLUMN     "suId" TEXT;
+
+-- AlterTable
+ALTER TABLE "Survey" ADD COLUMN     "singleUse" JSONB DEFAULT '{"enabled": false}';
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Response_surveyId_suId_key" ON "Response"("surveyId", "suId");
