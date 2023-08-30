34e4b99eb2cb4083babb4cb8fc71892ac6a1bb63
Johannes <johannes@formbricks.com>
2023-08-30 08:37:30 +0200

    UI tweaks

--- apps/web/app/(app)/environments/[environmentId]/integrations/google-sheets/Connect.tsx
+++ apps/web/app/(app)/environments/[environmentId]/integrations/google-sheets/Connect.tsx
@@ -1,39 +1,38 @@
-import { Button } from '@formbricks/ui'
-import FormbricksLogo from "@/images/logo.svg"
-import GoogleSheetLogo from "@/images/google-sheets-small.png"
-import Image from 'next/image'
-import { useState } from 'react'
-import { authorize } from '@formbricks/lib/client/google'
-import { WEBAPP_URL } from '@formbricks/lib/constants'
-
+import GoogleSheetLogo from "@/images/google-sheets-small.png";
+import FormbricksLogo from "@/images/logo.svg";
+import { authorize } from "@formbricks/lib/client/google";
+import { WEBAPP_URL } from "@formbricks/lib/constants";
+import { Button } from "@formbricks/ui";
+import Image from "next/image";
+import { useState } from "react";
 
 export default function Connect({ environmentId }: { environmentId: string }) {
+  const [isConnecting, setIsConnecting] = useState(false);
+  const handleGoogleLogin = async () => {
+    setIsConnecting(true);
+    authorize(environmentId, WEBAPP_URL).then((url: string) => {
+      if (url) {
+        window.location.replace(url);
+      }
+    });
+  };
 
-    const [isConnecting, setIsConnecting] = useState(false)
-    const handleGoogleLogin = async () => {
-        setIsConnecting(true)
-        authorize(environmentId, WEBAPP_URL).then((url:string) => {
-            if (url) {
-                window.location.replace(url);
-            }
-        })
-    }
-
-    return (
-        <div className="h-full w-full flex justify-center items-center">
-
-            <div className="bg-white shadow rounded-lg w-1/2 flex flex-col justify-center items-center p-8">
-                <div className="w-1/2 flex justify-center my-4 -space-x-4">
-                    <div className='w-32 h-32 rounded-full bg-white shadow-md flex items-center justify-center'>
-                        <Image className="w-1/2" src={FormbricksLogo} alt="Formbricks Logo" />
-                    </div>
-                    <div className='w-32 h-32 rounded-full bg-white shadow-md flex items-center justify-center'>
-                        <Image className="w-1/2" src={GoogleSheetLogo} alt="Google Sheet logo" />
-                    </div>
-                </div>
-                <p className='my-4'>Sync responses directly with google Sheets.</p>
-                <Button variant="darkCTA" loading={isConnecting} onClick={handleGoogleLogin}>Connect Google Sheets</Button>
-            </div>
+  return (
+    <div className="flex h-full w-full items-center justify-center">
+      <div className="flex w-1/2 flex-col items-center justify-center rounded-lg bg-white p-8 shadow">
+        <div className="flex w-1/2 justify-center -space-x-4">
+          <div className="flex h-32 w-32 items-center justify-center rounded-full bg-white p-4 shadow-md">
+            <Image className="w-1/2" src={FormbricksLogo} alt="Formbricks Logo" />
+          </div>
+          <div className="flex h-32 w-32 items-center justify-center rounded-full bg-white p-4 shadow-md">
+            <Image className="w-1/2" src={GoogleSheetLogo} alt="Google Sheet logo" />
+          </div>
         </div>
-    )
+        <p className="my-8">Sync responses directly with Google Sheets.</p>
+        <Button variant="darkCTA" loading={isConnecting} onClick={handleGoogleLogin}>
+          Connect with Google
+        </Button>
+      </div>
+    </div>
+  );
 }
--- apps/web/app/(app)/environments/[environmentId]/integrations/google-sheets/Home.tsx
+++ apps/web/app/(app)/environments/[environmentId]/integrations/google-sheets/Home.tsx
@@ -1,102 +1,123 @@
-"use client"
+"use client";
 
-import { Button } from "@formbricks/ui";
 import { deleteIntegrationAction } from "@/app/(app)/environments/[environmentId]/integrations/google-sheets/actions";
-import { useState } from "react";
 import DeleteDialog from "@/components/shared/DeleteDialog";
 import EmptySpaceFiller from "@/components/shared/EmptySpaceFiller";
 import { timeSince } from "@formbricks/lib/time";
-import toast from "react-hot-toast";
 import { TGoogleSheetIntegration, TGoogleSheetsConfigData } from "@formbricks/types/v1/integrations";
+import { Button } from "@formbricks/ui";
+import { useState } from "react";
+import toast from "react-hot-toast";
 
 interface HomeProps {
-    environmentId: string;
-    googleSheetIntegration: TGoogleSheetIntegration;
-    setOpenAddIntegrationModal: (v: boolean) => void;
-    setIsConnected: (v: boolean) => void;
-    setSelectedIntegration: (v: TGoogleSheetsConfigData & { index: number } | null) => void;
+  environmentId: string;
+  googleSheetIntegration: TGoogleSheetIntegration;
+  setOpenAddIntegrationModal: (v: boolean) => void;
+  setIsConnected: (v: boolean) => void;
+  setSelectedIntegration: (v: (TGoogleSheetsConfigData & { index: number }) | null) => void;
 }
 
-export default function Home({ environmentId, googleSheetIntegration, setOpenAddIntegrationModal, setIsConnected, setSelectedIntegration }:HomeProps) {
+export default function Home({
+  environmentId,
+  googleSheetIntegration,
+  setOpenAddIntegrationModal,
+  setIsConnected,
+  setSelectedIntegration,
+}: HomeProps) {
+  const [isDeleteIntegrationModalOpen, setIsDeleteIntegrationModalOpen] = useState(false);
+  const integrationArray = googleSheetIntegration ? googleSheetIntegration.config.data : [];
+  const [isDeleting, setisDeleting] = useState(false);
 
-    const [isDeleteIntegrationModalOpen, setIsDeleteIntegrationModalOpen] = useState(false)
-    const integrationArray = googleSheetIntegration ? googleSheetIntegration.config.data : []
-    const [isDeleting, setisDeleting] = useState(false)
-
-    const handleDeleteIntegration = async () => {
-        try {
-            setisDeleting(true)
-            await deleteIntegrationAction(googleSheetIntegration.id)
-            setIsConnected(false)
-            toast.success("Integration removed successfully")
-        } catch (error) {
-            toast.error(error.message)
-        }
-        finally {
-            setisDeleting(false)
-            setIsDeleteIntegrationModalOpen(false)
-        }
+  const handleDeleteIntegration = async () => {
+    try {
+      setisDeleting(true);
+      await deleteIntegrationAction(googleSheetIntegration.id);
+      setIsConnected(false);
+      toast.success("Integration removed successfully");
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setisDeleting(false);
+      setIsDeleteIntegrationModalOpen(false);
     }
+  };
 
-    const editIntegration = (index: number) => {
-        setSelectedIntegration({
-            ...googleSheetIntegration.config.data[index],
-            index: index
-        });
-        setOpenAddIntegrationModal(true)
-    }
+  const editIntegration = (index: number) => {
+    setSelectedIntegration({
+      ...googleSheetIntegration.config.data[index],
+      index: index,
+    });
+    setOpenAddIntegrationModal(true);
+  };
 
-    return (
-        <div className="flex flex-col justify-center items-center p-6 mt-6 w-full">
-            <div className="flex justify-end w-full">
-                <div className="mr-6 flex items-center">
-                    <span className="w-4 h-4 rounded-full bg-green-600 mr-4"></span>
-                    <span className="text-slate-500 cursor-pointer" onClick={() => { setIsDeleteIntegrationModalOpen(true) }}>Connected with {googleSheetIntegration.config.email}</span>
-                </div>
-                <Button variant="darkCTA" onClick={() => {
-                    setSelectedIntegration(null)
-                    setOpenAddIntegrationModal(true)
-                }}>Link new Sheet</Button>
-            </div>
-            {!integrationArray || integrationArray.length === 0 ? <div className="w-full mt-4">
-                <EmptySpaceFiller
-                    type="table"
-                    environmentId={environmentId}
-                    noWidgetRequired={true}
-                    emptyMessage="Your google sheet integrations will appear here as soon as you add them. ⏲️"
-                />
-            </div> :
-                <div className="flex flex-col items-center justify-center w-full mt-4">
-                    <div className="rounded-lg border border-slate-200 w-full mt-6">
-                        <div className="grid h-12 grid-cols-8 content-center rounded-lg bg-slate-100 text-left text-sm font-semibold text-slate-900">
-                            <div className="col-span-2 pl-6 ">Survey</div>
-                            <div className="col-span-2 text-center hidden sm:block">Google Sheet Name</div>
-                            <div className="col-span-2 text-center hidden sm:block">Questions</div>
-                            <div className="col-span-2 text-center hidden sm:block">Updated At</div>
-                        </div>
-                        {integrationArray && integrationArray.map((data, index) => {
-                            return <div key={index} className="m-2 grid h-16  grid-cols-8 content-center rounded-lg hover:bg-slate-100" onClick={() => { editIntegration(index) }}>
-                                <div className="col-span-2 text-center">{data.surveyName}</div>
-                                <div className="col-span-2 text-center">{data.spreadsheetName}</div>
-                                <div className="col-span-2 text-center">
-                                    {data.questions}
-                                </div>
-                                <div className="col-span-2 text-center">
-                                    {timeSince(data.createdAt.toString())}
-                                </div>
-                            </div>
-                        })}
-                    </div>
-                </div>}
-
-
-            <DeleteDialog
-                open={isDeleteIntegrationModalOpen}
-                setOpen={setIsDeleteIntegrationModalOpen}
-                deleteWhat="Google Sheet Integration"
-                onDelete={handleDeleteIntegration}
-                isDeleting={isDeleting}
-            />
+  return (
+    <div className="mt-6 flex w-full flex-col items-center justify-center p-6">
+      <div className="flex w-full justify-end">
+        <div className="mr-6 flex items-center">
+          <span className="mr-4 h-4 w-4 rounded-full bg-green-600"></span>
+          <span
+            className="cursor-pointer text-slate-500"
+            onClick={() => {
+              setIsDeleteIntegrationModalOpen(true);
+            }}>
+            Connected with {googleSheetIntegration.config.email}
+          </span>
         </div>
-    )
+        <Button
+          variant="darkCTA"
+          onClick={() => {
+            setSelectedIntegration(null);
+            setOpenAddIntegrationModal(true);
+          }}>
+          Link new Sheet
+        </Button>
+      </div>
+      {!integrationArray || integrationArray.length === 0 ? (
+        <div className="mt-4 w-full">
+          <EmptySpaceFiller
+            type="table"
+            environmentId={environmentId}
+            noWidgetRequired={true}
+            emptyMessage="Your google sheet integrations will appear here as soon as you add them. ⏲️"
+          />
+        </div>
+      ) : (
+        <div className="mt-4 flex w-full flex-col items-center justify-center">
+          <div className="mt-6 w-full rounded-lg border border-slate-200">
+            <div className="grid h-12 grid-cols-8 content-center rounded-lg bg-slate-100 text-left text-sm font-semibold text-slate-900">
+              <div className="col-span-2 pl-6 ">Survey</div>
+              <div className="col-span-2 hidden text-center sm:block">Google Sheet Name</div>
+              <div className="col-span-2 hidden text-center sm:block">Questions</div>
+              <div className="col-span-2 hidden text-center sm:block">Updated At</div>
+            </div>
+            {integrationArray &&
+              integrationArray.map((data, index) => {
+                return (
+                  <div
+                    key={index}
+                    className="m-2 grid h-16  grid-cols-8 content-center rounded-lg hover:bg-slate-100"
+                    onClick={() => {
+                      editIntegration(index);
+                    }}>
+                    <div className="col-span-2 text-center">{data.surveyName}</div>
+                    <div className="col-span-2 text-center">{data.spreadsheetName}</div>
+                    <div className="col-span-2 text-center">{data.questions}</div>
+                    <div className="col-span-2 text-center">{timeSince(data.createdAt.toString())}</div>
+                  </div>
+                );
+              })}
+          </div>
+        </div>
+      )}
+
+      <DeleteDialog
+        open={isDeleteIntegrationModalOpen}
+        setOpen={setIsDeleteIntegrationModalOpen}
+        deleteWhat="Google Connection"
+        onDelete={handleDeleteIntegration}
+        text="Are you sure? Your integrations will break."
+        isDeleting={isDeleting}
+      />
+    </div>
+  );
 }
