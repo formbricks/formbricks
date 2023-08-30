b7d4885e5171f8f87e88b011ba08ecdd713eb0f0
ShubhamPalriwala <spalriwalau@gmail.com>
2023-08-30 13:11:24 +0530

    fix: long single word was overflowing the thank you card

--- apps/web/components/preview/Subheader.tsx
+++ apps/web/components/preview/Subheader.tsx
@@ -1,6 +1,6 @@
 export default function Subheader({ subheader, questionId }: { subheader?: string; questionId: string }) {
   return (
-    <label htmlFor={questionId} className="block text-sm font-normal leading-6 text-slate-500">
+    <label htmlFor={questionId} className="block break-words text-sm font-normal leading-6 text-slate-500">
       {subheader}
     </label>
   );
