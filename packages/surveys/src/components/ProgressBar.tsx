b3ce061107778be2101f0cfb818a6916399906ac
Matthias Nannt <mail@matthiasnannt.com>
2023-09-01 16:30:37 +0900

    change logic for progress bar

--- packages/surveys/src/components/ProgressBar.tsx
+++ packages/surveys/src/components/ProgressBar.tsx
@@ -1,6 +1,6 @@
-import { useCallback, useEffect, useState } from "preact/hooks";
-import Progress from "./Progress";
 import { TSurvey } from "@formbricks/types/v1/surveys";
+import { useEffect, useState } from "preact/hooks";
+import Progress from "./Progress";
 
 interface ProgressBarProps {
   survey: TSurvey;
@@ -12,21 +12,18 @@ const PROGRESS_INCREMENT = 0.1;
 
 export default function ProgressBar({ survey, questionId, brandColor }: ProgressBarProps) {
   const [progress, setProgress] = useState(0); // [0, 1]
+  const [prevQuestionIdx, setPrevQuestionIdx] = useState(0); // [0, survey.questions.length
 
   useEffect(() => {
     // calculate progress
     setProgress(calculateProgress(questionId, survey, progress));
-  }, [questionId, survey]);
 
-  let questionIdxTemp: number;
-
-  const calculateProgress = useCallback(
-    (currentQuestionId: string, survey: TSurvey, currentProgress: number) => {
+    function calculateProgress(currentQuestionId: string, survey: TSurvey, currentProgress: number) {
       if (currentQuestionId === "end") return 1;
 
       const progressArray: number[] = new Array(survey.questions.length).fill(undefined);
-      const currentQuestionIdx = survey.questions.findIndex((e) => e.id === currentQuestionId);
-      const currentQuestion = survey.questions[currentQuestionIdx];
+      const currentprevQuestionIdx = survey.questions.findIndex((e) => e.id === currentQuestionId);
+      const currentQuestion = survey.questions[currentprevQuestionIdx];
       const surveyLength = survey.questions.length;
       const middleIdx = Math.floor(surveyLength / 2);
       const possibleNextQuestions = currentQuestion.logic?.map((l) => l.destination) || [];
@@ -39,37 +36,36 @@ export default function ProgressBar({ survey, questionId, brandColor }: Progress
         return survey.questions.findIndex((e) => e.id === lastQuestion?.id);
       };
 
-      let elementIdx = currentQuestionIdx || 0.5;
-      const lastQuestionIdx = getLastQuestionIndex();
+      let elementIdx = currentprevQuestionIdx || 0.5;
+      const lastprevQuestionIdx = getLastQuestionIndex();
 
-      if (lastQuestionIdx > 0) elementIdx = Math.min(middleIdx, lastQuestionIdx - 1);
+      if (lastprevQuestionIdx > 0) elementIdx = Math.min(middleIdx, lastprevQuestionIdx - 1);
       if (possibleNextQuestions.includes("end")) elementIdx = middleIdx;
 
       const newProgress = elementIdx / survey.questions.length;
 
       // Determine if user went backwards in the survey
-      const didUserGoBackwards = currentQuestionIdx < questionIdxTemp;
+      const didUserGoBackwards = currentprevQuestionIdx < prevQuestionIdx;
 
       // Update the progress array based on user's navigation
       let updatedProgress = currentProgress;
       if (didUserGoBackwards) {
-        if (!progressArray[currentQuestionIdx]) {
-          progressArray[currentQuestionIdx] = currentProgress - PROGRESS_INCREMENT;
+        if (!progressArray[currentprevQuestionIdx]) {
+          progressArray[currentprevQuestionIdx] = currentProgress - PROGRESS_INCREMENT;
         }
-        updatedProgress = progressArray[currentQuestionIdx];
+        updatedProgress = progressArray[currentprevQuestionIdx];
       } else if (newProgress > currentProgress) {
-        progressArray[currentQuestionIdx] = newProgress;
+        progressArray[currentprevQuestionIdx] = newProgress;
         updatedProgress = newProgress;
       } else if (newProgress <= currentProgress && currentProgress + PROGRESS_INCREMENT <= 1) {
-        progressArray[currentQuestionIdx] = currentProgress + PROGRESS_INCREMENT;
+        progressArray[currentprevQuestionIdx] = currentProgress + PROGRESS_INCREMENT;
         updatedProgress = currentProgress + PROGRESS_INCREMENT;
       }
 
-      questionIdxTemp = currentQuestionIdx;
+      setPrevQuestionIdx(currentprevQuestionIdx);
       return updatedProgress;
-    },
-    []
-  );
+    }
+  }, [questionId, survey, setPrevQuestionIdx]);
 
   return <Progress progress={progress} brandColor={brandColor} />;
 }
