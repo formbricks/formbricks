58def398eb316547d99e690e9206d5fa47293799
Matthias Nannt <mail@matthiasnannt.com>
2023-09-02 14:00:21 +0900

    remove console.log

--- apps/web/app/(app)/environments/[environmentId]/surveys/[surveyId]/edit/ResponseOptionsCard.tsx
+++ apps/web/app/(app)/environments/[environmentId]/surveys/[surveyId]/edit/ResponseOptionsCard.tsx
@@ -333,7 +333,6 @@ export default function ResponseOptionsCard({ localSurvey, setLocalSurvey }: Res
                   </div>
                 </div>
               </AdvancedOptionToggle>
-              {console.log(localSurvey)}
             </>
           )}
         </div>
