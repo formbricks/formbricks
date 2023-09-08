bf6ed3576c7cbfe2d01e6522804e2b98e892e3ed
Matti Nannt <mail@matthiasnannt.com>
2023-09-08 14:01:56 +0900

    feat: Add option to customize link survey url using NEXT_PUBLIC_SURVEY_BASE_URL (#787)

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -4,7 +4,10 @@ import { createId } from "@paralleldrive/cuid2";
 
 /** @type {import('next').NextConfig} */
 
+const isCloud = process.env.NEXT_PUBLIC_IS_FORMBRICKS_CLOUD === "1";
+
 const nextConfig = {
+  assetPrefix: isCloud ? process.env.NEXT_PUBLIC_WEBAPP_URL : undefined,
   output: "standalone",
   experimental: {
     serverActions: true,
