f1688502a2db6d1572bfa9f4e916dda6c901927b
Matti Nannt <mail@matthiasnannt.com>
2023-09-11 15:05:55 +0900

    chore: Update npm package dependencies (#794)
    * upgrade packages
    
    * upgrade packages in demo & formbricks-com
    
    * update web packages

--- packages/ee/billing/api/create-customer-portal-session.ts
+++ packages/ee/billing/api/create-customer-portal-session.ts
@@ -4,7 +4,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   // https://github.com/stripe/stripe-node#configuration
-  apiVersion: "2022-11-15",
+  apiVersion: "2023-08-16",
 });
 
 const webhookHandler = async (req: NextApiRequest, res: NextApiResponse) => {
