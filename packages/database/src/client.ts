18ee24f26affa1c95bbe655c5ca4e878ad62efb5
Matthias Nannt <mail@matthiasnannt.com>
2023-09-14 03:50:46 +0900

    add prisma accelerate support

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -1,10 +1,12 @@
 import { PrismaClient } from "@prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
 declare global {
-  var prisma: PrismaClient | undefined;
+  var prisma: any | undefined; // use any type for now. TODO: add support for Accelerate
 }
 
 export const prisma =
-  global.prisma || new PrismaClient({ datasources: { db: { url: process.env.DATABASE_URL } } });
+  global.prisma ||
+  new PrismaClient({ datasources: { db: { url: process.env.DATABASE_URL } } }).$extends(withAccelerate());
 
 if (process.env.NODE_ENV !== "production") global.prisma = prisma;
