f7f523c6507fd22a8f63a843e75ada05a31828e7
Matthias Nannt <mail@matthiasnannt.com>
2023-09-14 12:03:51 +0900

    update prisma runtime

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client/edge";
+import { PrismaClient } from "@prisma/client";
 import { withAccelerate } from "@prisma/extension-accelerate";
 
 function makePrisma() {
