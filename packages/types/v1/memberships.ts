ac938fcf9678186dd275478069035e055867601a
Matthias Nannt <mail@matthiasnannt.com>
2023-09-15 18:23:57 +0900

    remove duplicate membership types

--- packages/types/v1/memberships.ts
+++ packages/types/v1/memberships.ts
@@ -2,6 +2,8 @@ import z from "zod";
 
 export const ZMembershipRole = z.enum(["owner", "admin", "editor", "developer", "viewer"]);
 
+export type TMembershipRole = z.infer<typeof ZMembershipRole>;
+
 export const ZMembership = z.object({
   teamId: z.string(),
   userId: z.string(),
@@ -9,14 +11,4 @@ export const ZMembership = z.object({
   role: ZMembershipRole,
 });
 
-export const ZMember = z.object({
-  name: z.string().nullable(),
-  email: z.string(),
-  userId: z.string(),
-  accepted: z.boolean(),
-  role: ZMembershipRole,
-});
-
 export type TMembership = z.infer<typeof ZMembership>;
-export type TMember = z.infer<typeof ZMember>;
-export type TMembershipRole = z.infer<typeof ZMembershipRole>;
