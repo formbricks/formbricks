f313275a14a8e9dc04473d5f96d832cbdb09dcc8
Matti Nannt <mail@matthiasnannt.com>
2023-09-17 00:31:38 +0900

    fix: sync call gets called by other functions (#823)
    * fix: sync call gets called by other functions
    
    * fix: sync call gets called by other functions
    
    * remove unused imports
    
    * remove unused import
    
    * fix userId update

---
+++ apps/web/app/api/v1/js/sync/lib/sync.ts
@@ -0,0 +1,99 @@
+import { getSurveysCached } from "@/app/api/v1/js/surveys";
+import { getActionClassesCached } from "@formbricks/lib/services/actionClass";
+import { getEnvironmentCached } from "@formbricks/lib/services/environment";
+import { createPerson, getPersonCached } from "@formbricks/lib/services/person";
+import { getProductByEnvironmentIdCached } from "@formbricks/lib/services/product";
+import { createSession, extendSession, getSessionCached } from "@formbricks/lib/services/session";
+import { captureTelemetry } from "@formbricks/lib/telemetry";
+import { TEnvironment } from "@formbricks/types/v1/environment";
+import { TJsState } from "@formbricks/types/v1/js";
+import { TPerson } from "@formbricks/types/v1/people";
+import { TSession } from "@formbricks/types/v1/sessions";
+
+const captureNewSessionTelemetry = async (jsVersion?: string): Promise<void> => {
+  await captureTelemetry("session created", { jsVersion: jsVersion ?? "unknown" });
+};
+
+export const getUpdatedState = async (
+  environmentId: string,
+  personId?: string,
+  sessionId?: string,
+  jsVersion?: string
+): Promise<TJsState> => {
+  let environment: TEnvironment | null;
+  let person: TPerson;
+  let session: TSession | null;
+
+  // check if environment exists
+  environment = await getEnvironmentCached(environmentId);
+
+  if (!environment) {
+    throw new Error("Environment does not exist");
+  }
+
+  if (!personId) {
+    // create a new person
+    person = await createPerson(environmentId);
+    // create a new session
+    session = await createSession(person.id);
+  } else {
+    // check if person exists
+    const existingPerson = await getPersonCached(personId);
+    if (!existingPerson) {
+      // create a new person
+      person = await createPerson(environmentId);
+    } else {
+      person = existingPerson;
+    }
+  }
+  if (!sessionId) {
+    // create a new session
+    session = await createSession(person.id);
+  } else {
+    // check validity of person & session
+    session = await getSessionCached(sessionId);
+    if (!session) {
+      // create a new session
+      session = await createSession(person.id);
+      captureNewSessionTelemetry(jsVersion);
+    } else {
+      // check if session is expired
+      if (session.expiresAt < new Date()) {
+        // create a new session
+        session = await createSession(person.id);
+        captureNewSessionTelemetry(jsVersion);
+      } else {
+        // extend session (if about to expire)
+        const isSessionAboutToExpire =
+          new Date(session.expiresAt).getTime() - new Date().getTime() < 1000 * 60 * 10;
+
+        if (isSessionAboutToExpire) {
+          session = await extendSession(sessionId);
+        }
+      }
+    }
+  }
+  // we now have a valid person & session
+
+  // get/create rest of the state
+  const [surveys, noCodeActionClasses, product] = await Promise.all([
+    getSurveysCached(environmentId, person),
+    getActionClassesCached(environmentId),
+    getProductByEnvironmentIdCached(environmentId),
+  ]);
+
+  if (!product) {
+    throw new Error("Product not found");
+  }
+
+  // return state
+  const state: TJsState = {
+    person: person!,
+    session,
+    surveys,
+    noCodeActionClasses: noCodeActionClasses.filter((actionClass) => actionClass.type === "noCode"),
+    product,
+  };
+
+  return state;
+};
