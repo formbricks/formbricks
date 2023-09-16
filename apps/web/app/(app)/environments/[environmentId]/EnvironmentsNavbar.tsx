6dda347ebe4351c589466b4dfb256c1339b0a58b
ShubhamPalriwala <spalriwalau@gmail.com>
2023-09-16 15:03:03 +0530

    redo: isFormbricksCloud to navbar serverside page

--- apps/web/app/(app)/environments/[environmentId]/EnvironmentsNavbar.tsx
+++ apps/web/app/(app)/environments/[environmentId]/EnvironmentsNavbar.tsx
@@ -1,7 +1,7 @@
 export const revalidate = REVALIDATION_INTERVAL;
 
 import Navigation from "@/app/(app)/environments/[environmentId]/Navigation";
-import { REVALIDATION_INTERVAL } from "@formbricks/lib/constants";
+import { IS_FORMBRICKS_CLOUD, REVALIDATION_INTERVAL } from "@formbricks/lib/constants";
 import { getEnvironment, getEnvironments } from "@formbricks/lib/services/environment";
 import { getProducts } from "@formbricks/lib/services/product";
 import { getTeamByEnvironmentId, getTeamsByUserId } from "@formbricks/lib/services/team";
@@ -42,6 +42,7 @@ export default async function EnvironmentsNavbar({ environmentId, session }: Env
       products={products}
       environments={environments}
       session={session}
+      isFormbricksCloud={IS_FORMBRICKS_CLOUD}
     />
   );
 }
