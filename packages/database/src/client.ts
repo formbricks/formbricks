2356b7379419e4b3314139b2f56d25bef79d6811
Matthias Nannt <mail@matthiasnannt.com>
2023-09-16 20:52:27 +0900

    comment out prisma client logging

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -4,7 +4,7 @@ import { withAccelerate } from "@prisma/extension-accelerate";
 const prismaClientSingleton = () => {
   return new PrismaClient({
     datasources: { db: { url: process.env.DATABASE_URL } },
-    log: ["query", "info"],
+    /* log: ["query", "info"], */
   }).$extends(withAccelerate());
 };
 
