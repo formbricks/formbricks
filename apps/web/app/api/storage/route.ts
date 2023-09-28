2a1855fc47e429926b11adb46e86025dc0973057
pandeymangg <anshuman.pandey9999@gmail.com>
2023-09-28 13:23:45 +0530

    feat: upload and fetch files from local storage

--- apps/web/app/api/storage/route.ts
+++ apps/web/app/api/storage/route.ts
@@ -2,7 +2,15 @@ import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
 import { hasUserEnvironmentAccess } from "@/lib/api/apiHelper";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
-import { getFileFromS3, getSignedUrlForUpload } from "@formbricks/lib/services/storage";
+import {
+  getFileFromLocalStorage,
+  getFileFromS3,
+  putFileToLocalStorage,
+  putFileToS3,
+} from "@formbricks/lib/services/storage";
+import path from "path";
+
+const UPLOADS_DIR = path.resolve("./uploads");
 
 export async function GET(req: NextRequest) {
   const searchParams = req.nextUrl.searchParams;
@@ -25,6 +33,7 @@ export async function GET(req: NextRequest) {
   const fileNameParts = fileName.split("/");
   const environmentId = fileNameParts[0];
   const accessType = fileNameParts[1];
+  const baseFileName = fileNameParts[2];
 
   if (!environmentId || !accessType) {
     return NextResponse.json(
@@ -38,7 +47,37 @@ export async function GET(req: NextRequest) {
     );
   }
 
-  if (accessType === "public") {
+  const getFile = async () => {
+    if (
+      !process.env.AWS_ACCESS_KEY ||
+      !process.env.AWS_SECRET_KEY ||
+      !process.env.S3_REGION ||
+      !process.env.S3_BUCKET_NAME
+    ) {
+      try {
+        const { fileBuffer, metaData } = await getFileFromLocalStorage(
+          path.join(UPLOADS_DIR, environmentId, accessType, baseFileName)
+        );
+
+        return new Response(fileBuffer, {
+          headers: {
+            "Content-Type": metaData.contentType,
+            "Content-Disposition": "inline",
+          },
+        });
+      } catch (err) {
+        return NextResponse.json(
+          {
+            success: false,
+            message: "File not found",
+          },
+          {
+            status: 404,
+          }
+        );
+      }
+    }
+
     try {
       const { fileBuffer, metaData } = await getFileFromS3(fileName);
 
@@ -71,6 +110,10 @@ export async function GET(req: NextRequest) {
         );
       }
     }
+  };
+
+  if (accessType === "public") {
+    return await getFile();
   }
 
   if (accessType !== "private") {
@@ -115,42 +158,11 @@ export async function GET(req: NextRequest) {
     );
   }
 
-  try {
-    const { fileBuffer, metaData } = await getFileFromS3(fileName);
-
-    return new Response(fileBuffer, {
-      headers: {
-        "Content-Type": metaData.contentType,
-        "Content-Disposition": "inline",
-      },
-    });
-  } catch (err) {
-    if (err.name === "NoSuchKey") {
-      return NextResponse.json(
-        {
-          success: false,
-          message: "File not found",
-        },
-        {
-          status: 404,
-        }
-      );
-    } else {
-      return NextResponse.json(
-        {
-          success: false,
-          message: "Internal server error",
-        },
-        {
-          status: 500,
-        }
-      );
-    }
-  }
+  return await getFile();
 }
 
 export async function POST(req: NextRequest) {
-  const { fileName, fileType, accessType = "private", environmentId } = await req.json();
+  const { fileName, fileType, accessType = "private", environmentId, fileBuffer } = await req.json();
 
   if (!fileName || !fileType || !environmentId) {
     return NextResponse.json(
@@ -164,20 +176,94 @@ export async function POST(req: NextRequest) {
     );
   }
 
+  // check file size and if it is greater than 10MB, return error
+
+  const uploadFile = async () => {
+    // if s3 is not configured, we'll upload to a local folder named uploads
+
+    if (
+      !process.env.AWS_ACCESS_KEY ||
+      !process.env.AWS_SECRET_KEY ||
+      !process.env.S3_REGION ||
+      !process.env.S3_BUCKET_NAME
+    ) {
+      try {
+        await putFileToLocalStorage(fileName, fileBuffer, accessType, environmentId, UPLOADS_DIR);
+
+        return NextResponse.json(
+          {
+            success: true,
+            message: "File uploaded successfully",
+          },
+          {
+            status: 201,
+          }
+        );
+      } catch (err) {
+        if (err.name === "FileTooLargeError") {
+          return NextResponse.json(
+            {
+              success: false,
+              message: err.message,
+            },
+            {
+              status: 400,
+            }
+          );
+        }
+
+        return NextResponse.json(
+          {
+            success: false,
+            message: "Internal server error",
+          },
+          {
+            status: 500,
+          }
+        );
+      }
+    }
+
+    try {
+      await putFileToS3(fileName, fileType, fileBuffer, accessType, environmentId);
+
+      return NextResponse.json(
+        {
+          success: true,
+          message: "File uploaded successfully",
+        },
+        {
+          status: 201,
+        }
+      );
+    } catch (err) {
+      if (err.name === "FileTooLargeError") {
+        return NextResponse.json(
+          {
+            success: false,
+            message: err.message,
+          },
+          {
+            status: 400,
+          }
+        );
+      }
+
+      return NextResponse.json(
+        {
+          success: false,
+          message: "Internal server error",
+        },
+        {
+          status: 500,
+        }
+      );
+    }
+  };
+
   if (accessType === "public") {
     // dont auth and upload public file
-
-    const signedUrl = await getSignedUrlForUpload(`${environmentId}/${accessType}/${fileName}`, fileType);
-
-    return NextResponse.json(
-      {
-        success: true,
-        signedUrl,
-      },
-      {
-        status: 200,
-      }
-    );
+    return await uploadFile();
   }
 
   if (accessType !== "private") {
@@ -222,15 +308,5 @@ export async function POST(req: NextRequest) {
     );
   }
 
-  const signedUrl = await getSignedUrlForUpload(`${environmentId}/${accessType}/${fileName}`, fileType);
-
-  return NextResponse.json(
-    {
-      success: true,
-      signedUrl,
-    },
-    {
-      status: 200,
-    }
-  );
+  return await uploadFile();
 }
