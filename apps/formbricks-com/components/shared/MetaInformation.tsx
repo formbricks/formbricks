04e566cdc7820f0c853fecacf0c4f77832602d8f
Matthias Nannt <mail@matthiasnannt.com>
2023-09-28 18:13:29 +0200

    remove comments from .env.example, use constants in MetaInformation

--- apps/formbricks-com/components/shared/MetaInformation.tsx
+++ apps/formbricks-com/components/shared/MetaInformation.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { WEBAPP_URL } from "@formbricks/lib/constants";
 
 interface Props {
   title: string;
@@ -26,7 +27,7 @@ export default function MetaInformation({
       <meta name="description" content={description} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      <meta property="og:image" content={`https://${process.env.WEBAPP_URL}/social-image.png`} />
+      <meta property="og:image" content={`https://${WEBAPP_URL}/social-image.png`} />
       <meta property="og:image:alt" content="Open Source Experience Management, Privacy-first" />
       <meta property="og:image:type" content="image/png" />
       <meta property="og:image:width" content="1200" />
