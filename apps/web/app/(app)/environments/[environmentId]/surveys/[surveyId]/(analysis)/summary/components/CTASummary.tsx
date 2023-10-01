5fad91dcb7f5093a1510c23f5cb281263b99c56a
Piyush Gupta <piyushguptaa2z123@gmail.com>
2023-10-01 19:18:04 +0530

    feat:mandatory question indicator added

--- apps/web/app/(app)/environments/[environmentId]/surveys/[surveyId]/(analysis)/summary/components/CTASummary.tsx
+++ apps/web/app/(app)/environments/[environmentId]/surveys/[surveyId]/(analysis)/summary/components/CTASummary.tsx
@@ -1,3 +1,4 @@
+import Headline from "@/app/(app)/environments/[environmentId]/surveys/[surveyId]/(analysis)/summary/components/Headline";
 import type { QuestionSummary } from "@formbricks/types/responses";
 import { TSurveyCTAQuestion } from "@formbricks/types/v1/surveys";
 import { ProgressBar } from "@formbricks/ui";
@@ -27,11 +28,8 @@ export default function CTASummary({ questionSummary }: CTASummaryProps) {
   return (
     <div className=" rounded-lg border border-slate-200 bg-slate-50 shadow-sm">
       <div className="space-y-2 px-4 pb-5 pt-6 md:px-6">
-        <div>
-          <h3 className="pb-1 text-lg font-semibold text-slate-900 md:text-xl">
-            {questionSummary.question.headline}
-          </h3>
-        </div>
+        <Headline headline={questionSummary.question.headline} required={questionSummary.question.required} />
+
         <div className="flex space-x-2 text-xs font-semibold text-slate-600 md:text-sm">
           <div className="rounded-lg bg-slate-100 p-2 ">Call-to-Action</div>
           <div className=" flex items-center rounded-lg bg-slate-100 p-2">
--- packages/surveys/src/components/Headline.tsx
+++ packages/surveys/src/components/Headline.tsx
@@ -1,16 +1,31 @@
 interface HeadlineProps {
   headline?: string;
   questionId: string;
+  labelStyle?: any;
   style?: any;
+  required?: boolean;
 }
 
-export default function Headline({ headline, questionId, style }: HeadlineProps) {
+export default function Headline({
+  headline,
+  questionId,
+  labelStyle,
+  style,
+  required = true,
+}: HeadlineProps) {
   return (
     <label
       htmlFor={questionId}
-      className="mb-1.5 mr-8 block text-base font-semibold leading-6 text-slate-900"
-      style={style}>
-      {headline}
+      className="mb-1.5 block text-base font-semibold leading-6 text-slate-900"
+      style={labelStyle}>
+      <div className={"flex justify-between gap-4"} style={style}>
+        {headline}
+        {!required && (
+          <span className="self-start font-light text-gray-500" tabIndex={-1}>
+            Optional
+          </span>
+        )}
+      </div>
     </label>
   );
 }
