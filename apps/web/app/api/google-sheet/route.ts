c240687fb6aaddf452ed82e56af8435887ea192b
ShubhamPalriwala <spalriwalau@gmail.com>
2023-10-02 13:19:52 +0530

    fix: club caching methods and use authzn errors

--- apps/web/app/api/google-sheet/route.ts
+++ apps/web/app/api/google-sheet/route.ts
@@ -1,4 +1,4 @@
-import { hasUserEnvironmentAccessCached } from "@formbricks/lib/environment/auth";
+import { hasUserEnvironmentAccess } from "@formbricks/lib/environment/auth";
 import {
   GOOGLE_SHEETS_CLIENT_ID,
   GOOGLE_SHEETS_CLIENT_SECRET,
@@ -27,7 +27,7 @@ export async function GET(req: NextRequest) {
     return NextResponse.json({ Error: "Invalid session" }, { status: 400 });
   }
 
-  const canUserAccessEnvironment = await hasUserEnvironmentAccessCached(session?.user.id, environmentId);
+  const canUserAccessEnvironment = await hasUserEnvironmentAccess(session?.user.id, environmentId);
   if (!canUserAccessEnvironment) {
     return NextResponse.json({ Error: "You dont have access to environment" }, { status: 401 });
   }
--- packages/lib/apiKey/auth.ts
+++ packages/lib/apiKey/auth.ts
@@ -1,4 +1,4 @@
-import { hasUserEnvironmentAccessCached } from "../environment/auth";
+import { hasUserEnvironmentAccess } from "../environment/auth";
 import { getApiKey } from "./service";
 
 export const canUserAccessApiKey = async (userId: string, apiKeyId: string): Promise<boolean> => {
@@ -7,7 +7,7 @@ export const canUserAccessApiKey = async (userId: string, apiKeyId: string): Pro
   const apiKeyFromServer = await getApiKey(apiKeyId);
   if (!apiKeyFromServer) return false;
 
-  const hasAccessToEnvironment = await hasUserEnvironmentAccessCached(userId, apiKeyFromServer.environmentId);
+  const hasAccessToEnvironment = await hasUserEnvironmentAccess(userId, apiKeyFromServer.environmentId);
   if (!hasAccessToEnvironment) return false;
 
   return true;
