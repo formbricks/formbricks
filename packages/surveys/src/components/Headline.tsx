55c29c6a73c4604bc86c4e3341fd4108f415544e
Piyush Gupta <piyushguptaa2z123@gmail.com>
2023-10-02 10:06:11 +0530

    resolved review changes

--- packages/surveys/src/components/Headline.tsx
+++ packages/surveys/src/components/Headline.tsx
@@ -1,23 +1,13 @@
 interface HeadlineProps {
   headline?: string;
   questionId: string;
-  labelStyle?: any;
   style?: any;
   required?: boolean;
 }
 
-export default function Headline({
-  headline,
-  questionId,
-  labelStyle,
-  style,
-  required = true,
-}: HeadlineProps) {
+export default function Headline({ headline, questionId, style, required = true }: HeadlineProps) {
   return (
-    <label
-      htmlFor={questionId}
-      className="mb-1.5 block text-base font-semibold leading-6 text-slate-900"
-      style={labelStyle}>
+    <label htmlFor={questionId} className="mb-1.5 block text-base font-semibold leading-6 text-slate-900">
       <div className={"flex justify-between gap-4"} style={style}>
         {headline}
         {!required && (
