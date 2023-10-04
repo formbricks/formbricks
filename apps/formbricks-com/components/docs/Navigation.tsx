cce3a98f20838805020e847d1cefc18cfaf3108a
Shubham Palriwala <spalriwalau@gmail.com>
2023-10-04 14:33:13 +0530

    feat: add docs for management API (#930)
    * feat: docs for management API
    
    * fix: split api into client and management
    
    * update navigation and pages
    
    ---------
    
    Co-authored-by: Matthias Nannt <mail@matthiasnannt.com>

--- apps/formbricks-com/components/docs/Navigation.tsx
+++ apps/formbricks-com/components/docs/Navigation.tsx
@@ -262,15 +262,23 @@ export const navigation: Array<NavGroup> = [
     ],
   },
   {
-    title: "API",
+    title: "Client API",
     links: [
-      { title: "Overview", href: "/docs/api/overview" },
-      { title: "API Key Setup", href: "/docs/api/api-key-setup" },
-      { title: "Displays", href: "/docs/api/display" },
-      { title: "People", href: "/docs/api/people" },
-      { title: "Responses", href: "/docs/api/responses" },
-      { title: "Surveys", href: "/docs/api/surveys" },
-      { title: "Webhook", href: "/docs/api/webhooks" },
+      { title: "Overview", href: "/docs/api/client/overview" },
+      { title: "Displays", href: "/docs/api/client/displays" },
+      { title: "Responses", href: "/docs/api/client/responses" },
+    ],
+  },
+  {
+    title: "Management API",
+    links: [
+      { title: "API Key Setup", href: "/docs/api/management/api-key-setup" },
+      { title: "Action Classes", href: "/docs/api/management/action-classes" },
+      { title: "Attribute Classes", href: "/docs/api/management/attribute-classes" },
+      { title: "Me", href: "/docs/api/management/me" },
+      { title: "People", href: "/docs/api/management/people" },
+      { title: "Surveys", href: "/docs/api/management/surveys" },
+      { title: "Webhooks", href: "/docs/api/management/webhooks" },
     ],
   },
 ];
