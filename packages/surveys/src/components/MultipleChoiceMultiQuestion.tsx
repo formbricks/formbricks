8f64c5d806d54dab0368bfcb085945ea2217aa6a
joyal007 <joyalraphel00@gmail.com>
2023-10-07 00:55:08 +0530

    Enter event in option

--- packages/surveys/src/components/MultipleChoiceMultiQuestion.tsx
+++ packages/surveys/src/components/MultipleChoiceMultiQuestion.tsx
@@ -139,7 +139,6 @@ export default function MultipleChoiceSingleQuestion({
                 onKeyDown={(e) => {
                   if (e.key == "Enter") {
                     setOtherSelected(!otherSelected);
-                    onChange({ [question.id]: "" });
                   }
                 }}>
                 <span className="flex items-center text-sm">
@@ -178,6 +177,11 @@ export default function MultipleChoiceSingleQuestion({
                       setOtherValue(e.currentTarget.value);
                       removeItem(otherValue);
                       addItem(e.currentTarget.value);
+                    }}
+                    onKeyDown={(e) => {
+                      if (e.key == "Enter") {
+                        onSubmit({ [question.id]: value });
+                      }
                     }}
                     placeholder="Please specify"
                     className="mt-3 flex h-10 w-full rounded-md border border-slate-300 bg-transparent bg-white px-3 py-2 text-sm text-slate-800 placeholder:text-slate-400 focus:outline-none  focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-500 dark:text-slate-300"
