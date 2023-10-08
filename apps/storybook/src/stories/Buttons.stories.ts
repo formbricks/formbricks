6a73d65ac1b9148f3c6f10636e612601d6adc02f
Anjy Gupta <anjyofficial@gmail.com>
2023-10-08 11:11:28 +0000

    fix: added button story

--- apps/storybook/src/stories/Buttons.stories.ts
+++ apps/storybook/src/stories/Buttons.stories.ts
@@ -2,13 +2,11 @@ import type { Meta, StoryObj } from "@storybook/react";
 
 import { Button } from "@formbricks/ui";
 
-// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 const meta = {
-  title: "Example/Button",
+  title: "Button",
   component: Button,
   tags: ["autodocs"],
   argTypes: {
-    children: { control: "text", defaultValue: "Button" },
     variant: { control: "select" },
     onClick: { action: "clicked", type: "function" },
   },
@@ -17,10 +15,9 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Primary: Story = {
   args: {
-    variant: "default",
+    variant: "secondary",
     children: "Button",
   },
 };
