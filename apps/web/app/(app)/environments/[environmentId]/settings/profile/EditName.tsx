67ff99b19425aa176b557a8a8f895586c5d1ac5c
Aditya @ArchLinux <132184385+adityadeshlahre@users.noreply.github.com>
2023-10-08 18:53:40 +0530

    fix(bug): FullName value can be updated without any characters/value

--- apps/web/app/(app)/environments/[environmentId]/settings/profile/EditName.tsx
+++ apps/web/app/(app)/environments/[environmentId]/settings/profile/EditName.tsx
@@ -1,37 +1,45 @@
 "use client";
 
 import { Button, Input, Label } from "@formbricks/ui";
-import { useForm } from "react-hook-form";
+import { useForm, SubmitHandler } from "react-hook-form";
 import toast from "react-hot-toast";
 import { updateProfileAction } from "./actions";
 import { TProfile } from "@formbricks/types/v1/profile";
+
+type FormData = {
+  name: string;
+};
 
 export function EditName({ profile }: { profile: TProfile }) {
   const {
     register,
     handleSubmit,
-    formState: { isSubmitting },
-  } = useForm<{ name: string }>();
+    formState: { isSubmitting, errors },
+  } = useForm<FormData>();
+
+  const onSubmit: SubmitHandler<FormData> = async (data) => {
+    try {
+      if (data.name.trim() === "") {
+        throw new Error("Please enter at least one character.");
+      }
+      await updateProfileAction(data);
+      toast.success("Your name was updated successfully.");
+    } catch (error) {
+      toast.error(`Error: ${error.message}`);
+    }
+  };
 
   return (
     <>
-      <form
-        className="w-full max-w-sm items-center"
-        onSubmit={handleSubmit(async (data) => {
-          try {
-            await updateProfileAction(data);
-            toast.success("Your name was updated successfully.");
-          } catch (error) {
-            toast.error(`Error: ${error.message}`);
-          }
-        })}>
+      <form className="w-full max-w-sm items-center" onSubmit={handleSubmit(onSubmit)}>
         <Label htmlFor="fullname">Full Name</Label>
         <Input
           type="text"
           id="fullname"
-          defaultValue={profile.name ? profile.name : ""}
-          {...register("name")}
+          defaultValue={profile.name || ""}
+          {...register("name", { required: true })}
         />
+        {errors.name && <p className="text-red-500">Please enter at least one character.</p>}
 
         <div className="mt-4">
           <Label htmlFor="email">Email</Label>
