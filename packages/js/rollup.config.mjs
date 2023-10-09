b19ae2dfaf76283846fff29fd0d7bdac3fa1d31a
Matthias Nannt <mail@matthiasnannt.com>
2023-10-09 12:01:13 +0200

    remove building from formbricks-types

--- packages/js/rollup.config.mjs
+++ packages/js/rollup.config.mjs
@@ -6,21 +6,15 @@ import json from "@rollup/plugin-json";
 export default [
   // browser-friendly UMD build
   {
-    input: "src/index.ts",
+    input: 'src/index.ts',
     output: [
-      {
-        name: "howLongUntilLunch",
+      { 
         file: "dist/index.umd.js",
         format: "umd",
       },
       { file: "dist/index.js", format: "cjs" },
       { file: "dist/index.esm.js", format: "esm" },
     ],
-    plugins: [
-      typescript(),
-      json(),
-      resolve(), // so Rollup can find `ms`
-      commonjs(), // so Rollup can convert `ms` to an ES module
-    ],
+    plugins: [typescript(), resolve(), commonjs(), json()],
   },
 ];
