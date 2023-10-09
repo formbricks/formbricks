33946cd53e0645c6774c40a4a1c61cbd4883b624
ShubhamPalriwala <spalriwalau@gmail.com>
2023-10-09 10:31:11 +0530

    fix: build error due to survey status becoming optional

--- apps/web/app/api/v1/client/displays/route.ts
+++ apps/web/app/api/v1/client/displays/route.ts
@@ -25,6 +25,9 @@ export async function POST(request: Request): Promise<NextResponse> {
   }
 
   const displayInput = inputValidation.data;
+  if (!displayInput.surveyId) {
+    return responses.badRequestResponse("Missing surveyId");
+  }
 
   // find environmentId from surveyId
   let survey;
