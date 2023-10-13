491a582ab6ae8a549bf4787a2d845d697af6ff8b
Matthias Nannt <mail@matthiasnannt.com>
2023-10-13 15:14:05 +0200

    rename stories file

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -10,7 +10,7 @@ function getAbsolutePath(value: string): any {
   return dirname(require.resolve(join(value, "package.json")));
 }
 const config: StorybookConfig = {
-  stories: ["../../../packages/ui/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ["../../../packages/ui/**/stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     getAbsolutePath("@storybook/addon-links"),
     getAbsolutePath("@storybook/addon-essentials"),
