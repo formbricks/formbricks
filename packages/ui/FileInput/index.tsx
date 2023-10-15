9183fba4bb30b4e182de795cf575ccbd21f5cc39
Naitik <naitikjkapadia@gmail.com>
2023-10-15 20:42:14 +0530

    fix: Modify Checks for FileInput Drag && Uploading state

--- packages/ui/FileInput/index.tsx
+++ packages/ui/FileInput/index.tsx
@@ -1,7 +1,9 @@
 "use client";
+import { useState } from "react";
 import { uploadFile } from "./lib/fileUpload";
 import { ArrowUpTrayIcon } from "@heroicons/react/24/solid";
 import { FileIcon } from "lucide-react";
+import toast from "react-hot-toast";
 
 interface FileInputProps {
   allowedFileExtensions: string[];
@@ -16,6 +18,9 @@ const FileInput: React.FC<FileInputProps> = ({
   onFileUpload,
   fileUrl,
 }) => {
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [isUploaded, setIsUploaded] = useState<boolean>(!!fileUrl);
+
   const handleDragOver = (e: React.DragEvent<HTMLLabelElement>) => {
     e.preventDefault();
     e.stopPropagation();
@@ -27,10 +32,18 @@ const FileInput: React.FC<FileInputProps> = ({
     e.stopPropagation();
 
     const file = e.dataTransfer.files[0];
-
-    if (file) {
+    if (
+      file &&
+      file.type &&
+      allowedFileExtensions.includes(file.type.substring(file.type.lastIndexOf("/") + 1))
+    ) {
+      setIsUploaded(false);
+      setSelectedFile(file);
       const response = await uploadFile(file, allowedFileExtensions, environmentId);
+      setIsUploaded(true);
       onFileUpload(response.data.url);
+    } else {
+      toast.error("File not supported");
     }
   };
 
@@ -40,41 +53,58 @@ const FileInput: React.FC<FileInputProps> = ({
       className="relative flex h-64 w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed border-gray-300 bg-gray-50 hover:bg-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:border-gray-500 dark:hover:bg-gray-600 dark:hover:bg-gray-800"
       onDragOver={(e) => handleDragOver(e)}
       onDrop={(e) => handleDrop(e)}>
-      {
-        // if the file is an image, show the image preview
-        // otherwise, show the file name over a file icon
-
-        fileUrl ? (
-          <>
-            {fileUrl.endsWith("jpg") || fileUrl.endsWith("jpeg") || fileUrl.endsWith("png") ? (
-              <img
-                src={fileUrl}
-                alt="Company Logo"
-                className="max-h-full max-w-full rounded-lg object-contain"
-              />
-            ) : (
-              <div className="flex flex-col items-center justify-center pb-6 pt-5">
-                <FileIcon className="h-6 text-gray-500" />
-                <p className="mt-2 text-sm text-gray-500 dark:text-gray-400">
-                  <span className="font-semibold">{fileUrl.split("/").pop()}</span>
-                </p>
-              </div>
-            )}
-            <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-20 transition-opacity duration-300 hover:bg-opacity-60">
-              <label htmlFor="selectedFile" className="cursor-pointer text-sm font-semibold text-white">
-                Modify
-              </label>
+      {isUploaded && fileUrl ? (
+        <>
+          {fileUrl.endsWith("jpg") || fileUrl.endsWith("jpeg") || fileUrl.endsWith("png") ? (
+            <img
+              src={fileUrl}
+              alt="Company Logo"
+              className="max-h-full max-w-full rounded-lg object-contain"
+            />
+          ) : (
+            <div className="flex flex-col items-center justify-center pb-6 pt-5">
+              <FileIcon className="h-6 text-gray-500" />
+              <p className="dark.text-gray-400 mt-2 text-sm text-gray-500">
+                <span className="font-semibold">{fileUrl.split("/").pop()}</span>
+              </p>
             </div>
-          </>
-        ) : (
-          <div className="flex flex-col items-center justify-center pb-6 pt-5">
-            <ArrowUpTrayIcon className="h-6 text-gray-500" />
-            <p className="mt-2 text-sm text-gray-500 dark:text-gray-400">
-              <span className="font-semibold">Click or drag to upload files.</span>
-            </p>
+          )}
+          <div className="hover.bg-opacity-60 absolute inset-0 flex items-center justify-center bg-black bg-opacity-20 transition-opacity duration-300">
+            <label htmlFor="selectedFile" className="cursor-pointer text-sm font-semibold text-white">
+              Modify
+            </label>
           </div>
-        )
-      }
+        </>
+      ) : !isUploaded && selectedFile ? (
+        <>
+          {selectedFile.type.startsWith("image/") ? (
+            <img
+              src={URL.createObjectURL(selectedFile)}
+              alt="Company Logo"
+              className="max-h-full max-w-full rounded-lg object-contain"
+            />
+          ) : (
+            <div className="flex flex-col items-center justify-center pb-6 pt-5">
+              <FileIcon className="h-6 text-gray-500" />
+              <p className="dark.text-gray-400 mt-2 text-sm text-gray-500">
+                <span className="font-semibold">{selectedFile.name}</span>
+              </p>
+            </div>
+          )}
+          <div className="hover.bg-opacity-60 absolute inset-0 flex items-center justify-center bg-black bg-opacity-20 transition-opacity duration-300">
+            <label htmlFor="selectedFile" className="cursor-pointer text-sm font-semibold text-white">
+              Uploading
+            </label>
+          </div>
+        </>
+      ) : (
+        <div className="flex flex-col items-center justify-center pb-6 pt-5">
+          <ArrowUpTrayIcon className="h-6 text-gray-500" />
+          <p className="dark.text-gray-400 mt-2 text-sm text-gray-500">
+            <span className="font-semibold">Click or drag to upload files.</span>
+          </p>
+        </div>
+      )}
 
       <input
         type="file"
@@ -85,7 +115,10 @@ const FileInput: React.FC<FileInputProps> = ({
         onChange={async (e) => {
           const selectedFile = e.target?.files?.[0];
           if (selectedFile) {
+            setIsUploaded(false);
+            setSelectedFile(selectedFile);
             const response = await uploadFile(selectedFile, allowedFileExtensions, environmentId);
+            setIsUploaded(true);
             onFileUpload(response.data.url);
           }
         }}
