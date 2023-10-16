450c2f025dcd4754e48098a83230f02527f257c2
Matthias Nannt <mail@matthiasnannt.com>
2023-10-16 14:20:05 +0200

    fix: formbricks-js returns error when failed to locate surveys package

--- packages/surveys/vite.config.ts
+++ packages/surveys/vite.config.ts
@@ -6,6 +6,7 @@ import dts from "vite-plugin-dts";
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
+    emptyOutDir: false,
     minify: "terser",
     sourcemap: true,
     lib: {
@@ -17,5 +18,5 @@ export default defineConfig({
       fileName: "index",
     },
   },
-  plugins: [preact(), dts()],
+  plugins: [preact(), dts({ rollupTypes: true })],
 });
