b8c4688b50227ce9daf32f80a2faa37dc90071a4
Nilanjan Pramanik <nik.nilanjan@gmail.com>
2023-10-17 00:28:24 +0530

    update

--- apps/formbricks-com/components/shared/UseCaseCTA.tsx
+++ apps/formbricks-com/components/shared/UseCaseCTA.tsx
@@ -9,11 +9,11 @@ export default function UseCaseHeader({ href }: UseCaseCTAProps) {
   /*   const plausible = usePlausible(); */
   const router = useRouter();
   return (
-    <div className="my-8 flex flex-col space-x-2 space-y-2 whitespace-nowrap md:flex-row">
-      <Button className="mx-auto flex justify-center align-middle" variant="secondary" href={href}>
+    <div className="my-8 flex flex-col justify-center space-x-2 space-y-2 whitespace-nowrap align-top sm:flex-row">
+      <Button className="mx-auto mt-2 flex w-fit justify-center align-middle" variant="secondary" href={href}>
         Step-by-step manual
       </Button>
-      <div className="space-y-1 text-center ">
+      <div className="space-y-1 text-center">
         <Button
           className="bg-gray-800 text-gray-300 hover:text-white"
           onClick={() => {
