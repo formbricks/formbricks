d76ab7b05e25e1ca5c699951c636811569cef204
Matthias Nannt <mail@matthiasnannt.com>
2023-10-18 20:11:16 +0200

    chore: improve error component and display message to user

--- apps/web/app/error.tsx
+++ apps/web/app/error.tsx
@@ -1,16 +1,18 @@
 "use client"; // Error components must be Client components
 
 import { Button } from "@formbricks/ui/Button";
+import CodeBlock from "@formbricks/ui/CodeBlock";
 import { ErrorComponent } from "@formbricks/ui/ErrorComponent";
 
 export default function Error({ error, reset }: { error: Error; reset: () => void }) {
   if (process.env.NODE_ENV === "development") {
-    console.log(error);
+    console.log(error.message);
   }
 
   return (
     <div className="flex h-full w-full flex-col items-center justify-center">
       <ErrorComponent />
+      <CodeBlock language={"bash"}>{error.message}</CodeBlock>
       <Button
         variant="secondary"
         onClick={
