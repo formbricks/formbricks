cf0159664ad7699053f5912f5c1321b1fa799935
Henit Chobisa <chobisa.henit@gmail.com>
2023-10-19 11:16:51 +0530

    feat: added slack wrapper for handling `client` side components

--- apps/web/app/(app)/environments/[environmentId]/integrations/slack/page.tsx
+++ apps/web/app/(app)/environments/[environmentId]/integrations/slack/page.tsx
@@ -1,37 +1,41 @@
 import GoBackButton from "@/components/shared/GoBackButton";
 import { getIntegrations } from "@formbricks/lib/integration/service";
 import { getSurveys } from "@formbricks/lib/survey/service";
-import {
-  GOOGLE_SHEETS_CLIENT_ID,
-  WEBAPP_URL,
-  GOOGLE_SHEETS_CLIENT_SECRET,
-  GOOGLE_SHEETS_REDIRECT_URL,
-} from "@formbricks/lib/constants";
 import { getEnvironment } from "@formbricks/lib/environment/service";
-import Connect from "@/app/(app)/environments/[environmentId]/integrations/slack/Connect";
+import { SLACK_CLIENT_ID, SLACK_CLIENT_SECRET, WEBAPP_URL } from "@formbricks/lib/constants";
+import { TSlackConfigData, TSlackIntegration } from "@formbricks/types/v1/integrations";
+import SlackWrapper from "@/app/(app)/environments/[environmentId]/integrations/slack/SlackWrapper";
 
-export default async function GoogleSheet({ params }) {
-  const enabled = !!(GOOGLE_SHEETS_CLIENT_ID && GOOGLE_SHEETS_CLIENT_SECRET && GOOGLE_SHEETS_REDIRECT_URL);
-  const [environment] = await Promise.all([
+export default async function Slack({ params }) {
+  const enabled = !!(SLACK_CLIENT_ID && SLACK_CLIENT_SECRET);
+
+  const [surveys, integrations, environment] = await Promise.all([
     getSurveys(params.environmentId),
     getIntegrations(params.environmentId),
     getEnvironment(params.environmentId),
   ]);
+
+  const slackIntegration: TSlackIntegration | undefined = integrations?.find(
+    (integration): integration is TSlackIntegration => integration.type === "slack"
+  );
+
   if (!environment) {
     throw new Error("Environment not found");
   }
 
-  // const googleSheetIntegration: TGoogleSheetIntegration | undefined = integrations?.find(
-  //   (integration): integration is TGoogleSheetIntegration => integration.type === "googleSheets"
-  // );
-  // let spreadSheetArray: TGoogleSpreadsheet[] = [];
-  // if (googleSheetIntegration && googleSheetIntegration.config.key) {
-  //   spreadSheetArray = await getSpreadSheets(params.environmentId);
-  // }
   return (
     <>
-      <GoBackButton url={`${WEBAPP_URL}/environments/${params.environmentId}/integrations`} />
-      <Connect enabled={enabled} environmentId={environment.id} webAppUrl={WEBAPP_URL} />
+      <GoBackButton url={`"/environments/${params.environmentId}/integrations`} />
+      <div className="h-[75vh] w-full">
+        <SlackWrapper
+          enabled={enabled}
+          environment={environment}
+          surveys={surveys}
+          slackIntegration={slackIntegration}
+          webAppUrl={WEBAPP_URL}
+          environmentId={params.environmentId}
+        />
+      </div>
     </>
   );
 }
