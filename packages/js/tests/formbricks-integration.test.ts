ca31d1c3e8d805d486f4f96f87fc1eaa38bd5607
Dhruwang <dhruwangjariwala18@gmail.com>
2023-10-19 10:36:48 +0530

    changed package loading approach from CDN to local

--- packages/js/tests/formbricks-integration.test.ts
+++ packages/js/tests/formbricks-integration.test.ts
@@ -7,7 +7,7 @@ let browser;
 let page;
 
 beforeAll(async () => {
-  browser = await puppeteer.launch({ headless: true });
+  browser = await puppeteer.launch({ headless: "old" });
   page = await browser.newPage();
 });
 
@@ -18,14 +18,12 @@ afterAll(async () => {
 describe("formbricks integration test", () => {
   it("should load formbricks successfully", async () => {
     await page.goto(`file://${path.resolve(__dirname, "./test.html")}`);
-
-    // The `waitForFunction` will periodically check the page to see if formbricks has loaded
     const isFormbricksLoaded = await page.waitForFunction(
       () => {
         return window.formbricks !== undefined;
       },
       { timeout: 10000 }
-    ); // adjust the timeout as needed
+    );
 
     expect(isFormbricksLoaded).toBeTruthy();
   });
