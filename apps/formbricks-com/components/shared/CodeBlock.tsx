c301dcb8e627a35609c4dfe579c5b2832337ca77
Aman <138197666+aman44444@users.noreply.github.com>
2023-10-20 05:45:20 +0000

    created dark mode version of code block

--- apps/formbricks-com/components/shared/CodeBlock.tsx
+++ apps/formbricks-com/components/shared/CodeBlock.tsx
@@ -12,10 +12,18 @@ const CodeBlock: React.FC<CodeBlockProps> = ({ children }) => {
     Prism.highlightAll();
   }, [children]);
 
+  const codestyle = {
+    textShadow: "none",
+    background: "none !important",
+    color: "white !important",
+  };
+
   return (
     <div className="group relative mt-4 rounded-md text-sm font-light text-slate-200 sm:text-base">
-      <pre>
-        <code className="language-js">{children}</code>
+      <pre className="rounded-md dark:bg-slate-700">
+        <code style={codestyle} className="language-js">
+          {children}
+        </code>
       </pre>
     </div>
   );
