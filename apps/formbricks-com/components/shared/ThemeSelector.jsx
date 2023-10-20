396e473e57da9effad94b66f85d54bbfc7bb824e
Mohit Negi <mohitnegi237@gmail.com>
2023-10-20 16:45:05 +0000

    Refactor theme switcher from dropdown to toggle

--- apps/formbricks-com/components/shared/ThemeSelector.jsx
+++ apps/formbricks-com/components/shared/ThemeSelector.jsx
@@ -1,12 +1,4 @@
 import { useEffect, useState } from "react";
-import { Listbox } from "@headlessui/react";
-import clsx from "clsx";
-
-const themes = [
-  { name: "Light", value: "light", icon: LightIcon },
-  { name: "Dark", value: "dark", icon: DarkIcon },
-  { name: "System", value: "system", icon: SystemIcon },
-];
 
 function LightIcon(props) {
   return (
@@ -32,80 +24,36 @@ function DarkIcon(props) {
   );
 }
 
-function SystemIcon(props) {
-  return (
-    <svg aria-hidden="true" viewBox="0 0 16 16" {...props}>
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M1 4a3 3 0 0 1 3-3h8a3 3 0 0 1 3 3v4a3 3 0 0 1-3 3h-1.5l.31 1.242c.084.333.36.573.63.808.091.08.182.158.264.24A1 1 0 0 1 11 15H5a1 1 0 0 1-.704-1.71c.082-.082.173-.16.264-.24.27-.235.546-.475.63-.808L5.5 11H4a3 3 0 0 1-3-3V4Zm3-1a1 1 0 0 0-1 1v4a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1H4Z"
-      />
-    </svg>
-  );
-}
-
 export function ThemeSelector(props) {
-  const [selectedTheme, setSelectedTheme] = useState(null);
+  // Initialize the state with a default value
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
+  // Use a useEffect to run code on the client side
   useEffect(() => {
-    if (selectedTheme) {
-      document.documentElement.setAttribute("data-theme", selectedTheme.value);
-    } else {
-      setSelectedTheme(
-        themes.find((theme) => theme.value === document.documentElement.getAttribute("data-theme"))
-      );
-    }
-  }, [selectedTheme]);
+    // Check the system theme on the client side
+    const systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
 
-  useEffect(() => {
-    let handler = () =>
-      setSelectedTheme(themes.find((theme) => theme.value === (window.localStorage.theme ?? "system")));
+    // Update the state based on the system theme
+    setIsDarkMode(systemTheme);
 
-    window.addEventListener("storage", handler);
-
-    return () => window.removeEventListener("storage", handler);
+    // Set the HTML attribute based on the initial state
+    document.documentElement.setAttribute("data-theme", systemTheme ? "dark" : "light");
   }, []);
 
+  const toggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+    document.documentElement.setAttribute("data-theme", isDarkMode ? "light" : "dark");
+  };
+
   return (
-    <Listbox as="div" value={selectedTheme} onChange={setSelectedTheme} {...props}>
-      <Listbox.Label className="sr-only">Theme</Listbox.Label>
-      <Listbox.Button
-        className="flex h-6 w-6 items-center justify-center rounded-lg shadow-md shadow-black/5 ring-1 ring-black/5 dark:bg-slate-700 dark:ring-inset dark:ring-white/5"
-        aria-label={selectedTheme?.name}>
-        <LightIcon className="fill-brand hidden h-4 w-4 [[data-theme=light]_&]:block" />
-        <DarkIcon className="fill-brand hidden h-4 w-4 [[data-theme=dark]_&]:block" />
-        <LightIcon className="hidden h-4 w-4 fill-slate-400 [:not(.dark)[data-theme=system]_&]:block" />
-        <DarkIcon className="hidden h-4 w-4 fill-slate-400 [.dark[data-theme=system]_&]:block" />
-      </Listbox.Button>
-      <Listbox.Options className="absolute left-1/2 top-full mt-3 w-36 -translate-x-1/2 space-y-1 rounded-xl bg-white p-3 text-sm font-medium shadow-md shadow-black/5 ring-1 ring-black/5 dark:bg-slate-800 dark:ring-white/5">
-        {themes.map((theme) => (
-          <Listbox.Option
-            key={theme.value}
-            value={theme}
-            className={({ active, selected }) =>
-              clsx("flex cursor-pointer select-none items-center rounded-[0.625rem] p-1", {
-                "text-brand-dark dark:text-brand-light": selected,
-                "text-slate-800 dark:text-slate-100": active && !selected,
-                "text-slate-700 dark:text-slate-400": !active && !selected,
-                "bg-slate-100 dark:bg-slate-900/40": active,
-              })
-            }>
-            {({ selected }) => (
-              <>
-                <div className="rounded-md bg-white p-1 shadow ring-1 ring-slate-900/5 dark:bg-slate-700 dark:ring-inset dark:ring-white/5">
-                  <theme.icon
-                    className={clsx(
-                      "h-4 w-4",
-                      selected ? "fill-brand-dark dark:fill-brand-light" : "fill-slate-400"
-                    )}
-                  />
-                </div>
-                <div className="ml-3">{theme.name}</div>
-              </>
-            )}
-          </Listbox.Option>
-        ))}
-      </Listbox.Options>
-    </Listbox>
+    <div
+      className="shadow-black/8 mr-5 flex h-6 w-6 items-center justify-center rounded-lg shadow-md ring-1 ring-black/5 dark:bg-slate-700 dark:ring-inset dark:ring-white/5"
+      onClick={toggleTheme}>
+      {isDarkMode ? (
+        <DarkIcon className="fill-brand h-4 w-4" />
+      ) : (
+        <LightIcon className="fill-brand h-4 w-4" />
+      )}
+    </div>
   );
 }
