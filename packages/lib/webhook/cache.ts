845181e0d5bcb2b5f440e9763958e0a181e437a9
Rotimi Best <rotimiibitoyeemma@gmail.com>
2023-10-20 19:45:54 +0100

    chore: webhook caching

---
+++ packages/lib/webhook/cache.ts
@@ -0,0 +1,34 @@
+import { revalidateTag } from "next/cache";
+
+interface RevalidateProps {
+  id?: string;
+  environmentId?: string;
+  source?: string;
+}
+
+export const webhookCache = {
+  tag: {
+    byId(id: string) {
+      return `webhooks-${id}`;
+    },
+    byEnvironmentId(environmentId: string) {
+      return `environments-${environmentId}-webhooks`;
+    },
+    byEnvironmentIdAndSource(environmentId: string, source: string) {
+      return `environments-${environmentId}-sources-${source}-webhooks`;
+    },
+  },
+  revalidate({ id, environmentId, source }: RevalidateProps): void {
+    if (id) {
+      revalidateTag(this.tag.byId(id));
+    }
+
+    if (environmentId) {
+      revalidateTag(this.tag.byEnvironmentId(environmentId));
+    }
+
+    if (environmentId && source) {
+      revalidateTag(this.tag.byEnvironmentIdAndSource(environmentId, source));
+    }
+  },
+};
