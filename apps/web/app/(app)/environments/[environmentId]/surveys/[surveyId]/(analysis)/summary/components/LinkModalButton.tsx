168ebde5a3f389fee873b167a779ac26879d6603
Hemant <hrsmart2150@gmail.com>
2023-10-21 13:37:19 +0000

    fix: Share modal not opening

--- apps/web/app/(app)/environments/[environmentId]/surveys/[surveyId]/(analysis)/summary/components/LinkModalButton.tsx
+++ apps/web/app/(app)/environments/[environmentId]/surveys/[surveyId]/(analysis)/summary/components/LinkModalButton.tsx
@@ -40,7 +40,7 @@ export default function LinkSurveyShareButton({
         }}>
         <ShareIcon className="h-5 w-5" />
       </Button>
-      {showLinkModal && isSingleUse && (
+      {showLinkModal && (
         <ShareEmbedSurvey
           survey={survey}
           open={showLinkModal}
