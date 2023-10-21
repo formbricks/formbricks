3ef7f93d1556768d9ba8741b251de052e4d615d3
Abhinav Arya <itzabhinavarya@gmail.com>
2023-10-21 19:54:40 +0000

    removing background color of the operator

--- apps/formbricks-com/components/shared/CodeBlock.tsx
+++ apps/formbricks-com/components/shared/CodeBlock.tsx
@@ -7,10 +7,6 @@ interface CodeBlockProps {
   children: React.ReactNode;
 }
 
-const codeStyle = {
-  textShadow: "none !important",
-};
-
 const CodeBlock: React.FC<CodeBlockProps> = ({ children }) => {
   useEffect(() => {
     Prism.highlightAll();
@@ -19,7 +15,7 @@ const CodeBlock: React.FC<CodeBlockProps> = ({ children }) => {
   return (
     <div className="group relative mt-4 rounded-md text-sm font-light text-slate-200 sm:text-base">
       <pre className="!bg-transparent">
-        <code className="language-js !text-amber-400" style={codeStyle}>
+        <code className="language-js !text-amber-400" style={{ textShadow: "none" }}>
           {children}
         </code>
       </pre>
