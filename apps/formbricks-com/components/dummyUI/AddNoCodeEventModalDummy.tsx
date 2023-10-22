b908f0ef8a20d87fb9332ca9e92fd0b74f6f2644
Suman Kisku <sumankisku1@gmail.com>
2023-10-23 02:17:02 +0530

    Fixed the code as suggested by review-agent-prime

--- apps/formbricks-com/components/dummyUI/AddNoCodeEventModalDummy.tsx
+++ apps/formbricks-com/components/dummyUI/AddNoCodeEventModalDummy.tsx
@@ -99,8 +99,8 @@ export const AddNoCodeEventModalDummy: React.FC<EventDetailModalProps> = ({ open
               </div>
             </div>
           </div>
-          <div className="mt-6 flex justify-center px-6 pb-8 sm:mt-8 sm:justify-end">
-            <div className="flex space-x-4">
+          <div className="mt-6 flex justify-center pb-8 sm:mt-8 sm:justify-end">
+            <div className="mx-6 flex space-x-4">
               <Button
                 variant="minimal"
                 onClick={(e) => {
