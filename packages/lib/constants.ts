857f3bd0293088898b9468aa864d25e6f9d171e6
ShubhamPalriwala <spalriwalau@gmail.com>
2023-10-23 15:12:39 +0530

    feat: client side pagination in open text questions in responses summary

--- packages/lib/constants.ts
+++ packages/lib/constants.ts
@@ -61,6 +61,7 @@ export const MAIL_FROM = env.MAIL_FROM;
 export const NEXTAUTH_SECRET = env.NEXTAUTH_SECRET;
 export const NEXTAUTH_URL = env.NEXTAUTH_URL;
 export const ITEMS_PER_PAGE = 50;
+export const OPEN_TEXT_RESPONSES_PER_PAGE = 10;
 
 // Storage constants
 export const UPLOADS_DIR = path.resolve("./uploads");
