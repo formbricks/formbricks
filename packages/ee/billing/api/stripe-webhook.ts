28fdff2de1dc2dc96170ab9829113ca3edb6965b
ShubhamPalriwala <spalriwalau@gmail.com>
2023-10-24 15:48:53 +0530

    feat: billing now uses app dir

--- packages/ee/billing/api/stripe-webhook.ts
+++ packages/ee/billing/api/stripe-webhook.ts
@@ -1,7 +1,5 @@
-//import { buffer } from "micro";
 import { prisma } from "@formbricks/database";
-import { NextApiRequest, NextApiResponse } from "next";
-import type { Readable } from "stream";
+import { updateTeam } from "@formbricks/lib/team/service";
 
 import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
@@ -11,86 +9,54 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
 
 const webhookSecret: string = process.env.STRIPE_WEBHOOK_SECRET!;
 
-// Stripe requires the raw body to construct the event.
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
+const webhookHandler = async (requestBody: string, stripeSignature: string) => {
+  let event: Stripe.Event;
 
-async function buffer(readable: Readable): Promise<Buffer> {
-  const chunks: Buffer[] = [];
-  for await (const chunk of readable) {
-    chunks.push(typeof chunk === "string" ? Buffer.from(chunk) : chunk);
+  try {
+    event = stripe.webhooks.constructEvent(requestBody, stripeSignature, webhookSecret);
+  } catch (err) {
+    const errorMessage = err instanceof Error ? err.message : "Unknown error";
+    // On error, log and return the error message.
+    if (err! instanceof Error) console.log(err);
+    return { status: 400, message: `Webhook Error: ${errorMessage}` };
   }
-  return Buffer.concat(chunks);
-}
 
-const webhookHandler = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method === "POST") {
-    const buf = await buffer(req);
-    const sig = req.headers["stripe-signature"]!;
-
-    let event: Stripe.Event;
-
-    try {
-      event = stripe.webhooks.constructEvent(buf.toString(), sig, webhookSecret);
-    } catch (err) {
-      const errorMessage = err instanceof Error ? err.message : "Unknown error";
-      // On error, log and return the error message.
-      if (err! instanceof Error) console.log(err);
-      console.log(`Error message: ${errorMessage}`);
-      res.status(400).send(`Webhook Error: ${errorMessage}`);
-      return;
+  // Cast event data to Stripe object.
+  if (event.type === "checkout.session.completed") {
+    const checkoutSession = event.data.object as Stripe.Checkout.Session;
+    const teamId = checkoutSession.client_reference_id;
+    if (!teamId) {
+      return { status: 400, message: "skipping, no teamId found" };
     }
+    const stripeCustomerId = checkoutSession.customer as string;
+    const plan = "pro";
+    await updateTeam(teamId, { stripeCustomerId, plan });
 
-    // Cast event data to Stripe object.
-    if (event.type === "checkout.session.completed") {
-      const checkoutSession = event.data.object as Stripe.Checkout.Session;
-      const teamId = checkoutSession.client_reference_id;
-      if (!teamId) {
-        console.error("No teamId found in checkout session");
-        return res.json({ message: "skipping, no teamId found" });
-      }
-      const stripeCustomerId = checkoutSession.customer as string;
-      const plan = "pro";
-      await prisma.team.update({
-        where: { id: teamId },
-        data: {
-          stripeCustomerId,
-          plan,
-        },
-      });
-      // add teamId to subscription metadata in Stripe
-      const subscription = await stripe.subscriptions.retrieve(checkoutSession.subscription as string);
-      await stripe.subscriptions.update(subscription.id, {
-        metadata: {
-          teamId,
-        },
-      });
-    } else if (event.type === "customer.subscription.deleted") {
-      const subscription = event.data.object as Stripe.Subscription;
-      const teamId = subscription.metadata.teamId;
-      if (!teamId) {
-        console.error("No teamId found in subscription");
-        return res.json({ message: "skipping, no teamId found" });
-      }
-      await prisma.team.update({
-        where: { id: teamId },
-        data: {
-          plan: "free",
-        },
-      });
-    } else {
-      console.warn(`🤷‍♀️ Unhandled event type: ${event.type}`);
+    const subscription = await stripe.subscriptions.retrieve(checkoutSession.subscription as string);
+    await stripe.subscriptions.update(subscription.id, {
+      metadata: {
+        teamId,
+      },
+    });
+  } else if (event.type === "customer.subscription.deleted") {
+    const subscription = event.data.object as Stripe.Subscription;
+    const teamId = subscription.metadata.teamId;
+    if (!teamId) {
+      console.error("No teamId found in subscription");
+      return { status: 400, message: "skipping, no teamId found" };
     }
-
-    // Return a response to acknowledge receipt of the event.
-    res.json({ received: true });
+    await prisma.team.update({
+      where: { id: teamId },
+      data: {
+        plan: "free",
+      },
+    });
   } else {
-    res.setHeader("Allow", "POST");
-    res.status(405).end("Method Not Allowed");
+    console.warn(`🤷‍♀️ Unhandled event type: ${event.type}`);
   }
+
+  // Return a response to acknowledge receipt of the event.
+  return { status: 200, message: { received: true } };
 };
 
 export default webhookHandler;
