11a68a904eaf855439405af1e2ed2071adadac30
Matti Nannt <mail@matthiasnannt.com>
2023-10-24 12:58:23 +0200

    chore: Remove response limit in preparation for new pricing (#1443)

--- packages/lib/constants.ts
+++ packages/lib/constants.ts
@@ -2,7 +2,6 @@ import "server-only";
 import path from "path";
 import { env } from "@/env.mjs";
 
-export const RESPONSES_LIMIT_FREE = 100;
 export const IS_FORMBRICKS_CLOUD = env.IS_FORMBRICKS_CLOUD === "1";
 export const REVALIDATION_INTERVAL = 0; //TODO: find a good way to cache and revalidate data when it changes
 export const SERVICES_REVALIDATION_INTERVAL = 60 * 30; // 30 minutes
