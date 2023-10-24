75f8e5afba73e8b519e41c4cda1e009876505c28
Neil Chauhan <neilchauhan2@gmail.com>
2023-10-24 18:24:28 +0530

    feat: disable survey creation and deletion for viewer roles

--- apps/web/app/(app)/environments/[environmentId]/surveys/[surveyId]/edit/page.tsx
+++ apps/web/app/(app)/environments/[environmentId]/surveys/[surveyId]/edit/page.tsx
@@ -8,18 +8,43 @@ import { getResponseCountBySurveyId } from "@formbricks/lib/response/service";
 import { getSurvey } from "@formbricks/lib/survey/service";
 import { ErrorComponent } from "@formbricks/ui/ErrorComponent";
 import SurveyEditor from "./components/SurveyEditor";
+import { getTeamByEnvironmentId } from "@formbricks/lib/team/service";
+import { getMembershipByUserIdTeamId } from "@formbricks/lib/membership/service";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@formbricks/lib/authOptions";
 
 export default async function SurveysEditPage({ params }) {
-  const [survey, product, environment, actionClasses, attributeClasses, responseCount] = await Promise.all([
-    getSurvey(params.surveyId),
-    getProductByEnvironmentId(params.environmentId),
-    getEnvironment(params.environmentId),
-    getActionClasses(params.environmentId),
-    getAttributeClasses(params.environmentId),
-    getResponseCountBySurveyId(params.surveyId),
-  ]);
+  const [survey, product, environment, actionClasses, attributeClasses, responseCount, team, session] =
+    await Promise.all([
+      getSurvey(params.surveyId),
+      getProductByEnvironmentId(params.environmentId),
+      getEnvironment(params.environmentId),
+      getActionClasses(params.environmentId),
+      getAttributeClasses(params.environmentId),
+      getResponseCountBySurveyId(params.surveyId),
+      getTeamByEnvironmentId(params.environmentId),
+      getServerSession(authOptions),
+    ]);
 
-  if (!survey || !environment || !actionClasses || !attributeClasses || !product) {
+  if (!session) {
+    throw new Error("Session not found");
+  }
+
+  if (!team) {
+    throw new Error("Team not found");
+  }
+
+  const currentUserMembership = await getMembershipByUserIdTeamId(session?.user.id, team.id);
+  const isSurveyCreationDeletionDisabled = currentUserMembership?.role === "viewer";
+
+  if (
+    !survey ||
+    !environment ||
+    !actionClasses ||
+    !attributeClasses ||
+    !product ||
+    isSurveyCreationDeletionDisabled
+  ) {
     return <ErrorComponent />;
   }
 
