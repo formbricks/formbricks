fa88cbb95f58ebe743032e75956b57a1980564f0
Neil Chauhan <neilchauhan2@gmail.com>
2023-10-26 17:11:51 +0530

    fix: disable no code action creation on action level for viewer level roles

--- apps/web/app/(app)/environments/[environmentId]/(actionsAndAttributes)/actions/components/ActionClassesTable.tsx
+++ apps/web/app/(app)/environments/[environmentId]/(actionsAndAttributes)/actions/components/ActionClassesTable.tsx
@@ -82,6 +82,7 @@ export default function ActionClassesTable({
         environmentId={environmentId}
         open={isAddActionModalOpen}
         setOpen={setAddActionModalOpen}
+        isViewer={isViewer}
       />
     </>
   );
