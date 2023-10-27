e6a88cdacf533f741d06b56ba9e5833ff000ae16
Piyush Gupta <piyushguptaa2z123@gmail.com>
2023-10-27 23:54:47 +0530

    chore: removed redundant styles

--- packages/surveys/src/components/Headline.tsx
+++ packages/surveys/src/components/Headline.tsx
@@ -7,9 +7,7 @@ interface HeadlineProps {
 
 export default function Headline({ headline, questionId, style, required = true }: HeadlineProps) {
   return (
-    <label
-      htmlFor={questionId}
-      className="mb-1.5 block text-base font-semibold leading-6 text-slate-900 focus:text-red-600">
+    <label htmlFor={questionId} className="mb-1.5 block text-base font-semibold leading-6 text-slate-900">
       <div className={"flex justify-between gap-4"} style={style}>
         {headline}
         {!required && (
