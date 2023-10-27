e65a3b5e7e8ef2a8cf7f7e4dde2a595943d8c1f9
anjy7 <anjyofficial@gmail.com>
2023-10-27 22:51:19 +0530

    fix: fixed css bug

--- apps/web/app/(app)/share/[sharingKey]/layout.tsx
+++ apps/web/app/(app)/share/[sharingKey]/layout.tsx
@@ -1,9 +1,9 @@
 import { ResponseFilterProvider } from "@/app/(app)/environments/[environmentId]/components/ResponseFilterContext";
 
-export default async function EnvironmentLayout({ children, params }) {
+export default async function EnvironmentLayout({ children }) {
   return (
-    <>
+    <div className="flex-1">
       <ResponseFilterProvider>{children}</ResponseFilterProvider>
-    </>
+    </div>
   );
 }
