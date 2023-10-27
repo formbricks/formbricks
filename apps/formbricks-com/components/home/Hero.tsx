54b5416c368f74d2971240740540aa7fb1fb6680
Johannes <johannes@formbricks.com>
2023-10-27 12:33:40 +0100

    ph launch changes

--- apps/formbricks-com/components/home/Hero.tsx
+++ apps/formbricks-com/components/home/Hero.tsx
@@ -20,30 +20,26 @@ export const Hero: React.FC = ({}) => {
     <div className="relative">
       <div className="px-4 pb-20 pt-16 text-center sm:px-6 lg:px-8 lg:pb-32 lg:pt-20">
         <a
-          href="https://formbricks.com/formtribe"
+          href="https://www.producthunt.com/products/formbricks"
           target="_blank"
           className="border-brand-dark xs:text-sm animate-bounce rounded-full border px-4 py-1.5 text-xs text-slate-500 hover:bg-slate-100 dark:text-slate-300 dark:hover:bg-slate-800">
-          The FormTribe Hackathon is on 🔥
+          Support us on Product Hunt 🚀
           <ChevronRightIcon className="inline h-5 w-5 text-slate-300" />
         </a>
         <h1 className="mt-10 text-3xl font-bold tracking-tight text-slate-800 dark:text-slate-200 sm:text-4xl md:text-5xl">
-          <span className="xl:inline">Open-source Experience Management</span>
+          <span className="xl:inline">The Open Source Survey Suite</span>
         </h1>
 
         <p className="xs:max-w-none mx-auto mt-3 max-w-xs text-base text-slate-500 dark:text-slate-400 sm:text-lg md:mt-5 md:text-xl">
-          Understand what customers think & feel about your product.
-          <br />
-          <span className="hidden md:block">
-            Natively integrate user research with minimal dev attention,{" "}
-            <span className="decoration-brand-dark underline underline-offset-4">privacy-first.</span>
-          </span>
+          Run link surveys, in-app surveys and email surveys in one app —{" "}
+          <span className="decoration-brand-dark underline underline-offset-4">all privacy-first.</span>
         </p>
 
-        <div className="mx-auto mt-5 max-w-3xl items-center px-4 sm:flex sm:justify-center md:mt-8 md:space-x-8 md:px-0">
+        <div className="mx-auto mt-5 max-w-2xl items-center px-4 sm:flex sm:justify-center md:mt-6 md:space-x-8 md:px-0">
           <p className="hidden whitespace-nowrap pt-3 text-xs text-slate-400 dark:text-slate-500 md:block">
             Trusted by
           </p>
-          <div className="grid grid-cols-4 items-center gap-5 pt-2 md:gap-8">
+          <div className="grid grid-cols-4 items-center gap-6 pt-2 md:gap-8">
             <Image
               src={CalLogoLight}
               alt="Cal Logo"
@@ -69,12 +65,6 @@ export const Hero: React.FC = ({}) => {
               width={200}
             />
             <Image
-              src={ClovyrLogo}
-              alt="Clovyr Logo"
-              className="rounded-lg pb-1 hover:opacity-100 md:opacity-50"
-              width={200}
-            />
-            <Image
               src={NILogoDark}
               alt="Neverinstall Logo"
               className="block pb-1 hover:opacity-100 dark:hidden md:opacity-50"
@@ -84,6 +74,12 @@ export const Hero: React.FC = ({}) => {
               src={NILogoLight}
               alt="Neverinstall Logo"
               className="hidden  pb-1 hover:opacity-100 dark:block md:opacity-50"
+              width={200}
+            />
+            <Image
+              src={ClovyrLogo}
+              alt="Clovyr Logo"
+              className="rounded-lg pb-1 hover:opacity-100 md:opacity-50"
               width={200}
             />
           </div>
@@ -102,10 +98,10 @@ export const Hero: React.FC = ({}) => {
             variant="secondary"
             className="px-6"
             onClick={() => {
-              router.push("/demo");
-              plausible("Hero_CTA_LaunchDemo");
+              router.push("https://www.producthunt.com/products/formbricks");
+              /*  plausible("Hero_CTA_LaunchDemo"); */
             }}>
-            Live demo
+            Support Launch 🚀
           </Button>
         </div>
       </div>
