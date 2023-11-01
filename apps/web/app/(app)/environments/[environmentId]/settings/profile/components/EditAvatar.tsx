eeddaeee458324f3244a823318797484305bf0af
pandeymangg <anshuman.pandey9999@gmail.com>
2023-11-01 15:26:09 +0530

    fix: moves upload function to lib

--- apps/web/app/(app)/environments/[environmentId]/settings/profile/components/EditAvatar.tsx
+++ apps/web/app/(app)/environments/[environmentId]/settings/profile/components/EditAvatar.tsx
@@ -8,92 +8,32 @@ import toast from "react-hot-toast";
 import Image from "next/image";
 import { updateAvatarAction } from "@/app/(app)/environments/[environmentId]/settings/profile/actions";
 import { useRouter } from "next/navigation";
+import { handleFileUpload } from "../lib";
 
 export function EditAvatar({ session, environmentId }: { session: Session | null; environmentId: string }) {
   const inputRef = useRef<HTMLInputElement>(null);
   const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
 
-  const handleFileUpload = async (file: File) => {
-    if (!file) return;
-
-    if (!file.type.startsWith("image/")) {
-      toast.error("Please upload an image file.");
-      return;
-    }
-
-    if (file.size > 10 * 1024 * 1024) {
-      toast.error("File size must be less than 10MB.");
-      return;
-    }
-
-    const payload = {
-      fileName: file.name,
-      fileType: file.type,
-      environmentId,
-    };
-
+  const handleUpload = async (file: File, environmentId: string) => {
     setIsLoading(true);
-    const response = await fetch("/api/v1/management/storage", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(payload),
-    });
+    try {
+      const { url, error } = await handleFileUpload(file, environmentId);
 
-    if (!response.ok) {
-      throw new Error(`Upload failed with status: ${response.status}`);
-    }
+      if (error) {
+        toast.error(error);
+        setIsLoading(false);
+        return;
+      }
 
-    const json = await response.json();
-
-    const { data } = json;
-    const { signedUrl, fileUrl, signingData, presignedFields } = data;
-
-    let requestHeaders: Record<string, string> = {};
-
-    if (signingData) {
-      const { signature, timestamp, uuid } = signingData;
-
-      requestHeaders = {
-        fileType: file.type,
-        fileName: file.name,
-        environmentId: environmentId ?? "",
-        signature,
-        timestamp,
-        uuid,
-      };
-    }
-
-    const formData = new FormData();
-
-    if (presignedFields) {
-      Object.keys(presignedFields).forEach((key) => {
-        formData.append(key, presignedFields[key]);
-      });
-    }
-
-    // Add the actual file to be uploaded
-    formData.append("file", file);
-
-    const uploadResponse = await fetch(signedUrl, {
-      method: "POST",
-      ...(signingData ? { headers: requestHeaders } : {}),
-      body: formData,
-    });
-
-    if (!uploadResponse.ok) {
-      // throw new Error(`Upload failed with status: ${uploadResponse.status}`);
+      await updateAvatarAction(url);
+      router.refresh();
+    } catch (err) {
+      toast.error("Avatar update failed. Please try again.");
       setIsLoading(false);
-      toast.error("Upload failed. Please try again.");
-      return;
     }
 
     setIsLoading(false);
-    // Update the user's avatar
-    await updateAvatarAction(fileUrl);
-    router.refresh();
   };
 
   return (
@@ -144,12 +84,7 @@ export function EditAvatar({ session, environmentId }: { session: Session | null
           onChange={async (e) => {
             const file = e.target.files?.[0];
             if (file) {
-              try {
-                await handleFileUpload(file);
-              } catch (err) {
-                toast.error("Upload failed. Please try again.");
-                setIsLoading(false);
-              }
+              await handleUpload(file, environmentId);
             }
           }}
         />
