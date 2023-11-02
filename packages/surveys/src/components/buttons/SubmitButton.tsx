c2a941f53ea77eedad013c0279ff1aab75e35125
Neil Chauhan <neilchauhan2@gmail.com>
2023-11-02 14:19:27 +0530

    fix: brand color issue

--- packages/surveys/src/components/buttons/SubmitButton.tsx
+++ packages/surveys/src/components/buttons/SubmitButton.tsx
@@ -17,7 +17,6 @@ function SubmitButton({
   tabIndex = 1,
   focus = false,
   type = "submit",
-  brandColor,
 }: SubmitButtonProps) {
   const buttonRef = useCallback((currentButton: HTMLButtonElement | null) => {
     if (currentButton && focus) {
@@ -34,7 +33,6 @@ function SubmitButton({
       tabIndex={tabIndex}
       autoFocus={focus}
       className="flex items-center rounded-md border border-[--fb-submit-btn-border] bg-[--fb-brand-color] px-3 py-3 text-base font-medium leading-4 text-[--fb-brand-text-color] shadow-sm hover:opacity-90 focus:outline-none focus:ring-2 focus:ring-[--fb-focus-color] focus:ring-offset-2"
-      style={{ backgroundColor: brandColor }}
       onClick={onClick}>
       {buttonLabel || (isLastQuestion ? "Finish" : "Next")}
     </button>
