702c8b57a42a28483de476d9a283074e15d86c30
pandeymangg <anshuman.pandey9999@gmail.com>
2023-11-02 10:26:50 +0530

    fix: db migrations

--- packages/database/migrations/20231102043259_changes_image_to_image_url/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `image` on the `User` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "User" DROP COLUMN "image",
-ADD COLUMN     "imageUrl" TEXT;
