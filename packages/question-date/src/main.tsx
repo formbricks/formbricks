b8851ff382986d8e5d1c9a28f830572b1b6bf059
pandeymangg <anshuman.pandey9999@gmail.com>
2023-11-06 22:55:36 +0530

    fix: script not working

--- packages/question-date/src/main.tsx
+++ packages/question-date/src/main.tsx
@@ -1,11 +1,11 @@
 import { render } from "preact";
 import { App } from "./app.tsx";
-import globalCss from "./app.css?inline"
-import datePickerCss from 'react-date-picker/dist/DatePicker.css?inline';
-import calendarCss from 'react-calendar/dist/Calendar.css?inline';
-import "./app.css"
-import 'react-date-picker/dist/DatePicker.css';
-import 'react-calendar/dist/Calendar.css';
+import globalCss from "./app.css?inline";
+import datePickerCss from "react-date-picker/dist/DatePicker.css?inline";
+import calendarCss from "react-calendar/dist/Calendar.css?inline";
+import "./app.css";
+import "react-date-picker/dist/DatePicker.css";
+import "react-calendar/dist/Calendar.css";
 
 declare global {
   interface Window {
@@ -18,19 +18,19 @@ const addStylesToDom = () => {
   if (document.getElementById("formbricks__question_date_css") === null) {
     const styleElement = document.createElement("style");
     styleElement.id = "formbricks__question_date_css";
-    styleElement.innerHTML = globalCss + datePickerCss + calendarCss
-    document.head.appendChild(styleElement)
+    styleElement.innerHTML = globalCss + datePickerCss + calendarCss;
+    document.head.appendChild(styleElement);
   }
-}
+};
 
 const init = (element: HTMLElement) => {
-  addStylesToDom()
+  addStylesToDom();
   const container = document.createElement("div");
   container.id = "datePickerContainer";
   element.appendChild(container);
   render(<App />, container);
 };
 
-// render(<App />, document.getElementById("dpc")!)
+// render(<App />, document.getElementById("dpc")!);
 
-window.initDatePicker = init
+window.initDatePicker = init;
