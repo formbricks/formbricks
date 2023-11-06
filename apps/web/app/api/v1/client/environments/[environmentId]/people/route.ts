023e15038b1c0dc80f641d9e900c1c42d6d11064
ShubhamPalriwala <spalriwalau@gmail.com>
2023-11-06 13:43:35 +0530

    fix: missing static generation store in revalidation due to pages dir

---
+++ apps/web/app/api/v1/client/environments/[environmentId]/people/route.ts
@@ -0,0 +1,37 @@
+import { getSettings } from "@/app/lib/api/clientSettings";
+import { responses } from "@/app/lib/api/response";
+import { createPerson } from "@formbricks/lib/person/service";
+import { createSession } from "@formbricks/lib/session/service";
+import { NextResponse } from "next/server";
+
+export async function OPTIONS(): Promise<NextResponse> {
+  return responses.successResponse({}, true);
+}
+
+export async function POST(
+  _: Request,
+  { params }: { params: { environmentId: string } }
+): Promise<NextResponse> {
+  const { environmentId } = params;
+
+  if (!environmentId) {
+    return responses.badRequestResponse("Missing environmentId");
+  }
+
+  try {
+    const person = await createPerson(environmentId);
+    const session = await createSession(person.id);
+    const settings = await getSettings(environmentId, person.id);
+
+    return responses.successResponse(
+      {
+        person,
+        session,
+        settings,
+      },
+      true
+    );
+  } catch (error) {
+    return responses.internalServerErrorResponse(error.message);
+  }
+}
