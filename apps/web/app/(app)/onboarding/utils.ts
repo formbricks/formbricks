4041b07a1a585e20c5b6a2d881a3f4283fbc0d64
Harish Gautam <gautam33776@gmail.com>
2023-11-10 15:53:56 +0530

    minor fix

--- apps/web/app/(app)/onboarding/utils.ts
+++ apps/web/app/(app)/onboarding/utils.ts
@@ -16,7 +16,7 @@ export const handleTabNavigation = (fieldsetRef, setSelectedChoice) => (event) =
   ) as HTMLInputElement;
 
   if (!focusedRadioButton) {
-    // If no radio button is focused, focus on the first one by default
+    // If no radio button is focused, then it will focus on the first one by default
     const firstRadioButton = radioButtons[0] as HTMLInputElement;
     firstRadioButton.focus();
     setSelectedChoice(firstRadioButton.value);
@@ -26,7 +26,7 @@ export const handleTabNavigation = (fieldsetRef, setSelectedChoice) => (event) =
   const focusedIndex = Array.from(radioButtons).indexOf(focusedRadioButton);
   const lastIndex = radioButtons.length - 1;
 
-  // Calculate the next index, considering wrapping from the last to the first element
+  // Calculating the next index, considering wrapping from the last to the first element
   const nextIndex = focusedIndex === lastIndex ? 0 : focusedIndex + 1;
   const nextRadioButton = radioButtons[nextIndex] as HTMLInputElement;
   nextRadioButton.focus();
