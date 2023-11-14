4066fbf7d6f3e6dfb88db5a1400f480bd91137c9
anjy7 <anjyofficial@gmail.com>
2023-11-14 16:54:24 +0530

    fix: image bg file upload

--- apps/web/app/(app)/environments/[environmentId]/surveys/[surveyId]/edit/components/ImageSurveyBg.tsx
+++ apps/web/app/(app)/environments/[environmentId]/surveys/[surveyId]/edit/components/ImageSurveyBg.tsx
@@ -7,6 +7,19 @@ interface ImageSurveyBgBgProps {
 }
 
 export default function ImageSurveyBg({ localSurvey, handleBgChange }: ImageSurveyBgBgProps) {
+  const isUrl = (str: string) => {
+    try {
+      new URL(str);
+      return true;
+    } catch (error) {
+      return false;
+    }
+  };
+
+  const fileUrl = isUrl(localSurvey?.surveyBackground?.bg ?? "")
+    ? localSurvey?.surveyBackground?.bg ?? ""
+    : "";
+
   return (
     <div className="mb-2 mt-4 w-full rounded-lg border bg-slate-50 p-4">
       <div className="flex w-full items-center justify-center">
@@ -15,8 +28,13 @@ export default function ImageSurveyBg({ localSurvey, handleBgChange }: ImageSurv
           allowedFileExtensions={["png", "jpeg", "jpg"]}
           environmentId={localSurvey?.environmentId}
           onFileUpload={(url: string[]) => {
-            handleBgChange(url[0], "image");
+            if (url.length > 0) {
+              handleBgChange(url[0], "image");
+            } else {
+              handleBgChange("#ffff", "color");
+            }
           }}
+          fileUrl={fileUrl}
         />
       </div>
     </div>
