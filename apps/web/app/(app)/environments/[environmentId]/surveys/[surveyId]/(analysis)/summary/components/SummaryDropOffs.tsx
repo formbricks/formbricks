228c46402c6150585830f3991f25ccd4021592b6
Johannes <johannes@formbricks.com>
2023-11-15 10:54:09 +0100

    update UI

--- apps/web/app/(app)/environments/[environmentId]/surveys/[surveyId]/(analysis)/summary/components/SummaryDropOffs.tsx
+++ apps/web/app/(app)/environments/[environmentId]/surveys/[surveyId]/(analysis)/summary/components/SummaryDropOffs.tsx
@@ -1,9 +1,9 @@
 import { evaluateCondition } from "@/app/(app)/environments/[environmentId]/surveys/[surveyId]/(analysis)/summary/components/evaluateLogic";
 import { TResponse } from "@formbricks/types/responses";
 import { TSurvey } from "@formbricks/types/surveys";
-import { useMemo, useState, useEffect, useCallback } from "react";
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@formbricks/ui/Tooltip";
 import { TimerIcon } from "lucide-react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 
 interface SummaryDropOffsProps {
   survey: TSurvey;
@@ -150,40 +150,37 @@ export default function SummaryDropOffs({ responses, survey, displayCount }: Sum
   return (
     <div className="rounded-lg border border-slate-200 bg-slate-50 shadow-sm">
       <div className="rounded-b-lg bg-white ">
-        <div className="grid h-10 grid-cols-6 items-center border-y border-slate-200 bg-slate-100 text-sm font-bold text-slate-600">
+        <div className="grid h-10 grid-cols-6 items-center border-y border-slate-200 bg-slate-100 text-sm font-semibold text-slate-600">
           <div className="col-span-3 pl-4 md:pl-6">Questions</div>
-          <div className="pr-6 text-center md:pl-6">Drop-off</div>
-          <div className="flex justify-center px-4 text-center font-bold text-slate-600 md:px-6">
-            <TooltipProvider>
+          <div className="flex justify-center">
+            <TooltipProvider delayDuration={50}>
               <Tooltip>
                 <TooltipTrigger>
-                  <TimerIcon className="h-5 w-5 " />
+                  <TimerIcon className="h-5 w-5" />
                 </TooltipTrigger>
                 <TooltipContent side={"top"}>
-                  <p className="py-2 text-center text-xs text-slate-500 dark:text-slate-400">
-                    Average time to complete each question
-                  </p>
+                  <p className="text-center font-normal">Average time to complete each question.</p>
                 </TooltipContent>
               </Tooltip>
             </TooltipProvider>
           </div>
           <div className="px-4 text-center md:px-6">Views</div>
+          <div className="pr-6 text-center md:pl-6">Drop Offs</div>
         </div>
         {survey.questions.map((question, i) => (
           <div
             key={question.id}
             className="grid grid-cols-6 items-center border-b border-slate-100 py-2 text-sm text-slate-800 md:text-base">
             <div className="col-span-3 pl-4 md:pl-6">{question.headline}</div>
-            <div className=" pl-6 text-center md:px-6">
-              <span className="font-semibold">{dropoffMetrics.dropoffCount[i]} </span>
-              <span>({Math.round(dropoffMetrics.dropoffPercentage[i])}%)</span>
-            </div>
             <div className="whitespace-pre-wrap text-center font-semibold">
               {avgTtc[question.id] !== undefined ? (avgTtc[question.id] / 1000).toFixed(2) + "s" : "N/A"}
             </div>
-
             <div className="whitespace-pre-wrap text-center font-semibold">
               {dropoffMetrics.viewsCount[i]}
+            </div>
+            <div className=" pl-6 text-center md:px-6">
+              <span className="font-semibold">{dropoffMetrics.dropoffCount[i]} </span>
+              <span>({Math.round(dropoffMetrics.dropoffPercentage[i])}%)</span>
             </div>
           </div>
         ))}
