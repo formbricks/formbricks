e00f8d3ee59d8e120158c2b5033f535af21bdbba
Matthias Nannt <mail@matthiasnannt.com>
2023-11-16 22:41:10 +0100

    remove path from constants

--- packages/lib/constants.ts
+++ packages/lib/constants.ts
@@ -1,5 +1,4 @@
 import "server-only";
-import path from "path";
 import { env } from "./env.mjs";
 export const IS_FORMBRICKS_CLOUD = env.IS_FORMBRICKS_CLOUD === "1";
 export const REVALIDATION_INTERVAL = 0; //TODO: find a good way to cache and revalidate data when it changes
