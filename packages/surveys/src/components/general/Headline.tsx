822c48ff526f9051e51ca93f5ab69ebe57329c9e
Neil Chauhan <neilchauhan2@gmail.com>
2023-11-20 20:32:46 +0530

    fix: headline alignment issue (#1641)
    Co-authored-by: Matthias Nannt <mail@matthiasnannt.com>

--- packages/surveys/src/components/general/Headline.tsx
+++ packages/surveys/src/components/general/Headline.tsx
@@ -1,17 +1,20 @@
 interface HeadlineProps {
   headline?: string;
   questionId: string;
-  style?: any;
   required?: boolean;
+  alignTextCenter?: boolean;
 }
 
-export default function Headline({ headline, questionId, style, required = true }: HeadlineProps) {
+export default function Headline({
+  headline,
+  questionId,
+  required = true,
+  alignTextCenter = false,
+}: HeadlineProps) {
   return (
-    <label
-      htmlFor={questionId}
-      className="text-heading mb-1.5 block text-base font-semibold leading-6"
-      style={style}>
-      <div className={"flex items-center justify-center"} style={style}>
+    <label htmlFor={questionId} className="text-heading mb-1.5 block text-base font-semibold leading-6">
+      <div
+        className={`flex items-center  ${alignTextCenter ? "justify-center" : "mr-[3ch] justify-between"}`}>
         {headline}
         {!required && (
           <span className="text-info-text self-start	text-sm font-normal leading-7" tabIndex={-1}>
