6a0163191c91e0d0d9be7ef95c03fb454b59fa17
Neil Chauhan <neilchauhan2@gmail.com>
2023-11-20 12:33:37 +0530

    fix: thank you card headline issue

--- packages/surveys/src/components/general/Headline.tsx
+++ packages/surveys/src/components/general/Headline.tsx
@@ -11,7 +11,7 @@ export default function Headline({ headline, questionId, style, required = true 
       htmlFor={questionId}
       className="text-heading mb-1.5 block text-base font-semibold leading-6"
       style={style}>
-      <div className={"mr-[3ch] flex items-center justify-between"} style={style}>
+      <div className={"flex items-center justify-center"} style={style}>
         {headline}
         {!required && (
           <span className="text-info-text self-start	text-sm font-normal leading-7" tabIndex={-1}>
