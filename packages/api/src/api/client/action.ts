af181eabdc45db17402204ab6052d45448fc6b5e
Shubham Palriwala <spalriwalau@gmail.com>
2023-11-20 21:46:39 +0530

    feat: formbricks/api package as per js package 1.2.2 (#1640)
    Co-authored-by: Matthias Nannt <mail@matthiasnannt.com>

---
+++ packages/api/src/api/client/action.ts
@@ -0,0 +1,18 @@
+import { Result } from "@formbricks/types/errorHandlers";
+import { NetworkError } from "@formbricks/types/errors";
+import { TActionInput } from "@formbricks/types/actions";
+import { makeRequest } from "../../utils/makeRequest";
+
+export class ActionAPI {
+  private apiHost: string;
+  private environmentId: string;
+
+  constructor(apiHost: string, environmentId: string) {
+    this.apiHost = apiHost;
+    this.environmentId = environmentId;
+  }
+
+  async create(actionInput: Omit<TActionInput, "environmentId">): Promise<Result<{}, NetworkError | Error>> {
+    return makeRequest(this.apiHost, `/api/v1/client/${this.environmentId}/actions`, "POST", actionInput);
+  }
+}
