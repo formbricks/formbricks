5501122b9aa3af405adabfeec1e9f29918b71c6d
pandeymangg <anshuman.pandey9999@gmail.com>
2023-11-24 11:02:59 +0530

    fix: editor adding paragraphs at the top on every render

--- packages/ui/Editor/components/ToolbarPlugin.tsx
+++ packages/ui/Editor/components/ToolbarPlugin.tsx
@@ -202,6 +202,7 @@ function getSelectedNode(selection: RangeSelection) {
 
 export default function ToolbarPlugin(props: TextEditorProps) {
   const [editor] = useLexicalComposerContext();
+
   const toolbarRef = useRef(null);
   const [blockType, setBlockType] = useState("paragraph");
   const [isLink, setIsLink] = useState(false);
@@ -348,7 +349,8 @@ export default function ToolbarPlugin(props: TextEditorProps) {
         }
       });
     }
-  }, [props.updateTemplate]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [props.updateTemplate, props.firstRender]);
 
   useEffect(() => {
     if (props.setFirstRender) {
