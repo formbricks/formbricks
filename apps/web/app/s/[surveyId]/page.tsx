7467c8c72063e96041291320e4f9770be015d45b
Dhruv Gupta <108184993+dhruvgupta0503@users.noreply.github.com>
2023-11-28 19:50:46 +0530

    Update page.tsx

--- apps/web/app/s/[surveyId]/page.tsx
+++ apps/web/app/s/[surveyId]/page.tsx
@@ -53,6 +53,7 @@ export async function generateMetadata({ params }: LinkSurveyPageProps): Promise
   const ogImgURL = `/api/v1/og?brandColor=${brandColor}&name=${surveyName}`;
 
   return {
+    title:survey.name,
     metadataBase: new URL(WEBAPP_URL),
     openGraph: {
       title: survey.name,
