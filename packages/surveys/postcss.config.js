6635504434a2e6d4860f472a2f885d8ce54276da
pandeymangg <anshuman.pandey9999@gmail.com>
2023-11-28 17:21:03 +0530

    fix: rem to em plugin

--- packages/surveys/postcss.config.js
+++ packages/surveys/postcss.config.js
@@ -1,6 +1,17 @@
+const postcss = require("postcss");
+
+const remtoEm = postcss.plugin("rem-to-em", () => {
+  return (root) => {
+    root.walkDecls((decl) => {
+      if (decl.value.includes("rem")) {
+        decl.value = decl.value.replace(/([\d.]+)rem/g, (match, value) => {
+          return `${parseFloat(value) * 1}em`; // 1rem = 1em in this case
+        });
+      }
+    });
+  };
+});
+
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [require("tailwindcss"), require("autoprefixer"), remtoEm],
 };
