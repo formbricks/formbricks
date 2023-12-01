cba2a205ba0fee081fbe12e80f9e03a57a0b11cd
pandeymangg <anshuman.pandey9999@gmail.com>
2023-12-01 22:15:55 +0530

    fix: adds error message and loading state

--- packages/surveys/src/components/questions/DateQuestion.tsx
+++ packages/surveys/src/components/questions/DateQuestion.tsx
@@ -3,6 +3,7 @@ import SubmitButton from "@/components/buttons/SubmitButton";
 import Headline from "@/components/general/Headline";
 import Subheader from "@/components/general/Subheader";
 import { getUpdatedTtc, useTtc } from "@/lib/ttc";
+import { cn } from "@/lib/utils";
 import { TResponseData, TResponseTtc } from "@formbricks/types/responses";
 import type { TSurveyDateQuestion } from "@formbricks/types/surveys";
 import { useEffect, useState } from "preact/hooks";
@@ -32,16 +33,15 @@ export default function DateQuestion({
   ttc,
 }: DateQuestionProps) {
   const [startTime, setStartTime] = useState(performance.now());
+  const [errorMessage, setErrorMessage] = useState("");
+  const [loading, setLoading] = useState(true);
 
   useTtc(question.id, ttc, setTtc, startTime, setStartTime);
 
-  const [datePickerOpen, _setDatePickerOpen] = useState(true);
   const defaultDate = value ? new Date(value as string) : undefined;
 
   useEffect(() => {
     // Check if the DatePicker has already been loaded
-
-    if (!datePickerOpen) return;
 
     if (!window.initDatePicker) {
       const script = document.createElement("script");
@@ -59,6 +59,7 @@ export default function DateQuestion({
         // Initialize the DatePicker once the script is loaded
         // @ts-expect-error
         window.initDatePicker(document.getElementById("date-picker-root"), defaultDate, question.format);
+        setLoading(false);
       };
 
       return () => {
@@ -66,6 +67,7 @@ export default function DateQuestion({
       };
     } else {
       // If already loaded, remove the date picker and re-initialize it
+      setLoading(false);
 
       const datePickerContainer = document.getElementById("datePickerContainer");
       if (datePickerContainer) {
@@ -79,24 +81,31 @@ export default function DateQuestion({
     return () => {};
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [datePickerOpen, question.format]);
+  }, [question.format, question.id]);
 
   useEffect(() => {
     window.addEventListener("dateChange", (e) => {
       // @ts-expect-error
       const date = e.detail as Date;
+
       onChange({ [question.id]: date.toDateString() });
     });
+  }, [onChange, question.id]);
 
+  useEffect(() => {
+    if (value && errorMessage) {
+      setErrorMessage("");
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [value]);
 
   return (
     <form
       onSubmit={(e) => {
         e.preventDefault();
         if (question.required && !value) {
-          alert("Please select a date");
+          // alert("Please select a date");
+          setErrorMessage("Please select a date");
           return;
         }
         const updatedTtcObj = getUpdatedTtc(ttc, question.id, performance.now() - startTime);
@@ -107,7 +116,19 @@ export default function DateQuestion({
       <Headline headline={question.headline} questionId={question.id} required={question.required} />
       <Subheader subheader={question.subheader} questionId={question.id} />
 
-      <div className="my-4" id="date-picker-root"></div>
+      <div className={"text-red-500"}>
+        <span>{errorMessage}</span>
+      </div>
+
+      <div className={cn("my-4", errorMessage && "rounded-lg border-2 border-red-500")} id="date-picker-root">
+        {loading && (
+          <div className="relative flex h-12 w-full cursor-pointer appearance-none items-center justify-center rounded-lg border border-slate-300 bg-white text-left text-base font-normal text-slate-900 hover:bg-slate-50 focus:outline-none focus:ring-2 focus:ring-neutral-900 focus:ring-offset-1">
+            <span
+              className="h-6 w-6 animate-spin rounded-full border-b-2 border-neutral-900"
+              style={{ borderTopColor: "transparent" }}></span>
+          </div>
+        )}
+      </div>
 
       <div className="mt-4 flex w-full justify-between">
         <div>
