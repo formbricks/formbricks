6aa155e9de41ffe5bf84fa2aab1bcf5e2dbbf068
Henit Chobisa <chobisa.henit@gmail.com>
2023-12-01 15:28:27 +0530

    feat: implemented slask connection modals

--- apps/web/app/(app)/environments/[environmentId]/integrations/slack/SlackWrapper.tsx
+++ apps/web/app/(app)/environments/[environmentId]/integrations/slack/SlackWrapper.tsx
@@ -1,26 +1,28 @@
 "use client";
 
+import AddSlackConnectionModal from "@/app/(app)/environments/[environmentId]/integrations/slack/AddIntegrationModal";
 import Connect from "@/app/(app)/environments/[environmentId]/integrations/slack/Connect";
 import Home from "@/app/(app)/environments/[environmentId]/integrations/slack/Home";
 import { TEnvironment } from "@formbricks/types/v1/environment";
-import { TSlackConfigData, TSlackIntegration } from "@formbricks/types/v1/integrations";
+import { TSlackChannel, TSlackConfigData, TSlackIntegration } from "@formbricks/types/v1/integrations";
 import { TSurvey } from "@formbricks/types/v1/surveys";
 import { useState } from "react";
-
 interface SlackWrapperProps {
   enabled: boolean;
   environment: TEnvironment;
   surveys: TSurvey[];
+  channels: TSlackChannel[];
   environmentId: string;
   slackIntegration: TSlackIntegration | undefined;
   webAppUrl: string;
 }
 
-export default async function SlackWrapper({
+export default function SlackWrapper({
   enabled,
   environment,
   environmentId,
   surveys,
+  channels,
   slackIntegration,
   webAppUrl,
 }: SlackWrapperProps) {
@@ -35,14 +37,25 @@ export default async function SlackWrapper({
   };
 
   return isConnected && slackIntegration ? (
-    <Home
-      environment={environment}
-      slackIntegration={slackIntegration}
-      setOpenAddIntegrationModal={setModalOpen}
-      setIsConnected={setIsConnected}
-      setSelectedIntegration={setSelectedIntegration}
-      refreshSheet={refreshSheet}
-    />
+    <>
+      <AddSlackConnectionModal
+        environmentId={environment.id}
+        surveys={surveys}
+        open={isModalOpen}
+        setOpen={setModalOpen}
+        channels={channels}
+        slackIntegration={slackIntegration}
+        selectedIntegration={selectedIntegration}
+      />
+      <Home
+        environment={environment}
+        slackIntegration={slackIntegration}
+        setOpenAddIntegrationModal={setModalOpen}
+        setIsConnected={setIsConnected}
+        setSelectedIntegration={setSelectedIntegration}
+        refreshSheet={refreshSheet}
+      />
+    </>
   ) : (
     <Connect enabled={enabled} environmentId={environmentId} webAppUrl={webAppUrl} />
   );
