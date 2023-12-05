4fa1e773cd999ad69470e7a4d18ecd6d3df152d8
Matthias Nannt <mail@matthiasnannt.com>
2023-12-05 14:52:02 +0100

    add emailVerified to formatProfileDateFields

--- packages/lib/profile/util.ts
+++ packages/lib/profile/util.ts
@@ -7,6 +7,9 @@ export const formatProfileDateFields = (profile: TProfile): TProfile => {
   if (typeof profile.updatedAt === "string") {
     profile.updatedAt = new Date(profile.updatedAt);
   }
+  if (typeof profile.emailVerified === "string") {
+    profile.emailVerified = new Date(profile.emailVerified);
+  }
 
   return profile;
 };
