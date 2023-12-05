85f5425d892d8ca74abc50db83f78b326e08aa71
Matti Nannt <mail@matthiasnannt.com>
2023-12-05 14:56:32 +0100

    fix: onboarding ending in endless loop because of validation error (#1745)

--- packages/lib/response/util.ts
+++ packages/lib/response/util.ts
@@ -1,8 +1,8 @@
 import "server-only";
 
-import { TResponseDates, TResponseTtc } from "@formbricks/types/responses";
+import { TResponse, TResponseTtc } from "@formbricks/types/responses";
 
-export const formatResponseDateFields = (response: TResponseDates): TResponseDates => {
+export const formatResponseDateFields = (response: TResponse): TResponse => {
   if (typeof response.createdAt === "string") {
     response.createdAt = new Date(response.createdAt);
   }
