22fb5aec663f58bd8bc28e77e9b5eb392235190f
ShubhamPalriwala <spalriwalau@gmail.com>
2023-12-06 23:04:29 +0530

    fix: run web only

--- playwright.config.ts
+++ playwright.config.ts
@@ -68,7 +68,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "pnpm go",
+    command: "pnpm go --filter=web",
     url: "http://127.0.0.1:3000",
     reuseExistingServer: !process.env.PLAYWRIGHT_CI,
     // reuseExistingServer: true,
