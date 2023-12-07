2ed13b85153c059634b9b00d498a65f4f3bdf71e
ShubhamPalriwala <spalriwalau@gmail.com>
2023-12-07 16:17:17 +0530

    fix: get rid of new env

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,9 +14,9 @@ export default defineConfig({
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Retry on CI only */
-  retries: process.env.PLAYWRIGHT_CI ? 2 : 0,
+  retries: 2,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.PLAYWRIGHT_CI ? 1 : undefined,
+  workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
