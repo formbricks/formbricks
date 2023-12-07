39995c7b4a05d196c991689064a19ae0dead390e
ShubhamPalriwala <spalriwalau@gmail.com>
2023-12-07 14:25:08 +0530

    feat: lessgggooo

--- playwright.config.ts
+++ playwright.config.ts
@@ -65,6 +65,4 @@ export default defineConfig({
     //   use: { ...devices["Desktop Chrome"], channel: "chrome" },
     // },
   ],
-
-  /* Run your local dev server before starting the tests */
 });
