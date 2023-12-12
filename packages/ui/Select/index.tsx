3e2fed74143dd17a8ab87759eaa18c68150ab9ae
karishmas6 <carishmashukla28@gmail.com>
2023-12-12 13:24:00 +0530

    chore: remove commented import & code

--- packages/ui/Select/index.tsx
+++ packages/ui/Select/index.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react";
 import * as SelectPrimitive from "@radix-ui/react-select";
-import { /* Check, */ ChevronDown } from "lucide-react";
+import { ChevronDown } from "lucide-react";
 
 import { cn } from "@formbricks/lib/cn";
 
@@ -70,12 +70,6 @@ const SelectItem: React.ComponentType<SelectPrimitive.SelectItemProps> = React.f
       className
     )}
     {...props}>
-    {/*     <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
-      <SelectPrimitive.ItemIndicator>
-        <Check className="h-4 w-4" />
-      </SelectPrimitive.ItemIndicator>
-    </span> */}
-
     <SelectPrimitive.ItemText>{children}</SelectPrimitive.ItemText>
   </SelectPrimitive.Item>
 ));
