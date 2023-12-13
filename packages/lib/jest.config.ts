606d71d85ec3eb0a72c1dcaadf487df9e4622ead
pandeymangg <anshuman.pandey9999@gmail.com>
2023-12-13 13:38:49 +0530

    cleanup to make the tests run using turbo

--- packages/lib/jest.config.ts
+++ packages/lib/jest.config.ts
@@ -2,21 +2,21 @@ import { Config } from "jest";
 
 const config: Config = {
   preset: "ts-jest",
-  // collectCoverage: true,
+  collectCoverage: true,
   // on node 14.x coverage provider v8 offers good speed and more or less good report
-  // coverageProvider: "v8",
+  coverageProvider: "v8",
   testMatch: ["**/*.test.ts"],
-  // collectCoverageFrom: [
-  //   "**/*.{js,jsx,ts,tsx}",
-  //   "!**/*.d.ts",
-  //   "!**/node_modules/**",
-  //   "!<rootDir>/out/**",
-  //   "!<rootDir>/.next/**",
-  //   "!<rootDir>/*.config.js",
-  //   "!<rootDir>/*.config.ts",
-  //   "!<rootDir>/coverage/**",
-  //   "!<rootDir>/jest/**",
-  // ],
+  collectCoverageFrom: [
+    "**/*.{js,jsx,ts,tsx}",
+    "!**/*.d.ts",
+    "!**/node_modules/**",
+    "!<rootDir>/out/**",
+    "!<rootDir>/.next/**",
+    "!<rootDir>/*.config.js",
+    "!<rootDir>/*.config.ts",
+    "!<rootDir>/coverage/**",
+    "!<rootDir>/jest/**",
+  ],
   testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/"],
   setupFilesAfterEnv: ["<rootDir>/jest/jestSetup.ts", "<rootDir>/../database/src/jestClient.ts"],
   transform: {
