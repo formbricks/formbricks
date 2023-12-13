4a0678febb693de40048ed938887df2c87bd3837
ShubhamPalriwala <spalriwalau@gmail.com>
2023-12-13 11:16:34 +0530

    fix: drop off rate logic

--- apps/web/app/(app)/environments/[environmentId]/surveys/[surveyId]/(analysis)/summary/components/SummaryDropOffs.tsx
+++ apps/web/app/(app)/environments/[environmentId]/surveys/[surveyId]/(analysis)/summary/components/SummaryDropOffs.tsx
@@ -126,10 +126,16 @@ export default function SummaryDropOffs({ responses, survey, displayCount }: Sum
     });
 
     // Calculate drop-off percentages
-    dropoffPercentageArr[0] = (dropoffArr[0] / displayCount) * 100 || 0;
+    dropoffArr[0] = displayCount - viewsArr[0];
+    if (viewsArr[0] > displayCount) dropoffPercentageArr[0] = 0;
+
+    dropoffPercentageArr[0] =
+      viewsArr[0] - displayCount >= 0 ? 0 : ((displayCount - viewsArr[0]) / displayCount) * 100 || 0;
+
+    // dropoffPercentageArr[0] = viewsArr[0] = displayCount;
     for (let i = 1; i < survey.questions.length; i++) {
-      if (viewsArr[i - 1] !== 0) {
-        dropoffPercentageArr[i] = (dropoffArr[i] / viewsArr[i - 1]) * 100;
+      if (viewsArr[i] !== 0) {
+        dropoffPercentageArr[i] = (dropoffArr[i] / viewsArr[i]) * 100;
       }
     }
 
