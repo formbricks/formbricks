81a9717aacdf57b06267ae1a387a5783343dcfe4
neo773 <62795688+neo773@users.noreply.github.com>
2023-12-20 06:54:51 +0530

    Revert Question.tsx

--- packages/surveys/src/sideload/question-date/Question.tsx
+++ packages/surveys/src/sideload/question-date/Question.tsx
@@ -61,6 +61,7 @@ export default function Question({ defaultDate, format }: { defaultDate?: Date; 
           </div>
         </div>
       )}
+
       {/* @ts-expect-error */}
       <DatePicker
         key={datePickerOpen}
