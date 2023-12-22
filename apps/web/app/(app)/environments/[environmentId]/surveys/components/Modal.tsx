c25b9a0b86aba737ba9e1d55cc16787a95412707
Dhruwang <dhruwangjariwala18@gmail.com>
2023-12-22 18:21:22 +0530

    fix: scaling issues

--- apps/web/app/(app)/environments/[environmentId]/surveys/components/Modal.tsx
+++ apps/web/app/(app)/environments/[environmentId]/surveys/components/Modal.tsx
@@ -19,16 +19,44 @@ export default function Modal({
 }) {
   const [show, setShow] = useState(false);
   const modalRef = useRef<HTMLDivElement | null>(null);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const calculateScaling = () => {
+    const scaleValue = (() => {
+      if (windowWidth > 1600) return "1";
+      else if (windowWidth > 1200) return ".9";
+      else if (windowWidth > 900) return ".8";
+      return "0.7";
+    })();
+
+    const placements = (() => {
+      if (placement === "bottomLeft") return "bottom left";
+      else if (placement === "bottomRight") return "bottom right";
+      else if (placement === "topLeft") return "top left";
+      else if (placement === "topRight") return "top right";
+    })();
+
+    return {
+      transform: `scale(${scaleValue})`,
+      "transform-origin": placements,
+    };
+  };
+  const scalingClasses = calculateScaling();
+
+  useEffect(() => {
+    const handleResize = () => setWindowWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   const highlightBorderColorStyle = useMemo(() => {
     if (!highlightBorderColor)
       return {
-        overflow: "visible",
+        overflow: "auto",
       };
 
     return {
       border: `2px solid ${highlightBorderColor}`,
-      overflow: "visible",
+      overflow: "auto",
     };
   }, [highlightBorderColor]);
 
@@ -58,7 +86,7 @@ export default function Modal({
     <div aria-live="assertive" className="relative h-full w-full overflow-visible bg-slate-300">
       <div
         ref={modalRef}
-        style={highlightBorderColorStyle}
+        style={{ ...highlightBorderColorStyle, ...scalingClasses }}
         className={cn(
           "no-scrollbar pointer-events-auto absolute h-fit max-h-[90%] w-full max-w-sm overflow-y-auto rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition-all duration-500 ease-in-out ",
           previewMode === "desktop" ? getPlacementStyle(placement) : "max-w-full",
