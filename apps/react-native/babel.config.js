9490af7caf90f52e6b51cd7ed997a455f419df96
neo773 <62795688+neo773@users.noreply.github.com>
2023-12-22 21:44:20 +0530

    fix: demo react native app

--- apps/react-native/babel.config.js
+++ apps/react-native/babel.config.js
@@ -1,6 +1,3 @@
-// eslint-disable-next-line turbo/no-undeclared-env-vars
-process.env.EXPO_ROUTER_APP_ROOT = '../../apps/react-native/src/'
-
 module.exports = function (api) {
   api.cache(true);
   return {
