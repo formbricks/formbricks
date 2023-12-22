82f916d86bb3d74191397793a84860edd6e5284f
Johannes <72809645+jobenjada@users.noreply.github.com>
2023-12-22 15:39:16 -0600

    fix: PIN survey caused faulty layout due to background (#1826)

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="flex h-screen flex-col bg-slate-50">{children}</body>
+      <body className="flex h-screen flex-col">{children}</body>
     </html>
   );
 }
