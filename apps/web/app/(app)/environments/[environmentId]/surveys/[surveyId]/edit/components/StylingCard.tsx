6ef70793d6fe8c6367b6f5f92fe3d41c4265d17b
Johannes <johannes@formbricks.com>
2024-01-10 16:40:15 -0600

    streamlined settings tab

--- apps/web/app/(app)/environments/[environmentId]/surveys/[surveyId]/edit/components/StylingCard.tsx
+++ apps/web/app/(app)/environments/[environmentId]/surveys/[surveyId]/edit/components/StylingCard.tsx
@@ -2,8 +2,10 @@
 
 import { CheckCircleIcon } from "@heroicons/react/24/solid";
 import * as Collapsible from "@radix-ui/react-collapsible";
+import Link from "next/link";
 import { useState } from "react";
 
+import { cn } from "@formbricks/lib/cn";
 import { TPlacement } from "@formbricks/types/common";
 import { TSurvey, TSurveyBackgroundBgType } from "@formbricks/types/surveys";
 import { ColorPicker } from "@formbricks/ui/ColorPicker";
@@ -17,10 +19,16 @@ interface StylingCardProps {
   localSurvey: TSurvey;
   setLocalSurvey: React.Dispatch<React.SetStateAction<TSurvey>>;
   colours: string[];
+  environmentId: string;
 }
 
-export default function StylingCard({ localSurvey, setLocalSurvey, colours }: StylingCardProps) {
-  const [open, setOpen] = useState(false);
+export default function StylingCard({
+  localSurvey,
+  setLocalSurvey,
+  colours,
+  environmentId,
+}: StylingCardProps) {
+  const [open, setOpen] = useState(localSurvey.type === "link" ? true : false);
 
   const { type, productOverwrites, styling } = localSurvey;
   const { brandColor, clickOutsideClose, darkOverlay, placement, highlightBorderColor } =
@@ -179,7 +187,10 @@ export default function StylingCard({ localSurvey, setLocalSurvey, colours }: St
     <Collapsible.Root
       open={open}
       onOpenChange={setOpen}
-      className="w-full rounded-lg border border-slate-300 bg-white">
+      className={cn(
+        open ? "" : "hover:bg-slate-50",
+        "w-full space-y-2 rounded-lg border border-slate-300 bg-white "
+      )}>
       <Collapsible.CollapsibleTrigger asChild className="h-full w-full cursor-pointer">
         <div className="inline-flex px-4 py-4">
           <div className="flex items-center pl-2 pr-5">
@@ -194,6 +205,51 @@ export default function StylingCard({ localSurvey, setLocalSurvey, colours }: St
       <Collapsible.CollapsibleContent>
         <hr className="py-1 text-slate-600" />
         <div className="p-3">
+          {type == "link" && (
+            <>
+              <>
+                {/* Background */}
+                <div className="p-3">
+                  <div className="ml-2">
+                    <h3 className="text-sm font-semibold text-slate-700">Change Background</h3>
+                    <p className="text-xs font-normal text-slate-500">
+                      Pick a background from our library or upload your own.
+                    </p>
+                  </div>
+                  <SurveyBgSelectorTab
+                    localSurvey={localSurvey}
+                    handleBgChange={handleBgChange}
+                    colours={colours}
+                    bgType={bgType}
+                  />
+                </div>
+
+                {/* Overlay */}
+                <div className="my-3 p-3">
+                  <div className="ml-2">
+                    <h3 className="text-sm font-semibold text-slate-700">Background Overlay</h3>
+                    <p className="text-xs font-normal text-slate-500">
+                      Darken or lighten background of your choice.
+                    </p>
+                  </div>
+                  <div>
+                    <div className="mt-4 flex flex-col justify-center rounded-lg border bg-slate-50 p-6">
+                      <h3 className="mb-4 text-sm font-semibold text-slate-700">Brightness</h3>
+                      <input
+                        id="small-range"
+                        type="range"
+                        min="1"
+                        max="200"
+                        value={inputValue}
+                        onChange={handleInputChange}
+                        className="range-sm mb-6 h-1 w-full cursor-pointer appearance-none rounded-lg bg-gray-200 dark:bg-gray-700"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </>
+            </>
+          )}
           {/* Brand Color */}
           <div className="p-3">
             <div className="ml-2 flex items-center space-x-1">
@@ -214,67 +270,7 @@ export default function StylingCard({ localSurvey, setLocalSurvey, colours }: St
               </div>
             )}
           </div>
-          {type == "link" && (
-            <>
-              {/* Background */}
-              <div className="p-3">
-                <div className="ml-2 flex items-center space-x-1">
-                  <Switch id="autoCompleteBg" checked={!!bg} onCheckedChange={toggleBackgroundColor} />
-                  <Label htmlFor="autoCompleteBg" className="cursor-pointer">
-                    <div className="ml-2">
-                      <h3 className="text-sm font-semibold text-slate-700">Change Background</h3>
-                      <p className="text-xs font-normal text-slate-500">
-                        Pick a background from our library or upload your own.
-                      </p>
-                    </div>
-                  </Label>
-                </div>
-                {bg && (
-                  <SurveyBgSelectorTab
-                    localSurvey={localSurvey}
-                    handleBgChange={handleBgChange}
-                    colours={colours}
-                    bgType={bgType}
-                  />
-                )}
-              </div>
-              {/* Overlay */}
-              <div className="p-3">
-                <div className="ml-2 flex items-center space-x-1">
-                  <Switch
-                    id="autoCompleteOverlay"
-                    checked={!!brightness}
-                    onCheckedChange={toggleBrightness}
-                  />
-                  <Label htmlFor="autoCompleteOverlay" className="cursor-pointer">
-                    <div className="ml-2">
-                      <h3 className="text-sm font-semibold text-slate-700">Background Overlay</h3>
-                      <p className="text-xs font-normal text-slate-500">
-                        Darken or lighten background of your choice.
-                      </p>
-                    </div>
-                  </Label>
-                </div>
-                {brightness && (
-                  <div>
-                    <div className="mt-4 flex flex-col justify-center rounded-lg border bg-slate-50 p-4 px-8">
-                      <h3 className="mb-4 text-sm font-semibold text-slate-700">Transparency</h3>
-                      <input
-                        id="small-range"
-                        type="range"
-                        min="1"
-                        max="200"
-                        value={inputValue}
-                        onChange={handleInputChange}
-                        className="range-sm mb-6 h-1 w-full cursor-pointer appearance-none rounded-lg bg-gray-200 dark:bg-gray-700"
-                      />
-                    </div>
-                  </div>
-                )}
-              </div>
-            </>
-          )}
-          {/* positioning */}
+          {/* Positioning */}
           {type !== "link" && (
             <div className="p-3 ">
               <div className="ml-2 flex items-center space-x-1">
@@ -342,6 +338,17 @@ export default function StylingCard({ localSurvey, setLocalSurvey, colours }: St
               )}
             </div>
           )}
+          <div className="mt-2 flex items-center space-x-3 rounded-lg px-4 py-2 text-slate-500">
+            <p className="text-xs">
+              To keep the styling over all surveys consistent, you can{" "}
+              <Link
+                href={`/environments/${environmentId}/settings/lookandfeel`}
+                className="underline hover:text-slate-900"
+                target="_blank">
+                set global styles in the Look & Feel settings.
+              </Link>{" "}
+            </p>
+          </div>
         </div>
       </Collapsible.CollapsibleContent>
     </Collapsible.Root>
