b65c9661c25e6db43e376980fa5ea5cbd1008dbd
pandeymangg <anshuman.pandey9999@gmail.com>
2024-01-12 10:43:34 +0530

    fix: fixes edge case of wrong older file names

--- packages/lib/storage/utils.ts
+++ packages/lib/storage/utils.ts
@@ -2,7 +2,13 @@ export const getOriginalFileNameFromUrl = (fileURL: string) => {
   const fileNameFromURL = new URL(fileURL).pathname.split("/").pop();
   const fileExt = fileNameFromURL?.split(".").pop();
   const originalFileName = fileNameFromURL?.split("--fid--")[0];
-  const fileName = originalFileName ? decodeURIComponent(`${originalFileName}.${fileExt}` || "") : "";
+  const fileId = fileNameFromURL?.split("--fid--")[1];
 
+  if (!fileId) {
+    const fileName = originalFileName ? decodeURIComponent(originalFileName || "") : "";
+    return fileName;
+  }
+
+  const fileName = originalFileName ? decodeURIComponent(`${originalFileName}.${fileExt}` || "") : "";
   return fileName;
 };
