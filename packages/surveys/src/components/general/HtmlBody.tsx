e86b1c68b198f09c932b7d39a32d3c995940b00e
Dhruwang <dhruwangjariwala18@gmail.com>
2024-01-12 10:48:23 +0530

    chore: added DOMPurify to avoid xss

--- packages/surveys/src/components/general/HtmlBody.tsx
+++ packages/surveys/src/components/general/HtmlBody.tsx
@@ -1,11 +1,27 @@
-import { cleanHtml } from "@/lib/cleanHtml";
+import { useEffect, useState } from "react";
 
-export default function HtmlBody({ htmlString, questionId }: { htmlString?: string; questionId: string }) {
+interface HtmlBodyProps {
+  htmlString: string | undefined;
+  questionId: string;
+}
+
+export default function HtmlBody({ htmlString, questionId }: HtmlBodyProps) {
+  const [safeHtml, setSafeHtml] = useState("");
+
+  useEffect(() => {
+    if (htmlString) {
+      import("dompurify").then((DOMPurify) => {
+        setSafeHtml(DOMPurify.sanitize(htmlString));
+      });
+    }
+  }, [htmlString]);
+
   if (!htmlString) return null;
+
   return (
     <label
       htmlFor={questionId}
       className="fb-htmlbody" // styles are in global.css
-      dangerouslySetInnerHTML={{ __html: cleanHtml(htmlString) }}></label>
+      dangerouslySetInnerHTML={{ __html: safeHtml }}></label>
   );
 }
