c89d03022295f254b2ca3dd34211df4edf513d06
Dhruwang Jariwala <67850763+Dhruwang@users.noreply.github.com>
2024-01-22 14:41:06 +0530

    feat: report survey link (#1932)

--- apps/web/app/s/[surveyId]/components/LegalFooter.tsx
+++ apps/web/app/s/[surveyId]/components/LegalFooter.tsx
@@ -4,10 +4,26 @@ interface LegalFooterProps {
   bgColor?: string | null;
   IMPRINT_URL?: string;
   PRIVACY_URL?: string;
+  IS_FORMBRICKS_CLOUD: boolean;
+  surveyUrl: string;
 }
 
-export default function LegalFooter({ bgColor, IMPRINT_URL, PRIVACY_URL }: LegalFooterProps) {
-  if (!IMPRINT_URL && !PRIVACY_URL) return null;
+export default function LegalFooter({
+  bgColor,
+  IMPRINT_URL,
+  PRIVACY_URL,
+  IS_FORMBRICKS_CLOUD,
+  surveyUrl,
+}: LegalFooterProps) {
+  if (!IMPRINT_URL && !PRIVACY_URL && !IS_FORMBRICKS_CLOUD) return null;
+
+  const createMailToLink = (surveyLink) => {
+    const subject = encodeURIComponent("Reporting this survey");
+    const body = encodeURIComponent(
+      `I report the survey to the Formbricks team as it is spam, abusive or violates the terms.\n\n${surveyLink}`
+    );
+    return `mailto:hola@formbricks.com?subject=${subject}&body=${body}`;
+  };
 
   return (
     <div
@@ -25,6 +41,12 @@ export default function LegalFooter({ bgColor, IMPRINT_URL, PRIVACY_URL }: Legal
         {PRIVACY_URL && (
           <Link href={PRIVACY_URL} target="_blank" className="hover:underline">
             Privacy Policy
+          </Link>
+        )}
+        {PRIVACY_URL && IS_FORMBRICKS_CLOUD && <span className="px-2">|</span>}
+        {IS_FORMBRICKS_CLOUD && (
+          <Link href={createMailToLink(surveyUrl)} target="_blank" className="hover:underline">
+            Report Survey
           </Link>
         )}
       </div>
