9519e78c686ac85c4226126b45c608e2001c7aa8
Dhruwang <dhruwangjariwala18@gmail.com>
2024-02-01 14:31:13 +0530

    toast for response limit error

--- apps/web/app/(app)/environments/[environmentId]/surveys/[surveyId]/edit/components/SurveyMenuBar.tsx
+++ apps/web/app/(app)/environments/[environmentId]/surveys/[surveyId]/edit/components/SurveyMenuBar.tsx
@@ -228,6 +228,7 @@ export default function SurveyMenuBar({
      than the current count of accepted response and also it is not set to 0
     */
     if ((survey.autoComplete && responseCount >= survey.autoComplete) || survey?.autoComplete === 0) {
+      toast.error(`Response limit needs to exceed number of received responses (${responseCount}).`);
       return false;
     }
 
