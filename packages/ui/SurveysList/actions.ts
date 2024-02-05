da715bceab454b9dbcd4e23f4fe6da712ae61b47
Matthias Nannt <mail@matthiasnannt.com>
2024-02-05 10:48:42 +0100

    chore: prepare 1.5.1 release

--- packages/ui/SurveysList/actions.ts
+++ packages/ui/SurveysList/actions.ts
@@ -1,8 +1,9 @@
 "use server";
 
-import { Prisma as prismaClient } from "@prisma/client/";
+import { Prisma as prismaClient } from "@prisma/client";
 import { getServerSession } from "next-auth";
 
+import { prisma } from "@formbricks/database";
 import { authOptions } from "@formbricks/lib/authOptions";
 import { hasUserEnvironmentAccess } from "@formbricks/lib/environment/auth";
 import { canUserAccessSurvey, verifyUserRoleAccess } from "@formbricks/lib/survey/auth";
@@ -10,8 +11,6 @@ import { surveyCache } from "@formbricks/lib/survey/cache";
 import { deleteSurvey, duplicateSurvey, getSurvey } from "@formbricks/lib/survey/service";
 import { generateSurveySingleUseId } from "@formbricks/lib/utils/singleUseSurveys";
 import { AuthorizationError, ResourceNotFoundError } from "@formbricks/types/errors";
-
-import { prisma } from "../../database/src";
 
 export async function duplicateSurveyAction(environmentId: string, surveyId: string) {
   const session = await getServerSession(authOptions);
