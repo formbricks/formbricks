88c4d0214c91c606f609e4f4ce980815ef61ed42
jonas.hoebenreich <jonas.hoebenreich@flixbus.com>
2024-02-05 14:05:52 +0100

    add dynamic pixel server

---
+++ apps/web/app/api/js/route.tsx
@@ -0,0 +1,60 @@
+import { responses } from "@/app/lib/api/response";
+import fs from "fs/promises";
+import { NextRequest, NextResponse } from "next/server";
+
+import { WEBAPP_URL } from "@formbricks/lib/constants";
+import { getEnvironment } from "@formbricks/lib/environment/service";
+
+export async function GET(req: NextRequest) {
+  let path: string;
+  let append = "";
+  switch (req.nextUrl.searchParams.get("module")) {
+    case "widget":
+      path = `../../packages/surveys/dist/index.umd.js`;
+      break;
+    case "question-date":
+      path = `../../packages/surveys/dist/question-date.umd.js`;
+      break;
+    case null:
+      const format = ["umd", "iife"].includes(req.nextUrl.searchParams.get("format")!)
+        ? req.nextUrl.searchParams.get("format")!
+        : "umd";
+      path = `../../packages/js/dist/index.${format}.js`;
+      append = await handleEnvironment(req);
+      break;
+    default:
+      return responses.badRequestResponse("unknown module requested");
+  }
+
+  try {
+    const jsCode = await loadAndAppendCode(path, append);
+
+    return new NextResponse(jsCode, {
+      headers: {
+        "Content-Type": "application/javascript",
+        "Cache-Control": "public, s-maxage=600, max-age=1800, stale-while-revalidate=600, stale-if-error=600",
+      },
+    });
+  } catch (error) {
+    return responses.internalServerErrorResponse("file not found");
+  }
+}
+
+async function handleEnvironment(req: NextRequest) {
+  const environmentId = req.nextUrl.searchParams.get("environmentId");
+
+  if (environmentId) {
+    const environment = await getEnvironment(environmentId);
+
+    if (environment) {
+      return `formbricks.init({environmentId: "${environmentId}", apiHost: "${WEBAPP_URL}"});`;
+    }
+  }
+  return "";
+}
+
+async function loadAndAppendCode(path: string, append: string): Promise<string> {
+  let jsCode = await fs.readFile(path, "utf-8");
+
+  return jsCode + append;
+}
