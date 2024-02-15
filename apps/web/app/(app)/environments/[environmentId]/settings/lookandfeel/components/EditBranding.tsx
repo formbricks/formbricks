00f00a4978a2e632700b2aa949aa2bf6fc700e9c
Johannes <johannes@formbricks.com>
2024-02-15 11:29:36 +0100

    allow remove branding of in app surveys

--- apps/web/app/(app)/environments/[environmentId]/settings/lookandfeel/components/EditBranding.tsx
+++ apps/web/app/(app)/environments/[environmentId]/settings/lookandfeel/components/EditBranding.tsx
@@ -71,13 +71,6 @@ export function EditFormbricksBranding({
               />
             </div>
           )}
-          {type !== "linkSurvey" && !isFormbricksCloud && (
-            <UpgradePlanNotice
-              message="To remove the Formbricks branding from In-App Surveys, please"
-              textForUrl="request an Enterprise license."
-              url="https://formbricks.com/docs/self-hosting/enterprise"
-            />
-          )}
           {type !== "linkSurvey" && isFormbricksCloud && (
             <UpgradePlanNotice
               message="To remove the Formbricks branding from In-app Surveys, please"
