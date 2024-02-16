9263f5b9f1473f4240a6ecb511bcf8dd414b28ff
ShubhamPalriwala <spalriwalau@gmail.com>
2024-02-16 16:09:16 +0530

    feat: rate limit per user per env sync user identification endpoint

--- packages/lib/constants.ts
+++ packages/lib/constants.ts
@@ -135,6 +135,11 @@ export const SHARE_RATE_LIMIT = {
   allowedPerInterval: 30,
 };
 
+export const SYNC_USER_IDENTIFICATION_RATE_LIMIT = {
+  interval: 60 * 1000, // 1 minute
+  allowedPerInterval: 5,
+};
+
 export const DEBUG = process.env.DEBUG === "1";
 
 // Enterprise License constant
