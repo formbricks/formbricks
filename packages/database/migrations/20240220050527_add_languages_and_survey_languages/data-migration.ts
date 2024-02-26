6f2244fcfdc754d78800a4a82f5fe0bd1b256f51
Dhruwang <dhruwangjariwala18@gmail.com>
2024-02-26 09:14:02 +0530

    migration script for attributeClass

--- packages/database/migrations/20240220050527_add_languages_and_survey_languages/data-migration.ts
+++ packages/database/migrations/20240220050527_add_languages_and_survey_languages/data-migration.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from "@prisma/client";
+import { AttributeType } from "@prisma/client";
 
 import { translateSurvey } from "./lib/i18n";
 
@@ -28,6 +29,49 @@ async function main() {
         await tx.survey.update({
           where: { id: survey.id },
           data: { ...translatedSurvey },
+        });
+      }
+    }
+  });
+
+  await prisma.$transaction(async (tx) => {
+    const environments = await tx.environment.findMany({
+      select: {
+        id: true,
+        attributeClasses: true,
+      },
+    });
+
+    if (!environments) {
+      // stop the migration if there are no environments
+      return;
+    }
+
+    for (const environment of environments) {
+      const hasLanguageAttributeClass = environment.attributeClasses.find(
+        (attributeClass) => attributeClass.name === "language"
+      );
+      if (hasLanguageAttributeClass) {
+        // Update existing attributeClass
+        const attributeClassId = environment.attributeClasses[0].id;
+        await tx.attributeClass.update({
+          where: { id: attributeClassId },
+          data: {
+            type: AttributeType.automatic,
+            description: "The language used by the person",
+          },
+        });
+      } else {
+        // Create new attributeClass
+        await tx.attributeClass.create({
+          data: {
+            name: "language",
+            type: AttributeType.automatic,
+            description: "The language used by the person",
+            environment: {
+              connect: { id: environment.id },
+            },
+          },
         });
       }
     }
