c4a2680ae6fbb9095feaf024e237b77e70038d9f
Dhruwang <dhruwangjariwala18@gmail.com>
2024-02-27 16:50:52 +0530

    fix: preview bugs

--- packages/surveys/src/components/questions/RatingQuestion.tsx
+++ packages/surveys/src/components/questions/RatingQuestion.tsx
@@ -95,9 +95,9 @@ export default function RatingQuestion({
       <Headline headline={question.headline} questionId={question.id} required={question.required} />
       <Subheader subheader={question.subheader} questionId={question.id} />
       <div className="mb-4 mt-6 flex items-center justify-center">
-        <fieldset className="w-full ">
+        <div className="w-full">
           <legend className="sr-only">Choices</legend>
-          <div className="flex pb-2">
+          <fieldset className="flex w-full pb-2">
             {Array.from({ length: question.range }, (_, i) => i + 1).map((number, i, a) => (
               <span
                 key={number}
@@ -131,7 +131,7 @@ export default function RatingQuestion({
                       }
                     }}
                     className={cn(
-                      "flex h-full w-full justify-center focus:outline-none",
+                      "flex max-h-16 justify-center focus:outline-none",
                       number <= hoveredNumber || number <= (value as number)
                         ? "text-amber-400"
                         : "text-slate-300",
@@ -140,11 +140,7 @@ export default function RatingQuestion({
                     onFocus={() => setHoveredNumber(number)}
                     onBlur={() => setHoveredNumber(0)}>
                     <HiddenRadioInput number={number} />
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      fill="currentColor"
-                      className="h-14 max-h-full w-14">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
                       <path
                         fillRule="evenodd"
                         d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
@@ -154,7 +150,7 @@ export default function RatingQuestion({
                 ) : (
                   <label
                     className={cn(
-                      "flex h-full w-full justify-center",
+                      "flex max-h-16 justify-center",
                       value === number || hoveredNumber === number
                         ? "stroke-rating-selected text-rating-selected"
                         : "stroke-heading text-heading"
@@ -177,12 +173,12 @@ export default function RatingQuestion({
                 )}
               </span>
             ))}
-          </div>
+          </fieldset>
           <div className="text-subheading flex justify-between px-1.5 text-xs leading-6">
             <p className="w-1/2 text-left">{question.lowerLabel}</p>
             <p className="w-1/2 text-right">{question.upperLabel}</p>
           </div>
-        </fieldset>
+        </div>
       </div>
 
       <div className="mt-4 flex w-full justify-between">
