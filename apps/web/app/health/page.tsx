1df1419827ed775736f728c24da0d57f638f1c4b
Matti Nannt <mail@matthiasnannt.com>
2024-03-06 09:49:28 +0100

    chore: remove s3 check from health check for now (#2194)

--- apps/web/app/health/page.tsx
+++ apps/web/app/health/page.tsx
@@ -2,8 +2,6 @@ import { CheckBadgeIcon } from "@heroicons/react/24/outline";
 import { Metadata } from "next";
 
 import { prisma } from "@formbricks/database";
-import { IS_S3_CONFIGURED } from "@formbricks/lib/constants";
-import { testS3BucketAccess } from "@formbricks/lib/storage/service";
 
 export const dynamic = "force-dynamic"; // no caching
 
@@ -26,7 +24,7 @@ const checkDatabaseConnection = async () => {
   }
 };
 
-const checkS3Connection = async () => {
+/* const checkS3Connection = async () => {
   if (!IS_S3_CONFIGURED) {
     // dont try connecting if not in use
     return;
@@ -36,11 +34,12 @@ const checkS3Connection = async () => {
   } catch (e) {
     throw new Error("S3 Bucket cannot be accessed");
   }
-};
+}; */
 
 export default async function HealthPage() {
   await checkDatabaseConnection();
-  await checkS3Connection();
+  // Skipping S3 check for now until it's fixed
+  // await checkS3Connection();
 
   return (
     <div className="mx-auto flex h-full max-w-xl flex-col items-center justify-center text-center">
