206f12e552d62ab0db62714e38bb4788992e1969
Johannes <johannes@formbricks.com>
2024-03-06 09:22:40 +0100

    remove redundant code

--- packages/surveys/src/components/questions/AddressQuestion.tsx
+++ packages/surveys/src/components/questions/AddressQuestion.tsx
@@ -6,10 +6,10 @@ import Subheader from "@/components/general/Subheader";
 import { getUpdatedTtc, useTtc } from "@/lib/ttc";
 import { useState } from "preact/hooks";
 
-import { TResponseData } from "@formbricks/types/responses";
-import { TResponseTtc } from "@formbricks/types/responses";
+import { TResponseData, TResponseTtc } from "@formbricks/types/responses";
 import type { TSurveyAddressQuestion } from "@formbricks/types/surveys";
 
+/*
 interface AddressQuestionProps {
   question: TSurveyAddressQuestion;
   value: string[];
@@ -58,7 +58,7 @@ export default function AddressQuestion({
       {question.imageUrl && <QuestionImage imgUrl={question.imageUrl} />}
       <Headline headline={question.headline} questionId={question.id} required={question.required} />
       <Subheader subheader={question.subheader} questionId={question.id} />
-      <div className="mt-4">
+      <div className="mt-4 space-y-2">
         <input
           tabIndex={1}
           name={question.id}
@@ -71,8 +71,6 @@ export default function AddressQuestion({
           autoFocus={autoFocus}
           className="border-border bg-survey-bg focus:border-border-highlight block w-full rounded-md border p-2 shadow-sm focus:outline-none focus:ring-0 sm:text-sm"
         />
-      </div>
-      <div className="mt-4">
         <input
           tabIndex={1}
           name={question.id}
@@ -85,9 +83,6 @@ export default function AddressQuestion({
           autoFocus={autoFocus}
           className="border-border bg-survey-bg focus:border-border-highlight block w-full rounded-md border p-2 shadow-sm focus:outline-none focus:ring-0 sm:text-sm"
         />
-      </div>
-
-      <div className="mt-4">
         <input
           tabIndex={1}
           name={question.id}
@@ -100,8 +95,6 @@ export default function AddressQuestion({
           autoFocus={autoFocus}
           className="border-border bg-survey-bg focus:border-border-highlight block w-full rounded-md border p-2 shadow-sm focus:outline-none focus:ring-0 sm:text-sm"
         />
-      </div>
-      <div className="mt-4">
         <input
           tabIndex={1}
           name={question.id}
@@ -114,8 +107,6 @@ export default function AddressQuestion({
           autoFocus={autoFocus}
           className="border-border bg-survey-bg focus:border-border-highlight block w-full rounded-md border p-2 shadow-sm focus:outline-none focus:ring-0 sm:text-sm"
         />
-      </div>
-      <div className="mt-4">
         <input
           tabIndex={1}
           name={question.id}
@@ -128,8 +119,6 @@ export default function AddressQuestion({
           autoFocus={autoFocus}
           className="border-border bg-survey-bg focus:border-border-highlight block w-full rounded-md border p-2 shadow-sm focus:outline-none focus:ring-0 sm:text-sm"
         />
-      </div>
-      <div className="mt-4">
         <input
           tabIndex={1}
           name={question.id}
@@ -156,6 +145,87 @@ export default function AddressQuestion({
           />
         )}
         <div></div>
+        <SubmitButton buttonLabel={question.buttonLabel} isLastQuestion={isLastQuestion} onClick={() => {}} />
+      </div>
+    </form>
+  );
+}
+ */
+
+interface AddressQuestionProps {
+  question: TSurveyAddressQuestion;
+  value: [];
+  onChange: (responseData: TResponseData) => void;
+  onSubmit: (data: TResponseData, ttc: TResponseTtc) => void;
+  onBack: () => void;
+  isFirstQuestion: boolean;
+  isLastQuestion: boolean;
+  autoFocus?: boolean;
+  ttc: TResponseTtc;
+  setTtc: (ttc: TResponseTtc) => void;
+}
+
+export default function AddressQuestion({
+  question,
+  value,
+  onChange,
+  onSubmit,
+  onBack,
+  isFirstQuestion,
+  isLastQuestion,
+  autoFocus = true,
+  ttc,
+  setTtc,
+}: AddressQuestionProps) {
+  const [startTime, setStartTime] = useState(performance.now());
+
+  useTtc(question.id, ttc, setTtc, startTime, setStartTime);
+
+  const safeValue = Array.isArray(value) ? value : ["", "", "", "", "", ""];
+
+  const handleInputChange = (inputValue: string, index: number) => {
+    const updatedValue = [...safeValue];
+    updatedValue[index] = inputValue;
+    onChange({ [question.id]: updatedValue });
+  };
+
+  const handleSubmit = (e: Event) => {
+    e.preventDefault();
+    const updatedTtc = getUpdatedTtc(ttc, question.id, performance.now() - startTime);
+    setTtc(updatedTtc);
+    onSubmit({ [question.id]: value }, updatedTtc);
+  };
+
+  const inputPlaceholders = [
+    { placeholder: "Address e.g. Bay Street 69", required: question.required || question.addressRequired },
+    { placeholder: "Address line 2", required: question.addressLine2Required },
+    { placeholder: "City / Town", required: question.cityRequired },
+    { placeholder: "State / Region", required: question.stateRequired },
+    { placeholder: "ZIP / Post Code", required: question.zipRequired },
+    { placeholder: "Country", required: question.countryRequired },
+  ];
+
+  return (
+    <form key={question.id} onSubmit={handleSubmit} className="w-full">
+      {question.imageUrl && <QuestionImage imgUrl={question.imageUrl} />}
+      <Headline headline={question.headline} questionId={question.id} required={question.required} />
+      <Subheader subheader={question.subheader} questionId={question.id} />
+      <div className="mt-4 space-y-2">
+        {inputPlaceholders.map(({ placeholder, required }, index) => (
+          <input
+            name={`${question.id}-${index}`}
+            id={`${question.id}-${index}`}
+            placeholder={placeholder}
+            required={required}
+            value={safeValue[index] || ""}
+            onInput={(e) => handleInputChange(e.currentTarget.value, index)}
+            autoFocus={autoFocus && index === 0}
+            className="border-border bg-survey-bg focus:border-border-highlight block w-full rounded-md border p-2 shadow-sm focus:outline-none focus:ring-0 sm:text-sm"
+          />
+        ))}
+      </div>
+      <div className="mt-4 flex w-full justify-between">
+        {!isFirstQuestion && <BackButton backButtonLabel={question.backButtonLabel} onClick={onBack} />}
         <SubmitButton buttonLabel={question.buttonLabel} isLastQuestion={isLastQuestion} onClick={() => {}} />
       </div>
     </form>
