9f5b7a033973b8d3bd2c5d0a3d6ea4d65857effd
Piyush Gupta <piyushguptaa2z123@gmail.com>
2024-03-08 12:26:44 +0530

    fix: download responses not working

--- packages/lib/response/util.ts
+++ packages/lib/response/util.ts
@@ -307,6 +307,7 @@ export const buildWhereClause = (filterCriteria?: TResponseFilterCriteria) => {
 };
 
 export const getResponsesFileName = (surveyName: string, extension: string) => {
+  surveyName = surveyName.replaceAll("/", ":");
   const formattedDateString = getTodaysDateTimeFormatted("-");
   return `export-${surveyName.split(" ").join("-")}-${formattedDateString}.${extension}`.toLocaleLowerCase();
 };
@@ -328,7 +329,7 @@ export const extracMetadataKeys = (obj: TResponse["meta"]) => {
 };
 
 export const extractSurveyDetails = (survey: TSurvey, responses: TResponse[]) => {
-  const metaDataFields = extracMetadataKeys(responses[0].meta);
+  const metaDataFields = extracMetadataKeys(responses[0]?.meta);
   const questions = survey.questions.map((question, idx) => `${idx + 1}. ${question.headline}`);
   const hiddenFields = survey.hiddenFields?.fieldIds || [];
   const userAttributes = Array.from(
