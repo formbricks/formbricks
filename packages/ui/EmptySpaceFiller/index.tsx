989cb63a5be1c3be04c563590c97686a55210fa4
Piyush Gupta <piyushguptaa2z123@gmail.com>
2024-03-11 14:36:22 +0530

    feat: adds skeleton loader

--- packages/ui/EmptySpaceFiller/index.tsx
+++ packages/ui/EmptySpaceFiller/index.tsx
@@ -5,8 +5,10 @@ import React from "react";
 
 import { TEnvironment } from "@formbricks/types/environment";
 
+import { Skeleton } from "../Skeleton";
+
 type EmptySpaceFillerProps = {
-  type: "table" | "response" | "event" | "linkResponse" | "tag";
+  type: "table" | "response" | "event" | "linkResponse" | "tag" | "summary";
   environment: TEnvironment;
   noWidgetRequired?: boolean;
   emptyMessage?: string;
@@ -99,6 +101,26 @@ const EmptySpaceFiller: React.FC<EmptySpaceFillerProps> = ({
           </div>
           <div className="h-12 w-full rounded-full bg-slate-50/50"></div>
         </div>
+      </div>
+    );
+  }
+
+  if (type === "summary") {
+    return (
+      <div className="rounded-lg border border-slate-200 bg-slate-50 shadow-sm">
+        <Skeleton className="group space-y-4 rounded-lg bg-white p-6 ">
+          <div className="flex items-center space-x-4">
+            <div className=" h-6 w-full rounded-full bg-slate-100"></div>
+          </div>
+          <div className="space-y-4">
+            <div className="flex gap-4">
+              <div className="h-6 w-24 rounded-full bg-slate-100"></div>
+              <div className="h-6 w-24 rounded-full bg-slate-100"></div>
+            </div>
+            <div className=" flex h-12 w-full items-center justify-center rounded-full bg-slate-50 text-sm text-slate-500 hover:bg-slate-100"></div>
+            <div className="h-12 w-full rounded-full bg-slate-50/50"></div>
+          </div>
+        </Skeleton>
       </div>
     );
   }
