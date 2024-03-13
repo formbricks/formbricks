493880d8a6001f3c0bd5033867bc69f92ea4c527
Piyush Gupta <piyushguptaa2z123@gmail.com>
2024-03-13 14:19:01 +0530

    feat: added image link

--- packages/surveys/src/components/general/QuestionImage.tsx
+++ packages/surveys/src/components/general/QuestionImage.tsx
@@ -5,9 +5,30 @@ interface QuestionImageProps {
 
 export default function QuestionImage({ imgUrl, altText = "Image" }: QuestionImageProps) {
   return (
-    <div className="mb-4 rounded-md">
-      {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img src={imgUrl} alt={altText} className="mb-4 rounded-md" />
+    <div className="group mb-4 cursor-pointer rounded-md">
+      <a href={imgUrl} target="_blank" rel="noreferrer" className="relative block h-full w-full">
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img src={imgUrl} alt={altText} className="rounded-md" />
+
+        <div className="absolute bottom-2 left-1/2 flex -translate-x-1/2 items-center gap-2 rounded-md bg-gray-800 p-2 text-sm text-white opacity-0 transition duration-300 ease-in-out hover:bg-gray-700 group-hover:opacity-100">
+          <span>Open in new tab</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="22"
+            height="22"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-square-arrow-up-right">
+            <rect width="18" height="18" x="3" y="3" rx="2" />
+            <path d="M8 8h8v8" />
+            <path d="m8 16 8-8" />
+          </svg>
+        </div>
+      </a>
     </div>
   );
 }
--- packages/surveys/src/components/questions/PictureSelectionQuestion.tsx
+++ packages/surveys/src/components/questions/PictureSelectionQuestion.tsx
@@ -118,7 +118,7 @@ export default function PictureSelectionQuestion({
                   Array.isArray(value) && value.includes(choice.id)
                     ? `border-brand text-brand z-10 border-4 shadow-xl focus:border-4`
                     : "",
-                  "border-border focus:border-border-highlight focus:bg-accent-selected-bg relative box-border inline-block h-28 w-full overflow-hidden rounded-xl border focus:outline-none"
+                  "border-border focus:border-border-highlight focus:bg-accent-selected-bg group/image relative box-border inline-block h-28 w-full cursor-pointer overflow-hidden rounded-xl border focus:outline-none"
                 )}>
                 {/* eslint-disable-next-line @next/next/no-img-element */}
                 <img
@@ -127,6 +127,30 @@ export default function PictureSelectionQuestion({
                   alt={choice.imageUrl.split("/").pop()}
                   className="h-full w-full object-cover"
                 />
+
+                <a
+                  href={choice.imageUrl}
+                  target="_blank"
+                  rel="noreferrer"
+                  onClick={(e) => e.stopPropagation()}
+                  className="z-2 absolute bottom-2 left-1/2 flex -translate-x-1/2 items-center gap-2 whitespace-nowrap rounded-md bg-gray-800 p-2 text-xs text-white opacity-0 transition duration-300 ease-in-out hover:bg-gray-700 group-hover/image:opacity-100">
+                  <span>Open in new tab</span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="lucide lucide-square-arrow-up-right">
+                    <rect width="18" height="18" x="3" y="3" rx="2" />
+                    <path d="M8 8h8v8" />
+                    <path d="m8 16 8-8" />
+                  </svg>
+                </a>
                 {question.allowMulti ? (
                   <input
                     id={`${choice.id}-checked`}
