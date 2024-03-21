ba5de7627ec6275b5373df13115a8972971afb55
ShubhamPalriwala <spalriwalau@gmail.com>
2024-03-21 15:19:11 +0530

    feat: remove init hardcoding + graceful error handling

--- packages/js/src/index.ts
+++ packages/js/src/index.ts
@@ -4,69 +4,56 @@ declare global {
   }
 }
 
-interface InitOptions {
-  apiHost: string;
-  environmentId: string;
-  [key: string]: any; // Allows any additional properties required in the future
-}
-
 const methodCallsInProgress = new Set();
 
 const formbricksProxyHandler: ProxyHandler<any> = {
-  get(target, prop, _receiver) {
-    if (prop === "init") {
-      return target[prop];
-    }
+  get(_target, prop, _receiver) {
     return (...args: any[]) => {
       if (methodCallsInProgress.has(prop)) {
-        throw new Error(`Method ${String(prop)} is already being called`);
+        console.warn(`🧱 Formbricks - Method ${String(prop)} is already being called`);
+        return;
       }
       if (!window.formbricks) {
-        throw new Error("Formbricks SDK is not initialized");
-      }
-      if (typeof window.formbricks[prop] !== "function") {
-        throw new Error(`Formbricks SDK does not support method ${String(prop)}`);
-      }
-      methodCallsInProgress.add(prop);
-      try {
-        return window.formbricks[prop](...args);
-      } finally {
-        methodCallsInProgress.delete(prop);
+        const { apiHost } = args[0];
+        fetch(`${apiHost}/api/js`)
+          .then((res) => (res.ok ? res.text() : Promise.reject("Failed to load Formbricks SDK")))
+          .then((sdkScript) => {
+            const scriptTag = document.createElement("script");
+            scriptTag.innerHTML = sdkScript;
+            document.head.appendChild(scriptTag);
+            return new Promise((resolve, reject) => {
+              const checkInterval = setInterval(() => {
+                if (window.formbricks) {
+                  clearInterval(checkInterval);
+                  resolve(window.formbricks[prop](...args));
+                }
+              }, 100);
+              setTimeout(() => {
+                clearInterval(checkInterval);
+                reject(new Error("Formbricks SDK loading timed out"));
+              }, 10000);
+            });
+          })
+          .catch((error) => {
+            console.error(`🧱 Formbricks - Error loading SDK: ${error}`);
+          });
+        return;
+      } else {
+        if (typeof window.formbricks[prop] !== "function") {
+          console.error(`🧱 Formbricks - SDK does not support method ${String(prop)}`);
+          return;
+        }
+        methodCallsInProgress.add(prop);
+        try {
+          return window.formbricks[prop](...args);
+        } finally {
+          methodCallsInProgress.delete(prop);
+        }
       }
     };
   },
 };
 
-const formbricks = new Proxy(
-  {
-    init: async (options: InitOptions) => {
-      const { apiHost } = options;
-      await fetch(`${apiHost}/api/js`)
-        .then((res) => (res.ok ? res.text() : Promise.reject("Failed to load Formbricks SDK")))
-        .then((sdkScript) => {
-          const scriptTag = document.createElement("script");
-          scriptTag.innerHTML = sdkScript;
-          document.head.appendChild(scriptTag);
-          return new Promise((resolve, reject) => {
-            const checkInterval = setInterval(() => {
-              if (window.formbricks) {
-                clearInterval(checkInterval);
-                resolve(true);
-              }
-            }, 100);
-            setTimeout(() => {
-              clearInterval(checkInterval);
-              reject(new Error("Formbricks SDK loading timed out"));
-            }, 10000);
-          });
-        });
-
-      if (window.formbricks) {
-        window.formbricks.init(options);
-      }
-    },
-  },
-  formbricksProxyHandler
-);
+const formbricks = new Proxy({}, formbricksProxyHandler);
 
 export default formbricks;
