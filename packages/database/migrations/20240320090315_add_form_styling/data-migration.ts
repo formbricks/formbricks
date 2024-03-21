2ab7efd683014cc06a3de67065679a2ec48ee014
pandeymangg <anshuman.pandey9999@gmail.com>
2024-03-21 10:27:15 +0530

    migration wip

--- packages/database/migrations/20240320090315_add_form_styling/data-migration.ts
+++ packages/database/migrations/20240320090315_add_form_styling/data-migration.ts
@@ -118,40 +118,6 @@ async function main() {
             }
           }
         }
-
-        // find all surveys with product overwrites
-        // const surveysWithProductOverwrites = await tx.survey.findMany({
-        //   where: {
-        //     productOverwrites: { not: Prisma.JsonNull },
-        //   },
-        // });
-
-        // if (!surveysWithProductOverwrites || !surveysWithProductOverwrites.length) {
-        //   // no surveys with product overwrites found, return early
-        //   return;
-        // }
-
-        // for (const survey of surveysWithProductOverwrites) {
-        //   const { brandColor, highlightBorderColor, ...rest } = survey.productOverwrites ?? {};
-
-        //   // single query to update the survey with the product overwrites
-        //   await tx.survey.update({
-        //     where: { id: survey.id },
-        //     data: {
-        //       styling: {
-        //         ...(survey.styling ?? {}),
-        //         ...(brandColor && { brandColor: { light: brandColor } }),
-        //         ...(highlightBorderColor && { highlightBorderColor: { light: highlightBorderColor } }),
-        //         ...((brandColor || highlightBorderColor || Object.keys(survey.styling ?? {}).length > 0) && {
-        //           overwriteUnifiedStyling: true,
-        //         }),
-        //       },
-        //       productOverwrites: {
-        //         ...rest,
-        //       },
-        //     },
-        //   });
-        // }
       }
     },
     {
