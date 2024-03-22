012f79744cd567c51e751414f1fb57aefb691950
Dhruwang <dhruwangjariwala18@gmail.com>
2024-03-22 13:49:56 +0530

    fix: show responses in summary that doesn't fit the current schema

--- apps/web/app/(app)/environments/[environmentId]/surveys/[surveyId]/(analysis)/summary/components/MultipleChoiceSummary.tsx
+++ apps/web/app/(app)/environments/[environmentId]/surveys/[surveyId]/(analysis)/summary/components/MultipleChoiceSummary.tsx
@@ -76,7 +76,7 @@ export default function MultipleChoiceSummary({
             {result.others && result.others.length > 0 && (
               <div className="mt-4 rounded-lg border border-slate-200">
                 <div className="grid h-12 grid-cols-2 content-center rounded-t-lg bg-slate-100 text-left text-sm font-semibold text-slate-900">
-                  <div className="col-span-1 pl-6 ">Specified &quot;Other&quot; answers</div>
+                  <div className="col-span-1 pl-6 ">Other values found</div>
                   <div className="col-span-1 pl-6 ">{surveyType === "web" && "User"}</div>
                 </div>
                 {result.others
