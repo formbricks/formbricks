7a507c91af8597d7026f055868afb909febc3737
Matthias Nannt <mail@matthiasnannt.com>
2024-03-22 11:10:39 +0100

    chore: increase service caching period to 3 hours

--- packages/lib/constants.ts
+++ packages/lib/constants.ts
@@ -4,7 +4,7 @@ import { env } from "./env";
 
 export const IS_FORMBRICKS_CLOUD = env.IS_FORMBRICKS_CLOUD === "1";
 export const REVALIDATION_INTERVAL = 0; //TODO: find a good way to cache and revalidate data when it changes
-export const SERVICES_REVALIDATION_INTERVAL = 60 * 10; // 10 minutes
+export const SERVICES_REVALIDATION_INTERVAL = 60 * 60 * 3; // 3 hours
 export const MAU_LIMIT = IS_FORMBRICKS_CLOUD ? 9000 : 1000000;
 
 // URLs
