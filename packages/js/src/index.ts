b828d24aba3267f4de77f31a8d4e7c20c457d999
Matthias Nannt <mail@matthiasnannt.com>
2024-03-22 16:04:42 +0100

    remove methodCallsInProgress since we have a queue in js-core

--- packages/js/src/index.ts
+++ packages/js/src/index.ts
@@ -4,50 +4,47 @@ declare global {
   }
 }
 
-const methodCallsInProgress = new Set();
-
 const formbricksProxyHandler: ProxyHandler<any> = {
   get(_target, prop, _receiver) {
-    return (...args: any[]) => {
-      if (methodCallsInProgress.has(prop)) {
-        console.warn(`🧱 Formbricks - Method ${String(prop)} is already being called`);
-        return;
-      }
+    return async (...args: any[]) => {
       if (!window.formbricks) {
         const { apiHost } = args[0];
-        fetch(`${apiHost}/api/packages/js-core`)
-          .then((res) => (res.ok ? res.text() : Promise.reject("Failed to load Formbricks SDK")))
-          .then((sdkScript) => {
-            const scriptTag = document.createElement("script");
-            scriptTag.innerHTML = sdkScript;
-            document.head.appendChild(scriptTag);
-            return new Promise((resolve, reject) => {
-              const checkInterval = setInterval(() => {
-                if (window.formbricks) {
-                  clearInterval(checkInterval);
-                  resolve(window.formbricks[prop](...args));
-                }
-              }, 100);
-              setTimeout(() => {
+        try {
+          const res = await fetch(`${apiHost}/api/packages/js-core`);
+          if (!res.ok) throw new Error("Failed to load Formbricks SDK");
+          const sdkScript = await res.text();
+          const scriptTag = document.createElement("script");
+          scriptTag.innerHTML = sdkScript;
+          document.head.appendChild(scriptTag);
+
+          return new Promise((resolve, reject) => {
+            const checkInterval = setInterval(async () => {
+              if (window.formbricks) {
                 clearInterval(checkInterval);
-                reject(new Error("Formbricks SDK loading timed out"));
-              }, 10000);
-            });
-          })
-          .catch((error) => {
-            console.error(`🧱 Formbricks - Error loading SDK: ${error}`);
+                try {
+                  resolve(await window.formbricks[prop](...args));
+                } catch (e) {
+                  reject(e);
+                }
+              }
+            }, 100);
+            setTimeout(() => {
+              clearInterval(checkInterval);
+              reject(new Error("Formbricks SDK loading timed out"));
+            }, 10000);
           });
-        return;
+        } catch (error) {
+          console.error(`🧱 Formbricks - Error loading SDK: ${error}`);
+        }
       } else {
         if (typeof window.formbricks[prop] !== "function") {
           console.error(`🧱 Formbricks - SDK does not support method ${String(prop)}`);
           return;
         }
-        methodCallsInProgress.add(prop);
         try {
-          return window.formbricks[prop](...args);
-        } finally {
-          methodCallsInProgress.delete(prop);
+          return await window.formbricks[prop](...args);
+        } catch (error) {
+          console.error(error);
         }
       }
     };
