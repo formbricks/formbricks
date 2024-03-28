9ce581aa7b10466395dc059104164958426be92b
Dhruwang <dhruwangjariwala18@gmail.com>
2024-03-28 10:09:52 +0530

    file upload response email fixes

--- packages/lib/emails/ResponseFinishedEmail.tsx
+++ packages/lib/emails/ResponseFinishedEmail.tsx
@@ -1,4 +1,4 @@
-import { Column, Container, Hr, Img, Link, Row, Section, Text } from "@react-email/components";
+import { Column, Container, Hr, Link, Row, Section, Text } from "@react-email/components";
 import React from "react";
 
 import { TResponse } from "@formbricks/types/responses";
@@ -43,23 +43,18 @@ export const ResponseFinishedEmail = ({
               <Column className="w-full">
                 <Text className="mb-2 font-medium">{question.question}</Text>
                 {question.type === TSurveyQuestionType.FileUpload ? (
-                  <div>
+                  <Container>
                     {typeof question.answer !== "string" &&
                       question.answer.map((answer) => (
-                        <div
+                        <Link
+                          href={answer}
                           key={answer}
-                          className="flex items-center justify-center rounded-lg bg-gray-200 p-2 shadow-sm">
-                          <Img
-                            src="/icons/file-icon.svg" // Replace with actual SVG path
-                            alt="File attachment"
-                            width={24}
-                            height={24}
-                            className="mr-2"
-                          />
-                          <Text className="truncate">{getOriginalFileNameFromUrl(answer)}</Text>
-                        </div>
+                          className="mt-2 flex flex-col items-center justify-center rounded-lg bg-gray-200 p-2 text-black shadow-sm">
+                          <FileIcon />
+                          <Text className="mb-0 truncate">{getOriginalFileNameFromUrl(answer)}</Text>
+                        </Link>
                       ))}
-                  </div>
+                  </Container>
                 ) : (
                   <Text className="mt-0 whitespace-pre-wrap break-words font-bold">{question.answer}</Text>
                 )}
@@ -97,5 +92,24 @@ export const ResponseFinishedEmail = ({
         </Column>
       </Row>
     </Container>
+  );
+};
+
+const FileIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      className="lucide lucide-file">
+      <path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z" />
+      <path d="M14 2v4a2 2 0 0 0 2 2h4" />
+    </svg>
   );
 };
