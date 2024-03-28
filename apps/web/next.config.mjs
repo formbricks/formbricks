7f4ccaad655a14776fbda88ad6c2183a8dacbe89
Matthias Nannt <mail@matthiasnannt.com>
2024-03-28 11:57:16 +0100

    remove unused js package

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -21,11 +21,7 @@ const nextConfig = {
     serverComponentsExternalPackages: ["@aws-sdk"],
     instrumentationHook: true,
     outputFileTracingIncludes: {
-      "app/api/packages": [
-        "../../packages/js/dist/*",
-        "../../packages/js-core/dist/*",
-        "../../packages/surveys/dist/*",
-      ],
+      "app/api/packages": ["../../packages/js-core/dist/*", "../../packages/surveys/dist/*"],
     },
   },
   transpilePackages: ["@formbricks/database", "@formbricks/ee", "@formbricks/ui", "@formbricks/lib"],
