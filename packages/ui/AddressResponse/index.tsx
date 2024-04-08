cf0b3b8cf81b83238722eea5902b5a328a2a8ccc
Dhruwang <dhruwangjariwala18@gmail.com>
2024-04-08 10:42:52 +0530

    Updated AddressResponse component to include Address keys

--- packages/ui/AddressResponse/index.tsx
+++ packages/ui/AddressResponse/index.tsx
@@ -4,14 +4,33 @@ interface AddressResponseProps {
   value: string[];
 }
 
+const getAddressKey = (index: number): string => {
+  switch (index) {
+    case 0:
+      return "Address Line 1";
+    case 1:
+      return "Address Line 2";
+    case 2:
+      return "City / Town";
+    case 3:
+      return "State / Region";
+    case 4:
+      return "ZIP / Postcode";
+    case 5:
+      return "Country";
+    default:
+      return "";
+  }
+};
+
 export const AddressResponse = ({ value }: AddressResponseProps) => {
   return (
-    <div className="text-slate-700">
+    <div className="my-1 font-semibold text-slate-700">
       {value.map(
         (item, index) =>
           item && (
             <div key={index}>
-              {item}
+              {getAddressKey(index)} : {item}
               <br />
             </div>
           )
