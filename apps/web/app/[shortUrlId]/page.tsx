214917cdb882b794a11f2076f72b5d436d3fc7ef
Naitik Kapadia <88614335+KapadiaNaitik@users.noreply.github.com>
2024-04-09 13:51:21 +0530

    fix: Preview for short urls (#2174)
    Co-authored-by: Johannes <72809645+jobenjada@users.noreply.github.com>
    Co-authored-by: Shubham Palriwala <spalriwalau@gmail.com>
    Co-authored-by: Johannes <johannes@formbricks.com>

--- apps/web/app/[shortUrlId]/page.tsx
+++ apps/web/app/[shortUrlId]/page.tsx
@@ -1,8 +1,33 @@
+import { getMetadataForLinkSurvey } from "@/app/s/[surveyId]/metadata";
+import type { Metadata } from "next";
 import { notFound, redirect } from "next/navigation";
 
 import { getShortUrl } from "@formbricks/lib/shortUrl/service";
 import { ZShortUrlId } from "@formbricks/types/shortUrl";
 
+export async function generateMetadata({ params }): Promise<Metadata> {
+  if (!params.shortUrlId) {
+    notFound();
+  }
+
+  if (ZShortUrlId.safeParse(params.shortUrlId).success !== true) {
+    notFound();
+  }
+
+  let shortUrl;
+
+  try {
+    shortUrl = await getShortUrl(params.shortUrlId);
+    if (!shortUrl) {
+      notFound();
+    }
+  } catch (error) {
+    console.error(error);
+  }
+
+  const surveyId = shortUrl.url.substring(shortUrl.url.lastIndexOf("/") + 1);
+  return getMetadataForLinkSurvey(surveyId);
+}
 export default async function ShortUrlPage({ params }) {
   if (!params.shortUrlId) {
     notFound();
