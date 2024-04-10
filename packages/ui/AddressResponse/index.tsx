43b162a5913a2bc6cbd3fef9296dbb54164453f3
Matthias Nannt <mail@matthiasnannt.com>
2024-04-10 16:51:14 +0200

    remove labels from response display

--- packages/ui/AddressResponse/index.tsx
+++ packages/ui/AddressResponse/index.tsx
@@ -1,27 +1,6 @@
-"use client";
-
 interface AddressResponseProps {
   value: string[];
 }
-
-const getAddressKey = (index: number): string => {
-  switch (index) {
-    case 0:
-      return "Address Line 1";
-    case 1:
-      return "Address Line 2";
-    case 2:
-      return "City / Town";
-    case 3:
-      return "State / Region";
-    case 4:
-      return "ZIP / Postcode";
-    case 5:
-      return "Country";
-    default:
-      return "";
-  }
-};
 
 export const AddressResponse = ({ value }: AddressResponseProps) => {
   return (
@@ -30,7 +9,7 @@ export const AddressResponse = ({ value }: AddressResponseProps) => {
         (item, index) =>
           item && (
             <div key={index}>
-              {getAddressKey(index)}: {item}
+              {item}
               <br />
             </div>
           )
