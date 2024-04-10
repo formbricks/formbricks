6e85a28b49935cc2291f9058de19e0261347afb3
pandeymangg <anshuman.pandey9999@gmail.com>
2024-04-10 17:35:04 +0530

    feat: website surveys schema and migration

---
+++ packages/database/migrations/20240410111624_adds_website_and_inapp_survey/data-migration.ts
@@ -0,0 +1,80 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.$transaction(
+    async (tx) => {
+      // Move all the previous surveys with type "web" to "inApp" or "website"
+      // If a web survey has a response with personId set, then it should be moved to "inApp"
+      // otherwise it should be moved to "website"
+      const webSurveys = await tx.survey.findMany({
+        where: { type: "web" },
+        include: {
+          segment: true,
+        },
+      });
+
+      for (const webSurvey of webSurveys) {
+        // get the responses:
+        const identifiedResponse = await tx.response.findFirst({
+          where: {
+            surveyId: webSurvey.id,
+            personId: { not: null },
+          },
+        });
+
+        if (identifiedResponse) {
+          await tx.survey.update({
+            where: { id: webSurvey.id },
+            data: { type: "inApp" },
+          });
+        } else {
+          // This is a website survey, change the type of the survey to "website"
+
+          await tx.survey.update({
+            where: { id: webSurvey.id },
+            data: {
+              type: "website",
+            },
+          });
+
+          // if the segment with this survey is private, delete it
+          if (webSurvey.segment) {
+            const { isPrivate, id } = webSurvey.segment;
+
+            if (isPrivate) {
+              await tx.segment.delete({
+                where: { id },
+              });
+            } else {
+              await tx.survey.update({
+                where: { id: webSurvey.id },
+                data: {
+                  segment: { disconnect: true },
+                },
+              });
+            }
+          }
+
+          // find All the segments that are private and have the title as the webSurvey's id
+          await tx.segment.deleteMany({
+            where: {
+              title: webSurvey.id,
+              isPrivate: true,
+            },
+          });
+        }
+      }
+    },
+    {
+      timeout: 50000,
+    }
+  );
+}
+main()
+  .catch(async (e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => await prisma.$disconnect());
