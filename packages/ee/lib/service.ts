0d33c272953b16e64eabae2423480a5e5ccf1e22
Piyush Gupta <piyushguptaa2z123@gmail.com>
2024-04-12 16:29:00 +0530

    used unstable_cache instead of custom cache

--- packages/ee/lib/service.ts
+++ packages/ee/lib/service.ts
@@ -1,51 +1,43 @@
 import "server-only";
 
-import { ENTERPRISE_LICENSE_KEY, IS_FORMBRICKS_CLOUD } from "@formbricks/lib/constants";
-import { TTeam } from "@formbricks/types/teams";
+import { unstable_cache } from "next/cache";
 
-const licenseDetails: {
-  isValid: boolean | null;
-  lastChecked: Date;
-} = {
-  isValid: null,
-  lastChecked: new Date(0), // Initialize with an old date
-};
+import { ENTERPRISE_LICENSE_KEY, IS_FORMBRICKS_CLOUD } from "@formbricks/lib/constants";
+import { hashString } from "@formbricks/lib/hashString";
+import { TTeam } from "@formbricks/types/teams";
 
 export const getIsEnterpriseEdition = async (): Promise<boolean> => {
   if (ENTERPRISE_LICENSE_KEY && ENTERPRISE_LICENSE_KEY.length > 0) {
-    const currentTime = new Date();
-    const timeSinceLastChecked = currentTime.getTime() - licenseDetails.lastChecked.getTime(); // Difference in milliseconds
+    const hashedKey = hashString(ENTERPRISE_LICENSE_KEY);
 
-    // Check if the lastChecked time is within the last 24 hours
-    if (
-      licenseDetails.isValid !== null &&
-      timeSinceLastChecked < 1000 * 60 * 60 * 24 // 1000ms * 60s * 60min * 24hrs
-    ) {
-      return licenseDetails.isValid;
-    }
+    const res = await unstable_cache(
+      async () => {
+        try {
+          const res = await fetch("https://ee.formbricks.com/api/licenses/check", {
+            body: JSON.stringify({ licenseKey: ENTERPRISE_LICENSE_KEY }),
+            headers: { "Content-Type": "application/json" },
+            method: "POST",
+          });
 
-    try {
-      const res = await fetch("https://ee.formbricks.com/api/licenses/check", {
-        body: JSON.stringify({ licenseKey: ENTERPRISE_LICENSE_KEY }),
-        headers: { "Content-Type": "application/json" },
-        method: "POST",
-      });
+          let isValid = false;
 
-      let isValid = false;
+          if (res.ok) {
+            const responseJson = await res.json();
+            isValid = responseJson.data.status === "active";
+          }
 
-      if (res.ok) {
-        const responseJson = await res.json();
-        isValid = responseJson.data.status === "active";
-      }
+          return isValid;
+        } catch (error) {
+          console.error("Error while checking license", error);
+        }
+      },
+      [`getIsEnterpriseEdition-${hashedKey}`],
+      { revalidate: 60 * 60 * 24 }
+    )();
 
-      licenseDetails.isValid = isValid;
-      licenseDetails.lastChecked = new Date(); // Update the last checked time
-
-      return isValid;
-    } catch (error) {
-      console.error("Error while checking license", error);
-    }
+    return res ?? false;
   }
+
   return false;
 };
 
