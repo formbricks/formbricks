9e4b4af80ee6151301ed70b69a0181436d3010da
pandeymangg <anshuman.pandey9999@gmail.com>
2024-04-18 21:24:18 +0530

    fix: type error

--- packages/lib/pollyfills/structuredClone.ts
+++ packages/lib/pollyfills/structuredClone.ts
@@ -4,6 +4,9 @@ let structuredCloneExport: typeof structuredClonePolyfill;
 
 if (typeof structuredClone === "undefined") {
   structuredCloneExport = structuredClonePolyfill;
+} else {
+  // @ts-expect-error
+  structuredCloneExport = structuredClone;
 }
 
 export { structuredCloneExport as structuredClone };
