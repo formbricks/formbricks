1d261326d52cf2ff2171224bdb9f7230d390e665
Matthias Nannt <mail@matthiasnannt.com>
2024-04-19 10:21:54 +0200

    fix build error

--- apps/formbricks-com/app/[surveyId]/route.ts
+++ apps/formbricks-com/app/[surveyId]/route.ts
@@ -1,4 +1,4 @@
-export async function GET(request: Request, { params }: { params: { surveyId: string } }) {
+export async function GET(_: Request, { params }: { params: { surveyId: string } }) {
   const surveyId = params.surveyId;
   // redirect to Formbricks Cloud
   return Response.redirect(`https://app.formbricks.com/s/${surveyId}`, 301);
