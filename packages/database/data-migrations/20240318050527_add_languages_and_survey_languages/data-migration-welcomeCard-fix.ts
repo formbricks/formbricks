a6e771cb554fe3c48b7bb60545380ad7105cbdd3
Dhruwang <dhruwangjariwala18@gmail.com>
2024-04-25 17:58:13 +0530

    fix: migration for default translation missing in welcome card headline

---
+++ packages/database/data-migrations/20240318050527_add_languages_and_survey_languages/data-migration-welcomeCard-fix.ts
@@ -0,0 +1,50 @@
+// migration script to add empty strings to welcome card headline in default language, if it does not exist
+// WelcomeCard.headline = {} -> WelcomeCard.headline = {"default":""}
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.$transaction(
+    async (tx) => {
+      const surveys = await tx.survey.findMany({
+        select: {
+          id: true,
+          welcomeCard: true,
+        },
+      });
+
+      if (surveys.length === 0) {
+        // stop the migration if there are no surveys
+        return;
+      }
+      let count = 0;
+      for (const survey of surveys) {
+        const updatedSurvey = structuredClone(survey) as any;
+        if (
+          updatedSurvey.welcomeCard &&
+          updatedSurvey.welcomeCard.headline &&
+          !updatedSurvey.welcomeCard.headline.hasOwnProperty("default")
+        ) {
+          updatedSurvey.welcomeCard.headline["default"] = "";
+          count++;
+          await tx.survey.update({
+            where: { id: survey.id },
+            data: { ...updatedSurvey },
+          });
+        }
+      }
+      console.log(count, "surveys transformed");
+    },
+    {
+      timeout: 50000,
+    }
+  );
+}
+
+main()
+  .catch(async (e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => await prisma.$disconnect());
