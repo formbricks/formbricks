51cd17b698fd6c13b233fac1cfa31db4c2082453
Matthias Nannt <mail@matthiasnannt.com>
2024-04-26 16:46:24 +0200

    remove custom data validations

--- packages/lib/survey/util.ts
+++ packages/lib/survey/util.ts
@@ -5,44 +5,6 @@ import { Prisma } from "@prisma/client";
 import { TLegacySurvey } from "@formbricks/types/LegacySurvey";
 import { TSurvey, TSurveyFilterCriteria } from "@formbricks/types/surveys";
 
-export const formatSurveyDateFields = (survey: TSurvey): TSurvey => {
-  if (typeof survey.createdAt === "string") {
-    survey.createdAt = new Date(survey.createdAt);
-  }
-  if (typeof survey.updatedAt === "string") {
-    survey.updatedAt = new Date(survey.updatedAt);
-  }
-  if (typeof survey.runOnDate === "string") {
-    survey.runOnDate = new Date(survey.runOnDate);
-  }
-  if (typeof survey.closeOnDate === "string") {
-    survey.closeOnDate = new Date(survey.closeOnDate);
-  }
-
-  if (survey.segment) {
-    if (typeof survey.segment.createdAt === "string") {
-      survey.segment.createdAt = new Date(survey.segment.createdAt);
-    }
-
-    if (typeof survey.segment.updatedAt === "string") {
-      survey.segment.updatedAt = new Date(survey.segment.updatedAt);
-    }
-  }
-
-  if (survey.languages) {
-    survey.languages.forEach((surveyLanguage) => {
-      if (typeof surveyLanguage.language.createdAt === "string") {
-        surveyLanguage.language.createdAt = new Date(surveyLanguage.language.createdAt);
-      }
-      if (typeof surveyLanguage.language.updatedAt === "string") {
-        surveyLanguage.language.updatedAt = new Date(surveyLanguage.language.updatedAt);
-      }
-    });
-  }
-
-  return survey;
-};
-
 export const buildWhereClause = (filterCriteria?: TSurveyFilterCriteria) => {
   const whereClause: Prisma.SurveyWhereInput["AND"] = [];
 
