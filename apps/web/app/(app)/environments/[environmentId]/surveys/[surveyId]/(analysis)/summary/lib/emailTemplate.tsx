d73bff5f3ae72a99a08ed9cdcdd80f8c7f08f183
pandeymangg <anshuman.pandey9999@gmail.com>
2024-04-29 18:56:24 +0530

    fix: email embed styles

--- apps/web/app/(app)/environments/[environmentId]/surveys/[surveyId]/(analysis)/summary/lib/emailTemplate.tsx
+++ apps/web/app/(app)/environments/[environmentId]/surveys/[surveyId]/(analysis)/summary/lib/emailTemplate.tsx
@@ -1,10 +1,9 @@
 import { getPreviewEmailTemplateHtml } from "@formbricks/email/components/survey/PreviewEmailTemplste";
 import { WEBAPP_URL } from "@formbricks/lib/constants";
 import { getProductByEnvironmentId } from "@formbricks/lib/product/service";
-import { COLOR_DEFAULTS } from "@formbricks/lib/styling/constants";
 import { getSurvey } from "@formbricks/lib/survey/service";
 
-export const getEmailTemplateHtml = async (surveyId) => {
+export const getEmailTemplateHtml = async (surveyId: string) => {
   const survey = await getSurvey(surveyId);
   if (!survey) {
     throw new Error("Survey not found");
@@ -13,9 +12,30 @@ export const getEmailTemplateHtml = async (surveyId) => {
   if (!product) {
     throw new Error("Product not found");
   }
-  const brandColor = product.styling.brandColor?.light || COLOR_DEFAULTS.brandColor;
+
+  const getStyling = () => {
+    // allow style overwrite is disabled from the product
+    if (!product.styling.allowStyleOverwrite) {
+      return product.styling;
+    }
+
+    // allow style overwrite is enabled from the product
+    if (product.styling.allowStyleOverwrite) {
+      // survey style overwrite is disabled
+      if (!survey.styling?.overwriteThemeStyling) {
+        return product.styling;
+      }
+
+      // survey style overwrite is enabled
+      return survey.styling;
+    }
+
+    return product.styling;
+  };
+
+  const styling = getStyling();
   const surveyUrl = WEBAPP_URL + "/s/" + survey.id;
-  const html = getPreviewEmailTemplateHtml(survey, surveyUrl, brandColor);
+  const html = getPreviewEmailTemplateHtml(survey, surveyUrl, styling);
   const doctype =
     '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">';
   const htmlCleaned = html.toString().replace(doctype, "");
