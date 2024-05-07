ddd2b00c15d58702cd19886e97e0647aa08a93c7
Johannes <johannes@formbricks.com>
2024-05-07 13:06:56 +0200

    final tweaks

--- packages/surveys/src/components/general/SurveyCloseButton.tsx
+++ packages/surveys/src/components/general/SurveyCloseButton.tsx
@@ -4,13 +4,13 @@ interface SurveyCloseButtonProps {
 
 export const SurveyCloseButton = ({ onClose }: SurveyCloseButtonProps) => {
   return (
-    <div class=" absolute right-0 top-0 z-[1001] block pr-2 pt-2">
+    <div class="absolute right-0 top-0 z-[1001] block pr-1 pt-1">
       <button
         type="button"
         onClick={onClose}
-        class="text-close-button hover:text-close-button-focus focus:ring-close-button-focus relative h-5 w-5 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2">
+        class="text-close-button hover:text-close-button-focus focus:ring-close-button-focus relative h-4 w-4 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2">
         <svg
-          class="h-4 w-4"
+          class="h-3 w-3"
           fill="none"
           viewBox="0 0 24 24"
           strokeWidth="2"
