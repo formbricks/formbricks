ac2c85fbe2d263562858c5924b84b48ad7a3f62e
pandeymangg <anshuman.pandey9999@gmail.com>
2024-05-10 14:15:42 +0530

    fix: svg and links

--- packages/ui/icons/GoogleIcon.tsx
+++ packages/ui/icons/GoogleIcon.tsx
@@ -3,7 +3,7 @@ export const GoogleIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => {
     <svg
       stroke="currentColor"
       fill="currentColor"
-      stroke-width="0"
+      strokeWidth="0"
       viewBox="0 0 488 512"
       height="1em"
       width="1em"
