4898ec9e1075164a6f008382c14e8ce298c95a97
pandeymangg <anshuman.pandey9999@gmail.com>
2024-05-16 21:34:20 +0530

    fix: styling fix data migration

--- packages/database/data-migrations/20240320090315_add_form_styling/data-migration-fix.ts
+++ packages/database/data-migrations/20240320090315_add_form_styling/data-migration-fix.ts
@@ -3,52 +3,55 @@ import { Prisma, PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.$transaction(async (tx) => {
-    const startTime = Date.now();
-    console.log("Starting data migration for styling fixes...");
+  await prisma.$transaction(
+    async (tx) => {
+      const startTime = Date.now();
+      console.log("Starting data migration for styling fixes...");
 
-    const surveysWithProductOverwrites = await tx.survey.findMany({
-      where: {
-        productOverwrites: {
-          not: Prisma.JsonNull,
+      const surveysWithProductOverwrites = await tx.survey.findMany({
+        where: {
+          productOverwrites: {
+            not: Prisma.JsonNull,
+          },
         },
-      },
-    });
+      });
 
-    console.log(`Found ${surveysWithProductOverwrites.length} surveys with product overwrites to process.`);
+      console.log(`Found ${surveysWithProductOverwrites.length} surveys with product overwrites to process.`);
 
-    for (const survey of surveysWithProductOverwrites) {
-      if (survey.productOverwrites) {
-        const { brandColor, highlightBorderColor, ...rest } = survey.productOverwrites;
+      for (const survey of surveysWithProductOverwrites) {
+        if (survey.productOverwrites) {
+          const { brandColor, highlightBorderColor, ...rest } = survey.productOverwrites;
 
-        if (!brandColor && !highlightBorderColor) {
-          console.log(`Skipping survey ${survey.id} as it has no brandColor or highlightBorderColor`);
-          continue;
+          if (!brandColor && !highlightBorderColor) {
+            continue;
+          }
+
+          await tx.survey.update({
+            where: {
+              id: survey.id,
+            },
+            data: {
+              styling: {
+                ...(survey.styling ?? {}),
+                ...(brandColor && { brandColor: { light: brandColor } }),
+                ...(highlightBorderColor && { highlightBorderColor: { light: highlightBorderColor } }),
+                ...((brandColor || highlightBorderColor || Object.keys(survey.styling ?? {}).length > 0) && {
+                  overwriteThemeStyling: true,
+                }),
+              },
+              productOverwrites: {
+                ...rest,
+              },
+            },
+          });
         }
-
-        await tx.survey.update({
-          where: {
-            id: survey.id,
-          },
-          data: {
-            styling: {
-              ...(survey.styling ?? {}),
-              ...(brandColor && { brandColor: { light: brandColor } }),
-              ...(highlightBorderColor && { highlightBorderColor: { light: highlightBorderColor } }),
-              ...((brandColor || highlightBorderColor || Object.keys(survey.styling ?? {}).length > 0) && {
-                overwriteThemeStyling: true,
-              }),
-            },
-            productOverwrites: {
-              ...rest,
-            },
-          },
-        });
       }
-    }
 
-    console.log("Data migration for styling fixes complete.");
-  });
+      const endTime = Date.now();
+      console.log(`Data migration for styling fixes completed in ${(endTime - startTime) / 1000} seconds.`);
+    },
+    { timeout: 50000 }
+  );
 }
 
 main()
