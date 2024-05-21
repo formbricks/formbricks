fa379f83ca9c80dac0609213e30d63d192d72cba
pandeymangg <anshuman.pandey9999@gmail.com>
2024-05-21 14:45:01 +0530

    fix: previous result updated cache key

--- packages/ee/lib/service.ts
+++ packages/ee/lib/service.ts
@@ -7,27 +7,30 @@ import { TTeam } from "@formbricks/types/teams";
 
 import { prisma } from "../../database/src";
 
-const PREVIOUS_RESULTS_CACHE_TAG = "getPreviousResult";
+const getPreviousResultsCacheKeyAndTag = (hashedKey: string) => `getPreviousResult-${hashedKey}` as const;
 
 // This function is used to get the previous result of the license check from the cache
 // This might seem confusing at first since we only return the default value from this function,
 // but since we are using a cache and the cache key is the same, the cache will return the previous result - so this functions as a cache getter
-const getPreviousResult = (): Promise<{ active: boolean | null; lastChecked: Date }> =>
+const getPreviousResult = (hashedKey: string): Promise<{ active: boolean | null; lastChecked: Date }> =>
   cache(
     async () => ({
       active: null,
       lastChecked: new Date(0),
     }),
-    [PREVIOUS_RESULTS_CACHE_TAG],
+    [getPreviousResultsCacheKeyAndTag(hashedKey)],
     {
-      tags: [PREVIOUS_RESULTS_CACHE_TAG],
+      tags: [getPreviousResultsCacheKeyAndTag(hashedKey)],
     }
   )();
 
 // This function is used to set the previous result of the license check to the cache so that we can use it in the next call
 // Uses the same cache key as the getPreviousResult function
-const setPreviousResult = async (previousResult: { active: boolean | null; lastChecked: Date }) => {
-  revalidateTag(PREVIOUS_RESULTS_CACHE_TAG);
+const setPreviousResult = async (
+  hashedKey: string,
+  previousResult: { active: boolean | null; lastChecked: Date }
+) => {
+  revalidateTag(getPreviousResultsCacheKeyAndTag(hashedKey));
   const { lastChecked, active } = previousResult;
 
   await cache(
@@ -35,9 +38,9 @@ const setPreviousResult = async (previousResult: { active: boolean | null; lastC
       active,
       lastChecked,
     }),
-    [PREVIOUS_RESULTS_CACHE_TAG],
+    [getPreviousResultsCacheKeyAndTag(hashedKey)],
     {
-      tags: [PREVIOUS_RESULTS_CACHE_TAG],
+      tags: [getPreviousResultsCacheKeyAndTag(hashedKey)],
     }
   )();
 };
@@ -50,10 +53,10 @@ export const getIsEnterpriseEdition = async (): Promise<boolean> => {
   const hashedKey = hashString(ENTERPRISE_LICENSE_KEY);
 
   if (E2E_TESTING) {
-    const previousResult = await getPreviousResult();
+    const previousResult = await getPreviousResult(hashedKey);
     if (previousResult.lastChecked.getTime() === new Date(0).getTime()) {
       // first call
-      await setPreviousResult({ active: true, lastChecked: new Date() });
+      await setPreviousResult(hashedKey, { active: true, lastChecked: new Date() });
       return true;
     } else if (new Date().getTime() - previousResult.lastChecked.getTime() > 60 * 60 * 1000) {
       // Fail after 1 hour
@@ -107,17 +110,17 @@ export const getIsEnterpriseEdition = async (): Promise<boolean> => {
     { revalidate: 60 * 60 * 24 }
   )();
 
-  const previousResult = await getPreviousResult();
+  const previousResult = await getPreviousResult(hashedKey);
 
   if (previousResult.active === null) {
     if (isValid === null) {
-      await setPreviousResult({ active: false, lastChecked: new Date() });
+      await setPreviousResult(hashedKey, { active: false, lastChecked: new Date() });
       return false;
     }
   }
 
   if (isValid !== null) {
-    await setPreviousResult({ active: isValid, lastChecked: new Date() });
+    await setPreviousResult(hashedKey, { active: isValid, lastChecked: new Date() });
     return isValid;
   } else {
     // if result is undefined -> error
