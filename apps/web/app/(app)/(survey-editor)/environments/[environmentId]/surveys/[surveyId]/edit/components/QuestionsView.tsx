8b4f662f5cdc809526c4abae8cb27b55fec6485c
Dhruwang <dhruwangjariwala18@gmail.com>
2024-05-22 15:49:08 +0530

    fix: highlighting for question with cyclic logic

--- apps/web/app/(app)/(survey-editor)/environments/[environmentId]/surveys/[surveyId]/edit/components/QuestionsView.tsx
+++ apps/web/app/(app)/(survey-editor)/environments/[environmentId]/surveys/[surveyId]/edit/components/QuestionsView.tsx
@@ -19,7 +19,12 @@ import { checkForEmptyFallBackValue, extractRecallInfo } from "@formbricks/lib/u
 import { TProduct } from "@formbricks/types/product";
 import { TSurvey, TSurveyQuestion } from "@formbricks/types/surveys";
 
-import { isCardValid, validateQuestion, validateSurveyQuestionsInBatch } from "../lib/validation";
+import {
+  findQuestionsWithCyclicLogic,
+  isCardValid,
+  validateQuestion,
+  validateSurveyQuestionsInBatch,
+} from "../lib/validation";
 import { AddQuestionButton } from "./AddQuestionButton";
 import { EditThankYouCard } from "./EditThankYouCard";
 import { EditWelcomeCard } from "./EditWelcomeCard";
@@ -89,8 +94,12 @@ export const QuestionsView = ({
     const isFirstQuestion = question.id === localSurvey.questions[0].id;
     let temp = structuredClone(invalidQuestions);
     if (validateQuestion(question, surveyLanguages, isFirstQuestion)) {
-      temp = invalidQuestions.filter((id) => id !== question.id);
-      setInvalidQuestions(temp);
+      // If question is valid, we now check for cyclic logic
+      const questionsWithCyclicLogic = findQuestionsWithCyclicLogic(localSurvey.questions);
+      if (!questionsWithCyclicLogic.includes(question.id)) {
+        temp = invalidQuestions.filter((id) => id !== question.id);
+        setInvalidQuestions(temp);
+      }
     } else if (!invalidQuestions.includes(question.id)) {
       temp.push(question.id);
       setInvalidQuestions(temp);
