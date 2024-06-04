9d3507a9f59e074cb5aa2690c147584f1fe06d19
Matthias Nannt <mail@matthiasnannt.com>
2024-06-04 16:47:12 +0200

    fix: storybook app

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -5,23 +5,27 @@ import { dirname, join } from "path";
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
-const getAbsolutePath = (value: string): any => {
+function getAbsolutePath(value) {
   return dirname(require.resolve(join(value, "package.json")));
-};
+}
 
-export const config: StorybookConfig = {
-  stories: ["../../../packages/ui/**/stories.@(js|jsx|mjs|ts|tsx)"],
+const config: StorybookConfig = {
+  stories: [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    "../../../packages/ui/**/stories.@(js|jsx|mjs|ts|tsx)",
+  ],
+  //stories: ["../../../packages/ui/**/stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
+    getAbsolutePath("@storybook/addon-onboarding"),
     getAbsolutePath("@storybook/addon-links"),
     getAbsolutePath("@storybook/addon-essentials"),
-    getAbsolutePath("@storybook/addon-onboarding"),
+    getAbsolutePath("@chromatic-com/storybook"),
     getAbsolutePath("@storybook/addon-interactions"),
   ],
   framework: {
     name: getAbsolutePath("@storybook/react-vite"),
     options: {},
   },
-  docs: {
-    autodocs: "tag",
-  },
 };
+export default config;
