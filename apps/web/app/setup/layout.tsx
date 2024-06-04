c6a3341b69619f62ebd2a5762f81496b8cd9a321
Matthias Nannt <mail@matthiasnannt.com>
2024-06-04 13:57:16 +0200

    small design changes

--- apps/web/app/setup/layout.tsx
+++ apps/web/app/setup/layout.tsx
@@ -7,7 +7,7 @@ const SetupLayout = async ({ children }: { children: React.ReactNode }) => {
     <>
       <Toaster />
       <div className="flex h-full w-full items-center justify-center bg-slate-50">
-        <div className="flex w-[40rem] flex-col items-center space-y-4 rounded-lg border bg-white p-20 text-center shadow">
+        <div className="flex w-[40rem] flex-col items-center space-y-4 rounded-lg border bg-white p-12 text-center shadow">
           <FormbricksLogo className="h-20 w-20 rounded-lg bg-slate-900 p-2" />
           {children}
         </div>
