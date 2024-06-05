990de1532da60b40fb1bfdcf43ca6ba367ef4ff1
Matthias Nannt <mail@matthiasnannt.com>
2024-06-05 11:34:01 +0200

    use arrow function

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -5,9 +5,9 @@ import { dirname, join } from "path";
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
-function getAbsolutePath(value) {
+const getAbsolutePath = (value) => {
   return dirname(require.resolve(join(value, "package.json")));
-}
+};
 
 const config: StorybookConfig = {
   stories: [
