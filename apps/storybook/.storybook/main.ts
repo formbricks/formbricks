4d207cee18c69b9a69525b8c6bf983f72fda145c
Matthias Nannt <mail@matthiasnannt.com>
2024-06-05 11:35:08 +0200

    re-add type

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -5,7 +5,7 @@ import { dirname, join } from "path";
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
-const getAbsolutePath = (value) => {
+const getAbsolutePath = (value: string) => {
   return dirname(require.resolve(join(value, "package.json")));
 };
 
