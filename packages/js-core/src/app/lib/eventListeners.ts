915fe00434ab0f98c63ab5db61ceb05b512ed302
Piyush Gupta <56182734+gupta-piyush19@users.noreply.github.com>
2024-06-11 13:27:46 +0530

    fix: vercel build error and app reset method (#2750)

--- packages/js-core/src/app/lib/eventListeners.ts
+++ packages/js-core/src/app/lib/eventListeners.ts
@@ -7,27 +7,27 @@ import {
   removeExitIntentListener,
   removePageUrlEventListeners,
   removeScrollDepthListener,
-} from "../../shared/noCodeActions";
+} from "../lib/noCodeActions";
 import { addExpiryCheckListener, removeExpiryCheckListener } from "./sync";
 
 let areRemoveEventListenersAdded = false;
 
 export const addEventListeners = (): void => {
   addExpiryCheckListener();
-  addPageUrlEventListeners("app");
-  addClickEventListener("app");
-  addExitIntentListener("app");
-  addScrollDepthListener("app");
+  addPageUrlEventListeners();
+  addClickEventListener();
+  addExitIntentListener();
+  addScrollDepthListener();
 };
 
 export const addCleanupEventListeners = (): void => {
   if (areRemoveEventListenersAdded) return;
   window.addEventListener("beforeunload", () => {
     removeExpiryCheckListener();
-    removePageUrlEventListeners("app");
-    removeClickEventListener("app");
-    removeExitIntentListener("app");
-    removeScrollDepthListener("app");
+    removePageUrlEventListeners();
+    removeClickEventListener();
+    removeExitIntentListener();
+    removeScrollDepthListener();
   });
   areRemoveEventListenersAdded = true;
 };
@@ -36,19 +36,19 @@ export const removeCleanupEventListeners = (): void => {
   if (!areRemoveEventListenersAdded) return;
   window.removeEventListener("beforeunload", () => {
     removeExpiryCheckListener();
-    removePageUrlEventListeners("app");
-    removeClickEventListener("app");
-    removeExitIntentListener("app");
-    removeScrollDepthListener("app");
+    removePageUrlEventListeners();
+    removeClickEventListener();
+    removeExitIntentListener();
+    removeScrollDepthListener();
   });
   areRemoveEventListenersAdded = false;
 };
 
 export const removeAllEventListeners = (): void => {
   removeExpiryCheckListener();
-  removePageUrlEventListeners("app");
-  removeClickEventListener("app");
-  removeExitIntentListener("app");
-  removeScrollDepthListener("app");
+  removePageUrlEventListeners();
+  removeClickEventListener();
+  removeExitIntentListener();
+  removeScrollDepthListener();
   removeCleanupEventListeners();
 };
