7ff69d6ebbd1d5a8f252cbb18f3777d55f7b9628
Piyush Gupta <piyushguptaa2z123@gmail.com>
2024-06-14 04:08:23 +0530

    fix: product creation bug

--- apps/web/app/api/v1/users/route.ts
+++ apps/web/app/api/v1/users/route.ts
@@ -103,10 +103,6 @@ export const POST = async (request: Request) => {
         await createMembership(organization.id, user.id, { role: "owner", accepted: true });
         const product = await createProduct(organization.id, {
           name: "My Product",
-          config: {
-            channel: null,
-            industry: null,
-          },
         });
 
         const updatedNotificationSettings = {
