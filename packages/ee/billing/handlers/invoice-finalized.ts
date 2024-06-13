e94955085034726c5b6df2317f81f24051a01ab7
pandeymangg <anshuman.pandey9999@gmail.com>
2024-06-13 20:39:54 +0530

    fix: invoice period

--- packages/ee/billing/handlers/invoice-finalized.ts
+++ packages/ee/billing/handlers/invoice-finalized.ts
@@ -1,12 +1,5 @@
 import Stripe from "stripe";
-import { STRIPE_API_VERSION } from "@formbricks/lib/constants";
-import { env } from "@formbricks/lib/env";
 import { getOrganization, updateOrganization } from "@formbricks/lib/organization/service";
-
-const stripe = new Stripe(env.STRIPE_SECRET_KEY!, {
-  // https://github.com/stripe/stripe-node#configuration
-  apiVersion: STRIPE_API_VERSION,
-});
 
 export const handleInvoiceFinalized = async (event: Stripe.Event) => {
   const invoice = event.data.object as Stripe.Invoice;
