85356b40d3a2ba4575edf0b485cb43d19750d1e2
Piyush Gupta <piyushguptaa2z123@gmail.com>
2024-06-14 11:32:56 +0530

    feat: add file size limit error message for FileUploadQuestionForm

--- apps/web/app/(app)/(survey-editor)/environments/[environmentId]/surveys/[surveyId]/edit/components/FileUploadQuestionForm.tsx
+++ apps/web/app/(app)/(survey-editor)/environments/[environmentId]/surveys/[surveyId]/edit/components/FileUploadQuestionForm.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { PlusIcon, XCircleIcon } from "lucide-react";
+import Link from "next/link";
 import { useMemo, useState } from "react";
 import { toast } from "react-hot-toast";
 import { extractLanguageCodes } from "@formbricks/lib/i18n/utils";
@@ -26,6 +27,7 @@ interface FileUploadFormProps {
   setSelectedLanguageCode: (languageCode: string) => void;
   isInvalid: boolean;
   attributeClasses: TAttributeClass[];
+  isFormbricksCloud: boolean;
 }
 
 export const FileUploadQuestionForm = ({
@@ -38,8 +40,10 @@ export const FileUploadQuestionForm = ({
   selectedLanguageCode,
   setSelectedLanguageCode,
   attributeClasses,
+  isFormbricksCloud,
 }: FileUploadFormProps): JSX.Element => {
   const [extension, setExtension] = useState("");
+  const [isMaxSizeError, setMaxSizeError] = useState(false);
   const {
     billingInfo,
     error: billingInfoError,
@@ -191,6 +195,9 @@ export const FileUploadQuestionForm = ({
 
                   if (parsedValue > maxSizeInMBLimit) {
                     toast.error(`Max file size limit is ${maxSizeInMBLimit} MB`);
+                    if (isFormbricksCloud) {
+                      setMaxSizeError(true);
+                    }
                     updateQuestion(questionIdx, { maxSizeInMB: maxSizeInMBLimit });
                     return;
                   }
@@ -201,6 +208,19 @@ export const FileUploadQuestionForm = ({
               />
               MB
             </p>
+            {isMaxSizeError && (
+              <p className="text-xs text-red-500">
+                Max file size limit is {maxSizeInMBLimit} MB. If you need more, please upgrade your plan. You
+                can upgrade your plan in the{" "}
+                <Link
+                  className="underline"
+                  target="_blank"
+                  href={`/environments/${localSurvey.environmentId}/settings/billing`}>
+                  Billing & Plan
+                </Link>{" "}
+                section.
+              </p>
+            )}
           </label>
         </AdvancedOptionToggle>
 
