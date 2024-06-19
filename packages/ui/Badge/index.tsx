be1d1b31d0816d05bb2fde0dfd4633923165d49d
Johannes <johannes@formbricks.com>
2024-06-19 16:00:35 +0200

    add launch and pricing ui tweaks

--- packages/ui/Badge/index.tsx
+++ packages/ui/Badge/index.tsx
@@ -21,14 +21,21 @@ export const Badge: React.FC<BadgeProps> = ({
 }) => {
   const bgColor = {
     warning: "bg-amber-100",
-    success: "bg-green-100",
+    success: "bg-emerald-100",
     error: "bg-red-100",
     gray: "bg-slate-100",
   };
 
+  const borderColor = {
+    warning: "border-amber-200",
+    success: "border-emerald-200",
+    error: "border-red-200",
+    gray: "border-slate-200",
+  };
+
   const textColor = {
     warning: "text-amber-800",
-    success: "text-green-800",
+    success: "text-emerald-800",
     error: "text-red-800",
     gray: "text-slate-600",
   };
@@ -44,8 +51,9 @@ export const Badge: React.FC<BadgeProps> = ({
   return (
     <span
       className={cn(
-        "inline-flex cursor-default items-center rounded-full font-medium",
+        "inline-flex cursor-default items-center rounded-full border font-medium",
         bgColor[type],
+        borderColor[type],
         textColor[type],
         padding[size],
         textSize,
