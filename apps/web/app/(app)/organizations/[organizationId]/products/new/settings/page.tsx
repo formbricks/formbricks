2512400ef80311b60998be9a200ffb053c53ae2d
Matthias Nannt <mail@matthiasnannt.com>
2024-06-19 12:20:30 +0200

    add X button to all onboarding pages

--- apps/web/app/(app)/organizations/[organizationId]/products/new/settings/page.tsx
+++ apps/web/app/(app)/organizations/[organizationId]/products/new/settings/page.tsx
@@ -1,9 +1,12 @@
 import { ProductSettings } from "@/app/(app)/organizations/[organizationId]/products/new/settings/components/ProductSettings";
+import { XIcon } from "lucide-react";
 import { notFound } from "next/navigation";
 import { DEFAULT_BRAND_COLOR } from "@formbricks/lib/constants";
+import { getProducts } from "@formbricks/lib/product/service";
 import { getCustomHeadline } from "@formbricks/lib/utils/strings";
 import { startsWithVowel } from "@formbricks/lib/utils/strings";
 import { TProductConfigChannel, TProductConfigIndustry } from "@formbricks/types/product";
+import { Button } from "@formbricks/ui/Button";
 import { Header } from "@formbricks/ui/Header";
 
 interface ProductSettingsPageProps {
@@ -21,6 +24,7 @@ const Page = async ({ params, searchParams }: ProductSettingsPageProps) => {
   const industry = searchParams.industry;
   if (!channel || !industry) return notFound();
   const customHeadline = getCustomHeadline(channel, industry);
+  const products = await getProducts(params.organizationId);
 
   return (
     <div className="flex min-h-full min-w-full flex-col items-center justify-center space-y-12">
@@ -41,6 +45,14 @@ const Page = async ({ params, searchParams }: ProductSettingsPageProps) => {
         industry={industry}
         defaultBrandColor={DEFAULT_BRAND_COLOR}
       />
+      {products.length >= 1 && (
+        <Button
+          className="absolute right-5 top-5 !mt-0 text-slate-500 hover:text-slate-700"
+          variant="minimal"
+          href={"/"}>
+          <XIcon className="h-7 w-7" strokeWidth={1.5} />
+        </Button>
+      )}
     </div>
   );
 };
