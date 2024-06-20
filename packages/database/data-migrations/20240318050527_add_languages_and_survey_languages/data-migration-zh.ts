eb6e81bcd5c8bf2a5524eaf1a7ff7895e95e8d8d
Dhruwang <dhruwangjariwala18@gmail.com>
2024-06-20 16:33:04 +0530

    migration script for chinese language

---
+++ packages/database/data-migrations/20240318050527_add_languages_and_survey_languages/data-migration-zh.ts
@@ -0,0 +1,121 @@
+import { PrismaClient } from "@prisma/client";
+import {
+  TSurveyLanguage,
+  TSurveyQuestion,
+  TSurveyThankYouCard,
+  TSurveyWelcomeCard,
+} from "@formbricks/types/surveys";
+import {
+  updateLanguageCodeForQuestion,
+  updateLanguageCodeForWelcomeCard,
+  updateLanguageCodeThankYouCard,
+} from "./lib/i18n";
+
+const prisma = new PrismaClient();
+
+const main = async () => {
+  await prisma.$transaction(
+    async (tx) => {
+      const startTime = Date.now();
+      console.log("Starting data migration...");
+
+      // Fetch all surveys
+      const surveys: {
+        id: string;
+        questions: TSurveyQuestion[];
+        welcomeCard: TSurveyWelcomeCard;
+        thankYouCard: TSurveyThankYouCard;
+        languages: TSurveyLanguage[];
+      }[] = await tx.survey.findMany({
+        select: {
+          id: true,
+          questions: true,
+          welcomeCard: true,
+          thankYouCard: true,
+          languages: {
+            select: {
+              default: true,
+              enabled: true,
+              language: {
+                select: {
+                  id: true,
+                  code: true,
+                  alias: true,
+                  createdAt: true,
+                  updatedAt: true,
+                },
+              },
+            },
+          },
+        },
+      });
+
+      if (surveys.length === 0) {
+        // stop the migration if there are no surveys
+        console.log("No Surveys found");
+        return;
+      }
+      let transformedSurveyCount = 0;
+
+      for (const survey of surveys) {
+        const containsChineseTranslations = survey.languages.find((surveyLanguage) => {
+          return surveyLanguage.language.code === "zh";
+        });
+        if (!containsChineseTranslations) {
+          continue;
+        }
+        const updatedSurvey = structuredClone(survey);
+        console.log("Transforming survey with id:", survey.id);
+        transformedSurveyCount++;
+        // check welcome card
+        updatedSurvey.welcomeCard = updateLanguageCodeForWelcomeCard(survey.welcomeCard, "zh", "zh-Hans");
+
+        // check Thank you card
+        updatedSurvey.thankYouCard = updateLanguageCodeThankYouCard(survey.thankYouCard, "zh", "zh-Hans");
+
+        // check questions
+        if (updatedSurvey.questions.length > 0) {
+          updatedSurvey.questions = survey.questions.map((question) => {
+            return updateLanguageCodeForQuestion(question, "zh", "zh-Hans");
+          });
+        }
+        // Save the translated survey
+        await tx.survey.update({
+          where: { id: survey.id },
+          data: {
+            welcomeCard: updatedSurvey.welcomeCard,
+            thankYouCard: updatedSurvey.thankYouCard,
+            questions: updatedSurvey.questions,
+          },
+        });
+      }
+
+      console.log(transformedSurveyCount, " surveys transformed");
+
+      console.log("updating survey languages");
+      await tx.language.updateMany({
+        where: {
+          code: "zh",
+        },
+        data: {
+          code: "zh-Hans",
+        },
+      });
+      console.log("survey language updated");
+      const endTime = Date.now();
+      console.log(`Data migration completed. Total time: ${(endTime - startTime) / 1000}s`);
+    },
+    {
+      timeout: 180000, // 3 minutes
+    }
+  );
+};
+
+main()
+  .catch((e: Error) => {
+    console.error("Error during migration: ", e.message);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
