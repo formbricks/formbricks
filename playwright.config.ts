2253578f1ce7a5d7cfff835f39f0b76b0d5153f9
pandeymangg <anshuman.pandey9999@gmail.com>
2024-06-24 09:40:36 +0530

    fix: e2e

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,5 @@
 import { defineConfig, devices } from "@playwright/test";
 
-// import os from "os";
-
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
@@ -20,7 +18,6 @@ export default defineConfig({
   /* Timeout for each test */
   timeout: 120000,
   /* Opt out of parallel tests on CI. */
-  // workers: os.cpus().length,
   workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
@@ -39,7 +36,6 @@ export default defineConfig({
     {
       name: "chromium",
       use: { ...devices["Desktop Chrome"] },
-      testMatch: "**/*.spec.ts",
     },
 
     // {
