a3a2b6011f819ce5536b949140cde557078adfc8
pandeymangg <anshuman.pandey9999@gmail.com>
2024-06-25 17:29:41 +0530

    wip

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,7 @@ import { defineConfig, devices } from "@playwright/test";
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-require("dotenv").config({ path: ".env" });
+// require("dotenv").config({ path: ".env" });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
