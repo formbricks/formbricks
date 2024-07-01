eb0d176ecd959eacf63005eb8f7c1611daf50dc8
pandeymangg <anshuman.pandey9999@gmail.com>
2024-07-01 12:31:35 +0530

    fix: cache headers

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -148,6 +148,19 @@ const nextConfig = {
           },
         ],
       },
+      {
+        // Force disable caching for any NextAuth api routes. We need to do this because by default
+        // these API endpoints do not return a Cache-Control header. If the header is missing, FrontDoor
+        // CDN **will** cache the pages, which is a security risk and can return the wrong user:
+        // https://docs.microsoft.com/en-us/azure/frontdoor/front-door-caching#cache-expiration
+        source: "/api/auth/:slug",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "private, no-store",
+          },
+        ],
+      },
     ];
   },
   env: {
