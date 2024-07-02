257e84d3126f02c1ef6b092aa1ed0ea64a1fbb94
Dhruwang <dhruwangjariwala18@gmail.com>
2024-07-02 14:33:01 +0530

    dost to unix

--- docker/formbricks.sh
+++ docker/formbricks.sh
@@ -1,71 +1,58 @@
+#!/bin/env bash
 set -e
 ubuntu_version=$(lsb_release -a 2>/dev/null | grep -v "No LSB modules are available." | grep "Description:" | awk -F "Description:\t" '{print $2}')
-
 install_formbricks() {
   # Friendly welcome
-  echo "🧱 Welcome to the Formbricks Setup Script"
+  echo ">Ò Welcome to the Formbricks Setup Script"
   echo ""
-  echo "🛸 Fasten your seatbelts! We're setting up your Formbricks environment on your $ubuntu_version server."
+  echo "=¯ Fasten your seatbelts! We're setting up your Formbricks environment on your $ubuntu_version server."
   echo ""
-
   # Remove any old Docker installations, without stopping the script if they're not found
-  echo "🧹 Time to sweep away any old Docker installations."
+  echo ">˘ Time to sweep away any old Docker installations."
   sudo apt-get remove docker docker-engine docker.io containerd runc >/dev/null 2>&1 || true
-
   # Update package list
-  echo "🔄 Updating your package list."
+  echo "= Updating your package list."
   sudo apt-get update >/dev/null 2>&1
-
   # Install dependencies
-  echo "📦 Installing the necessary dependencies."
+  echo "=Ê Installing the necessary dependencies."
   sudo apt-get install -y \
     ca-certificates \
     curl \
     gnupg \
     lsb-release >/dev/null 2>&1
-
   # Set up Docker's official GPG key & stable repository
-  echo "🔑 Adding Docker's official GPG key and setting up the stable repository."
+  echo "= Adding Docker's official GPG key and setting up the stable repository."
   sudo mkdir -m 0755 -p /etc/apt/keyrings >/dev/null 2>&1
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg >/dev/null 2>&1
   echo \
     "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null 2>&1
-
   # Update package list again
-  echo "🔄 Updating your package list again."
+  echo "= Updating your package list again."
   sudo apt-get update >/dev/null 2>&1
-
   # Install Docker
-  echo "🐳 Installing Docker."
+  echo "=3 Installing Docker."
   sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin >/dev/null 2>&1
-
   # Test Docker installation
-  echo "🚀 Testing your Docker installation."
+  echo "=Ä Testing your Docker installation."
   if docker --version >/dev/null 2>&1; then
-    echo "🎉 Docker is installed!"
+    echo "<â Docker is installed!"
   else
-    echo "❌ Docker is not installed. Please install Docker before proceeding."
+    echo "L Docker is not installed. Please install Docker before proceeding."
     exit 1
   fi
-
   # Adding your user to the Docker group
-  echo "🐳 Adding your user to the Docker group to avoid using sudo with docker commands."
+  echo "=3 Adding your user to the Docker group to avoid using sudo with docker commands."
   sudo groupadd docker >/dev/null 2>&1 || true
   sudo usermod -aG docker $USER >/dev/null 2>&1
-
-  echo "🎉 Hooray! Docker is all set and ready to go. You're now ready to run your Formbricks instance!"
-
+  echo "<â Hooray! Docker is all set and ready to go. You're now ready to run your Formbricks instance!"
   mkdir -p formbricks && cd formbricks
-  echo "📁 Created Formbricks Quickstart directory at ./formbricks."
-
+  echo "=¡ Created Formbricks Quickstart directory at ./formbricks."
   # Ask the user for their email address
-  echo "💡 Please enter your email address for the SSL certificate:"
+  echo "=° Please enter your email address for the SSL certificate:"
   read email_address
-
   # Installing Traefik
-  echo "🚗 Configuring Traefik..."
-
+  echo "=ó Configuring Traefik..."
   cat <<EOT >traefik.yaml
 entryPoints:
   web:
@@ -93,40 +80,29 @@ certificatesResolvers:
       caServer: "https://acme-v01.api.letsencrypt.org/directory"
       tlsChallenge: {}
 EOT
-
-  echo "💡 Created traefik.yaml file with your provided email address."
-
+  echo "=° Created traefik.yaml file with your provided email address."
   touch acme.json
   chmod 600 acme.json
-  echo "💡 Created acme.json file with correct permissions."
-
+  echo "=° Created acme.json file with correct permissions."
   # Ask the user for their domain name
-  echo "🔗 Please enter your domain name for the SSL certificate (🚨 do NOT enter the protocol (http/https/etc)):"
+  echo "= Please enter your domain name for the SSL certificate (=® do NOT enter the protocol (http/https/etc)):"
   read domain_name
-
   # Prompt for email service setup
   read -p "Do you want to set up the email service? (yes/no) You will need SMTP credentials for the same! " email_service
   if [[ $email_service == "yes" ]]; then
     echo "Please provide the following email service details: "
-
     echo -n "Enter your SMTP configured Email ID: "
     read mail_from
-
     echo -n "Enter your SMTP Host URL: "
     read smtp_host
-
     echo -n "Enter your SMTP Host Port: "
     read smtp_port
-
     echo -n "Enter your SMTP username: "
     read smtp_user
-
     echo -n "Enter your SMTP password: "
     read smtp_password
-
     echo -n "Enable Secure SMTP (use SSL)? Enter 1 for yes and 0 for no: "
     read smtp_secure_enabled
-
   else
     mail_from=""
     smtp_host=""
@@ -135,19 +111,14 @@ EOT
     smtp_password=""
     smtp_secure_enabled=0
   fi
-
-  echo "📥 Downloading docker-compose.yml from Formbricks GitHub repository..."
+  echo "=Â Downloading docker-compose.yml from Formbricks GitHub repository..."
   curl -o docker-compose.yml https://raw.githubusercontent.com/formbricks/formbricks/main/docker/docker-compose.yml
-
-  echo "🚙 Updating docker-compose.yml with your custom inputs..."
+  echo "=ô Updating docker-compose.yml with your custom inputs..."
   sed -i "/WEBAPP_URL:/s|WEBAPP_URL:.*|WEBAPP_URL: \"https://$domain_name\"|" docker-compose.yml
-
   nextauth_secret=$(openssl rand -hex 32) && sed -i "/NEXTAUTH_SECRET:$/s/NEXTAUTH_SECRET:.*/NEXTAUTH_SECRET: $nextauth_secret/" docker-compose.yml
-  echo "🚗 NEXTAUTH_SECRET updated successfully!"
-
+  echo "=ó NEXTAUTH_SECRET updated successfully!"
   encryption_key=$(openssl rand -hex 32) && sed -i "/ENCRYPTION_KEY:$/s/ENCRYPTION_KEY:.*/ENCRYPTION_KEY: $encryption_key/" docker-compose.yml
-  echo "🚗 ENCRYPTION_KEY updated successfully!"
-
+  echo "=ó ENCRYPTION_KEY updated successfully!"
   if [[ -n $mail_from ]]; then
     sed -i "s|# MAIL_FROM:|MAIL_FROM: \"$mail_from\"|" docker-compose.yml
     sed -i "s|# SMTP_HOST:|SMTP_HOST: \"$smtp_host\"|" docker-compose.yml
@@ -156,7 +127,6 @@ EOT
     sed -i "s|# SMTP_USER:|SMTP_USER: \"$smtp_user\"|" docker-compose.yml
     sed -i "s|# SMTP_PASSWORD:|SMTP_PASSWORD: \"$smtp_password\"|" docker-compose.yml
   fi
-
   awk -v domain_name="$domain_name" '
 /formbricks:/,/^ *$/ {
     if ($0 ~ /depends_on:/) {
@@ -192,65 +162,53 @@ EOT
 }
 1
 ' docker-compose.yml >tmp.yml && mv tmp.yml docker-compose.yml
-
   newgrp docker <<END
-
 docker compose up -d
-
-echo "🔗 To edit more variables and deeper config, go to the formbricks/docker-compose.yml, edit the file, and restart the container!"
-
-echo "🚨 Make sure you have set up the DNS records as well as inbound rules for the domain name and IP address of this instance."
+echo "= To edit more variables and deeper config, go to the formbricks/docker-compose.yml, edit the file, and restart the container!"
+echo "=® Make sure you have set up the DNS records as well as inbound rules for the domain name and IP address of this instance."
 echo ""
-echo "🎉 All done! Please setup your Formbricks instance by visiting your domain at https://$domain_name. You can check the status of Formbricks & Traefik with 'cd formbricks && sudo docker compose ps.'"
-
+echo "<â All done! Please setup your Formbricks instance by visiting your domain at https://$domain_name. You can check the status of Formbricks & Traefik with 'cd formbricks && sudo docker compose ps.'"
 END
-
 }
-
 uninstall_formbricks() {
-  echo "🗑️ Preparing to Uninstalling Formbricks..."
+  echo "=— Preparing to Uninstalling Formbricks..."
   read -p "Are you sure you want to uninstall Formbricks? This will delete all the data associated with it! (yes/no): " uninstall_confirmation
   if [[ $uninstall_confirmation == "yes" ]]; then
     cd formbricks
     sudo docker compose down
     cd ..
     sudo rm -rf formbricks
-    echo "🛑 Formbricks uninstalled successfully!"
+    echo "=— Formbricks uninstalled successfully!"
   else
-    echo "❌ Uninstalling Formbricks has been cancelled."
+    echo "L Uninstalling Formbricks has been cancelled."
   fi
 }
-
 stop_formbricks() {
-  echo "🛑 Stopping Formbricks..."
+  echo "=— Stopping Formbricks..."
   cd formbricks
   sudo docker compose down
-  echo "🎉 Formbricks instance stopped successfully!"
+  echo "<â Formbricks instance stopped successfully!"
 }
-
 update_formbricks() {
-  echo "🔄 Updating Formbricks..."
+  echo "= Updating Formbricks..."
   cd formbricks
   sudo docker compose pull
   sudo docker compose down
   sudo docker compose up -d
-  echo "🎉 Formbricks updated successfully!"
-  echo "🎉 Check the status of Formbricks & Traefik with 'cd formbricks && sudo docker compose logs.'"
+  echo "<â Formbricks updated successfully!"
+  echo "<â Check the status of Formbricks & Traefik with 'cd formbricks && sudo docker compose logs.'"
 }
-
 restart_formbricks() {
-  echo "🔄 Restarting Formbricks..."
+  echo "= Restarting Formbricks..."
   cd formbricks
   sudo docker compose restart
-  echo "🎉 Formbricks restarted successfully!"
+  echo "<â Formbricks restarted successfully!"
 }
-
 get_logs() {
-  echo "📃 Getting Formbricks logs..."
+  echo "=√ Getting Formbricks logs..."
   cd formbricks
   sudo docker compose logs
 }
-
 case "$1" in
 install)
   install_formbricks
@@ -271,7 +229,7 @@ uninstall)
   uninstall_formbricks
   ;;
 *)
-  echo "🚀 Executing default step of installing Formbricks"
+  echo "=Ä Executing default step of installing Formbricks"
   install_formbricks
   ;;
 esac
