b06620246a182bbc39e55ab4091d34bc0681c0ce
Piyush Gupta <piyushguptaa2z123@gmail.com>
2024-07-08 15:38:00 +0530

    chore: Remove defineMetadataSchema from actionClient

--- packages/lib/actionClient/index.ts
+++ packages/lib/actionClient/index.ts
@@ -1,16 +1,9 @@
 import { getServerSession } from "next-auth";
 import { DEFAULT_SERVER_ERROR_MESSAGE, createSafeActionClient } from "next-safe-action";
-import { z } from "zod";
-import { ZOperation, ZResource } from "@formbricks/types/actionClient";
 import { AuthenticationError, AuthorizationError, ResourceNotFoundError } from "@formbricks/types/errors";
 import { authOptions } from "../authOptions";
 
 export const actionClient = createSafeActionClient({
-  defineMetadataSchema() {
-    return z.object({
-      rules: z.tuple([ZResource, ZOperation]),
-    });
-  },
   handleReturnedServerError(e) {
     if (e instanceof ResourceNotFoundError) {
       return e.message;
