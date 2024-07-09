ddb3056223e416b1c93850001705cb692b9cbb4a
Matthias Nannt <mail@matthiasnannt.com>
2024-07-09 11:07:20 +0200

    chore: prepare 2.3.1 release

--- packages/lib/constants.ts
+++ packages/lib/constants.ts
@@ -167,7 +167,7 @@ export const CUSTOMER_IO_SITE_ID = env.CUSTOMER_IO_SITE_ID;
 export const CUSTOMER_IO_API_KEY = env.CUSTOMER_IO_API_KEY;
 export const UNSPLASH_ACCESS_KEY = env.UNSPLASH_ACCESS_KEY;
 
-export const STRIPE_API_VERSION = "2024-04-10";
+export const STRIPE_API_VERSION = "2024-06-20";
 
 // Maximum number of attribute classes allowed:
 export const MAX_ATTRIBUTE_CLASSES_PER_ENVIRONMENT = 150 as const;
