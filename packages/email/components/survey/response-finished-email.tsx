057fcd1cf8c9d9f9b61fc1572d52c725815e1194
Dhruwang <dhruwangjariwala18@gmail.com>
2024-07-10 20:24:18 +0530

    add hidden field to response finished email

--- packages/email/components/survey/response-finished-email.tsx
+++ packages/email/components/survey/response-finished-email.tsx
@@ -91,6 +91,23 @@ export function ResponseFinishedEmail({
               </Row>
             );
           })}
+          {survey.hiddenFields.fieldIds?.map((hiddenField) => {
+            const hiddenFieldResponse = response.data[hiddenField];
+            if (hiddenFieldResponse && typeof hiddenFieldResponse === "string") {
+              return (
+                <Row key={hiddenField}>
+                  <Column className="w-full">
+                    <Text className="mb-2 flex items-center gap-2 font-medium">
+                      {hiddenField} <EyeOffIcon />
+                    </Text>
+                    <Text className="mt-0 whitespace-pre-wrap break-words font-bold">
+                      {hiddenFieldResponse}
+                    </Text>
+                  </Column>
+                </Row>
+              );
+            }
+          })}
           <EmailButton
             href={`${WEBAPP_URL}/environments/${environmentId}/surveys/${survey.id}/responses?utm_source=email_notification&utm_medium=email&utm_content=view_responses_CTA`}
             label={
@@ -125,7 +142,7 @@ export function ResponseFinishedEmail({
   );
 }
 
-function FileIcon() {
+const FileIcon = () => {
   return (
     <svg
       className="lucide lucide-file"
@@ -142,4 +159,25 @@ function FileIcon() {
       <path d="M14 2v4a2 2 0 0 0 2 2h4" />
     </svg>
   );
-}
+};
+
+const EyeOffIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      className="lucide lucide-eye-off h-4 w-4 rounded-lg bg-slate-200 p-1">
+      <path d="M9.88 9.88a3 3 0 1 0 4.24 4.24" />
+      <path d="M10.73 5.08A10.43 10.43 0 0 1 12 5c7 0 10 7 10 7a13.16 13.16 0 0 1-1.67 2.68" />
+      <path d="M6.61 6.61A13.526 13.526 0 0 0 2 12s3 7 10 7a9.74 9.74 0 0 0 5.39-1.61" />
+      <line x1="2" x2="22" y1="2" y2="22" />
+    </svg>
+  );
+};
