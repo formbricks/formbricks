bcc75adbd21ae0c9e39b63c180286af5744ba70f
Matthias Nannt <mail@matthiasnannt.com>
2024-07-11 17:44:57 +0200

    chore: simplify person detail page by removing activity timeline

--- apps/web/app/(app)/environments/[environmentId]/actions/components/ActionActivityTab.tsx
+++ apps/web/app/(app)/environments/[environmentId]/actions/components/ActionActivityTab.tsx
@@ -5,41 +5,21 @@ import { useEffect, useState } from "react";
 import { convertDateTimeStringShort } from "@formbricks/lib/time";
 import { capitalizeFirstLetter } from "@formbricks/lib/utils/strings";
 import { TActionClass } from "@formbricks/types/action-classes";
-import { TProductConfigChannel } from "@formbricks/types/product";
 import { ErrorComponent } from "@formbricks/ui/ErrorComponent";
 import { Label } from "@formbricks/ui/Label";
 import { LoadingSpinner } from "@formbricks/ui/LoadingSpinner";
-import {
-  getActionCountInLast7DaysAction,
-  getActionCountInLast24HoursAction,
-  getActionCountInLastHourAction,
-  getActiveInactiveSurveysAction,
-} from "../actions";
+import { getActiveInactiveSurveysAction } from "../actions";
 
 interface ActivityTabProps {
   actionClass: TActionClass;
   environmentId: string;
-  isUserTargetingEnabled: boolean;
-  currentProductChannel: TProductConfigChannel;
 }
 
-export const EventActivityTab = ({
-  actionClass,
-  environmentId,
-  isUserTargetingEnabled,
-  currentProductChannel,
-}: ActivityTabProps) => {
-  // const { eventClass, isLoadingEventClass, isErrorEventClass } = useEventClass(environmentId, actionClass.id);
-
-  const [numEventsLastHour, setNumEventsLastHour] = useState<number | undefined>();
-  const [numEventsLast24Hours, setNumEventsLast24Hours] = useState<number | undefined>();
-  const [numEventsLast7Days, setNumEventsLast7Days] = useState<number | undefined>();
+export const ActionActivityTab = ({ actionClass, environmentId }: ActivityTabProps) => {
   const [activeSurveys, setActiveSurveys] = useState<string[] | undefined>();
   const [inactiveSurveys, setInactiveSurveys] = useState<string[] | undefined>();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<Error | null>(null);
-
-  const shouldShowActivity = isUserTargetingEnabled && currentProductChannel !== "website";
 
   useEffect(() => {
     setLoading(true);
@@ -47,20 +27,7 @@ export const EventActivityTab = ({
     const updateState = async () => {
       try {
         setLoading(true);
-        const [
-          numEventsLastHourData,
-          numEventsLast24HoursData,
-          numEventsLast7DaysData,
-          activeInactiveSurveys,
-        ] = await Promise.all([
-          shouldShowActivity ? getActionCountInLastHourAction(actionClass.id, environmentId) : 0,
-          shouldShowActivity ? getActionCountInLast24HoursAction(actionClass.id, environmentId) : 0,
-          shouldShowActivity ? getActionCountInLast7DaysAction(actionClass.id, environmentId) : 0,
-          getActiveInactiveSurveysAction(actionClass.id, environmentId),
-        ]);
-        setNumEventsLastHour(numEventsLastHourData);
-        setNumEventsLast24Hours(numEventsLast24HoursData);
-        setNumEventsLast7Days(numEventsLast7DaysData);
+        const activeInactiveSurveys = await getActiveInactiveSurveysAction(actionClass.id, environmentId);
         setActiveSurveys(activeInactiveSurveys.activeSurveys);
         setInactiveSurveys(activeInactiveSurveys.inactiveSurveys);
       } catch (err) {
@@ -71,7 +38,7 @@ export const EventActivityTab = ({
     };
 
     updateState();
-  }, [actionClass.id, environmentId, shouldShowActivity]);
+  }, [actionClass.id, environmentId]);
 
   if (loading) return <LoadingSpinner />;
   if (error) return <ErrorComponent />;
@@ -79,26 +46,6 @@ export const EventActivityTab = ({
   return (
     <div className="grid grid-cols-3 pb-2">
       <div className="col-span-2 space-y-4 pr-6">
-        {shouldShowActivity && (
-          <div>
-            <Label className="text-slate-500">Occurrences</Label>
-            <div className="mt-1 grid w-fit grid-cols-3 rounded-lg border-slate-100 bg-slate-50">
-              <div className="border-r border-slate-200 px-4 py-2 text-center">
-                <p className="font-bold text-slate-800">{numEventsLastHour}</p>
-                <p className="text-xs text-slate-500">last hour</p>
-              </div>
-              <div className="border-r border-slate-200 px-4 py-2 text-center">
-                <p className="font-bold text-slate-800">{numEventsLast24Hours}</p>
-                <p className="text-xs text-slate-500">last 24 hours</p>
-              </div>
-              <div className="px-4 py-2 text-center">
-                <p className="font-bold text-slate-800">{numEventsLast7Days}</p>
-                <p className="text-xs text-slate-500">last week</p>
-              </div>
-            </div>
-          </div>
-        )}
-
         <div>
           <Label className="text-slate-500">Active surveys</Label>
           {activeSurveys?.length === 0 && <p className="text-sm text-slate-900">-</p>}
