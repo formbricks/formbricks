50a9c050e60afae86e8a71fbe67a61f788d6aa3c
Dhruwang <dhruwangjariwala18@gmail.com>
2024-07-15 15:15:10 +0530

    chore: migrate db package to new linting rules

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -1,21 +1,17 @@
 import { PrismaClient } from "@prisma/client";
 import { withAccelerate } from "@prisma/extension-accelerate";
 
-const prismaClientSingleton = () => {
-  return new PrismaClient({
-    datasources: { db: { url: process.env.DATABASE_URL } },
-    ...(process.env.DEBUG === "1" && {
-      log: ["query", "info"],
-    }),
-  }).$extends(withAccelerate());
-};
-
-type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+const prismaClientSingleton = new PrismaClient({
+  datasources: { db: { url: process.env.DATABASE_URL } },
+  ...(process.env.DEBUG === "1" && {
+    log: ["query", "info"],
+  }),
+}).$extends(withAccelerate());
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClientSingleton | undefined;
+  prisma: typeof prismaClientSingleton | undefined;
 };
 
-export const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+export const prisma = globalForPrisma.prisma ?? prismaClientSingleton;
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
