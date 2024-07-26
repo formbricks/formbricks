73c77b7f96a1c25458c30a513360630642bc31d9
Saurabh Chaddha <92564392+Saurabhchaddha6@users.noreply.github.com>
2024-07-26 17:54:16 +0530

    Update stories.ts

--- packages/ui/v2/LoadingSpinner/stories.ts
+++ packages/ui/v2/LoadingSpinner/stories.ts
@@ -1,17 +1,11 @@
-// src/LoadingSpinner.stories.jsx
 import type { Meta, StoryObj } from "@storybook/react";
 import { LoadingSpinner } from "./index";
 
 const meta: Meta<typeof LoadingSpinner> = {
   title: "ui/LoadingSpinner",
   component: LoadingSpinner,
-  tags: ["autodocs"],
   parameters: {
     layout: "centered",
-  },
-  argTypes: {
-    size: { control: "select", options: ["small", "medium", "large"] },
-    color: { control: "color" },
   },
 };
 
@@ -19,30 +13,4 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-  args: {
-    size: "medium",
-    color: "text-slate-700",
-  },
-};
-
-export const SmallSpinner: Story = {
-  args: {
-    size: "small",
-    color: "text-slate-700",
-  },
-};
-
-export const LargeSpinner: Story = {
-  args: {
-    size: "large",
-    color: "text-slate-700",
-  },
-};
-
-export const CustomColor: Story = {
-  args: {
-    size: "medium",
-    color: "#ff5733",
-  },
-};
+export const Default: Story = {};
