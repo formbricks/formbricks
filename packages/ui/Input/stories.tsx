d97f19f49a8fc27bae7e8b6a7af56924bfdb0d73
Piyush Gupta <piyushguptaa2z123@gmail.com>
2024-07-26 12:16:41 +0530

    consistent usage of the componenyt

--- packages/ui/Input/stories.tsx
+++ packages/ui/Input/stories.tsx
@@ -13,8 +13,7 @@ const meta = {
     It supports all standard HTML input attributes, along with some additional props to handle specific use cases:
     - \`isInvalid\`: Adds a visual indicator for invalid input.
     - \`crossOrigin\`: Specifies how the element handles cross-origin requests.
-    - \`dangerouslySetInnerHTML\`: Allows setting inner HTML content directly, similar to the native \`dangerouslySetInnerHTML\` in React.
-                    `,
+    - \`dangerouslySetInnerHTML\`: Allows setting inner HTML content directly, similar to the native \`dangerouslySetInnerHTML\` in React.`,
       },
     },
   },
@@ -24,74 +23,65 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = {
+export const Default: Story = {
   args: {
+    value: "",
     className: "",
     isInvalid: false,
     disabled: false,
     placeholder: "Enter text",
-    value: "",
   },
 };
 
 export const Invalid: Story = {
   args: {
-    className: "",
     isInvalid: true,
     disabled: false,
     placeholder: "Invalid input",
-    value: "",
   },
 };
 
 export const Disabled: Story = {
   args: {
-    className: "",
     isInvalid: false,
     disabled: true,
     placeholder: "Disabled input",
-    value: "",
   },
 };
 
 export const WithValue: Story = {
   args: {
-    className: "",
+    value: "Prefilled text",
     isInvalid: false,
     disabled: false,
     placeholder: "Enter text",
-    value: "Prefilled text",
   },
 };
 
 export const WithCustomClass: Story = {
   args: {
-    className: " rounded-lg bg-gray-50 text-base ",
+    className: "rounded-lg bg-gray-50 text-base",
     isInvalid: false,
     disabled: false,
     placeholder: "Input with custom class",
-    value: "",
   },
 };
 
 export const Password: Story = {
   args: {
-    className: "",
+    type: "password",
+    value: "abcd",
     isInvalid: false,
     disabled: false,
     placeholder: "Enter password",
-    value: "abcd",
-    type: "password",
   },
 };
 
 export const Email: Story = {
   args: {
-    className: "",
+    type: "email",
     isInvalid: false,
     disabled: false,
     placeholder: "john.doe@email.com",
-    value: "",
-    type: "email",
   },
 };
