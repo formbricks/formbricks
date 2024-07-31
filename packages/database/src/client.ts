4c7e28f7ecb6d10c145a722b031169209fe36217
Dhruwang <dhruwangjariwala18@gmail.com>
2024-07-31 16:29:48 +0530

    converted prismaClientSingleton to a function

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -1,17 +1,21 @@
 import { PrismaClient } from "@prisma/client";
 import { withAccelerate } from "@prisma/extension-accelerate";
 
-const prismaClientSingleton = new PrismaClient({
-  datasources: { db: { url: process.env.DATABASE_URL } },
-  ...(process.env.DEBUG === "1" && {
-    log: ["query", "info"],
-  }),
-}).$extends(withAccelerate());
-
-const globalForPrisma = globalThis as unknown as {
-  prisma: typeof prismaClientSingleton | undefined;
+const prismaClientSingleton = () => {
+  return new PrismaClient({
+    datasources: { db: { url: process.env.DATABASE_URL } },
+    ...(process.env.DEBUG === "1" && {
+      log: ["query", "info"],
+    }),
+  }).$extends(withAccelerate());
 };
 
-export const prisma = globalForPrisma.prisma ?? prismaClientSingleton;
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
+
+export const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
