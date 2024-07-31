f77dd71b711db83bd09fa2034846c37545e326ad
Paolo Leggio - DaVinci Care <p.leggio@davinci.care>
2024-07-31 17:49:48 +0200

    fix iframe summary page

---
+++ apps/web/app/admin-iframe/[environmentId]/surveys/[surveyId]/summary/page.tsx
@@ -0,0 +1,98 @@
+import { SurveyAnalysisNavigation } from "@/app/(app)/environments/[environmentId]/surveys/[surveyId]/(analysis)/components/SurveyAnalysisNavigation";
+import { SummaryPage } from "@/app/(app)/environments/[environmentId]/surveys/[surveyId]/(analysis)/summary/components/SummaryPage";
+import { SurveyAnalysisCTA } from "@/app/(app)/environments/[environmentId]/surveys/[surveyId]/(analysis)/summary/components/SurveyAnalysisCTA";
+import { getServerSession } from "next-auth";
+import { notFound } from "next/navigation";
+import { getAttributeClasses } from "@formbricks/lib/attributeClass/service";
+import { authOptions } from "@formbricks/lib/authOptions";
+import { WEBAPP_URL } from "@formbricks/lib/constants";
+import { getEnvironment } from "@formbricks/lib/environment/service";
+import { getMembershipByUserIdOrganizationId } from "@formbricks/lib/membership/service";
+import { getAccessFlags } from "@formbricks/lib/membership/utils";
+import { getOrganizationByEnvironmentId } from "@formbricks/lib/organization/service";
+import { getProductByEnvironmentId } from "@formbricks/lib/product/service";
+import { getResponseCountBySurveyId } from "@formbricks/lib/response/service";
+import { getSurvey } from "@formbricks/lib/survey/service";
+import { getUser } from "@formbricks/lib/user/service";
+import { PageContentWrapper } from "@formbricks/ui/PageContentWrapper";
+import { PageHeader } from "@formbricks/ui/PageHeader";
+
+const Page = async ({ params }) => {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    throw new Error("Unauthorized");
+  }
+
+  const surveyId = params.surveyId;
+
+  if (!surveyId) {
+    return notFound();
+  }
+
+  const [survey, environment, attributeClasses] = await Promise.all([
+    getSurvey(params.surveyId),
+    getEnvironment(params.environmentId),
+    getAttributeClasses(params.environmentId),
+  ]);
+  if (!environment) {
+    throw new Error("Environment not found");
+  }
+  if (!survey) {
+    throw new Error("Survey not found");
+  }
+
+  const product = await getProductByEnvironmentId(environment.id);
+  if (!product) {
+    throw new Error("Product not found");
+  }
+
+  const user = await getUser(session.user.id);
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  const organization = await getOrganizationByEnvironmentId(params.environmentId);
+
+  if (!organization) {
+    throw new Error("Organization not found");
+  }
+  const currentUserMembership = await getMembershipByUserIdOrganizationId(session?.user.id, organization.id);
+  const totalResponseCount = await getResponseCountBySurveyId(params.surveyId);
+
+  const { isViewer } = getAccessFlags(currentUserMembership?.role);
+
+  return (
+    <PageContentWrapper>
+      {/* <PageHeader
+        pageTitle={survey.name}
+        cta={
+          <SurveyAnalysisCTA
+            environment={environment}
+            survey={survey}
+            isViewer={isViewer}
+            webAppUrl={WEBAPP_URL}
+            user={user}
+          />
+        }
+        >
+        <SurveyAnalysisNavigation
+          environmentId={environment.id}
+          responseCount={totalResponseCount}
+          surveyId={survey.id}
+          activeId="summary"
+        />
+      </PageHeader> */}
+      <SummaryPage
+        environment={environment}
+        survey={survey}
+        surveyId={params.surveyId}
+        webAppUrl={WEBAPP_URL}
+        user={user}
+        totalResponseCount={totalResponseCount}
+        attributeClasses={attributeClasses}
+      />
+    </PageContentWrapper>
+  );
+};
+
+export default Page;
