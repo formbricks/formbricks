ff42d87e935ddd286bbf58db1bc9320c4312fdae
Dhruwang <dhruwangjariwala18@gmail.com>
2024-08-01 19:40:31 +0530

    fix: cal calHost

--- apps/web/app/lib/questions.tsx
+++ apps/web/app/lib/questions.tsx
@@ -185,7 +185,6 @@ export const questionTypes: TQuestion[] = [
     preset: {
       headline: { default: "Schedule a call with me" },
       calUserName: "rick/get-rick-rolled",
-      calHost: "cal.com",
     } as Partial<TSurveyCalQuestion>,
   },
   {
