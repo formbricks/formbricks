460ea6156f4dc837bb9d7516a5fa68d206833e2b
Matthias Nannt <mail@matthiasnannt.com>
2024-08-12 16:44:40 +0200

    update function to use arrow function

--- packages/lib/telemetry.ts
+++ packages/lib/telemetry.ts
@@ -9,9 +9,9 @@ const crypto = require("crypto");
 // We are using the hashed CRON_SECRET as the distinct identifier for the instance for telemetry.
 // The hash cannot be traced back to the original value or the instance itself.
 // This is to ensure that the telemetry data is anonymous but still unique to the instance.
-function getTelemetryId() {
+const getTelemetryId = (): string => {
   return crypto.createHash("sha256").update(env.CRON_SECRET).digest("hex");
-}
+};
 
 export const captureTelemetry = async (eventName: string, properties = {}) => {
   if (env.TELEMETRY_DISABLED !== "1" && process.env.NODE_ENV === "production") {
