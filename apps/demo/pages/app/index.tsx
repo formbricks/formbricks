a732c672fc81c917ca696f2969330da5db8cf1a1
pandeymangg <anshuman.pandey9999@gmail.com>
2024-08-15 17:58:15 +0530

    fix

--- apps/demo/pages/app/index.tsx
+++ apps/demo/pages/app/index.tsx
@@ -40,14 +40,12 @@ const AppPage = ({}) => {
         "Init Attribute 2": "two",
       };
 
-      setTimeout(() => {
-        formbricks.init({
-          environmentId: process.env.NEXT_PUBLIC_FORMBRICKS_ENVIRONMENT_ID,
-          apiHost: process.env.NEXT_PUBLIC_FORMBRICKS_API_HOST,
-          userId,
-          attributes: userInitAttributes,
-        });
-      }, 5000);
+      formbricks.init({
+        environmentId: process.env.NEXT_PUBLIC_FORMBRICKS_ENVIRONMENT_ID,
+        apiHost: process.env.NEXT_PUBLIC_FORMBRICKS_API_HOST,
+        userId,
+        attributes: userInitAttributes,
+      });
     }
 
     // Connect next.js router to Formbricks
@@ -142,14 +140,6 @@ const AppPage = ({}) => {
 
           <div className="p-6">
             <div>
-              <button
-                onClick={() => {
-                  formbricks.track("code", { hiddenFields: { hf1: "Hey!" } });
-                }}
-                className="mb-4 rounded-lg bg-slate-800 px-6 py-3 text-white hover:bg-slate-700 dark:bg-slate-700 dark:hover:bg-slate-600">
-                Test action
-              </button>
-
               <button className="mb-4 rounded-lg bg-slate-800 px-6 py-3 text-white hover:bg-slate-700 dark:bg-slate-700 dark:hover:bg-slate-600">
                 No-Code Action
               </button>
