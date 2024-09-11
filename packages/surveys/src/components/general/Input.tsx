674b1f483313980eb520a0f9f1e55fc6489e1376
pandeymangg <anshuman.pandey9999@gmail.com>
2024-09-11 10:47:57 +0530

    fix: UI fixes

--- packages/surveys/src/components/general/Input.tsx
+++ packages/surveys/src/components/general/Input.tsx
@@ -9,7 +9,7 @@ export const Input = ({ className, ...props }: InputProps) => {
   return (
     <input
       className={cn(
-        "focus:fb-border-brand fb-flex fb-w-full fb-rounded-md fb-border fb-border-border fb-bg-transparent fb-px-3 fb-py-2 fb-text-sm fb-text-subheading placeholder:fb-text-placeholder focus:fb-outline-none focus:fb-ring-2 focus:fb-ring-offset-2 disabled:fb-cursor-not-allowed disabled:fb-opacity-50 dark:fb-border-slate-500 dark:fb-text-slate-300",
+        "focus:fb-border-brand fb-flex fb-w-full fb-border fb-border-border fb-bg-transparent fb-rounded-custom fb-px-3 fb-py-2 fb-text-sm fb-text-subheading placeholder:fb-text-placeholder focus:fb-outline-none focus:fb-ring-2 focus:fb-ring-offset-2 disabled:fb-cursor-not-allowed disabled:fb-opacity-50 dark:fb-border-slate-500 dark:fb-text-slate-300",
         className ?? ""
       )}
       {...props}
