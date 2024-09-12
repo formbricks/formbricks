688dc25990612c5f8d3410d0df112e822142254b
Jonas Höbenreich <64426524+jonas-hoebenreich@users.noreply.github.com>
2024-09-12 12:26:33 +0200

    fix: cuid validation 🚨 (#3130)

--- packages/types/displays.ts
+++ packages/types/displays.ts
@@ -1,30 +1,30 @@
 import { z } from "zod";
 
 export const ZDisplay = z.object({
-  id: z.string().cuid(),
+  id: z.string().cuid2(),
   createdAt: z.date(),
   updatedAt: z.date(),
-  personId: z.string().cuid().nullable(),
-  surveyId: z.string().cuid(),
-  responseId: z.string().cuid().nullable(),
+  personId: z.string().cuid2().nullable(),
+  surveyId: z.string().cuid2(),
+  responseId: z.string().cuid2().nullable(),
   status: z.enum(["seen", "responded"]).nullable(),
 });
 
 export type TDisplay = z.infer<typeof ZDisplay>;
 
 export const ZDisplayCreateInput = z.object({
-  environmentId: z.string().cuid(),
-  surveyId: z.string().cuid(),
+  environmentId: z.string().cuid2(),
+  surveyId: z.string().cuid2(),
   userId: z.string().optional(),
-  responseId: z.string().cuid().optional(),
+  responseId: z.string().cuid2().optional(),
 });
 
 export type TDisplayCreateInput = z.infer<typeof ZDisplayCreateInput>;
 
 export const ZDisplayUpdateInput = z.object({
-  environmentId: z.string().cuid(),
+  environmentId: z.string().cuid2(),
   userId: z.string().optional(),
-  responseId: z.string().cuid().optional(),
+  responseId: z.string().cuid2().optional(),
 });
 
 export type TDisplayUpdateInput = z.infer<typeof ZDisplayUpdateInput>;
@@ -42,7 +42,7 @@ export const ZDisplayFilters = z.object({
       max: z.date().optional(),
     })
     .optional(),
-  responseIds: z.array(z.string().cuid()).optional(),
+  responseIds: z.array(z.string().cuid2()).optional(),
 });
 
 export type TDisplayFilters = z.infer<typeof ZDisplayFilters>;
