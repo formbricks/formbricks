524e3d5f3eefb58bca8e938f80dfb44f43ef1968
Piyush Gupta <piyushguptaa2z123@gmail.com>
2024-09-20 11:04:19 +0530

    pr review suggestions

--- apps/web/app/(app)/(onboarding)/environments/[environmentId]/xm-templates/lib/utils.ts
+++ apps/web/app/(app)/(onboarding)/environments/[environmentId]/xm-templates/lib/utils.ts
@@ -1,29 +1,6 @@
-import { getLocalizedValue } from "@formbricks/lib/i18n/utils";
+import { replaceQuestionPresetPlaceholders } from "@formbricks/lib/utils/templates";
 import { TProduct } from "@formbricks/types/product";
-import { TSurveyQuestion } from "@formbricks/types/surveys/types";
 import { TXMTemplate } from "@formbricks/types/templates";
-
-export const replaceQuestionPresetPlaceholders = (
-  question: TSurveyQuestion,
-  product: TProduct
-): TSurveyQuestion => {
-  if (!product) return question;
-  const newQuestion = structuredClone(question);
-  const defaultLanguageCode = "default";
-  if (newQuestion.headline) {
-    newQuestion.headline[defaultLanguageCode] = getLocalizedValue(
-      newQuestion.headline,
-      defaultLanguageCode
-    ).replace("{{productName}}", product.name);
-  }
-  if (newQuestion.subheader) {
-    newQuestion.subheader[defaultLanguageCode] = getLocalizedValue(
-      newQuestion.subheader,
-      defaultLanguageCode
-    )?.replace("{{productName}}", product.name);
-  }
-  return newQuestion;
-};
 
 // replace all occurences of productName with the actual product name in the current template
 export const replacePresetPlaceholders = (template: TXMTemplate, product: TProduct) => {
