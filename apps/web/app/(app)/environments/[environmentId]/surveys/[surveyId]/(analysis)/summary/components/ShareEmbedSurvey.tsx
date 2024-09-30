91b4b9c893ba92cb77972d5cbe96a36402295419
SatyanarayanPrashar <satyanarayanprashar73@gmail.com>
2024-10-01 00:44:34 +0530

    Fixed: Rework Survey Summary CTA bar #3217

--- apps/web/app/(app)/environments/[environmentId]/surveys/[surveyId]/(analysis)/summary/components/ShareEmbedSurvey.tsx
+++ apps/web/app/(app)/environments/[environmentId]/surveys/[surveyId]/(analysis)/summary/components/ShareEmbedSurvey.tsx
@@ -23,12 +23,20 @@ import { PanelInfoView } from "./shareEmbedModal/PanelInfoView";
 interface ShareEmbedSurveyProps {
   survey: TSurvey;
   open: boolean;
+  modalType: string;
   setOpen: React.Dispatch<React.SetStateAction<boolean>>;
   webAppUrl: string;
   user: TUser;
 }
 
-export const ShareEmbedSurvey = ({ survey, open, setOpen, webAppUrl, user }: ShareEmbedSurveyProps) => {
+export const ShareEmbedSurvey = ({
+  survey,
+  open,
+  modalType,
+  setOpen,
+  webAppUrl,
+  user,
+}: ShareEmbedSurveyProps) => {
   const router = useRouter();
   const environmentId = survey.environmentId;
   const isSingleUseLinkSurvey = survey.singleUse?.enabled ?? false;
@@ -48,7 +56,7 @@ export const ShareEmbedSurvey = ({ survey, open, setOpen, webAppUrl, user }: Sha
   const handleOpenChange = (open: boolean) => {
     setActiveId(tabs[0].id);
     setOpen(open);
-    setShowView(open ? "start" : ""); // Reset to initial page when modal opens or closes
+    setShowView(open ? "start" : "");
 
     // fetch latest responses
     router.refresh();
@@ -58,60 +66,85 @@ export const ShareEmbedSurvey = ({ survey, open, setOpen, webAppUrl, user }: Sha
     setShowView("start");
   };
 
-  return (
-    <Dialog open={open} onOpenChange={handleOpenChange}>
-      <DialogContent className="w-full max-w-xl bg-white p-0 md:max-w-3xl lg:h-[700px] lg:max-w-5xl">
-        {showView === "start" ? (
-          <div className="h-full max-w-full overflow-hidden">
-            <div className="flex h-[200px] w-full flex-col items-center justify-center space-y-6 p-8 text-center lg:h-2/5">
-              <DialogTitle>
-                <p className="pt-2 text-xl font-semibold text-slate-800">Your survey is public 🎉</p>
-              </DialogTitle>
-              <DialogDescription className="hidden" />
-              <ShareSurveyLink
-                survey={survey}
-                webAppUrl={webAppUrl}
-                surveyUrl={surveyUrl}
-                setSurveyUrl={setSurveyUrl}
-              />
-            </div>
-            <div className="flex h-[300px] flex-col items-center justify-center gap-8 rounded-b-lg bg-slate-50 px-8 lg:h-3/5">
-              <p className="-mt-8 text-sm text-slate-500">What&apos;s next?</p>
-              <div className="grid grid-cols-4 gap-2">
-                <button
-                  type="button"
-                  onClick={() => setShowView("embed")}
-                  className="flex flex-col items-center gap-3 rounded-lg border border-slate-100 bg-white p-4 text-sm text-slate-500 hover:border-slate-200 md:p-8">
-                  <Code2Icon className="h-6 w-6 text-slate-700" />
-                  Embed survey
-                </button>
-                <Link
-                  href={`/environments/${environmentId}/settings/notifications`}
-                  className="flex flex-col items-center gap-3 rounded-lg border border-slate-100 bg-white p-4 text-sm text-slate-500 hover:border-slate-200 md:p-8">
-                  <BellRing className="h-6 w-6 text-slate-700" />
-                  Configure alerts
-                </Link>
-                <Link
-                  href={`/environments/${environmentId}/integrations`}
-                  className="flex flex-col items-center gap-3 rounded-lg border border-slate-100 bg-white p-4 text-sm text-slate-500 hover:border-slate-200 md:p-8">
-                  <BlocksIcon className="h-6 w-6 text-slate-700" />
-                  Setup integrations
-                </Link>
-                <button
-                  type="button"
-                  onClick={() => setShowView("panel")}
-                  className="relative flex flex-col items-center gap-3 rounded-lg border border-slate-100 bg-white p-4 text-sm text-slate-500 hover:border-slate-200 md:p-8">
-                  <UsersRound className="h-6 w-6 text-slate-700" />
-                  Send to panel
-                  <Badge size="tiny" type="success" text="New" className="absolute right-3 top-3" />
-                </button>
+  if (modalType == "start") {
+    return (
+      <Dialog open={open} onOpenChange={handleOpenChange}>
+        <DialogContent className="w-full max-w-xl bg-white p-0 md:max-w-3xl lg:h-[700px] lg:max-w-5xl">
+          {showView === "start" ? (
+            <div className="h-full max-w-full overflow-hidden">
+              <div className="flex h-[200px] w-full flex-col items-center justify-center space-y-6 p-8 text-center lg:h-2/5">
+                <DialogTitle>
+                  <p className="pt-2 text-xl font-semibold text-slate-800">Your survey is public 🎉</p>
+                </DialogTitle>
+                <DialogDescription className="hidden" />
+                <ShareSurveyLink
+                  survey={survey}
+                  webAppUrl={webAppUrl}
+                  surveyUrl={surveyUrl}
+                  setSurveyUrl={setSurveyUrl}
+                />
+              </div>
+              <div className="flex h-[300px] flex-col items-center justify-center gap-8 rounded-b-lg bg-slate-50 px-8 lg:h-3/5">
+                <p className="-mt-8 text-sm text-slate-500">What&apos;s next?</p>
+                <div className="grid grid-cols-4 gap-2">
+                  <button
+                    type="button"
+                    onClick={() => setShowView("embed")}
+                    className="flex flex-col items-center gap-3 rounded-lg border border-slate-100 bg-white p-4 text-sm text-slate-500 hover:border-slate-200 md:p-8">
+                    <Code2Icon className="h-6 w-6 text-slate-700" />
+                    Embed survey
+                  </button>
+                  <Link
+                    href={`/environments/${environmentId}/settings/notifications`}
+                    className="flex flex-col items-center gap-3 rounded-lg border border-slate-100 bg-white p-4 text-sm text-slate-500 hover:border-slate-200 md:p-8">
+                    <BellRing className="h-6 w-6 text-slate-700" />
+                    Configure alerts
+                  </Link>
+                  <Link
+                    href={`/environments/${environmentId}/integrations`}
+                    className="flex flex-col items-center gap-3 rounded-lg border border-slate-100 bg-white p-4 text-sm text-slate-500 hover:border-slate-200 md:p-8">
+                    <BlocksIcon className="h-6 w-6 text-slate-700" />
+                    Setup integrations
+                  </Link>
+                  <button
+                    type="button"
+                    onClick={() => setShowView("panel")}
+                    className="relative flex flex-col items-center gap-3 rounded-lg border border-slate-100 bg-white p-4 text-sm text-slate-500 hover:border-slate-200 md:p-8">
+                    <UsersRound className="h-6 w-6 text-slate-700" />
+                    Send to panel
+                    <Badge size="tiny" type="success" text="New" className="absolute right-3 top-3" />
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-        ) : showView === "embed" ? (
+          ) : showView === "embed" ? (
+            <EmbedView
+              handleInitialPageButton={handleInitialPageButton}
+              tabs={tabs}
+              unableBack={true}
+              activeId={activeId}
+              environmentId={environmentId}
+              setActiveId={setActiveId}
+              survey={survey}
+              email={email}
+              surveyUrl={surveyUrl}
+              setSurveyUrl={setSurveyUrl}
+              webAppUrl={webAppUrl}
+            />
+          ) : showView === "panel" ? (
+            <PanelInfoView handleInitialPageButton={handleInitialPageButton} unableBack={true} />
+          ) : null}
+        </DialogContent>
+      </Dialog>
+    );
+  } else if (modalType === "embed") {
+    return (
+      <Dialog open={open} onOpenChange={handleOpenChange}>
+        <DialogContent className="w-full max-w-xl bg-white p-0 md:max-w-3xl lg:h-[700px] lg:max-w-5xl">
           <EmbedView
             handleInitialPageButton={handleInitialPageButton}
             tabs={tabs}
+            unableBack={false}
             activeId={activeId}
             environmentId={environmentId}
             setActiveId={setActiveId}
@@ -121,10 +154,16 @@ export const ShareEmbedSurvey = ({ survey, open, setOpen, webAppUrl, user }: Sha
             setSurveyUrl={setSurveyUrl}
             webAppUrl={webAppUrl}
           />
-        ) : showView === "panel" ? (
-          <PanelInfoView handleInitialPageButton={handleInitialPageButton} />
-        ) : null}
-      </DialogContent>
-    </Dialog>
-  );
+        </DialogContent>
+      </Dialog>
+    );
+  } else if (modalType === "panel") {
+    return (
+      <Dialog open={open} onOpenChange={handleOpenChange}>
+        <DialogContent className="w-full max-w-xl bg-white p-0 md:max-w-3xl lg:h-[700px] lg:max-w-5xl">
+          <PanelInfoView handleInitialPageButton={handleInitialPageButton} unableBack={false} />
+        </DialogContent>
+      </Dialog>
+    );
+  }
 };
--- apps/web/app/(app)/environments/[environmentId]/surveys/[surveyId]/(analysis)/summary/components/SurveyAnalysisCTA.tsx
+++ apps/web/app/(app)/environments/[environmentId]/surveys/[surveyId]/(analysis)/summary/components/SurveyAnalysisCTA.tsx
@@ -3,14 +3,31 @@
 import { ShareEmbedSurvey } from "@/app/(app)/environments/[environmentId]/surveys/[surveyId]/(analysis)/summary/components/ShareEmbedSurvey";
 import { SuccessMessage } from "@/app/(app)/environments/[environmentId]/surveys/[surveyId]/(analysis)/summary/components/SuccessMessage";
 import { SurveyStatusDropdown } from "@/app/(app)/environments/[environmentId]/surveys/[surveyId]/components/SurveyStatusDropdown";
-import { ArrowUpRightFromSquareIcon, SquarePenIcon } from "lucide-react";
+import {
+  ArrowUpRightFromSquareIcon,
+  BellRing,
+  Code2Icon,
+  LinkIcon,
+  MoreVertical,
+  SquarePenIcon,
+  UsersRound,
+} from "lucide-react";
+import Link from "next/link";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
+import toast from "react-hot-toast";
 import { TEnvironment } from "@formbricks/types/environment";
 import { TSurvey } from "@formbricks/types/surveys/types";
 import { TUser } from "@formbricks/types/user";
 import { Badge } from "@formbricks/ui/components/Badge";
 import { Button } from "@formbricks/ui/components/Button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@formbricks/ui/components/DropdownMenu";
 
 export const SurveyAnalysisCTA = ({
   survey,
@@ -28,8 +45,13 @@ export const SurveyAnalysisCTA = ({
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const router = useRouter();
+  const [isDropDownOpen, setIsDropDownOpen] = useState(false);
 
   const [showShareSurveyModal, setShowShareSurveyModal] = useState(searchParams.get("share") === "true");
+  const [showEmbedSurveyModal, setShowEmbedSurveyModal] = useState(false);
+  const [showPanelSurveyModal, setShowPanelSurveyModal] = useState(false);
+
+  const surveyUrl = useMemo(() => webAppUrl + "/s/" + survey.id, [survey.id, webAppUrl]);
 
   const widgetSetupCompleted =
     survey.type === "app" ? environment.appSetupCompleted : environment.websiteSetupCompleted;
@@ -55,6 +77,7 @@ export const SurveyAnalysisCTA = ({
 
     router.push(`${pathname}?${searchParams.toString()}`);
   };
+
   return (
     <div className="hidden justify-end gap-x-1.5 sm:flex">
       {survey.resultShareKey && (
@@ -63,13 +86,30 @@ export const SurveyAnalysisCTA = ({
       {(widgetSetupCompleted || survey.type === "link") && survey.status !== "draft" ? (
         <SurveyStatusDropdown environment={environment} survey={survey} />
       ) : null}
+      {!isViewer && (
+        <Button
+          variant="secondary"
+          size="sm"
+          onClick={() => {
+            setShowEmbedSurveyModal(true);
+          }}
+          EndIcon={Code2Icon}>
+          Embed
+        </Button>
+      )}
       {survey.type === "link" && (
         <>
           <Button
             variant="secondary"
             size="sm"
             onClick={() => {
-              setOpenShareSurveyModal(true);
+              let previewUrl = surveyUrl;
+              if (previewUrl.includes("?")) {
+                previewUrl += "&preview=true";
+              } else {
+                previewUrl += "?preview=true";
+              }
+              window.open(previewUrl, "_blank");
             }}
             EndIcon={ArrowUpRightFromSquareIcon}>
             Preview
@@ -85,6 +125,78 @@ export const SurveyAnalysisCTA = ({
           Edit
         </Button>
       )}
+
+      {!isViewer && (
+        <div
+          id={`${survey.name.toLowerCase().split(" ").join("-")}-survey-actions`}
+          onClick={(e) => e.stopPropagation()}>
+          <DropdownMenu open={isDropDownOpen} onOpenChange={setIsDropDownOpen}>
+            <DropdownMenuTrigger className="z-10 cursor-pointer" asChild>
+              <div className="rounded-lg border p-2 hover:bg-slate-50">
+                <span className="sr-only">Open options</span>
+                <MoreVertical className="h-full w-full" aria-hidden="true" />
+              </div>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="mr-8 w-40">
+              <DropdownMenuGroup>
+                <>
+                  <DropdownMenuItem>
+                    <button
+                      type="button"
+                      className="flex w-full items-center"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        setIsDropDownOpen(false);
+                        navigator.clipboard.writeText(surveyUrl);
+                        toast.success("Copied link to clipboard");
+                        router.refresh();
+                      }}>
+                      <LinkIcon className="mr-2 h-4 w-4" />
+                      Copy Link
+                    </button>
+                  </DropdownMenuItem>
+                </>
+                <>
+                  <DropdownMenuItem>
+                    <button
+                      type="button"
+                      className="flex w-full items-center"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        setIsDropDownOpen(false);
+                        setShowPanelSurveyModal(true);
+                        router.refresh();
+                      }}>
+                      <UsersRound className="mr-2 h-4 w-4" />
+                      Send to panel
+                    </button>
+                  </DropdownMenuItem>
+                </>
+                <>
+                  <DropdownMenuItem>
+                    <button
+                      type="button"
+                      className="flex w-full items-center"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        setIsDropDownOpen(false);
+                        router.refresh();
+                      }}>
+                      <Link
+                        href={`/environments/${survey.environmentId}/settings/notifications`}
+                        className="flex w-full items-center">
+                        <BellRing className="mr-2 h-4 w-4" />
+                        Configure alerts
+                      </Link>
+                    </button>
+                  </DropdownMenuItem>
+                </>
+              </DropdownMenuGroup>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      )}
+
       {showShareSurveyModal && user && (
         <ShareEmbedSurvey
           survey={survey}
@@ -92,6 +204,27 @@ export const SurveyAnalysisCTA = ({
           setOpen={setOpenShareSurveyModal}
           webAppUrl={webAppUrl}
           user={user}
+          modalType={"start"}
+        />
+      )}
+      {showEmbedSurveyModal && user && (
+        <ShareEmbedSurvey
+          survey={survey}
+          open={showEmbedSurveyModal}
+          setOpen={setShowEmbedSurveyModal}
+          webAppUrl={webAppUrl}
+          user={user}
+          modalType={"embed"}
+        />
+      )}
+      {showPanelSurveyModal && user && (
+        <ShareEmbedSurvey
+          survey={survey}
+          open={showPanelSurveyModal}
+          setOpen={setShowPanelSurveyModal}
+          webAppUrl={webAppUrl}
+          user={user}
+          modalType={"panel"}
         />
       )}
 
