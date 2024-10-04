086222ca7b7faf37d4ec6f251813c226fee856ad
Piyush Garg <piyushgarg.dev@gmail.com>
2024-10-05 02:27:35 +0530

    chore: increase the max w of link survey card from md to xl

--- packages/ui/components/PreviewSurvey/index.tsx
+++ packages/ui/components/PreviewSurvey/index.tsx
@@ -373,7 +373,7 @@ export const PreviewSurvey = ({
                     <ClientLogo environmentId={environment.id} product={product} previewSurvey />
                   )}
                 </div>
-                <div className="z-0 w-full max-w-md rounded-lg border-transparent">
+                <div className="z-0 w-full max-w-xl rounded-lg">
                   <SurveyInline
                     survey={{ ...survey, type: "link" }}
                     isBrandingEnabled={product.linkSurveyBranding}
