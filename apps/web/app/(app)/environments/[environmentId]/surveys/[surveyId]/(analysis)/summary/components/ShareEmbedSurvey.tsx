5613181b3029afe101ff2e75ff4d2de73075ee2f
Nawin <nawinsharma60@gmail.com>
2024-10-07 15:25:42 +0530

    fix: back button

--- apps/web/app/(app)/environments/[environmentId]/surveys/[surveyId]/(analysis)/summary/components/ShareEmbedSurvey.tsx
+++ apps/web/app/(app)/environments/[environmentId]/surveys/[surveyId]/(analysis)/summary/components/ShareEmbedSurvey.tsx
@@ -50,13 +50,13 @@ export const ShareEmbedSurvey = ({
   ];
 
   const [activeId, setActiveId] = useState(tabs[0].id);
-  const [showView, setShowView] = useState("start");
+  const [showView, setShowView] = useState(modalType);
   const [surveyUrl, setSurveyUrl] = useState("");
 
   const handleOpenChange = (open: boolean) => {
     setActiveId(tabs[0].id);
     setOpen(open);
-    setShowView(open ? "start" : "");
+    setShowView(open ? modalType : "start");
     router.refresh();
   };
 
@@ -64,81 +64,57 @@ export const ShareEmbedSurvey = ({
     setShowView("start");
   };
 
-  if (modalType === "start") {
-    return (
-      <Dialog open={open} onOpenChange={handleOpenChange}>
-        <DialogContent className="w-full max-w-xl bg-white p-0 md:max-w-3xl lg:h-[700px] lg:max-w-5xl">
-          {showView === "start" ? (
-            <div className="h-full max-w-full overflow-hidden">
-              <div className="flex h-[200px] w-full flex-col items-center justify-center space-y-6 p-8 text-center lg:h-2/5">
-                <DialogTitle>
-                  <p className="pt-2 text-xl font-semibold text-slate-800">Your survey is public 🎉</p>
-                </DialogTitle>
-                <DialogDescription className="hidden" />
-                <ShareSurveyLink
-                  survey={survey}
-                  webAppUrl={webAppUrl}
-                  surveyUrl={surveyUrl}
-                  setSurveyUrl={setSurveyUrl}
-                />
-              </div>
-              <div className="flex h-[300px] flex-col items-center justify-center gap-8 rounded-b-lg bg-slate-50 px-8 lg:h-3/5">
-                <p className="-mt-8 text-sm text-slate-500">What&apos;s next?</p>
-                <div className="grid grid-cols-4 gap-2">
-                  <button
-                    type="button"
-                    onClick={() => setShowView("embed")}
-                    className="flex flex-col items-center gap-3 rounded-lg border border-slate-100 bg-white p-4 text-sm text-slate-500 hover:border-slate-200 md:p-8">
-                    <Code2Icon className="h-6 w-6 text-slate-700" />
-                    Embed survey
-                  </button>
-                  <Link
-                    href={`/environments/${environmentId}/settings/notifications`}
-                    className="flex flex-col items-center gap-3 rounded-lg border border-slate-100 bg-white p-4 text-sm text-slate-500 hover:border-slate-200 md:p-8">
-                    <BellRing className="h-6 w-6 text-slate-700" />
-                    Configure alerts
-                  </Link>
-                  <Link
-                    href={`/environments/${environmentId}/integrations`}
-                    className="flex flex-col items-center gap-3 rounded-lg border border-slate-100 bg-white p-4 text-sm text-slate-500 hover:border-slate-200 md:p-8">
-                    <BlocksIcon className="h-6 w-6 text-slate-700" />
-                    Setup integrations
-                  </Link>
-                  <button
-                    type="button"
-                    onClick={() => setShowView("panel")}
-                    className="relative flex flex-col items-center gap-3 rounded-lg border border-slate-100 bg-white p-4 text-sm text-slate-500 hover:border-slate-200 md:p-8">
-                    <UsersRound className="h-6 w-6 text-slate-700" />
-                    Send to panel
-                    <Badge size="tiny" type="success" text="New" className="absolute right-3 top-3" />
-                  </button>
-                </div>
+  return (
+    <Dialog open={open} onOpenChange={handleOpenChange}>
+      <DialogContent className="w-full max-w-xl bg-white p-0 md:max-w-3xl lg:h-[700px] lg:max-w-5xl">
+        {showView === "start" ? (
+          <div className="h-full max-w-full overflow-hidden">
+            <div className="flex h-[200px] w-full flex-col items-center justify-center space-y-6 p-8 text-center lg:h-2/5">
+              <DialogTitle>
+                <p className="pt-2 text-xl font-semibold text-slate-800">Your survey is public 🎉</p>
+              </DialogTitle>
+              <DialogDescription className="hidden" />
+              <ShareSurveyLink
+                survey={survey}
+                webAppUrl={webAppUrl}
+                surveyUrl={surveyUrl}
+                setSurveyUrl={setSurveyUrl}
+              />
+            </div>
+            <div className="flex h-[300px] flex-col items-center justify-center gap-8 rounded-b-lg bg-slate-50 px-8 lg:h-3/5">
+              <p className="-mt-8 text-sm text-slate-500">What&apos;s next?</p>
+              <div className="grid grid-cols-4 gap-2">
+                <button
+                  type="button"
+                  onClick={() => setShowView("embed")}
+                  className="flex flex-col items-center gap-3 rounded-lg border border-slate-100 bg-white p-4 text-sm text-slate-500 hover:border-slate-200 md:p-8">
+                  <Code2Icon className="h-6 w-6 text-slate-700" />
+                  Embed survey
+                </button>
+                <Link
+                  href={`/environments/${environmentId}/settings/notifications`}
+                  className="flex flex-col items-center gap-3 rounded-lg border border-slate-100 bg-white p-4 text-sm text-slate-500 hover:border-slate-200 md:p-8">
+                  <BellRing className="h-6 w-6 text-slate-700" />
+                  Configure alerts
+                </Link>
+                <Link
+                  href={`/environments/${environmentId}/integrations`}
+                  className="flex flex-col items-center gap-3 rounded-lg border border-slate-100 bg-white p-4 text-sm text-slate-500 hover:border-slate-200 md:p-8">
+                  <BlocksIcon className="h-6 w-6 text-slate-700" />
+                  Setup integrations
+                </Link>
+                <button
+                  type="button"
+                  onClick={() => setShowView("panel")}
+                  className="relative flex flex-col items-center gap-3 rounded-lg border border-slate-100 bg-white p-4 text-sm text-slate-500 hover:border-slate-200 md:p-8">
+                  <UsersRound className="h-6 w-6 text-slate-700" />
+                  Send to panel
+                  <Badge size="tiny" type="success" text="New" className="absolute right-3 top-3" />
+                </button>
               </div>
             </div>
-          ) : showView === "embed" ? (
-            <EmbedView
-              handleInitialPageButton={handleInitialPageButton}
-              tabs={tabs}
-              disableBack={true}
-              activeId={activeId}
-              environmentId={environmentId}
-              setActiveId={setActiveId}
-              survey={survey}
-              email={email}
-              surveyUrl={surveyUrl}
-              setSurveyUrl={setSurveyUrl}
-              webAppUrl={webAppUrl}
-            />
-          ) : showView === "panel" ? (
-            <PanelInfoView handleInitialPageButton={handleInitialPageButton} disableBack={true} />
-          ) : null}
-        </DialogContent>
-      </Dialog>
-    );
-  } else if (modalType === "embed") {
-    return (
-      <Dialog open={open} onOpenChange={handleOpenChange}>
-        <DialogContent className="w-full max-w-xl bg-white p-0 md:max-w-3xl lg:h-[700px] lg:max-w-5xl">
+          </div>
+        ) : showView === "embed" ? (
           <EmbedView
             handleInitialPageButton={handleInitialPageButton}
             tabs={tabs}
@@ -152,16 +128,10 @@ export const ShareEmbedSurvey = ({
             setSurveyUrl={setSurveyUrl}
             webAppUrl={webAppUrl}
           />
-        </DialogContent>
-      </Dialog>
-    );
-  } else if (modalType === "panel") {
-    return (
-      <Dialog open={open} onOpenChange={handleOpenChange}>
-        <DialogContent className="w-full max-w-xl bg-white p-0 md:max-w-3xl lg:h-[700px] lg:max-w-5xl">
+        ) : showView === "panel" ? (
           <PanelInfoView handleInitialPageButton={handleInitialPageButton} disableBack={false} />
-        </DialogContent>
-      </Dialog>
-    );
-  }
+        ) : null}
+      </DialogContent>
+    </Dialog>
+  );
 };
