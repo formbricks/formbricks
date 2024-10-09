3168c5a8c55672248e15d89ac1bc9da493617d10
jonas-hoebenreich <jonas.hoebenreich@tum.de>
2024-10-09 15:27:57 +0200

    move copy code to dedicated file

--- packages/js-core/vite.config.ts
+++ packages/js-core/vite.config.ts
@@ -1,8 +1,8 @@
-import { copyFileSync, readdirSync } from "fs";
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 import webPackageJson from "../../apps/web/package.json";
+import { copyCompiledAssetsPlugin } from "../copyCompiledAssetsPlugin/vite.config";
 
 const config = () => {
   return defineConfig({
@@ -28,23 +28,7 @@ const config = () => {
         rollupTypes: true,
         bundledPackages: ["@formbricks/api", "@formbricks/types"],
       }),
-      {
-        name: "copy-output",
-        closeBundle() {
-          const outputDir = resolve(__dirname, "../../apps/web/public/js");
-          const distDir = resolve(__dirname, "dist");
-
-          const filesToCopy = readdirSync(distDir);
-
-          filesToCopy.forEach((file) => {
-            const srcFile = `${distDir}/${file}`;
-            const destFile = `${outputDir}/${file.replace("index", "formbricks")}`;
-            copyFileSync(srcFile, destFile);
-          });
-
-          console.log(`Copied ${filesToCopy.length} files to ${outputDir}`);
-        },
-      },
+      copyCompiledAssetsPlugin({ filename: "formbricks", distDir: resolve(__dirname, "dist") }),
     ],
   });
 };
--- packages/surveys/vite.config.ts
+++ packages/surveys/vite.config.ts
@@ -1,9 +1,9 @@
 import preact from "@preact/preset-vite";
-import { copyFileSync, readdirSync } from "fs";
 import { resolve } from "path";
 import { defineConfig, loadEnv } from "vite";
 import dts from "vite-plugin-dts";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { copyCompiledAssetsPlugin } from "../copyCompiledAssetsPlugin/vite.config";
 
 const config = ({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
@@ -31,23 +31,7 @@ const config = ({ mode }) => {
       preact(),
       dts({ rollupTypes: true }),
       tsconfigPaths(),
-      {
-        name: "copy-output",
-        closeBundle() {
-          const outputDir = resolve(__dirname, "../../apps/web/public/js");
-          const distDir = resolve(__dirname, "dist");
-
-          const filesToCopy = readdirSync(distDir);
-
-          filesToCopy.forEach((file) => {
-            const srcFile = `${distDir}/${file}`;
-            const destFile = `${outputDir}/${file.replace("index", "surveys")}`;
-            copyFileSync(srcFile, destFile);
-          });
-
-          console.log(`Copied ${filesToCopy.length} files to ${outputDir}`);
-        },
-      },
+      copyCompiledAssetsPlugin({ filename: "surveys", distDir: resolve(__dirname, "dist") }),
     ],
   });
 };
