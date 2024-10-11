4d18fffa885e679c316d9eb0c7799395addb7634
pandeymangg <anshuman.pandey9999@gmail.com>
2024-10-11 11:40:29 +0530

    fix: combines all migrations into one

--- packages/database/migrations/20241010133706_xm_user_identification/migration.sql
+++ packages/database/migrations/20241010133706_xm_user_identification/migration.sql
@@ -1,3 +1,29 @@
+-- Rename table "Person" to "Contact"
+ALTER TABLE "Person" RENAME TO "Contact";
+ALTER TABLE "Contact" RENAME CONSTRAINT "Person_pkey" TO "Contact_pkey";
+ALTER TABLE "Contact" RENAME CONSTRAINT "Person_environmentId_fkey" TO "Contact_environmentId_fkey";
+
+-- Rename column "personId" to "contactId" in "Attribute" table
+ALTER TABLE "Attribute" RENAME COLUMN "personId" TO "contactId";
+
+-- Rename column "personId" to "contactId" in "Response" table
+ALTER TABLE "Response" RENAME COLUMN "personId" TO "contactId";
+
+-- Rename column "personId" to "contactId" in "Display" table
+ALTER TABLE "Display" RENAME COLUMN "personId" TO "contactId";
+
+-- If there are any foreign key constraints involving "personId", they should be renamed to "contactId" as well.
+ALTER TABLE "Attribute" RENAME CONSTRAINT "Attribute_personId_fkey" TO "Attribute_contactId_fkey";
+ALTER TABLE "Response" RENAME CONSTRAINT "Response_personId_fkey" TO "Response_contactId_fkey";
+ALTER TABLE "Display" RENAME CONSTRAINT "Display_personId_fkey" TO "Display_contactId_fkey";
+
+-- Rename indexes
+ALTER INDEX "Person_environmentId_idx" RENAME TO "Contact_environmentId_idx";
+ALTER INDEX "Person_environmentId_userId_key" RENAME TO "Contact_environmentId_userId_key";
+ALTER INDEX "Attribute_personId_attributeClassId_key" RENAME TO "Attribute_contactId_attributeClassId_key";
+ALTER INDEX "Response_personId_created_at_idx" RENAME TO "Response_contactId_created_at_idx";
+ALTER INDEX "Display_personId_created_at_idx" RENAME TO "Display_contactId_created_at_idx";
+
 -- Renaming the tables
 ALTER TABLE "AttributeClass" RENAME TO "ContactAttributeKey";
 ALTER TABLE "ContactAttributeKey" RENAME CONSTRAINT "AttributeClass_pkey" TO "ContactAttributeKey_pkey";
@@ -19,16 +45,15 @@ ALTER INDEX "AttributeClass_name_environmentId_key" RENAME TO "ContactAttributeK
 ALTER INDEX "AttributeClass_environmentId_created_at_idx" RENAME TO "ContactAttributeKey_environmentId_created_at_idx";
 ALTER INDEX "AttributeClass_environmentId_archived_idx" RENAME TO "ContactAttributeKey_environmentId_archived_idx";
 
-
 -- Step 1: Create the new enum type
 CREATE TYPE "ContactAttributeType" AS ENUM ('default', 'custom');
 
 -- Step 2: Add the new temporary column for 'type'
-ALTER TABLE "ContactAttributeKey" ADD COLUMN "attributeType" "ContactAttributeType";
+ALTER TABLE "ContactAttributeKey" ADD COLUMN "type_new" "ContactAttributeType";
 
 -- Step 3: Update the new 'type_new' column with mapped values
 UPDATE "ContactAttributeKey"
-SET "attributeType" = CASE
+SET "type_new" = CASE
     WHEN "type" = 'automatic' THEN 'default'::"ContactAttributeType"
     ELSE 'custom'::"ContactAttributeType"
 END;
@@ -36,12 +61,15 @@ END;
 -- Step 4: Drop the old 'type' column
 ALTER TABLE "ContactAttributeKey" DROP COLUMN "type";
 
--- AlterTable
-ALTER TABLE "ContactAttributeKey" ALTER COLUMN "attributeType" SET NOT NULL,
-ALTER COLUMN "attributeType" SET DEFAULT 'default';
-
 -- DropEnum
 DROP TYPE "AttributeType";
+
+-- Step 5: Rename the new 'type_new' column to 'type'
+ALTER TABLE "ContactAttributeKey" RENAME COLUMN "type_new" TO "type";
+
+-- AlterTable
+ALTER TABLE "ContactAttributeKey" ALTER COLUMN "type" SET NOT NULL,
+ALTER COLUMN "type" SET DEFAULT 'default';
 
 -- Step 7: Add the new 'key' column with a default value
 ALTER TABLE "ContactAttributeKey" ADD COLUMN "key" TEXT NOT NULL DEFAULT '';
