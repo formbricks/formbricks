8ca7c38defe82b19d23a7c813c910da9fccbcd42
Piyush Gupta <56182734+gupta-piyush19@users.noreply.github.com>
2024-10-15 01:58:47 +0530

    fix: added logging in actionClient (#3695)

--- packages/lib/actionClient/index.ts
+++ packages/lib/actionClient/index.ts
@@ -11,6 +11,7 @@ export const actionClient = createSafeActionClient({
     } else if (e instanceof AuthorizationError) {
       return e.message;
     }
+    console.error("SERVER ERROR: ", e);
 
     return DEFAULT_SERVER_ERROR_MESSAGE;
   },
