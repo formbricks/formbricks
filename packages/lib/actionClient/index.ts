62c8d3b2148ff5497d0492d224d302062de75f91
Piyush Gupta <piyushguptaa2z123@gmail.com>
2024-10-14 16:56:19 +0530

    log -> error

--- packages/lib/actionClient/index.ts
+++ packages/lib/actionClient/index.ts
@@ -11,7 +11,7 @@ export const actionClient = createSafeActionClient({
     } else if (e instanceof AuthorizationError) {
       return e.message;
     }
-    console.log("SERVER ERROR: ", e);
+    console.error("SERVER ERROR: ", e);
 
     return DEFAULT_SERVER_ERROR_MESSAGE;
   },
