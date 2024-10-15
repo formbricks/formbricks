c7023e507c4ee016b11edb28fcdcc6d6c78c0cad
Johannes <johannes@formbricks.com>
2024-10-14 17:17:54 -0700

    tweak UI

--- apps/web/app/(app)/(survey-editor)/environments/[environmentId]/surveys/[surveyId]/edit/components/AddQuestionButton.tsx
+++ apps/web/app/(app)/(survey-editor)/environments/[environmentId]/surveys/[surveyId]/edit/components/AddQuestionButton.tsx
@@ -12,7 +12,6 @@ import {
   universalQuestionPresets,
 } from "@formbricks/lib/utils/questions";
 import { TProduct } from "@formbricks/types/product";
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@formbricks/ui/components/Tooltip";
 
 interface AddQuestionButtonProps {
   addQuestion: (question: any) => void;
@@ -22,6 +21,7 @@ interface AddQuestionButtonProps {
 
 export const AddQuestionButton = ({ addQuestion, product, isCxMode }: AddQuestionButtonProps) => {
   const [open, setOpen] = useState(false);
+  const [hoveredQuestionId, setHoveredQuestionId] = useState<string | null>(null);
 
   const availableQuestionTypes = isCxMode ? CXQuestionTypes : questionTypes;
 
@@ -31,7 +31,7 @@ export const AddQuestionButton = ({ addQuestion, product, isCxMode }: AddQuestio
       onOpenChange={setOpen}
       className={cn(
         open ? "shadow-lg" : "shadow-md",
-        "group w-full space-y-2 rounded-lg border border-slate-300 bg-white duration-300 hover:cursor-pointer hover:bg-slate-50"
+        "group w-full space-y-2 rounded-lg border border-slate-300 bg-white duration-200 hover:cursor-pointer hover:bg-slate-50"
       )}>
       <Collapsible.CollapsibleTrigger asChild className="group h-full w-full">
         <div className="inline-flex">
@@ -47,33 +47,32 @@ export const AddQuestionButton = ({ addQuestion, product, isCxMode }: AddQuestio
       <Collapsible.CollapsibleContent className="justify-left flex flex-col">
         {/* <hr className="py-1 text-slate-600" /> */}
         {availableQuestionTypes.map((questionType) => (
-          <TooltipProvider key={questionType.id} delayDuration={50}>
-            <Tooltip>
-              <TooltipContent align="start">
-                <p className="py-2 text-center text-xs text-slate-500 dark:text-slate-400">
-                  {questionType.description}
-                </p>
-              </TooltipContent>
-              <TooltipTrigger asChild>
-                <button
-                  type="button"
-                  key={questionType.id}
-                  className="mx-2 inline-flex items-center rounded p-0.5 px-4 py-2 text-sm font-medium text-slate-700 last:mb-2 hover:bg-slate-100 hover:text-slate-800"
-                  onClick={() => {
-                    addQuestion({
-                      ...universalQuestionPresets,
-                      ...getQuestionDefaults(questionType.id, product),
-                      id: createId(),
-                      type: questionType.id,
-                    });
-                    setOpen(false);
-                  }}>
-                  <questionType.icon className="text-brand-dark -ml-0.5 mr-2 h-4 w-4" aria-hidden="true" />
-                  {questionType.label}
-                </button>
-              </TooltipTrigger>
-            </Tooltip>
-          </TooltipProvider>
+          <button
+            type="button"
+            key={questionType.id}
+            className="group relative mx-2 inline-flex items-center justify-between rounded p-0.5 px-4 py-2 text-sm font-medium text-slate-700 last:mb-2 hover:bg-slate-100 hover:text-slate-800"
+            onClick={() => {
+              addQuestion({
+                ...universalQuestionPresets,
+                ...getQuestionDefaults(questionType.id, product),
+                id: createId(),
+                type: questionType.id,
+              });
+              setOpen(false);
+            }}
+            onMouseEnter={() => setHoveredQuestionId(questionType.id)}
+            onMouseLeave={() => setHoveredQuestionId(null)}>
+            <div className="flex items-center">
+              <questionType.icon className="text-brand-dark -ml-0.5 mr-2 h-4 w-4" aria-hidden="true" />
+              {questionType.label}
+            </div>
+            <div
+              className={`absolute right-4 text-xs font-light text-slate-500 transition-opacity duration-200 ${
+                hoveredQuestionId === questionType.id ? "opacity-100" : "opacity-0"
+              }`}>
+              {questionType.description}
+            </div>
+          </button>
         ))}
       </Collapsible.CollapsibleContent>
     </Collapsible.Root>
