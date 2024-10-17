44e731b30cbaea066a9087785ab54ba8e19da06e
Matthias Nannt <mail@matthiasnannt.com>
2024-10-17 11:37:45 +0200

    update experience insights data fetching

--- apps/web/app/(ee)/(insights)/environments/[environmentId]/experience/actions.ts
+++ apps/web/app/(ee)/(insights)/environments/[environmentId]/experience/actions.ts
@@ -1,23 +1,23 @@
 "use server";
 
-import { getStats } from "@/app/(ee)/(insights)/environments/[environmentId]/experience/lib/stats";
 import { z } from "zod";
 import { authenticatedActionClient } from "@formbricks/lib/actionClient";
 import { checkAuthorization } from "@formbricks/lib/actionClient/utils";
-import { getInsights } from "@formbricks/lib/insight/service";
 import { getOrganizationIdFromEnvironmentId } from "@formbricks/lib/organization/utils";
 import { ZId } from "@formbricks/types/common";
 import { ZInsightFilterCriteria } from "@formbricks/types/insights";
+import { getInsights } from "./lib/insights";
+import { getStats } from "./lib/stats";
 
-const ZGetInsightsAction = z.object({
+const ZGetEnvironmentInsightsAction = z.object({
   environmentId: ZId,
   limit: z.number().optional(),
   offset: z.number().optional(),
   insightsFilter: ZInsightFilterCriteria.optional(),
 });
 
-export const getInsightsAction = authenticatedActionClient
-  .schema(ZGetInsightsAction)
+export const getEnvironmentInsightsAction = authenticatedActionClient
+  .schema(ZGetEnvironmentInsightsAction)
   .action(async ({ ctx, parsedInput }) => {
     await checkAuthorization({
       userId: ctx.user.id,
