4d8d861a6314565de9814dd8582ed8e326d6c01b
Piyush Gupta <piyushguptaa2z123@gmail.com>
2024-10-22 12:08:04 +0530

    feat: adds schema migration for the granular team roles

---
+++ packages/database/migrations/20241022063447_granular_team_roles/migration.sql
@@ -0,0 +1,77 @@
+/*
+  Warnings:
+
+  - The `role` column on the `Invite` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+
+*/
+-- CreateEnum
+CREATE TYPE "OrganizationRole" AS ENUM ('owner', 'manager', 'member', 'billing');
+
+-- CreateEnum
+CREATE TYPE "TeamRole" AS ENUM ('admin', 'contributor');
+
+-- AlterTable
+ALTER TABLE "Invite" DROP COLUMN "role",
+ADD COLUMN     "role" "OrganizationRole" NOT NULL DEFAULT 'member';
+
+-- AlterTable
+ALTER TABLE "Membership" ADD COLUMN     "organizationRole" "OrganizationRole" NOT NULL DEFAULT 'member',
+ALTER COLUMN "role" DROP NOT NULL;
+
+-- CreateTable
+CREATE TABLE "Team" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+    "organizationId" TEXT NOT NULL,
+
+    CONSTRAINT "Team_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "TeamMembership" (
+    "teamId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "role" "TeamRole" NOT NULL,
+
+    CONSTRAINT "TeamMembership_pkey" PRIMARY KEY ("teamId","userId")
+);
+
+-- CreateTable
+CREATE TABLE "ProductTeam" (
+    "productId" TEXT NOT NULL,
+    "teamId" TEXT NOT NULL,
+
+    CONSTRAINT "ProductTeam_pkey" PRIMARY KEY ("productId","teamId")
+);
+
+-- CreateIndex
+CREATE INDEX "Team_organizationId_idx" ON "Team"("organizationId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Team_organizationId_name_key" ON "Team"("organizationId", "name");
+
+-- CreateIndex
+CREATE INDEX "TeamMembership_userId_idx" ON "TeamMembership"("userId");
+
+-- CreateIndex
+CREATE INDEX "ProductTeam_productId_idx" ON "ProductTeam"("productId");
+
+-- CreateIndex
+CREATE INDEX "ProductTeam_teamId_idx" ON "ProductTeam"("teamId");
+
+-- AddForeignKey
+ALTER TABLE "Team" ADD CONSTRAINT "Team_organizationId_fkey" FOREIGN KEY ("organizationId") REFERENCES "Organization"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TeamMembership" ADD CONSTRAINT "TeamMembership_teamId_fkey" FOREIGN KEY ("teamId") REFERENCES "Team"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TeamMembership" ADD CONSTRAINT "TeamMembership_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProductTeam" ADD CONSTRAINT "ProductTeam_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProductTeam" ADD CONSTRAINT "ProductTeam_teamId_fkey" FOREIGN KEY ("teamId") REFERENCES "Team"("id") ON DELETE CASCADE ON UPDATE CASCADE;
