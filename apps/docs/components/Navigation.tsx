e9d7500ecc793c7455907bba3c6768f0dc9af2db
Divyanshu Lohani <divyanshulohani2305@gmail.com>
2024-10-26 08:35:42 +0530

    fix: impliment the suggestion by code rabbit

--- apps/docs/components/Navigation.tsx
+++ apps/docs/components/Navigation.tsx
@@ -163,22 +163,29 @@ const NavigationGroup = ({
   // We need to expand the group with the current link so we loop over all links
   // Until we find the one and then expand the groups
   useEffect(() => {
-    navigation.some((group) => {
-      return group.links.some((link) => {
-        if (link.children) {
-          return link.children.some((clink) => {
-            if (clink.href.startsWith(pathname)) {
-              setOpenGroups([`${group.title}-${link.title}`]);
-              setActiveGroup(group);
-              return true;
-            }
-          });
+    const findMatchingGroup = () => {
+      for (const group of navigation) {
+        for (const link of group.links) {
+          if (!link.children) continue;
+
+          const matchingChild = link.children.find((child) => pathname && child.href.startsWith(pathname));
+
+          if (matchingChild) {
+            setOpenGroups([`${group.title}-${link.title}`]);
+            setActiveGroup(group);
+            return;
+          }
         }
-      });
-    });
-    // We do not need any dependencies this should run on page load only
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+      }
+    };
+
+    findMatchingGroup();
+
+    return () => {
+      setOpenGroups([]);
+      setActiveGroup(null);
+    };
+  }, [pathname, setActiveGroup, setOpenGroups]);
 
   useEffect(() => {
     setIsActiveGroup(activeGroup?.title === group.title);
