7271e5d6bef194e4a10fa9becc15b739e6edf479
pandeymangg <anshuman.pandey9999@gmail.com>
2024-10-29 16:55:47 +0530

    fix: api pkg

--- packages/api/src/api/client/index.ts
+++ packages/api/src/api/client/index.ts
@@ -1,14 +1,12 @@
 import { type ApiConfig } from "../../types";
 import { AttributeAPI } from "./attribute";
 import { DisplayAPI } from "./display";
-import { PeopleAPI } from "./people";
 import { ResponseAPI } from "./response";
 import { StorageAPI } from "./storage";
 
 export class Client {
   response: ResponseAPI;
   display: DisplayAPI;
-  people: PeopleAPI;
   storage: StorageAPI;
   attribute: AttributeAPI;
 
@@ -17,7 +15,6 @@ export class Client {
 
     this.response = new ResponseAPI(apiHost, environmentId);
     this.display = new DisplayAPI(apiHost, environmentId);
-    this.people = new PeopleAPI(apiHost, environmentId);
     this.attribute = new AttributeAPI(apiHost, environmentId);
     this.storage = new StorageAPI(apiHost, environmentId);
   }
