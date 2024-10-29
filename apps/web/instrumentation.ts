b55e0fc27e84a910d055563cd0a47e164816239d
Matthias Nannt <mail@matthiasnannt.com>
2024-10-29 14:05:36 +0100

    introduce required env variables

--- apps/web/instrumentation.ts
+++ apps/web/instrumentation.ts
@@ -3,24 +3,29 @@ import { LangfuseExporter } from "langfuse-vercel";
 import { env } from "@formbricks/lib/env";
 
 export async function register() {
-  if (env.LANGFUSE_SECRET_KEY && env.LANGFUSE_PUBLIC_KEY && env.LANGFUSE_BASEURL) {
-    registerOTel({
-      serviceName: "formbricks-cloud-dev",
-      traceExporter: new LangfuseExporter({
-        debug: false,
-        secretKey: env.LANGFUSE_SECRET_KEY,
-        publicKey: env.LANGFUSE_PUBLIC_KEY,
-        baseUrl: env.LANGFUSE_BASEURL,
-      }),
-    });
-  }
-
   if (process.env.NEXT_RUNTIME === "nodejs") {
-    await import("./sentry.server.config");
-    const { startOtel } = await import("@infrastack/otel");
-    startOtel({
-      serviceName: "formbricks-web",
-    });
+    if (env.LANGFUSE_SECRET_KEY && env.LANGFUSE_PUBLIC_KEY && env.LANGFUSE_BASEURL) {
+      registerOTel({
+        serviceName: "formbricks-cloud-dev",
+        traceExporter: new LangfuseExporter({
+          debug: false,
+          secretKey: env.LANGFUSE_SECRET_KEY,
+          publicKey: env.LANGFUSE_PUBLIC_KEY,
+          baseUrl: env.LANGFUSE_BASEURL,
+        }),
+      });
+    }
+
+    if (process.env.NEXT_PUBLIC_SENTRY_DSN) {
+      await import("./sentry.server.config");
+    }
+
+    if (process.env.OTEL_EXPORTER_OTLP_ENDPOINT && process.env.OTEL_EXPORTER_OTLP_HEADERS) {
+      const { startOtel } = await import("@infrastack/otel");
+      startOtel({
+        serviceName: "formbricks-web",
+      });
+    }
   }
 
   if (process.env.NEXT_RUNTIME === "edge") {
