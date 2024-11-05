70675d74f130a92b61f87ef837ae1debf5455eb1
khAja <khajashaik1000@gmail.com>
2024-11-05 11:18:13 +0530

    fix: Modal's back button padding and autofocus

--- apps/web/app/(app)/environments/[environmentId]/surveys/[surveyId]/(analysis)/summary/components/shareEmbedModal/EmbedView.tsx
+++ apps/web/app/(app)/environments/[environmentId]/surveys/[surveyId]/(analysis)/summary/components/shareEmbedModal/EmbedView.tsx
@@ -43,7 +43,7 @@ export const EmbedView = ({
   return (
     <div className="h-full overflow-hidden">
       {!disableBack && (
-        <div className="border-b border-slate-200 py-2">
+        <div className="border-b border-slate-200 px-4 py-2">
           <Button
             variant="minimal"
             className="focus:ring-0"
@@ -70,6 +70,7 @@ export const EmbedView = ({
                     ? "border-slate-200 bg-slate-100 font-semibold text-slate-900"
                     : "border-transparent text-slate-500 hover:text-slate-700"
                 )}
+                autoFocus={tab.id === activeId ? true : false}
                 aria-current={tab.id === activeId ? "page" : undefined}>
                 {tab.label}
               </Button>
