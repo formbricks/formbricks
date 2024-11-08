1bcdf06b43884edadea8544b3b120c68fe38d25b
Dhruwang Jariwala <67850763+Dhruwang@users.noreply.github.com>
2024-11-08 14:56:27 +0530

    fix: slack integration crashing (#4254)

--- apps/web/modules/ee/teams/team-list/page.tsx
+++ apps/web/modules/ee/teams/team-list/page.tsx
@@ -2,6 +2,7 @@ import { OrganizationSettingsNavbar } from "@/app/(app)/environments/[environmen
 import { TeamsView } from "@/modules/ee/teams/team-list/components/teams-view";
 import { getTeams } from "@/modules/ee/teams/team-list/lib/teams";
 import { getServerSession } from "next-auth";
+import { getTranslations } from "next-intl/server";
 import { notFound } from "next/navigation";
 import { getRoleManagementPermission } from "@formbricks/ee/lib/service";
 import { authOptions } from "@formbricks/lib/authOptions";
@@ -13,14 +14,15 @@ import { PageContentWrapper } from "@formbricks/ui/components/PageContentWrapper
 import { PageHeader } from "@formbricks/ui/components/PageHeader";
 
 export const TeamsPage = async ({ params }) => {
+  const t = await getTranslations();
   const session = await getServerSession(authOptions);
   if (!session) {
-    throw new Error("Unauthenticated");
+    throw new Error(t("common.session_not_found"));
   }
   const organization = await getOrganizationByEnvironmentId(params.environmentId);
 
   if (!organization) {
-    throw new Error("Organization not found");
+    throw new Error(t("common.organization_not_found"));
   }
 
   const canDoRoleManagement = await getRoleManagementPermission(organization);
@@ -34,12 +36,12 @@ export const TeamsPage = async ({ params }) => {
   const teams = await getTeams(session.user.id, organization.id);
 
   if (!teams) {
-    throw new Error("Teams not found");
+    throw new Error(t("common.teams_not_found"));
   }
 
   return (
     <PageContentWrapper>
-      <PageHeader pageTitle="Organization Settings">
+      <PageHeader pageTitle={t("environments.settings.general.organization_settings")}>
         <OrganizationSettingsNavbar
           environmentId={params.environmentId}
           isFormbricksCloud={IS_FORMBRICKS_CLOUD}
