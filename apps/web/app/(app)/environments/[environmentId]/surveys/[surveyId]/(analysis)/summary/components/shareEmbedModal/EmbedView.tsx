6956aa38657593358d1bd20c60abc1656fcbcd68
Piyush Gupta <piyushguptaa2z123@gmail.com>
2024-11-11 17:44:19 +0530

    fix: margin -> padding

--- apps/web/app/(app)/environments/[environmentId]/surveys/[surveyId]/(analysis)/summary/components/shareEmbedModal/EmbedView.tsx
+++ apps/web/app/(app)/environments/[environmentId]/surveys/[surveyId]/(analysis)/summary/components/shareEmbedModal/EmbedView.tsx
@@ -43,10 +43,10 @@ export const EmbedView = ({
   return (
     <div className="h-full overflow-hidden">
       {!disableBack && (
-        <div className="border-b border-slate-200 py-2">
+        <div className="border-b border-slate-200 py-2 pl-2">
           <Button
             variant="minimal"
-            className="ml-2 focus:ring-0"
+            className="focus:ring-0"
             onClick={handleInitialPageButton}
             StartIcon={ArrowLeftIcon}>
             {t("common.back")}
