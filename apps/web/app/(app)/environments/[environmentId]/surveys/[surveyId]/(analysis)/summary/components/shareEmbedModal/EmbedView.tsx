b8d2f5a34658cef9f53de272e43c33be57129d91
Piyush Gupta <piyushguptaa2z123@gmail.com>
2024-11-11 17:40:51 +0530

    fix: back button styles in embed survey modal

--- apps/web/app/(app)/environments/[environmentId]/surveys/[surveyId]/(analysis)/summary/components/shareEmbedModal/EmbedView.tsx
+++ apps/web/app/(app)/environments/[environmentId]/surveys/[surveyId]/(analysis)/summary/components/shareEmbedModal/EmbedView.tsx
@@ -46,7 +46,7 @@ export const EmbedView = ({
         <div className="border-b border-slate-200 py-2">
           <Button
             variant="minimal"
-            className="focus:ring-0"
+            className="ml-2 focus:ring-0"
             onClick={handleInitialPageButton}
             StartIcon={ArrowLeftIcon}>
             {t("common.back")}
@@ -63,6 +63,7 @@ export const EmbedView = ({
                 variant="minimal"
                 key={tab.id}
                 onClick={() => setActiveId(tab.id)}
+                autoFocus={tab.id === activeId}
                 className={cn(
                   "rounded-md border px-4 py-2 text-slate-600",
                   // "focus:ring-0 focus:ring-offset-0", // enable these classes to remove the focus rings on buttons
