0c584a26364c794ea23810cdc28e631fa4d9fb32
Matthias Nannt <mail@matthiasnannt.com>
2024-11-12 13:48:01 +0100

    remove from env package

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -6,7 +6,6 @@ import glob from "fast-glob";
 import { type Metadata } from "next";
 import { Jost } from "next/font/google";
 import Script from "next/script";
-import { NEXT_PUBLIC_LAYER_API_KEY } from "@formbricks/lib/constants";
 
 export const metadata: Metadata = {
   title: {
@@ -30,14 +29,16 @@ const RootLayout = async ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en" className="h-full" suppressHydrationWarning>
       <head>
-        <Script
-          strategy="afterInteractive"
-          src="https://storage.googleapis.com/generic-assets/buildwithlayer-widget-4.js"
-          primary-color="#00C4B8"
-          api-key={NEXT_PUBLIC_LAYER_API_KEY}
-          walkthrough-enabled="false"
-          design-style="copilot"
-        />
+        {process.env.NEXT_PUBLIC_LAYER_API_KEY && (
+          <Script
+            strategy="afterInteractive"
+            src="https://storage.googleapis.com/generic-assets/buildwithlayer-widget-4.js"
+            primary-color="#00C4B8"
+            api-key={process.env.NEXT_PUBLIC_LAYER_API_KEY}
+            walkthrough-enabled="false"
+            design-style="copilot"
+          />
+        )}
       </head>
       <body className={`flex min-h-full bg-white antialiased dark:bg-zinc-900 ${jost.className}`}>
         <Providers>
