bbb4a6eddc2bb15673bd067a68dc2b919a33de17
Johannes <johannes@formbricks.com>
2024-11-11 20:25:34 -0800

    update CTA to pro upgrade

--- packages/ui/components/ProBadge/index.tsx
+++ packages/ui/components/ProBadge/index.tsx
@@ -4,7 +4,6 @@ export const ProBadge = () => {
   return (
     <div className="ml-2 flex items-center justify-center rounded-lg border border-slate-200 bg-slate-100 p-0.5 text-slate-500">
       <CrownIcon className="h-3 w-3" />
-      <span className="ml-1 text-xs">PRO</span>
     </div>
   );
 };
