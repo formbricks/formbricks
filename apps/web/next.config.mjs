f80c7d03e267f6b6b027d6b5c0133709adf28e14
Piyush Gupta <56182734+gupta-piyush19@users.noreply.github.com>
2024-11-22 13:25:42 +0530

    fix: next js 15 stale experimental flags (#4350)

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -20,13 +20,12 @@ const nextConfig = {
   assetPrefix: process.env.ASSET_PREFIX_URL || undefined,
   output: "standalone",
   poweredByHeader: false,
+  serverExternalPackages: ["@aws-sdk"],
+  outputFileTracingIncludes: {
+    "app/api/packages": ["../../packages/js-core/dist/*", "../../packages/surveys/dist/*"],
+  },
   experimental: {
-    serverComponentsExternalPackages: ["@aws-sdk"],
     after: true,
-    instrumentationHook: true,
-    outputFileTracingIncludes: {
-      "app/api/packages": ["../../packages/js-core/dist/*", "../../packages/surveys/dist/*"],
-    },
   },
   transpilePackages: ["@formbricks/database", "@formbricks/lib"],
   images: {
