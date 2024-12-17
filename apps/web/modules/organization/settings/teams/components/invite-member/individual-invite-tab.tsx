4aff6c672e6d811b94118f1a9729dd4b47421132
Piyush Gupta <piyushguptaa2z123@gmail.com>
2024-12-17 18:48:18 +0530

    hook form in team ids

--- apps/web/modules/organization/settings/teams/components/invite-member/individual-invite-tab.tsx
+++ apps/web/modules/organization/settings/teams/components/invite-member/individual-invite-tab.tsx
@@ -4,6 +4,7 @@ import { AddMemberRole } from "@/modules/ee/role-management/components/add-membe
 import { TOrganizationTeam } from "@/modules/ee/teams/team-list/types/team";
 import { Alert, AlertDescription } from "@/modules/ui/components/alert";
 import { Button } from "@/modules/ui/components/button";
+import { FormField, FormItem, FormLabel } from "@/modules/ui/components/form";
 import { Input } from "@/modules/ui/components/input";
 import { Label } from "@/modules/ui/components/label";
 import { MultiSelect } from "@/modules/ui/components/multi-select";
@@ -12,7 +13,7 @@ import { UpgradePlanNotice } from "@/modules/ui/components/upgrade-plan-notice";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { OrganizationRole } from "@prisma/client";
 import { useTranslations } from "next-intl";
-import { useForm } from "react-hook-form";
+import { FormProvider, useForm } from "react-hook-form";
 import { z } from "zod";
 import { TOrganizationRole, ZOrganizationRole } from "@formbricks/types/memberships";
 import { ZUserName } from "@formbricks/types/user";
@@ -43,6 +44,14 @@ export const IndividualInviteTab = ({
 
   type TFormData = z.infer<typeof ZFormSchema>;
   const t = useTranslations();
+  const form = useForm<TFormData>({
+    resolver: zodResolver(ZFormSchema),
+    defaultValues: {
+      role: "owner",
+      teamIds: [],
+    },
+  });
+
   const {
     register,
     getValues,
@@ -50,15 +59,8 @@ export const IndividualInviteTab = ({
     reset,
     control,
     watch,
-    setValue,
     formState: { isSubmitting, errors },
-  } = useForm<TFormData>({
-    resolver: zodResolver(ZFormSchema),
-    defaultValues: {
-      role: "owner",
-      teamIds: [],
-    },
-  });
+  } = form;
 
   const submitEventClass = async () => {
     const data = getValues();
@@ -74,89 +76,95 @@ export const IndividualInviteTab = ({
   }));
 
   return (
-    <form onSubmit={handleSubmit(submitEventClass)} className="flex flex-col gap-6">
-      <div className="flex flex-col space-y-2">
-        <Label htmlFor="memberNameInput">{t("common.full_name")}</Label>
-        <Input
-          id="memberNameInput"
-          placeholder="e.g. Bob"
-          {...register("name", { required: true, validate: (value) => value.trim() !== "" })}
-        />
-        {errors.name && <p className="mt-1 text-sm text-red-500">{errors.name.message}</p>}
-      </div>
-      <div className="flex flex-col space-y-2">
-        <Label htmlFor="memberEmailInput">{t("common.email")}</Label>
-        <Input
-          id="memberEmailInput"
-          type="email"
-          placeholder="e.g. bob@work.com"
-          {...register("email", { required: true })}
-        />
-        {errors.email && <p className="mt-1 text-sm text-red-500">{errors.email.message}</p>}
-      </div>
-      <div>
-        <AddMemberRole
-          control={control}
-          canDoRoleManagement={canDoRoleManagement}
-          isFormbricksCloud={isFormbricksCloud}
-        />
-        {watch("role") === "member" && (
-          <Alert className="mt-2" variant="info">
-            <AlertDescription>{t("environments.settings.teams.member_role_info_message")}</AlertDescription>
-          </Alert>
-        )}
-      </div>
-
-      {canDoRoleManagement && (
+    <FormProvider {...form}>
+      <form onSubmit={handleSubmit(submitEventClass)} className="flex flex-col gap-6">
         <div className="flex flex-col space-y-2">
-          <Label htmlFor="memberEmailInput">{t("common.add_to_team")}</Label>
-          <div>
-            <MultiSelect
-              value={watch("teamIds")}
-              options={teamOptions}
-              placeholder={t("environments.settings.teams.team_select_placeholder")}
-              disabled={!teamOptions.length}
-              onChange={(teamIds) => {
-                if (teamIds.length) {
-                  setValue("teamIds", teamIds);
-                }
-              }}
-            />
-            {!teamOptions.length && (
-              <Small className="italic">{t("environments.settings.teams.create_first_team_message")}</Small>
-            )}
-          </div>
+          <Label htmlFor="memberNameInput">{t("common.full_name")}</Label>
+          <Input
+            id="memberNameInput"
+            placeholder="e.g. Bob"
+            {...register("name", { required: true, validate: (value) => value.trim() !== "" })}
+          />
+          {errors.name && <p className="mt-1 text-sm text-red-500">{errors.name.message}</p>}
         </div>
-      )}
+        <div className="flex flex-col space-y-2">
+          <Label htmlFor="memberEmailInput">{t("common.email")}</Label>
+          <Input
+            id="memberEmailInput"
+            type="email"
+            placeholder="e.g. bob@work.com"
+            {...register("email", { required: true })}
+          />
+          {errors.email && <p className="mt-1 text-sm text-red-500">{errors.email.message}</p>}
+        </div>
+        <div>
+          <AddMemberRole
+            control={control}
+            canDoRoleManagement={canDoRoleManagement}
+            isFormbricksCloud={isFormbricksCloud}
+          />
+          {watch("role") === "member" && (
+            <Alert className="mt-2" variant="info">
+              <AlertDescription>{t("environments.settings.teams.member_role_info_message")}</AlertDescription>
+            </Alert>
+          )}
+        </div>
 
-      {!canDoRoleManagement &&
-        (isFormbricksCloud ? (
-          <UpgradePlanNotice
-            message={t("environments.settings.teams.upgrade_plan_notice_message")}
-            url={`/environments/${environmentId}/settings/billing`}
-            textForUrl={t("environments.settings.teams.upgrade_plan_notice_text_for_url_cloud")}
+        {canDoRoleManagement && (
+          <FormField
+            control={control}
+            name="teamIds"
+            render={({ field }) => (
+              <FormItem className="flex flex-col space-y-2">
+                <FormLabel>{t("common.add_to_team")} </FormLabel>
+                <div>
+                  <MultiSelect
+                    value={field.value}
+                    options={teamOptions}
+                    placeholder={t("environments.settings.teams.team_select_placeholder")}
+                    disabled={!teamOptions.length}
+                    onChange={(val) => field.onChange(val)}
+                  />
+                  {!teamOptions.length && (
+                    <Small className="italic">
+                      {t("environments.settings.teams.create_first_team_message")}
+                    </Small>
+                  )}
+                </div>
+              </FormItem>
+            )}
           />
-        ) : (
-          <UpgradePlanNotice
-            message={t("environments.settings.teams.upgrade_plan_notice_message")}
-            url={`/environments/${environmentId}/settings/enterprise`}
-            textForUrl={t("environments.settings.teams.upgrade_plan_notice_text_for_url_enterprise")}
-          />
-        ))}
-      <div className="flex justify-between">
-        <Button
-          size="default"
-          type="button"
-          variant="outline"
-          onClick={() => {
-            setOpen(false);
-          }}>
-          {t("common.cancel")}
-        </Button>
-        <Button type="submit" size="default" loading={isSubmitting}>
-          {t("common.invite")}
-        </Button>
-      </div>
-    </form>
+        )}
+
+        {!canDoRoleManagement &&
+          (isFormbricksCloud ? (
+            <UpgradePlanNotice
+              message={t("environments.settings.teams.upgrade_plan_notice_message")}
+              url={`/environments/${environmentId}/settings/billing`}
+              textForUrl={t("environments.settings.teams.upgrade_plan_notice_text_for_url_cloud")}
+            />
+          ) : (
+            <UpgradePlanNotice
+              message={t("environments.settings.teams.upgrade_plan_notice_message")}
+              url={`/environments/${environmentId}/settings/enterprise`}
+              textForUrl={t("environments.settings.teams.upgrade_plan_notice_text_for_url_enterprise")}
+            />
+          ))}
+        <div className="flex justify-between">
+          <Button
+            size="default"
+            type="button"
+            variant="outline"
+            onClick={() => {
+              setOpen(false);
+            }}>
+            {t("common.cancel")}
+          </Button>
+          <Button type="submit" size="default" loading={isSubmitting}>
+            {t("common.invite")}
+          </Button>
+        </div>
+      </form>
+    </FormProvider>
   );
 };
