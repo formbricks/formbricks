fc7e7a476e0cfe326784af20da3af6d39cea57ad
Dhruwang <dhruwangjariwala18@gmail.com>
2024-12-19 17:34:02 +0530

    fix: test (part 1)

--- apps/web/playwright/utils/helper.ts
+++ apps/web/playwright/utils/helper.ts
@@ -237,8 +237,28 @@ export const createSurvey = async (page: Page, params: CreateSurveyParams) => {
     .click();
   await page.getByRole("button", { name: "Picture Selection" }).click();
   await page.getByLabel("Question*").fill(params.pictureSelectQuestion.question);
-  await page.getByRole("button", { name: "Add description", exact: true }).click();
+  await page.getByRole("button", { name: "Add description" }).click();
   await page.locator('input[name="subheader"]').fill(params.pictureSelectQuestion.description);
+
+  // Handle file uploads
+  const fileInput = page.locator('input[type="file"]');
+  const response1 = await fetch("https://formbricks-cdn.s3.eu-central-1.amazonaws.com/puppy-1-small.jpg");
+  const response2 = await fetch("https://formbricks-cdn.s3.eu-central-1.amazonaws.com/puppy-2-small.jpg");
+  const buffer1 = Buffer.from(await response1.arrayBuffer());
+  const buffer2 = Buffer.from(await response2.arrayBuffer());
+
+  await fileInput.setInputFiles([
+    {
+      name: "puppy-1-small.jpg",
+      mimeType: "image/jpeg",
+      buffer: buffer1,
+    },
+    {
+      name: "puppy-2-small.jpg",
+      mimeType: "image/jpeg",
+      buffer: buffer2,
+    },
+  ]);
 
   // File Upload Question
   await page
@@ -263,14 +283,17 @@ export const createSurvey = async (page: Page, params: CreateSurveyParams) => {
   await page.locator("#row-0").fill(params.matrix.rows[0]);
   await page.locator("#row-1").click();
   await page.locator("#row-1").fill(params.matrix.rows[1]);
+  await page.getByRole("button", { name: "Add row" }).click();
   await page.locator("#row-2").click();
   await page.locator("#row-2").fill(params.matrix.rows[2]);
   await page.locator("#column-0").click();
   await page.locator("#column-0").fill(params.matrix.columns[0]);
   await page.locator("#column-1").click();
   await page.locator("#column-1").fill(params.matrix.columns[1]);
+  await page.getByRole("button", { name: "Add column" }).click();
   await page.locator("#column-2").click();
   await page.locator("#column-2").fill(params.matrix.columns[2]);
+  await page.getByRole("button", { name: "Add column" }).click();
   await page.locator("#column-3").click();
   await page.locator("#column-3").fill(params.matrix.columns[3]);
 
@@ -308,6 +331,20 @@ export const createSurvey = async (page: Page, params: CreateSurveyParams) => {
     .nth(1)
     .click();
   await page.getByRole("button", { name: "Ranking" }).click();
+  await page.getByLabel("Question*").fill(params.ranking.question);
+  await page.getByPlaceholder("Option 1").click();
+  await page.getByPlaceholder("Option 1").fill(params.ranking.choices[0]);
+  await page.getByPlaceholder("Option 2").click();
+  await page.getByPlaceholder("Option 2").fill(params.ranking.choices[1]);
+  await page.getByRole("button", { name: "Add option" }).click();
+  await page.getByPlaceholder("Option 3").click();
+  await page.getByPlaceholder("Option 3").fill(params.ranking.choices[2]);
+  await page.getByRole("button", { name: "Add option" }).click();
+  await page.getByPlaceholder("Option 4").click();
+  await page.getByPlaceholder("Option 4").fill(params.ranking.choices[3]);
+  await page.getByRole("button", { name: "Add option" }).click();
+  await page.getByPlaceholder("Option 5").click();
+  await page.getByPlaceholder("Option 5").fill(params.ranking.choices[4]);
 
   // Thank You Card
   await page
@@ -398,6 +435,25 @@ export const createSurveyWithLogic = async (page: Page, params: CreateSurveyWith
   await page.getByLabel("Question*").fill(params.pictureSelectQuestion.question);
   await page.getByRole("button", { name: "Add description" }).click();
   await page.locator('input[name="subheader"]').fill(params.pictureSelectQuestion.description);
+
+  const response1 = await fetch("https://formbricks-cdn.s3.eu-central-1.amazonaws.com/puppy-1-small.jpg");
+  const response2 = await fetch("https://formbricks-cdn.s3.eu-central-1.amazonaws.com/puppy-2-small.jpg");
+  const buffer1 = Buffer.from(await response1.arrayBuffer());
+  const buffer2 = Buffer.from(await response2.arrayBuffer());
+
+  await page.locator('input[type="file"][name="selected-file"]').setInputFiles([
+    {
+      name: "puppy-1-small.jpg",
+      mimeType: "image/jpeg",
+      buffer: buffer1,
+    },
+    {
+      name: "puppy-2-small.jpg",
+      mimeType: "image/jpeg",
+      buffer: buffer2,
+    },
+  ]);
+
   await page.getByLabel("Required").click();
 
   // Rating Question
@@ -497,6 +553,7 @@ export const createSurveyWithLogic = async (page: Page, params: CreateSurveyWith
     .click();
   await page.getByRole("button", { name: "Date" }).click();
   await page.getByLabel("Question*").fill(params.date.question);
+  await page.getByLabel("Required").click();
 
   // Cal Question
   await page
