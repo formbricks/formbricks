bd8724c1e26f8819ecb77291477521433cdaf291
Dhruwang Jariwala <67850763+Dhruwang@users.noreply.github.com>
2024-12-19 13:48:10 +0530

    fix: auto zoom on ios (#4502)

--- apps/web/app/s/[surveyId]/layout.tsx
+++ apps/web/app/s/[surveyId]/layout.tsx
@@ -1,3 +1,13 @@
+import { Viewport } from "next";
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1.0,
+  maximumScale: 1.0,
+  userScalable: false,
+  viewportFit: "contain",
+};
+
 const SurveyLayout = ({ children }) => {
   return <div className="h-dvh">{children}</div>;
 };
