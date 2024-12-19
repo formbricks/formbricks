54311751eb3749bb1c7fe6067d69d6a0548afc68
Dhruwang <dhruwangjariwala18@gmail.com>
2024-12-19 17:41:17 +0530

    fix: urls in email footer

--- apps/web/modules/email/components/email-template.tsx
+++ apps/web/modules/email/components/email-template.tsx
@@ -1,11 +1,12 @@
-import { Body, Column, Container, Html, Img, Link, Row, Section, Tailwind } from "@react-email/components";
+import { Body, Column, Container, Html, Img, Link, Section, Tailwind } from "@react-email/components";
+import { IMPRINT_URL, PRIVACY_URL } from "@formbricks/lib/constants";
 
 export function EmailTemplate({ children }): React.JSX.Element {
   return (
     <Html>
       <Tailwind>
         <Body
-          className="m-0 h-full w-full justify-center bg-slate-100 bg-slate-50 p-6 text-center text-base font-medium text-slate-800"
+          className="m-0 h-full w-full justify-center bg-slate-50 p-6 text-center text-base font-medium text-slate-800"
           style={{
             fontFamily: "'Jost', 'Helvetica Neue', 'Segoe UI', 'Helvetica', 'sans-serif'",
           }}>
@@ -20,44 +21,43 @@ export function EmailTemplate({ children }): React.JSX.Element {
           </Section>
           <Container className="mx-auto my-8 max-w-xl bg-white p-4 text-left">{children}</Container>
 
-          <Section>
-            <Row>
-              <Column align="right" key="twitter">
-                <Link href="https://twitter.com/formbricks" target="_blank">
-                  <Img
-                    alt="Tw"
-                    src="https://s3.eu-central-1.amazonaws.com/listmonk-formbricks/Twitter-transp.png"
-                    title="Twitter"
-                    width="32"
-                  />
-                </Link>
-              </Column>
-              <Column align="center" className="w-20" key="github">
-                <Link href="https://formbricks.com/github" target="_blank">
-                  <Img
-                    alt="GitHub"
-                    src="https://s3.eu-central-1.amazonaws.com/listmonk-formbricks/Github-transp.png"
-                    title="GitHub"
-                    width="32"
-                  />
-                </Link>
-              </Column>
-            </Row>
+          <Section className="flex justify-center">
+            <Column align="center" className="px-2" key="twitter">
+              <Link href="https://twitter.com/formbricks" target="_blank" className="w-fit">
+                <Img
+                  alt="Tw"
+                  src="https://s3.eu-central-1.amazonaws.com/listmonk-formbricks/Twitter-transp.png"
+                  title="Twitter"
+                  width="32"
+                />
+              </Link>
+            </Column>
+            <Column align="center" className="px-2" key="github">
+              <Link href="https://formbricks.com/github" target="_blank" className="w-fit">
+                <Img
+                  alt="GitHub"
+                  src="https://s3.eu-central-1.amazonaws.com/listmonk-formbricks/Github-transp.png"
+                  title="GitHub"
+                  width="32"
+                />
+              </Link>
+            </Column>
           </Section>
           <Section className="mt-4 text-center text-sm">
             Formbricks {new Date().getFullYear()}. All rights reserved.
             <br />
-            <Link
-              href="https://formbricks.com/imprint?utm_source=email_footer&utm_medium=email"
-              target="_blank">
-              Imprint
-            </Link>{" "}
-            |{" "}
-            <Link
-              href="https://formbricks.com/privacy-policy?utm_source=email_footer&utm_medium=email"
-              target="_blank">
-              Privacy Policy
-            </Link>
+            {IMPRINT_URL && (
+              <Link href={IMPRINT_URL} target="_blank" rel="noopener noreferrer">
+                Imprint{" "}
+              </Link>
+            )}
+            {IMPRINT_URL && PRIVACY_URL && "|"}
+            {PRIVACY_URL && (
+              <Link href={PRIVACY_URL} target="_blank" rel="noopener noreferrer">
+                {" "}
+                Privacy Policy
+              </Link>
+            )}
           </Section>
         </Body>
       </Tailwind>
