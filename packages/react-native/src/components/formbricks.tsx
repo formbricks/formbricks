cb60bbe4cd407641f10695f6a99890a9b4922225
pandeymangg <anshuman.pandey9999@gmail.com>
2025-02-01 15:56:57 +0530

    fix

--- packages/react-native/src/components/formbricks.tsx
+++ packages/react-native/src/components/formbricks.tsx
@@ -1,8 +1,8 @@
-import React, { useCallback, useEffect, useSyncExternalStore } from "react";
 import { SurveyWebView } from "@/components/survey-web-view";
 import { init } from "@/lib/common/initialize";
 import { Logger } from "@/lib/common/logger";
 import { SurveyStore } from "@/lib/survey/store";
+import React, { useCallback, useEffect, useSyncExternalStore } from "react";
 
 interface FormbricksProps {
   appUrl: string;
@@ -21,7 +21,10 @@ export function Formbricks({ appUrl, environmentId }: FormbricksProps): React.JS
           environmentId,
           appUrl,
         });
-      } catch {
+      } catch (e) {
+        // eslint-disable-next-line no-console -- logging is allowed in demo apps
+        console.log("Error aa gayi betichod: ", e);
+
         logger.debug("Initialization failed");
       }
     };
