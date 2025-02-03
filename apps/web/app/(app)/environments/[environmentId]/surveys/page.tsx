0c6b8a83e7802f7cb2c4503de70f3fe3cb7791ad
Dhruwang <dhruwangjariwala18@gmail.com>
2025-02-03 15:25:25 +0530

    test

--- apps/web/app/(app)/environments/[environmentId]/surveys/page.tsx
+++ apps/web/app/(app)/environments/[environmentId]/surveys/page.tsx
@@ -103,6 +103,7 @@ const SurveysPage = async ({ params: paramsProps, searchParams: searchParamsProp
       {surveyCount > 0 ? (
         <>
           <PageHeader pageTitle={t("common.surveys")} cta={isReadOnly ? <></> : <CreateSurveyButton />} />
+          <h2>{t("this_is_a_test")}</h2>
           <SurveysList
             environment={environment}
             otherEnvironment={otherEnvironment}
