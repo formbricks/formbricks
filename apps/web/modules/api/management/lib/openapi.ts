7fe0c0144fb4536568ff3ebde8584317fe07c4b0
Matthias Nannt <mail@matthiasnannt.com>
2025-02-19 16:30:19 +0100

    update file structure

--- apps/web/modules/api/management/lib/openapi.ts
+++ apps/web/modules/api/management/lib/openapi.ts
@@ -1,9 +1,9 @@
+import { createResponseEndpoint, getResponsesEndpoint } from "@/modules/api/lib/openapi";
 import {
   deleteResponseEndpoint,
   getResponseEndpoint,
   updateResponseEndpoint,
 } from "@/modules/api/management/responses/[responseId]/lib/openapi";
-import { createResponseEndpoint, getResponsesEndpoint } from "@/modules/api/management/responses/lib/openapi";
 import { ZodOpenApiPathsObject } from "zod-openapi";
 
 export const responsePaths: ZodOpenApiPathsObject = {
--- apps/web/modules/api/openapi-document.ts
+++ apps/web/modules/api/openapi-document.ts
@@ -1,4 +1,3 @@
-import { responsePaths } from "@/modules/api/management/responses/lib/openapi";
 import * as yaml from "yaml";
 import { z } from "zod";
 import { createDocument, extendZodWithOpenApi } from "zod-openapi";
@@ -10,16 +9,16 @@ const document = createDocument({
   openapi: "3.1.0",
   info: {
     title: "Formbricks API",
-    description: "An API for managing surveys and responses.",
-    version: "1.0.0",
+    description: "Manage Formbricks ressources programmatically.",
+    version: "2.0.0",
   },
-  paths: {
+  /*   paths: {
     ...responsePaths,
-  },
+  }, */
   servers: [
     {
       url: "https://app.formbricks.com/api",
-      description: "The production server.",
+      description: "Formbricks Cloud",
     },
   ],
   tags: [
