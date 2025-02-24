0aa77bce60a7908637a3b6b2757589dc54afbb9f
Piyush Gupta <piyushguptaa2z123@gmail.com>
2025-02-24 15:08:27 +0530

    refactor: improve SAML SSO authorization handling

--- apps/web/modules/ee/auth/saml/authorize.ts
+++ apps/web/modules/ee/auth/saml/authorize.ts
@@ -1,20 +1,16 @@
+import { responses } from "@/app/lib/api/response";
 import jackson from "@/modules/ee/auth/saml/jackson";
 import { getIsSAMLSSOEnabled } from "@/modules/ee/license-check/lib/utils";
 import type { OAuthReq } from "@boxyhq/saml-jackson";
-import type { NextApiResponse } from "next";
 import { NextRequest, NextResponse } from "next/server";
 
-export const GET = async (req: NextRequest, res: NextApiResponse) => {
+export const GET = async (req: NextRequest) => {
   const { oauthController } = await jackson();
   const searchParams = Object.fromEntries(req.nextUrl.searchParams);
   const isSAMLSSOEnabled = await getIsSAMLSSOEnabled();
 
   if (!isSAMLSSOEnabled) {
-    return res.status(400).send("SAML SSO is not enabled in your Formbricks license");
-  }
-
-  if (req.method !== "GET") {
-    return res.status(400).send("Method not allowed");
+    return responses.badRequestResponse("SAML SSO is not enabled in your Formbricks license");
   }
 
   try {
@@ -22,8 +18,8 @@ export const GET = async (req: NextRequest, res: NextApiResponse) => {
 
     return NextResponse.redirect(redirect_url as string);
   } catch (err) {
-    const { message, statusCode = 500 } = err;
+    const { message } = err;
 
-    return Response.json({ message }, { status: statusCode });
+    return responses.internalServerErrorResponse(message);
   }
 };
