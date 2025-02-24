33c71cc43ddfbf9b2a122c659f82e18e42b852f5
Piyush Gupta <piyushguptaa2z123@gmail.com>
2025-02-24 17:24:48 +0530

    fix: saml sso check

--- apps/web/modules/ee/license-check/lib/utils.ts
+++ apps/web/modules/ee/license-check/lib/utils.ts
@@ -382,8 +382,7 @@ export const getIsSAMLSSOEnabled = async (): Promise<boolean> => {
   }
   const licenseFeatures = await getLicenseFeatures();
   if (!licenseFeatures) return false;
-  return true;
-  // return licenseFeatures.sso && licenseFeatures.saml;
+  return licenseFeatures.sso && licenseFeatures.saml;
 };
 
 export const getIsOrganizationAIReady = async (billingPlan: Organization["billing"]["plan"]) => {
