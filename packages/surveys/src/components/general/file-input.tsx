444b8dbcdda1575b15b771347e4ff33ba0b2fe71
Dhruwang <dhruwangjariwala18@gmail.com>
2025-02-27 17:46:25 +0530

    span->label

--- packages/surveys/src/components/general/file-input.tsx
+++ packages/surveys/src/components/general/file-input.tsx
@@ -248,11 +248,11 @@ export function FileInput({
                   d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
                 />
               </svg>
-              <span
+              <label
                 className="fb-text-placeholder fb-mt-2 fb-text-sm dark:fb-text-slate-400"
                 id={`${uniqueHtmlFor}-label`}>
                 Click or drag to upload files.
-              </span>
+              </label>
               <input
                 type="file"
                 id={uniqueHtmlFor}
