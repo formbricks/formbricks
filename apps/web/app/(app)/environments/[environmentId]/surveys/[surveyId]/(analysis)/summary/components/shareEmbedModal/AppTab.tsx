510d3a558f455768a82e89f66da2d26bb6c999e7
Dhruwang <dhruwangjariwala18@gmail.com>
2025-03-12 17:22:10 +0530

    fixed components

--- apps/web/app/(app)/environments/[environmentId]/surveys/[surveyId]/(analysis)/summary/components/shareEmbedModal/AppTab.tsx
+++ apps/web/app/(app)/environments/[environmentId]/surveys/[surveyId]/(analysis)/summary/components/shareEmbedModal/AppTab.tsx
@@ -1,13 +1,12 @@
 "use client";
 
-import { Alert, AlertDescription, AlertTitle } from "@/modules/ui/components/alert";
-import { Button } from "@/modules/ui/components/button";
+import { MobileAppTab } from "@/app/(app)/environments/[environmentId]/surveys/[surveyId]/(analysis)/summary/components/shareEmbedModal/MobileAppTab";
+import { WebAppTab } from "@/app/(app)/environments/[environmentId]/surveys/[surveyId]/(analysis)/summary/components/shareEmbedModal/WebAppTab";
 import { OptionsSwitch } from "@/modules/ui/components/options-switch";
 import { useTranslate } from "@tolgee/react";
-import Link from "next/link";
 import { useState } from "react";
 
-export const AppTab = ({}) => {
+export const AppTab = () => {
   const { t } = useTranslate();
   const [selectedTab, setSelectedTab] = useState("webapp");
 
@@ -24,43 +23,5 @@ export const AppTab = ({}) => {
 
       <div className="mt-4">{selectedTab === "webapp" ? <WebAppTab /> : <MobileAppTab />}</div>
     </div>
-  );
-};
-
-const MobileAppTab = () => {
-  const { t } = useTranslate();
-  return (
-    <Alert>
-      <AlertTitle>{t("environments.surveys.summary.quickstart_mobile_apps")}</AlertTitle>
-      <AlertDescription>
-        {t("environments.surveys.summary.quickstart_mobile_apps_description")}
-        <Button asChild className="w-fit" size="sm" variant="link">
-          <Link
-            href="https://formbricks.com/docs/xm-and-surveys/surveys/website-app-surveys/framework-guides"
-            target="_blank">
-            {t("common.learn_more")}
-          </Link>
-        </Button>
-      </AlertDescription>
-    </Alert>
-  );
-};
-
-const WebAppTab = ({}) => {
-  const { t } = useTranslate();
-  return (
-    <Alert>
-      <AlertTitle>{t("environments.surveys.summary.quickstart_web_apps")}</AlertTitle>
-      <AlertDescription>
-        {t("environments.surveys.summary.quickstart_web_apps_description")}
-        <Button asChild className="w-fit" size="sm" variant="link">
-          <Link
-            href="https://formbricks.com/docs/xm-and-surveys/surveys/website-app-surveys/quickstart"
-            target="_blank">
-            {t("common.learn_more")}
-          </Link>
-        </Button>
-      </AlertDescription>
-    </Alert>
   );
 };
