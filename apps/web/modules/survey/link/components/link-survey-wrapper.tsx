0164eca206efab0104145555ee39423fef076ffa
Johannes <72809645+jobenjada@users.noreply.github.com>
2025-03-13 04:27:57 -0700

    fix: survey display and card width (#4937)

--- apps/web/modules/survey/link/components/link-survey-wrapper.tsx
+++ apps/web/modules/survey/link/components/link-survey-wrapper.tsx
@@ -80,7 +80,7 @@ export const LinkSurveyWrapper = ({
           onBackgroundLoaded={handleBackgroundLoaded}>
           <div className="flex max-h-dvh min-h-dvh items-center justify-center overflow-clip">
             {!styling.isLogoHidden && project.logo?.url && <ClientLogo projectLogo={project.logo} />}
-            <div className="h-full w-full max-w-lg space-y-6 px-1.5">
+            <div className="h-full w-full max-w-4xl space-y-6 px-1.5">
               {isPreview && (
                 <div className="fixed left-0 top-0 flex w-full items-center justify-between bg-slate-600 p-2 px-4 text-center text-sm text-white shadow-sm">
                   <div />
