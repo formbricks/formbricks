edb7cb8e883d5099c70a573d83faebe51cb16421
pandeymangg <anshuman.pandey9999@gmail.com>
2025-03-20 17:20:28 +0530

    changes

--- packages/logger/src/logger.ts
+++ packages/logger/src/logger.ts
@@ -1,5 +1,4 @@
 import Pino, { type Logger, type LoggerOptions, stdSerializers } from "pino";
-import "pino-pretty";
 import { LOG_LEVELS, type TLogLevel, ZLogLevel } from "../types/logger";
 
 const IS_PRODUCTION = !process.env.NODE_ENV || process.env.NODE_ENV === "production";
