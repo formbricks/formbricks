b120de550f93e397b78f5a37cba2e73ebb2dbbdb
pandeymangg <anshuman.pandey9999@gmail.com>
2025-03-25 09:07:17 +0530

    fix: api route

--- apps/web/modules/ee/contacts/api/bulk/route.ts
+++ apps/web/modules/ee/contacts/api/bulk/route.ts
@@ -1,162 +1,147 @@
-import { transformErrorToDetails } from "@/app/lib/api/validator";
 import { responses } from "@/modules/api/v2/lib/response";
-import { authenticateRequest } from "@/modules/api/v2/management/auth/authenticate-request";
+import { authenticatedApiClient } from "@/modules/api/v2/management/auth/authenticated-api-client";
 import { upsertBulkContacts } from "@/modules/ee/contacts/api/bulk/lib/contact";
 import { ZContactBulkUploadRequest } from "@/modules/ee/contacts/types/contact";
 import { getIsContactsEnabled } from "@/modules/ee/license-check/lib/utils";
-import { NextRequest } from "next/server";
 import { z } from "zod";
 import { ZUserEmail } from "@formbricks/types/user";
 
-export const PUT = async (request: NextRequest) => {
-  try {
-    const authentication = await authenticateRequest(request);
-    if (!authentication.ok) return responses.unauthorizedResponse();
+export const PUT = async (request: Request) =>
+  authenticatedApiClient({
+    request,
+    schemas: {
+      body: ZContactBulkUploadRequest,
+    },
+    handler: async ({ authentication, parsedInput }) => {
+      const isContactsEnabled = await getIsContactsEnabled();
+      if (!isContactsEnabled) {
+        return responses.forbiddenResponse();
+      }
 
-    const isContactsEnabled = await getIsContactsEnabled();
-    if (!isContactsEnabled) {
-      return responses.forbiddenResponse();
-    }
+      const { contacts } = parsedInput.body ?? { contacts: [] };
+      const { environmentId } = authentication;
 
-    const json = await request.json();
-    const parsedInput = ZContactBulkUploadRequest.safeParse(json);
+      const emailKey = "email";
 
-    if (!parsedInput.success) {
-      const details = Object.entries(transformErrorToDetails(parsedInput.error)).map(([field, issue]) => ({
-        field,
-        issue,
-      }));
+      const seenEmails = new Set<string>();
+      const duplicateEmails = new Set<string>();
 
-      return responses.badRequestResponse({
-        details,
-      });
-    }
+      for (const contact of contacts) {
+        const email = contact.attributes.find((attr) => attr.attributeKey.key === emailKey)?.value;
 
-    const { contacts } = parsedInput.data;
-    const { environmentId } = authentication.data;
-
-    const emailKey = "email";
-
-    const seenEmails = new Set<string>();
-    const duplicateEmails = new Set<string>();
-
-    for (const contact of contacts) {
-      const email = contact.attributes.find((attr) => attr.attributeKey.key === emailKey)?.value;
-
-      if (email) {
-        if (seenEmails.has(email)) {
-          duplicateEmails.add(email);
-        } else {
-          seenEmails.add(email);
+        if (email) {
+          if (seenEmails.has(email)) {
+            duplicateEmails.add(email);
+          } else {
+            seenEmails.add(email);
+          }
         }
       }
-    }
 
-    // Filter out any contacts that have a duplicate email.
-    // All contacts with an email that appears more than once will be excluded.
-    const filteredContactsByEmail = contacts.filter((contact) => {
-      const email = contact.attributes.find((attr) => attr.attributeKey.key === emailKey)?.value;
-      return email && !duplicateEmails.has(email);
-    });
-
-    if (filteredContactsByEmail.length === 0) {
-      return responses.badRequestResponse({
-        details: [
-          { field: "contacts", issue: "No valid contacts to process after filtering duplicate emails" },
-        ],
+      // Filter out any contacts that have a duplicate email.
+      // All contacts with an email that appears more than once will be excluded.
+      const filteredContactsByEmail = contacts.filter((contact) => {
+        const email = contact.attributes.find((attr) => attr.attributeKey.key === emailKey)?.value;
+        return email && !duplicateEmails.has(email);
       });
-    }
 
-    // duplicate userIds
-    const seenUserIds = new Set<string>();
-    const duplicateUserIds = new Set<string>();
+      if (filteredContactsByEmail.length === 0) {
+        return responses.badRequestResponse({
+          details: [
+            { field: "contacts", issue: "No valid contacts to process after filtering duplicate emails" },
+          ],
+        });
+      }
 
-    for (const contact of filteredContactsByEmail) {
-      const userId = contact.attributes.find((attr) => attr.attributeKey.key === "userId")?.value;
+      // duplicate userIds
+      const seenUserIds = new Set<string>();
+      const duplicateUserIds = new Set<string>();
 
-      if (userId) {
-        if (seenUserIds.has(userId)) {
-          duplicateUserIds.add(userId);
-        } else {
-          seenUserIds.add(userId);
+      for (const contact of filteredContactsByEmail) {
+        const userId = contact.attributes.find((attr) => attr.attributeKey.key === "userId")?.value;
+
+        if (userId) {
+          if (seenUserIds.has(userId)) {
+            duplicateUserIds.add(userId);
+          } else {
+            seenUserIds.add(userId);
+          }
         }
       }
-    }
 
-    // userIds need to be unique, so we get rid of all the contacts with duplicate userIds
-    const filteredContacts = filteredContactsByEmail.filter((contact) => {
-      const userId = contact.attributes.find((attr) => attr.attributeKey.key === "userId")?.value;
-      if (userId) {
-        return !duplicateUserIds.has(userId);
+      // userIds need to be unique, so we get rid of all the contacts with duplicate userIds
+      const filteredContacts = filteredContactsByEmail.filter((contact) => {
+        const userId = contact.attributes.find((attr) => attr.attributeKey.key === "userId")?.value;
+        if (userId) {
+          return !duplicateUserIds.has(userId);
+        }
+
+        return true;
+      });
+
+      if (filteredContacts.length === 0) {
+        return responses.badRequestResponse({
+          details: [
+            { field: "contacts", issue: "No valid contacts to process after filtering duplicate userIds" },
+          ],
+        });
       }
 
-      return true;
-    });
+      const emails = filteredContacts
+        .map((contact) => contact.attributes.find((attr) => attr.attributeKey.key === emailKey)?.value)
+        .filter((email): email is string => Boolean(email));
 
-    if (filteredContacts.length === 0) {
-      return responses.badRequestResponse({
-        details: [
-          { field: "contacts", issue: "No valid contacts to process after filtering duplicate userIds" },
-        ],
-      });
-    }
+      if (!emails.length) {
+        return responses.badRequestResponse({
+          details: [
+            { field: "contacts", issue: "No email found for any contact, please check your contacts" },
+          ],
+        });
+      }
 
-    const emails = filteredContacts
-      .map((contact) => contact.attributes.find((attr) => attr.attributeKey.key === emailKey)?.value)
-      .filter((email): email is string => Boolean(email));
+      const parsedEmails = z.array(ZUserEmail).safeParse(emails);
+      if (!parsedEmails.success) {
+        return responses.badRequestResponse({
+          details: [
+            { field: "contacts", issue: "Invalid email found for some contacts, please check your contacts" },
+          ],
+        });
+      }
 
-    if (!emails.length) {
-      return responses.badRequestResponse({
-        details: [{ field: "contacts", issue: "No email found for any contact, please check your contacts" }],
-      });
-    }
+      const { contactIdxWithConflictingUserIds } = await upsertBulkContacts(
+        filteredContacts,
+        environmentId,
+        parsedEmails.data
+      );
 
-    const parsedEmails = z.array(ZUserEmail).safeParse(emails);
-    if (!parsedEmails.success) {
-      return responses.badRequestResponse({
-        details: [
-          { field: "contacts", issue: "Invalid email found for some contacts, please check your contacts" },
-        ],
-      });
-    }
+      if (contactIdxWithConflictingUserIds.length) {
+        return responses.multiStatusResponse({
+          data: {
+            status: "success",
+            message:
+              "Contacts bulk upload partially successful. Some contacts were skipped due to conflicting userIds.",
+            skippedContacts: contactIdxWithConflictingUserIds.map((idx) => `contact_${idx + 1}`),
+          },
+        });
+      }
 
-    const { contactIdxWithConflictingUserIds } = await upsertBulkContacts(
-      filteredContacts,
-      environmentId,
-      parsedEmails.data
-    );
-
-    if (contactIdxWithConflictingUserIds.length) {
-      return responses.multiStatusResponse({
+      return responses.successResponse({
         data: {
           status: "success",
-          message:
-            "Contacts bulk upload partially successful. Some contacts were skipped due to conflicting userIds.",
-          skippedContacts: contactIdxWithConflictingUserIds.map((idx) => `contact_${idx + 1}`),
+          message: "Contacts bulk upload successful",
+          processed: filteredContacts.length,
+          ...(duplicateEmails.size > 0 || duplicateUserIds.size > 0
+            ? {
+                skipped: {
+                  total: contacts.length - filteredContacts.length,
+                  conflicts: {
+                    duplicateEmails: Array.from(duplicateEmails),
+                    duplicateUserIds: Array.from(duplicateUserIds),
+                  },
+                },
+              }
+            : {}),
         },
       });
-    }
-
-    return responses.successResponse({
-      data: {
-        status: "success",
-        message: "Contacts bulk upload successful",
-        processed: filteredContacts.length,
-        ...(duplicateEmails.size > 0 || duplicateUserIds.size > 0
-          ? {
-              skipped: {
-                total: contacts.length - filteredContacts.length,
-                conflicts: {
-                  duplicateEmails: Array.from(duplicateEmails),
-                  duplicateUserIds: Array.from(duplicateUserIds),
-                },
-              },
-            }
-          : {}),
-      },
-    });
-  } catch (error) {
-    console.log(error.stack);
-    return responses.internalServerErrorResponse();
-  }
-};
+    },
+  });
