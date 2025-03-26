4d192d1c4e189d17adb716c57cd6f2095f90aace
Victor Santos <victor@formbricks.com>
2025-03-26 17:28:10 +0700

    added the endpoint, documentation, unity and e2e tests

---
+++ apps/web/modules/api/v2/management/roles/lib/roles.ts
@@ -0,0 +1,26 @@
+import { ApiErrorResponseV2 } from "@/modules/api/v2/types/api-error";
+import { ApiResponse } from "@/modules/api/v2/types/api-success";
+import { prisma } from "@formbricks/database";
+import { Result, err, ok } from "@formbricks/types/error-handlers";
+
+export const getRoles = async (): Promise<Result<ApiResponse<string[]>, ApiErrorResponseV2>> => {
+  try {
+    // We use a raw query to get all the roles because we can't list enum options with prisma
+    const results = await prisma.$queryRaw<{ unnest: string }[]>`
+        SELECT unnest(enum_range(NULL::"MembershipRole"));
+    `;
+
+    if (!results) {
+      // We set internal_server_error because it's an enum and we should always have the roles
+      return err({ type: "internal_server_error", details: [{ field: "roles", issue: "not found" }] });
+    }
+
+    const roles = results.map((row) => row.unnest);
+
+    return ok({
+      data: roles,
+    });
+  } catch (error) {
+    return err({ type: "internal_server_error", details: [{ field: "roles", issue: error.message }] });
+  }
+};
