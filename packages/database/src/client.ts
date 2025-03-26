7a8c8d0b475207c741f86c6f966ec487ac6c7757
Dhruwang <dhruwangjariwala18@gmail.com>
2025-03-26 17:52:53 +0530

    remoevd irrelevant code

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -2,10 +2,6 @@ import { PrismaClient } from "@prisma/client";
 import { withAccelerate } from "@prisma/extension-accelerate";
 
 const prismaClientSingleton = () => {
-  if (!process.env.DATABASE_URL) {
-    throw new Error("DATABASE_URL is not set");
-  }
-
   return new PrismaClient({
     datasources: { db: { url: process.env.DATABASE_URL } },
     ...(process.env.DEBUG === "1" && {
