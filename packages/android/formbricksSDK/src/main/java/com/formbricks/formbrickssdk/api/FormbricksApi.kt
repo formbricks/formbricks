3c92c3fed48776a53dedbd9cb2e3dc3137d3f2bf
longvantruong <longtv.mobiledeveloper@gmail.com>
2025-03-31 14:40:12 +0700

    feat: add retry mechanism for postUser API call

--- packages/android/formbricksSDK/src/main/java/com/formbricks/formbrickssdk/api/FormbricksApi.kt
+++ packages/android/formbricksSDK/src/main/java/com/formbricks/formbrickssdk/api/FormbricksApi.kt
@@ -6,10 +6,25 @@ import com.formbricks.formbrickssdk.model.user.PostUserBody
 import com.formbricks.formbrickssdk.model.user.UserResponse
 import com.formbricks.formbrickssdk.network.FormbricksApiService
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.withContext
 
 object FormbricksApi {
     var service = FormbricksApiService()
+
+    private suspend fun <T> retryApiCall(
+        retries: Int = 2,
+        delayTime: Long = 1000,
+        block: suspend () -> Result<T>
+    ): Result<T> {
+        repeat(retries) { attempt ->
+            val result = block()
+            if (result.isSuccess) return result
+            println("⚠️ Retry ${attempt + 1} due to error: ${result.exceptionOrNull()?.localizedMessage}")
+            delay(delayTime)
+        }
+        return block()
+    }
 
     fun initialize() {
         service.initialize(
@@ -20,21 +35,29 @@ object FormbricksApi {
     }
 
     suspend fun getEnvironmentState(): Result<EnvironmentDataHolder> = withContext(Dispatchers.IO) {
-        try {
-            val response = service.getEnvironmentStateObject(Formbricks.environmentId)
-            val result = response.getOrThrow()
-            Result.success(result)
-        } catch (e: Exception) {
-            Result.failure(e)
+        retryApiCall {
+            try {
+                val response = service.getEnvironmentStateObject(Formbricks.environmentId)
+                val result = response.getOrThrow()
+                Result.success(result)
+            } catch (e: Exception) {
+                Result.failure(e)
+            }
         }
     }
 
-    suspend fun postUser(userId: String, attributes: Map<String, *>?): Result<UserResponse> = withContext(Dispatchers.IO) {
-        try {
-            val result = service.postUser(Formbricks.environmentId, PostUserBody.create(userId, attributes)).getOrThrow()
-            Result.success(result)
-        } catch (e: Exception) {
-            Result.failure(e)
+    suspend fun postUser(userId: String, attributes: Map<String, *>?): Result<UserResponse> =
+        withContext(Dispatchers.IO) {
+            retryApiCall {
+                try {
+                    val result = service.postUser(
+                        Formbricks.environmentId,
+                        PostUserBody.create(userId, attributes)
+                    ).getOrThrow()
+                    Result.success(result)
+                } catch (e: Exception) {
+                    Result.failure(e)
+                }
+            }
         }
-    }
 }
