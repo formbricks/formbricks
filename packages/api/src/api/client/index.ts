ed490e339267010790993ca9c46247906cec5847
pandeymangg <anshuman.pandey9999@gmail.com>
2025-04-02 10:05:53 +0700

    removes attribute, response and display from the api package

--- packages/api/src/api/client/index.ts
+++ packages/api/src/api/client/index.ts
@@ -1,16 +1,8 @@
 import { type ApiConfig } from "../../types";
-import { AttributeAPI } from "./attribute";
-import { DisplayAPI } from "./display";
 import { EnvironmentAPI } from "./environment";
-import { ResponseAPI } from "./response";
-import { StorageAPI } from "./storage";
 import { UserAPI } from "./user";
 
 export class Client {
-  response: ResponseAPI;
-  display: DisplayAPI;
-  storage: StorageAPI;
-  attribute: AttributeAPI;
   user: UserAPI;
   environment: EnvironmentAPI;
 
@@ -18,10 +10,6 @@ export class Client {
     const { appUrl, environmentId, isDebug } = options;
     const isDebugMode = isDebug ?? false;
 
-    this.response = new ResponseAPI(appUrl, environmentId, isDebugMode);
-    this.display = new DisplayAPI(appUrl, environmentId, isDebugMode);
-    this.attribute = new AttributeAPI(appUrl, environmentId, isDebugMode);
-    this.storage = new StorageAPI(appUrl, environmentId);
     this.user = new UserAPI(appUrl, environmentId, isDebugMode);
     this.environment = new EnvironmentAPI(appUrl, environmentId, isDebugMode);
   }
