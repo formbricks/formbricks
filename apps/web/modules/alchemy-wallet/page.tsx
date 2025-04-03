fbaa0f7bcec8896c623d9adce5522bc88346b60a
parth2035 <parth.2035@gmail.com>
2025-04-03 17:01:11 -0400

    reorganized wallet + added buttons + useTranslate fixes in wallet module

--- apps/web/modules/alchemy-wallet/page.tsx
+++ apps/web/modules/alchemy-wallet/page.tsx
@@ -2,7 +2,7 @@ import { PageContentWrapper } from "@/modules/ui/components/page-content-wrapper
 import { PageHeader } from "@/modules/ui/components/page-header";
 import { getTranslate } from "@/tolgee/server";
 import WalletButton from "@/modules/alchemy-wallet/components/wallet-button";
-import WalletBalanceCard from "@/modules/alchemy-wallet/components/wallet-balance-card";
+import WalletBalanceCard from "@/modules/alchemy-wallet/components/WalletBalanceCard";
 
 export const AlchemyWalletPage = async () => {
   const t = await getTranslate();
@@ -10,10 +10,8 @@ export const AlchemyWalletPage = async () => {
   return (
     <PageContentWrapper>
       <PageHeader pageTitle={t("common.wallet")}/>
-      <div>
-        <WalletBalanceCard/>
-        <WalletButton/>
-      </div>
+      <WalletBalanceCard/>
+      <WalletButton/>
     </PageContentWrapper>
   );
 };
