df2a4e4a3979d04933ee318c943aaedab494d565
kb-wonderfi <kartik@wonder.fi>
2025-04-08 09:48:54 -0700

    Using user address instead of smart account

--- apps/web/modules/alchemy-wallet/components/WalletBalanceCard/components/wallet-address.tsx
+++ apps/web/modules/alchemy-wallet/components/WalletBalanceCard/components/wallet-address.tsx
@@ -1,40 +1,40 @@
 "use client";
 
-import React from "react";
-import { useSmartAccountClient } from "@account-kit/react";
-import { formatAddress } from "@/modules/alchemy-wallet/lib/utils/format";
-import { CopyIcon } from "lucide-react";
 import IconButton from "@/modules/alchemy-wallet/components/common/IconButton";
-import toast from "react-hot-toast";
+import { formatAddress } from "@/modules/alchemy-wallet/lib/utils/format";
+import { useUser } from "@account-kit/react";
 import { useTranslate } from "@tolgee/react";
+import { CopyIcon } from "lucide-react";
+import React from "react";
+import toast from "react-hot-toast";
 
 export function WalletAddress(): React.JSX.Element {
-  const { address } = useSmartAccountClient({});
+  const user = useUser();
   const { t } = useTranslate();
   const handleCopyAddress = () => {
-    if (!address){
+    if (!user?.address) {
       toast.error(t("environments.wallet.address.copy.error"));
       return;
     }
-    navigator.clipboard.writeText(address).then(() => {
+    navigator.clipboard.writeText(user.address).then(() => {
       toast.success(t("environments.wallet.address.copy.success"));
-    })
-  }
+    });
+  };
 
   return (
     <div className="flex flex-row items-center gap-2">
-      {address ? 
+      {user?.address ? (
         <>
-          <span>
-            {formatAddress(address)}
-          </span>
-          <IconButton label={t("environments.wallet.address.copy.button")} icon={CopyIcon} onClick={handleCopyAddress}/>
+          <span>{formatAddress(user.address)}</span>
+          <IconButton
+            label={t("environments.wallet.address.copy.button")}
+            icon={CopyIcon}
+            onClick={handleCopyAddress}
+          />
         </>
-        :
-        <span>
-          {formatAddress("0x00000000",3)}
-        </span>
-      }
+      ) : (
+        <span>{formatAddress("0x00000000", 3)}</span>
+      )}
     </div>
   );
 }
