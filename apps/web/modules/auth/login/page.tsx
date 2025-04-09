ebd99af901af7af1cdc92e73d68ff3c50189655e
kb-wonderfi <kartik@wonder.fi>
2025-04-09 11:20:27 -0700

    Login page changes

--- apps/web/modules/auth/login/page.tsx
+++ apps/web/modules/auth/login/page.tsx
@@ -1,5 +1,4 @@
 import { FormWrapper } from "@/modules/auth/components/form-wrapper";
-import { Testimonial } from "@/modules/auth/components/testimonial";
 import { Metadata } from "next";
 import { EMAIL_AUTH_ENABLED, PASSWORD_RESET_DISABLED, SIGNUP_ENABLED } from "@formbricks/lib/constants";
 import { LoginForm } from "./components/login-form";
@@ -13,11 +12,8 @@ export const LoginPage = async () => {
   const [isMultiOrgEnabled] = await Promise.all([false]);
 
   return (
-    <div className="grid min-h-screen w-full bg-gradient-to-tr from-slate-100 to-slate-50 lg:grid-cols-5">
-      <div className="col-span-2 hidden lg:flex">
-        <Testimonial />
-      </div>
-      <div className="col-span-3 flex flex-col items-center justify-center">
+    <div className="grid min-h-screen w-full bg-gradient-to-tr from-slate-100 to-slate-50">
+      <div className="flex flex-col items-center justify-center">
         <FormWrapper>
           <LoginForm
             emailAuthEnabled={EMAIL_AUTH_ENABLED}
