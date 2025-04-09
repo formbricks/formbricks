3ae61239502d74c99a914a587da114475f9fa8a0
Matthias Nannt <mail@matthiasnannt.com>
2025-04-09 21:28:50 +0900

    fix sonarqube issues

--- packages/surveys/src/components/general/label.tsx
+++ packages/surveys/src/components/general/label.tsx
@@ -2,6 +2,6 @@ interface LabelProps {
   text: string;
 }
 
-export function Label({ text }: LabelProps) {
+export function Label({ text }: Readonly<LabelProps>) {
   return <label className="fb-text-subheading fb-font-normal fb-text-sm">{text}</label>;
 }
