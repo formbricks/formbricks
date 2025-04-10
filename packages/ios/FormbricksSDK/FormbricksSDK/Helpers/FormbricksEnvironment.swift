56f79827602f70f6b93f8194b5d0db4867a96a77
pandeymangg <anshuman.pandey9999@gmail.com>
2025-04-10 15:18:31 +0530

    coderabbit feedback

--- packages/ios/FormbricksSDK/FormbricksSDK/Helpers/FormbricksEnvironment.swift
+++ packages/ios/FormbricksSDK/FormbricksSDK/Helpers/FormbricksEnvironment.swift
@@ -3,6 +3,8 @@ import Foundation
 class FormbricksEnvironment {
     public static let baseApiUrl: String = Formbricks.appUrl ?? "http://localhost:3000"
     public static let surveyScriptUrl: String = "\(baseApiUrl)/js/surveys.umd.cjs"
+    /// Endpoint for getting environment data. Replace {environmentId} with the actual environment ID.
     public static let getEnvironmentRequestEndpoint: String = "/api/v2/client/{environmentId}/environment"
+    /// Endpoint for posting user data. Replace {environmentId} with the actual environment ID.
     public static let postUserRequestEndpoint: String = "/api/v2/client/{environmentId}/user"
 }
