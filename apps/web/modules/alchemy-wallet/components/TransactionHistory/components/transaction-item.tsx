14a89412df3446657163dc0820dce1917469e738
kb-wonderfi <kartik@wonder.fi>
2025-04-14 09:21:49 -0700

    Adding transfers api

--- apps/web/modules/alchemy-wallet/components/TransactionHistory/components/transaction-item.tsx
+++ apps/web/modules/alchemy-wallet/components/TransactionHistory/components/transaction-item.tsx
@@ -1,32 +1,32 @@
-import { cn } from "@formbricks/lib/cn"
+import { TokenTransfer } from "@wonderchain/sdk/dist/blockscout-client";
 import { SendIcon } from "lucide-react";
+import { cn } from "@formbricks/lib/cn";
 
 interface TransactionItemProps {
-
+  transfer: TokenTransfer;
 }
 
-export function TransactionItem({}:TransactionItemProps) {
-
-    return (
-        <div
-        className={cn(
-            "flex flex-row items-center gap-3 px-2 py-1",
-        )}>
-            <div className="flex h-8 w-8 items-center justify-center rounded-full bg-secondary text-green-600">
-                <SendIcon className="h-4 w-4" strokeWidth={2} />
-            </div>
-            <div className="flex flex-col gap-1 flex-1">
-                <div className="flex flex-row gap-4 flex-1 justify-between">
-                    <p className="text-sm font-semibold">Recieved from 0x3a8d...e92f</p>
-                    <span className="text-sm font-normal text-green-600">+25 USDC</span>
-                </div>
-                <div className="flex flex-row gap-4 flex-1 justify-between">
-                    <span className="text-xs">Survey Reward</span>
-                    <span className="text-xs">Mar 25, 2025 at 05:15 AM</span>
-                </div>
-            </div>
+export function TransactionItem({ transfer }: TransactionItemProps) {
+  return (
+    <>
+      <div className={cn("flex flex-row items-center gap-3 px-2 py-1")}>
+        <div className="bg-secondary flex h-8 w-8 items-center justify-center rounded-full text-green-600">
+          <SendIcon className="h-4 w-4" strokeWidth={2} />
         </div>
-    )
+        <div className="flex flex-1 flex-col gap-1">
+          <div className="flex flex-1 flex-row justify-between gap-4">
+            <p className="text-sm font-semibold">Recieved from 0x3a8d...e92f</p>
+            <span className="text-sm font-normal text-green-600">+25 USDC</span>
+          </div>
+          <div className="flex flex-1 flex-row justify-between gap-4">
+            <span className="text-xs">Survey Reward</span>
+            <span className="text-xs">Mar 25, 2025 at 05:15 AM</span>
+          </div>
+        </div>
+      </div>
+      {JSON.stringify(transfer)}
+    </>
+  );
 }
 
-export default TransactionItem
+export default TransactionItem;
--- apps/web/modules/alchemy-wallet/components/TransactionHistory/index.tsx
+++ apps/web/modules/alchemy-wallet/components/TransactionHistory/index.tsx
@@ -2,29 +2,44 @@
 
 // import React, { useState } from "react";
 // import { useTranslate } from "@tolgee/react";
-import { cn } from "@formbricks/lib/cn";
 // import { useSmartAccountClient } from "@account-kit/react";
 import TransactionItem from "@/modules/alchemy-wallet/components/TransactionHistory/components/transaction-item";
+import { useUser } from "@account-kit/react";
+import { TokenTransfer } from "@wonderchain/sdk/dist/blockscout-client";
+import { useEffect, useState } from "react";
+import { cn } from "@formbricks/lib/cn";
+import { useBlockscoutApi } from "@formbricks/web3";
 
-export function TransactionHistory({className=""}:{
-  className?: string;
-}): React.JSX.Element {
+export function TransactionHistory({ className = "" }: { className?: string }) {
+  const user = useUser();
+  const address = user?.address || "";
   // const { t } = useTranslate();
   // const [showBalance, setShowBalance] = useState(true);
   // const { address } = useSmartAccountClient({});
+  const blockscoutApi = useBlockscoutApi();
+  const [transfers, setTransfers] = useState<TokenTransfer[] | null>(null);
 
+  useEffect(() => {
+    (async () => {
+      if (!address || !blockscoutApi) return;
+      const data = await blockscoutApi.getAddressTokenTransfers(address);
+      setTransfers(data.data.items);
+    })();
+  }, [blockscoutApi, address]);
+
+  if (!transfers?.length) {
+    return null;
+  }
   return (
     <div
       className={cn(
-        "relative my-4 p-4 flex flex-col gap-4 w-full rounded-xl border border-slate-200 bg-white shadow-sm",
+        "relative my-4 flex w-full flex-col gap-4 rounded-xl border border-slate-200 bg-white p-4 shadow-sm",
         className
       )}
-      id={"transaction-history"}
-    >
-      <TransactionItem />
-      <TransactionItem />
-      <TransactionItem />
-      <TransactionItem />
+      id={"transaction-history"}>
+      {transfers.map((t) => (
+        <TransactionItem transfer={t} key={t.transaction_hash + t.token + t.type + t.from + t.to} />
+      ))}
     </div>
   );
 }
