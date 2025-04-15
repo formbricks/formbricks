5bc17515e053c1e32a3aa76b0fe0c78f866b164a
pandeymangg <anshuman.pandey9999@gmail.com>
2025-04-15 13:04:10 +0530

    completes tests for follow-up email component

--- apps/web/modules/email/emails/survey/follow-up.test.tsx
+++ apps/web/modules/email/emails/survey/follow-up.test.tsx
@@ -3,6 +3,8 @@ import "@testing-library/jest-dom/vitest";
 import { cleanup, render, screen } from "@testing-library/react";
 import { DefaultParamType, TFnType, TranslationKey } from "@tolgee/react/server";
 import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
+import { TResponse } from "@formbricks/types/responses";
+import { TSurvey, TSurveyQuestionTypeEnum } from "@formbricks/types/surveys/types";
 import { FollowUpEmail } from "./follow-up";
 
 vi.mock("@formbricks/lib/constants", () => ({
@@ -17,9 +19,41 @@ vi.mock("@/tolgee/server", () => ({
   getTranslate: vi.fn(),
 }));
 
+vi.mock("@/modules/email/emails/lib/utils", () => ({
+  renderEmailResponseValue: vi.fn(() => <p data-testid="response-value">user@example.com</p>),
+}));
+
 const defaultProps = {
   html: "<p>Test HTML Content</p>",
   logoUrl: "https://example.com/custom-logo.png",
+  attachResponseData: false,
+  survey: {
+    questions: [
+      {
+        id: "vjniuob08ggl8dewl0hwed41",
+        type: "openText" as TSurveyQuestionTypeEnum.OpenText,
+        headline: {
+          default: "What would you like to know?‌‌‍‍‌‍‍‍‌‌‌‍‍‌‍‍‌‌‌‌‌‌‍‌‍‌‌",
+        },
+        required: true,
+        charLimit: {},
+        inputType: "email",
+        longAnswer: false,
+        buttonLabel: {
+          default: "Next‌‌‍‍‌‍‍‍‌‌‌‍‍‍‌‌‌‌‌‌‌‌‍‌‍‌‌",
+        },
+        placeholder: {
+          default: "example@email.com",
+        },
+      },
+    ],
+  } as unknown as TSurvey,
+  response: {
+    data: {
+      vjniuob08ggl8dewl0hwed41: "user@example.com",
+    },
+    language: null,
+  } as unknown as TResponse,
 };
 
 describe("FollowUpEmail", () => {
@@ -88,5 +122,16 @@ describe("FollowUpEmail", () => {
 
     expect(screen.getByText("emails.powered_by_formbricks")).toBeInTheDocument();
     expect(screen.getByText("Imprint Address")).toBeInTheDocument();
+  });
+
+  it("renders the response data if attachResponseData is true", async () => {
+    const followUpEmailElement = await FollowUpEmail({
+      ...defaultProps,
+      attachResponseData: true,
+    });
+
+    render(followUpEmailElement);
+
+    expect(screen.getByTestId("response-value")).toBeInTheDocument();
   });
 });
