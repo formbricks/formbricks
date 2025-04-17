fc91ed526fc409f5442f7ff4f2107e3546e0802e
Johannes <johannes@formbricks.com>
2025-04-17 12:35:30 +0700

    add backlink on signup page

--- apps/web/modules/auth/components/form-wrapper.tsx
+++ apps/web/modules/auth/components/form-wrapper.tsx
@@ -1,4 +1,5 @@
 import { Logo } from "@/modules/ui/components/logo";
+import Link from "next/link";
 
 interface FormWrapperProps {
   children: React.ReactNode;
@@ -9,7 +10,9 @@ export const FormWrapper = ({ children }: FormWrapperProps) => {
     <div className="mx-auto flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
       <div className="mx-auto w-full max-w-sm rounded-xl bg-white p-8 shadow-xl lg:w-96">
         <div className="mb-8 text-center">
-          <Logo className="mx-auto w-3/4" />
+          <Link target="_blank" href="https://formbricks.com?utm_source=ce">
+            <Logo className="mx-auto w-3/4" />
+          </Link>
         </div>
         {children}
       </div>
