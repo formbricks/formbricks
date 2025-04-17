97e57eff087b8a765aa9ad7407ae9e6077ca3688
kb-wonderfi <kartik@wonder.fi>
2025-04-17 10:54:28 -0700

    Adding logo exception

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -168,6 +168,6 @@ export const middleware = async (originalRequest: NextRequest) => {
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|js|css|images|fonts|icons|public|api/v1/og).*)", // Exclude the Open Graph image generation route from middleware
+    "/((?!_next/static|_next/image|favicon.ico|logo.png|logo-small.png|sitemap.xml|robots.txt|js|css|images|fonts|icons|public|api/v1/og).*)", // Exclude the Open Graph image generation route from middleware
   ],
 };
