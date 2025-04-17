687f773ba70740cdd27a1b0d7b3a620b6cd25438
pandeymangg <anshuman.pandey9999@gmail.com>
2025-04-17 11:55:10 +0530

    fix: feedback

--- apps/web/modules/auth/components/form-wrapper.tsx
+++ apps/web/modules/auth/components/form-wrapper.tsx
@@ -10,7 +10,7 @@ export const FormWrapper = ({ children }: FormWrapperProps) => {
     <div className="mx-auto flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
       <div className="mx-auto w-full max-w-sm rounded-xl bg-white p-8 shadow-xl lg:w-96">
         <div className="mb-8 text-center">
-          <Link target="_blank" href="https://formbricks.com?utm_source=ce">
+          <Link target="_blank" href="https://formbricks.com?utm_source=ce" rel="noopener noreferrer">
             <Logo className="mx-auto w-3/4" />
           </Link>
         </div>
