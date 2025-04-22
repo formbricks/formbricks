6d74c84be3c4bd34732b9a5374173cc330cec580
Dhruwang <dhruwangjariwala18@gmail.com>
2025-04-22 10:14:20 +0530

    removed unrelated changes

--- docker/verify-components.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# verify-components.sh
-echo "Checking security-critical components..."
-busybox --help | head -n1
-openssl version
-sqlite3 --version
-apk info -v libxml2
-apk info -v zstd
