e403855b6e727ba86338e274c9dfdea66a2ddaa6
pandeymangg <anshuman.pandey9999@gmail.com>
2025-04-23 17:53:16 +0530

    removes random query param

--- packages/android/formbricksSDK/src/main/java/com/formbricks/formbrickssdk/network/FormbricksService.kt
+++ packages/android/formbricksSDK/src/main/java/com/formbricks/formbrickssdk/network/FormbricksService.kt
@@ -7,14 +7,13 @@ import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Path
-import retrofit2.http.Query
 
 interface FormbricksService {
     @GET("$API_PREFIX/client/{environmentId}/environment")
-    fun getEnvironmentState(@Path("environmentId") environmentId: String, @Query("randQuery") randQuery: String = System.currentTimeMillis().toString()): Call<Map<String, Any>>
+    fun getEnvironmentState(@Path("environmentId") environmentId: String): Call<Map<String, Any>>
 
     @POST("$API_PREFIX/client/{environmentId}/user")
-    fun postUser(@Path("environmentId") environmentId: String, @Body body: PostUserBody, @Query("randQuery") randQuery: String = System.currentTimeMillis().toString()): Call<UserResponse>
+    fun postUser(@Path("environmentId") environmentId: String, @Body body: PostUserBody): Call<UserResponse>
 
     companion object {
         const val API_PREFIX = "/api/v2"
