d40f33771aec1b05db0fc9b41c0c459112de30d5
kb-wonderfi <kartik@wonder.fi>
2025-04-24 09:47:34 -0700

    Removing check on template page

--- apps/web/modules/survey/templates/page.tsx
+++ apps/web/modules/survey/templates/page.tsx
@@ -1,7 +1,6 @@
 import { getEnvironmentAuth } from "@/modules/environments/lib/utils";
 import { getProjectByEnvironmentId } from "@/modules/survey/lib/project";
 import { getTranslate } from "@/tolgee/server";
-import { redirect } from "next/navigation";
 import { TProjectConfigChannel, TProjectConfigIndustry } from "@formbricks/types/project";
 import { TTemplateRole } from "@formbricks/types/templates";
 import { TemplateContainerWithPreview } from "./components/template-container";
@@ -23,16 +22,12 @@ export const SurveyTemplatesPage = async (props: SurveyTemplateProps) => {
   const params = await props.params;
   const environmentId = params.environmentId;
 
-  const { session, environment, isReadOnly } = await getEnvironmentAuth(environmentId);
+  const { session, environment } = await getEnvironmentAuth(environmentId);
 
   const project = await getProjectByEnvironmentId(environmentId);
 
   if (!project) {
     throw new Error(t("common.project_not_found"));
-  }
-
-  if (isReadOnly) {
-    return redirect(`/environments/${environment.id}/surveys`);
   }
 
   const prefilledFilters = [project.config.channel, project.config.industry, searchParams.role ?? null];
