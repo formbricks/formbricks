b8315e2ee64b1d65608fa9e8deac7db09446b3c1
pandeymangg <anshuman.pandey9999@gmail.com>
2025-05-01 13:17:33 +0530

    hall

--- packages/surveys/src/components/general/survey.tsx
+++ packages/surveys/src/components/general/survey.tsx
@@ -706,7 +706,7 @@ export function Survey({
             {content()}
           </div>
           <div className="fb-space-y-4">
-            <div className="space-y-2">
+            <div className="fb-px-4 space-y-2">
               {isBrandingEnabled ? <FormbricksBranding /> : null}
               {isSpamProtectionEnabled ? <RecaptchaBranding /> : null}
             </div>
