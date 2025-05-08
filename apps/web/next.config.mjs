7712a695cbe130ede5602fb12619e3014c310ce2
Matthias Nannt <mail@matthiasnannt.com>
2025-05-08 15:43:58 +0200

    rename cache-handler, replace redis -> valkey

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -16,7 +16,7 @@ const getHostname = (url) => {
 
 const nextConfig = {
   assetPrefix: process.env.ASSET_PREFIX_URL || undefined,
-  cacheHandler: require.resolve("./cache-handler.mjs"),
+  cacheHandler: require.resolve("./cache-handler.js"),
   cacheMaxMemorySize: 0, // disable default in-memory caching
   output: "standalone",
   poweredByHeader: false,
