ab480bd5753a2194f361765f3a73b12065324ac6
Victor Santos <victor@formbricks.com>
2025-05-14 13:17:31 -0300

    color picker

--- apps/web/modules/ui/components/color-picker/components/popover-picker.tsx
+++ apps/web/modules/ui/components/color-picker/components/popover-picker.tsx
@@ -17,7 +17,7 @@ export const PopoverPicker = ({ color, onChange, disabled = false }: PopoverPick
 
   return (
     <div className="picker relative">
-      <div
+      <button
         id="color-picker"
         className="h-6 w-10 cursor-pointer rounded border border-slate-200"
         style={{ backgroundColor: color, opacity: disabled ? 0.5 : 1 }}
