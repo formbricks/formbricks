7d1f9f22eeda393c9fb6910c07df4df46c6cf14e
Jakob Schott <jakob@formbricks.com>
2025-05-28 16:58:20 +0200

    staged changes

--- apps/web/modules/ui/components/modal-with-tabs/index.tsx
+++ apps/web/modules/ui/components/modal-with-tabs/index.tsx
@@ -1,4 +1,5 @@
-import { Modal } from "@/modules/ui/components/modal";
+// import { Modal } from "@/modules/ui/components/modal";
+import { Dialog, DialogContent, DialogDescription, DialogTitle } from "@/modules/ui/components/dialog";
 import { useEffect, useState } from "react";
 
 interface ModalWithTabsProps {
@@ -26,7 +27,7 @@ export const ModalWithTabs = ({
   label,
   description,
   closeOnOutsideClick,
-  size = "lg",
+  // size = "lg",
   restrictOverflow = false,
 }: ModalWithTabsProps) => {
   const [activeTab, setActiveTab] = useState(0);
@@ -42,41 +43,43 @@ export const ModalWithTabs = ({
   }, [open]);
 
   return (
-    <Modal
-      open={open}
-      setOpen={setOpen}
-      noPadding
-      closeOnOutsideClick={closeOnOutsideClick}
-      size={size}
-      restrictOverflow={restrictOverflow}>
-      <div className="flex h-full flex-col rounded-lg">
-        <div className="rounded-t-lg bg-slate-100">
-          <div className="mr-20 flex items-center justify-between truncate p-6">
-            <div className="flex items-center space-x-2">
-              {icon && <div className="mr-1.5 h-6 w-6 text-slate-500">{icon}</div>}
-              <div>
-                {label && <div className="text-xl font-medium text-slate-700">{label}</div>}
-                {description && <div className="text-sm text-slate-500">{description}</div>}
+    <Dialog open={open}>
+      <DialogContent
+        // setOpen={setOpen}
+        // noPadding
+        disableCloseOnOutsideClick={closeOnOutsideClick}
+        // size={size}
+        // restrictOverflow={restrictOverflow}
+      >
+        <div className="flex h-full flex-col rounded-lg">
+          <div className="rounded-t-lg bg-slate-100">
+            <div className="mr-20 flex items-center justify-between truncate p-6">
+              <div className="flex items-center space-x-2">
+                {icon && <div className="mr-1.5 h-6 w-6 text-slate-500">{icon}</div>}
+                <div>
+                  {label && <div className="text-xl font-medium text-slate-700">{label}</div>}
+                  {description && <div className="text-sm text-slate-500">{description}</div>}
+                </div>
               </div>
             </div>
           </div>
+          <div className="flex h-full w-full items-center justify-center space-x-2 border-b border-slate-200 px-6">
+            {tabs.map((tab, index) => (
+              <button
+                key={index}
+                className={`mr-4 px-1 pb-3 pt-6 focus:outline-none ${
+                  activeTab === index
+                    ? "border-brand-dark border-b-2 font-semibold text-slate-900"
+                    : "text-slate-500 hover:text-slate-700"
+                }`}
+                onClick={() => handleTabClick(index)}>
+                {tab.title}
+              </button>
+            ))}
+          </div>
+          <div className="flex-1 p-6">{tabs[activeTab].children}</div>
         </div>
-        <div className="flex h-full w-full items-center justify-center space-x-2 border-b border-slate-200 px-6">
-          {tabs.map((tab, index) => (
-            <button
-              key={index}
-              className={`mr-4 px-1 pb-3 pt-6 focus:outline-none ${
-                activeTab === index
-                  ? "border-brand-dark border-b-2 font-semibold text-slate-900"
-                  : "text-slate-500 hover:text-slate-700"
-              }`}
-              onClick={() => handleTabClick(index)}>
-              {tab.title}
-            </button>
-          ))}
-        </div>
-        <div className="flex-1 p-6">{tabs[activeTab].children}</div>
-      </div>
-    </Modal>
+      </DialogContent>
+    </Dialog>
   );
 };
