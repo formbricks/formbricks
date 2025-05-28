39b9f8d51967963d4ec187aaddf296b0464cdc3d
Jakob Schott <jakob@formbricks.com>
2025-05-28 19:11:15 +0200

    restructured create new action tab to accomedate new dialog

--- apps/web/modules/survey/editor/components/create-new-action-tab.tsx
+++ apps/web/modules/survey/editor/components/create-new-action-tab.tsx
@@ -175,7 +175,7 @@ export const CreateNewActionTab = ({
     <div>
       <FormProvider {...form}>
         <form onSubmit={handleSubmit(submitHandler)}>
-          <div className="max-h-[400px] w-full space-y-4 overflow-y-auto pr-4">
+          <div className="w-full space-y-4">
             <div className="w-3/5">
               <FormField
                 name={`type`}
