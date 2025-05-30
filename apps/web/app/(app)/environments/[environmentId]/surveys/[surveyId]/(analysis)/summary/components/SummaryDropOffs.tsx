4419de3af07124f5989f6e47d086c087328cd908
Jakob Schott <jakob@formbricks.com>
2025-05-30 18:53:09 +0200

    fixed sizing

--- apps/web/app/(app)/environments/[environmentId]/surveys/[surveyId]/(analysis)/summary/components/SummaryDropOffs.tsx
+++ apps/web/app/(app)/environments/[environmentId]/surveys/[surveyId]/(analysis)/summary/components/SummaryDropOffs.tsx
@@ -23,9 +23,9 @@ export const SummaryDropOffs = ({ dropOff, survey }: SummaryDropOffsProps) => {
   return (
     <div className="rounded-xl border border-slate-200 bg-white shadow-sm">
       <div className="">
-        <div className="grid h-10 grid-cols-6 items-center border-y border-slate-200 bg-slate-100 text-sm font-semibold text-slate-600">
-          <div className="col-span-3 pl-4 md:pl-6">{t("common.questions")}</div>
-          <div className="flex justify-center">
+        <div className="grid h-10 grid-cols-6 items-center border-y border-slate-200 bg-slate-100 text-lg font-semibold text-slate-600">
+          <div className="col-span-3 pl-4 md:pl-6 md:pr-14">{t("common.questions")}</div>
+          <div className="flex justify-end px-12">
             <TooltipProvider delayDuration={50}>
               <Tooltip>
                 <TooltipTrigger>
@@ -37,13 +37,17 @@ export const SummaryDropOffs = ({ dropOff, survey }: SummaryDropOffsProps) => {
               </Tooltip>
             </TooltipProvider>
           </div>
-          <div className="px-4 text-center md:px-6">{t("environments.surveys.summary.impressions")}</div>
-          <div className="pr-6 text-center md:pl-6">{t("environments.surveys.summary.drop_offs")}</div>
+          <div className="px-4 text-right md:pl-6 md:pr-12">
+            {t("environments.surveys.summary.impressions")}
+          </div>
+          <div className="pr-6 text-right md:pl-6 md:pr-12">
+            {t("environments.surveys.summary.drop_offs")}
+          </div>
         </div>
         {dropOff.map((quesDropOff) => (
           <div
             key={quesDropOff.questionId}
-            className="grid grid-cols-6 items-center border-b border-slate-100 py-2 text-sm text-slate-800 md:text-base">
+            className="grid grid-cols-6 items-center border-b border-slate-100 px-6 py-2 text-xs text-slate-800 md:text-base">
             <div className="col-span-3 flex gap-3 pl-4 md:pl-6">
               {getIcon(quesDropOff.questionType)}
               <p>
@@ -57,17 +61,21 @@ export const SummaryDropOffs = ({ dropOff, survey }: SummaryDropOffsProps) => {
                     "default"
                   )["default"],
                   "@",
-                  ["text-lg"]
+                  ["text-base"]
                 )}
               </p>
             </div>
-            <div className="whitespace-pre-wrap text-center font-semibold">
-              {quesDropOff.ttc > 0 ? (quesDropOff.ttc / 1000).toFixed(2) + "s" : "N/A"}
+            <div className="whitespace-pre-wrap px-6 text-right font-mono font-medium">
+              {quesDropOff.ttc > 0 ? (quesDropOff.ttc / 1000).toFixed(2) + " s" : "N/A  "}
             </div>
-            <div className="whitespace-pre-wrap text-center font-semibold">{quesDropOff.impressions}</div>
-            <div className="pl-6 text-center md:px-6">
-              <span className="mr-1.5 font-semibold">{quesDropOff.dropOffCount}</span>
-              <span>({Math.round(quesDropOff.dropOffPercentage)}%)</span>
+            <div className="whitespace-pre-wrap px-6 text-right font-mono font-medium">
+              {quesDropOff.impressions}
+            </div>
+            <div className="pl-6 text-right">
+              <span className="mr-1.5 font-mono font-medium">{quesDropOff.dropOffCount}</span>
+              <span className="inline-block w-14 text-left text-base">
+                ({Math.round(quesDropOff.dropOffPercentage)}%)
+              </span>
             </div>
           </div>
         ))}
