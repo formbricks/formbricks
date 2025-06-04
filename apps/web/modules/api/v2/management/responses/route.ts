fb1a7a9104d677033cfb1aef94419c19151cf55b
Victor Santos <victor@formbricks.com>
2025-06-04 17:06:47 -0300

    added type assertion

--- apps/web/modules/api/v2/management/responses/route.ts
+++ apps/web/modules/api/v2/management/responses/route.ts
@@ -90,7 +90,7 @@ export const POST = async (request: Request) =>
 
       const surveyQuestions = await getSurveyQuestions(body.surveyId);
       if (!surveyQuestions.ok) {
-        return handleApiError(request, surveyQuestions.error as ApiErrorResponseV2, auditLog); // NOSONAR // We need to check or we get a type error
+        return handleApiError(request, surveyQuestions.error as ApiErrorResponseV2, auditLog); // NOSONAR // We need to assert or we get a type error
       }
 
       if (!validateFileUploads(body.data, surveyQuestions.data.questions)) {
