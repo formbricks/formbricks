dcf32680a28cb9a80f8cca24ff58da3342d82cb1
harshsbhat <harsh121102@gmail.com>
2025-06-09 09:50:27 +0530

    chore: rabbit

--- apps/web/lib/constants.ts
+++ apps/web/lib/constants.ts
@@ -237,8 +237,6 @@ export enum STRIPE_PROJECT_NAMES {
 export enum STRIPE_PRICE_LOOKUP_KEYS {
   STARTUP_MAY25_MONTHLY = "STARTUP_MAY25_MONTHLY",
   STARTUP_MAY25_YEARLY = "STARTUP_MAY25_YEARLY",
-  SCALE_MAY25_MONTHLY = "formbricks_scale_monthly",
-  SCALE_MAY25_YEARLY = "formbricks_scale_yearly",
 }
 
 export const BILLING_LIMITS = {
