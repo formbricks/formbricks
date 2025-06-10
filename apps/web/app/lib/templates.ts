aa011eb786293f584d16699045a17e307c979be7
Piyush Gupta <piyushguptaa2z123@gmail.com>
2025-06-10 09:58:22 +0530

    fix: translations

--- apps/web/app/lib/templates.ts
+++ apps/web/app/lib/templates.ts
@@ -3011,7 +3011,7 @@ const understandLowEngagement = (t: TFnType): TTemplate => {
             reusableOptionIds[1],
             reusableOptionIds[2],
             reusableOptionIds[3],
-            reusableOptionIds[4],
+            "other",
           ],
           headline: t("templates.understand_low_engagement_question_1_headline"),
           required: true,
