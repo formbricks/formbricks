43b72ece80b89f2720e6909a4df3076a0223872c
pandeymangg <anshuman.pandey9999@gmail.com>
2025-06-13 11:04:01 +0530

    fix: sonarqube tests

--- packages/database/vite.config.ts
+++ packages/database/vite.config.ts
@@ -49,13 +49,11 @@ export default defineConfig(async (): Promise<UserConfig> => {
             format: "esm",
             entryFileNames: "[name].js",
             chunkFileNames: "[name].js",
-            assetFileNames: "[name].[ext]",
           },
           {
             format: "cjs",
             entryFileNames: "[name].cjs",
             chunkFileNames: "[name].cjs",
-            assetFileNames: "[name].[ext]",
           },
         ],
         external: [
