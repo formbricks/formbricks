4432517067580c3b2112720d8b4e0ebc8cc2f2c1
Jakob Schott <jakob@formbricks.com>
2025-06-17 13:52:49 +0200

    fixed responsive issues and icon title alignment

--- apps/web/modules/ui/components/dialog/index.tsx
+++ apps/web/modules/ui/components/dialog/index.tsx
@@ -2,7 +2,7 @@
 
 import { cn } from "@/lib/cn";
 import * as DialogPrimitive from "@radix-ui/react-dialog";
-import { X } from "lucide-react";
+import { XIcon } from "lucide-react";
 import * as React from "react";
 
 const Dialog = DialogPrimitive.Root;
@@ -11,7 +11,7 @@ const DialogTrigger = DialogPrimitive.Trigger;
 
 const DialogPortal = ({ children, ...props }: DialogPrimitive.DialogPortalProps) => (
   <DialogPrimitive.Portal {...props}>
-    <div className="fixed inset-0 z-50 flex items-start justify-center md:items-center">{children}</div>
+    <div className="fixed inset-0 z-50 flex items-end justify-center md:items-center">{children}</div>
   </DialogPrimitive.Portal>
 );
 DialogPortal.displayName = DialogPrimitive.Portal.displayName;
@@ -50,7 +50,7 @@ const DialogContent = React.forwardRef<
       <DialogPrimitive.Content
         ref={ref}
         className={cn(
-          "animate-in data-[state=open]:fade-in-90 data-[state=open]:slide-in-from-bottom-10 md:zoom-in-90 data-[state=open]:md:slide-in-from-bottom-0 fixed z-50 flex w-full flex-col space-y-4 overflow-hidden rounded-b-lg border bg-white p-4 shadow-lg md:max-h-[90dvh] md:rounded-lg",
+          "animate-in data-[state=open]:fade-in-90 data-[state=open]:slide-in-from-bottom-10 md:zoom-in-90 data-[state=open]:md:slide-in-from-bottom-0 fixed z-50 flex max-h-[90dvh] w-full flex-col space-y-4 overflow-y-auto rounded-t-lg border bg-white p-4 shadow-lg md:overflow-hidden md:rounded-lg",
           width === "default" ? "md:w-[720px]" : "md:w-[720px] lg:w-[960px]",
           className
         )}
@@ -59,8 +59,8 @@ const DialogContent = React.forwardRef<
         {...props}>
         {children}
         {!hideCloseButton && (
-          <DialogPrimitive.Close className="ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute right-3 top-[-0.25rem] z-10 rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none">
-            <X className="size-4" />
+          <DialogPrimitive.Close className="ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute right-3 top-[-0.25rem] z-10 mb-4 rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none">
+            <XIcon className="size-4" />
             <span className="sr-only">Close</span>
           </DialogPrimitive.Close>
         )}
@@ -79,8 +79,8 @@ type DialogHeaderProps = Omit<React.HTMLAttributes<HTMLDivElement>, "dangerously
 const DialogHeader = ({ className, ...props }: DialogHeaderProps) => (
   <div
     className={cn(
-      "sticky top-[-32px] z-10 flex min-h-4 flex-shrink-0 flex-col gap-y-1 bg-white text-center md:text-left",
-      "[&>svg]:text-primary [&>svg]:absolute [&>svg]:size-4 md:[&>svg~*]:flex md:[&>svg~*]:items-center md:[&>svg~*]:pl-6 [&svg]:top-[-0.25rem]",
+      "sticky top-[-32px] z-10 flex flex-shrink-0 flex-col gap-y-1 bg-white text-left",
+      "[&>svg]:text-primary [&>svg]:absolute [&>svg]:size-4 [&>svg~*]:items-center [&>svg~*]:pl-6 md:[&>svg~*]:flex",
       className
     )}
     {...props}
@@ -97,7 +97,7 @@ type DialogFooterProps = Omit<React.HTMLAttributes<HTMLDivElement>, "dangerously
 const DialogFooter = ({ className, ...props }: DialogFooterProps) => (
   <div
     className={cn(
-      "sticky bottom-0 z-10 flex flex-shrink-0 flex-col-reverse bg-white md:flex-row md:justify-end md:space-x-2",
+      "bottom-0 z-10 flex flex-shrink-0 flex-col-reverse bg-white md:sticky md:flex-row md:justify-end",
       className
     )}
     {...props}
@@ -107,7 +107,7 @@ const DialogFooter = ({ className, ...props }: DialogFooterProps) => (
 DialogFooter.displayName = "DialogFooter";
 
 const DialogBody = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
-  <div className={cn("mt-4 flex-1 overflow-y-auto text-sm", className)} {...props} />
+  <div className={cn("mt-4 flex-1 text-sm md:overflow-y-auto", className)} {...props} />
 );
 DialogBody.displayName = "DialogBody";
 
@@ -117,7 +117,7 @@ const DialogTitle = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <DialogPrimitive.Title
     ref={ref}
-    className={cn("text-primary text-sm font-medium leading-none tracking-tight", className)}
+    className={cn("text-primary min-h-4 text-sm font-medium leading-none tracking-tight", className)}
     {...props}
   />
 ));
@@ -129,7 +129,7 @@ const DialogDescription = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <DialogPrimitive.Description
     ref={ref}
-    className={cn("font-regular text-sm text-slate-500", className)} //muted does not work here for some reason
+    className={cn("font-regular text-sm text-slate-500", className)}
     {...props}
   />
 ));
