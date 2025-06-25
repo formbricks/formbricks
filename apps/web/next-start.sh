a5a484e78d4cbee2d8f0c054f90330e80a273e1d
pandeymangg <anshuman.pandey9999@gmail.com>
2025-06-25 09:40:17 +0530

    fix: moves script to another file

---
+++ apps/web/next-start.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+if [ "${DOCKER_CRON_ENABLED:-1}" = "1" ]; then
+  echo "Starting cron jobs...";
+  supercronic -quiet /app/docker/cronjobs &
+else
+  echo "Docker cron jobs are disabled via DOCKER_CRON_ENABLED=0";
+fi;
+(cd packages/database && npm run db:migrate:deploy) &&
+(cd packages/database && npm run db:create-saml-database:deploy) &&
+exec node apps/web/server.js
