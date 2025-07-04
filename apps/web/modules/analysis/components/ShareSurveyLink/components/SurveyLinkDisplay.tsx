7576e329a108388c2c29b79e2aa15e0dec5f8db2
Dhruwang <dhruwangjariwala18@gmail.com>
2025-07-04 12:27:59 +0530

    fix survey url issue

--- apps/web/modules/analysis/components/ShareSurveyLink/components/SurveyLinkDisplay.tsx
+++ apps/web/modules/analysis/components/ShareSurveyLink/components/SurveyLinkDisplay.tsx
@@ -12,7 +12,8 @@ export const SurveyLinkDisplay = ({ surveyUrl }: SurveyLinkDisplayProps) => {
           data-testid="survey-url-input"
           autoFocus={true}
           className="mt-2 w-full min-w-96 text-ellipsis rounded-lg border bg-white px-4 py-2 text-slate-800 caret-transparent"
-          defaultValue={surveyUrl}
+          value={surveyUrl}
+          readOnly
         />
       ) : (
         //loading state
