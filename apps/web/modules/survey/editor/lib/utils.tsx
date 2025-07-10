9189e569ee0f4f9d18ad4bdaf6559ed9c5319325
pandeymangg <anshuman.pandey9999@gmail.com>
2025-07-10 17:49:13 +0530

    refactors combobox UI

--- apps/web/modules/survey/editor/lib/utils.tsx
+++ apps/web/modules/survey/editor/lib/utils.tsx
@@ -109,41 +109,56 @@ export const getConditionValueOptions = (
   const questions = localSurvey.questions;
 
   const groupedOptions: TComboboxGroupedOption[] = [];
-  const questionOptions = questions
+  const questionOptions: TComboboxOption[] = [];
+
+  questions
     .filter((_, idx) => idx <= currQuestionIdx)
-    .map((question) => {
+    .forEach((question) => {
       if (question.type === TSurveyQuestionTypeEnum.Matrix) {
+        // Rows submenu
         const rows = question.rows.map((row, rowIdx) => ({
           icon: getQuestionIconMapping(t)[question.type],
           label: `${getLocalizedValue(row, "default")} (${getLocalizedValue(question.headline, "default")})`,
           value: `${question.id}.${rowIdx}`,
           meta: {
             type: "question",
-            rowIdx: rowIdx,
+            rowIdx: rowIdx.toString(),
           },
         }));
 
-        const questionEntry = {
+        questionOptions.push({
           icon: getQuestionIconMapping(t)[question.type],
           label: getLocalizedValue(question.headline, "default"),
           value: question.id,
           meta: {
             type: "question",
           },
-        };
-        return [questionEntry, ...rows];
+          children: [
+            {
+              label: t("environments.surveys.edit.matrix_rows", "Rows"),
+              value: `${question.id}-rows`,
+              children: rows,
+            },
+            {
+              label: t("environments.surveys.edit.matrix_all_fields", "All fields"),
+              value: question.id,
+              meta: {
+                type: "question",
+              },
+            },
+          ],
+        });
+      } else {
+        questionOptions.push({
+          icon: getQuestionIconMapping(t)[question.type],
+          label: getLocalizedValue(question.headline, "default"),
+          value: question.id,
+          meta: {
+            type: "question",
+          },
+        });
       }
-
-      return {
-        icon: getQuestionIconMapping(t)[question.type],
-        label: getLocalizedValue(question.headline, "default"),
-        value: question.id,
-        meta: {
-          type: "question",
-        },
-      };
-    })
-    .flat();
+    });
 
   const variableOptions = variables.map((variable) => {
     return {
