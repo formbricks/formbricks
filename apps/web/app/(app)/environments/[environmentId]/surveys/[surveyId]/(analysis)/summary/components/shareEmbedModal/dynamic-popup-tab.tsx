100bae39d2db5531b238908426b043b78d5d06f2
Dhruwang <dhruwangjariwala18@gmail.com>
2025-07-15 17:18:25 +0530

    tweak

--- apps/web/app/(app)/environments/[environmentId]/surveys/[surveyId]/(analysis)/summary/components/shareEmbedModal/dynamic-popup-tab.tsx
+++ apps/web/app/(app)/environments/[environmentId]/surveys/[surveyId]/(analysis)/summary/components/shareEmbedModal/dynamic-popup-tab.tsx
@@ -14,37 +14,33 @@ export const DynamicPopupTab = ({ environmentId, surveyId }: DynamicPopupTabProp
   const { t } = useTranslate();
 
   return (
-    <>
-      <div className="flex h-full flex-col justify-between space-y-4">
-        <Alert variant="info" size="default">
-          <AlertTitle>{t("environments.surveys.share.dynamic_popup.alert_title")}</AlertTitle>
-          <AlertDescription>
-            {t("environments.surveys.share.dynamic_popup.alert_description")}
-          </AlertDescription>
-          <AlertButton asChild>
-            <Link href={`/environments/${environmentId}/surveys/${surveyId}/edit`}>
-              {t("environments.surveys.share.dynamic_popup.alert_button")}
-            </Link>
-          </AlertButton>
-        </Alert>
+    <div className="flex h-full flex-col justify-between space-y-4">
+      <Alert variant="info" size="default">
+        <AlertTitle>{t("environments.surveys.share.dynamic_popup.alert_title")}</AlertTitle>
+        <AlertDescription>{t("environments.surveys.share.dynamic_popup.alert_description")}</AlertDescription>
+        <AlertButton asChild>
+          <Link href={`/environments/${environmentId}/surveys/${surveyId}/edit`}>
+            {t("environments.surveys.share.dynamic_popup.alert_button")}
+          </Link>
+        </AlertButton>
+      </Alert>
 
-        <DocumentationLinks
-          links={[
-            {
-              title: t("environments.surveys.share.dynamic_popup.attribute_based_targeting"),
-              href: "https://formbricks.com/docs/xm-and-surveys/surveys/website-app-surveys/advanced-targeting",
-            },
-            {
-              title: t("environments.surveys.share.dynamic_popup.code_no_code_triggers"),
-              href: "https://formbricks.com/docs/xm-and-surveys/surveys/website-app-surveys/actions",
-            },
-            {
-              title: t("environments.surveys.share.dynamic_popup.recontact_options"),
-              href: "https://formbricks.com/docs/xm-and-surveys/surveys/website-app-surveys/recontact",
-            },
-          ]}
-        />
-      </div>
-    </>
+      <DocumentationLinks
+        links={[
+          {
+            title: t("environments.surveys.share.dynamic_popup.attribute_based_targeting"),
+            href: "https://formbricks.com/docs/xm-and-surveys/surveys/website-app-surveys/advanced-targeting",
+          },
+          {
+            title: t("environments.surveys.share.dynamic_popup.code_no_code_triggers"),
+            href: "https://formbricks.com/docs/xm-and-surveys/surveys/website-app-surveys/actions",
+          },
+          {
+            title: t("environments.surveys.share.dynamic_popup.recontact_options"),
+            href: "https://formbricks.com/docs/xm-and-surveys/surveys/website-app-surveys/recontact",
+          },
+        ]}
+      />
+    </div>
   );
 };
