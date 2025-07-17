bfb1a18229c6e6f2c201eccf8b7d42071d8fd91d
Jakob Schott <jakob@formbricks.com>
2025-07-17 10:21:28 +0200

    included cursor feedback

--- apps/web/modules/ui/components/tab-nav/index.tsx
+++ apps/web/modules/ui/components/tab-nav/index.tsx
@@ -15,9 +15,10 @@ interface NavProps {
   activeId: string;
   setActiveId: (id: string) => void;
   activeTabClassName?: string;
+  disabled?: boolean;
 }
 
-const Nav: React.FC<NavProps> = ({ tabs, activeId, setActiveId, activeTabClassName }) => {
+const Nav: React.FC<NavProps> = ({ tabs, activeId, setActiveId, activeTabClassName, disabled }) => {
   return (
     <nav className="flex h-full items-center space-x-3" aria-label="Tabs">
       {tabs.map((tab) => (
@@ -25,11 +26,14 @@ const Nav: React.FC<NavProps> = ({ tabs, activeId, setActiveId, activeTabClassNa
           type="button"
           key={tab.id}
           onClick={() => setActiveId(tab.id)}
+          disabled={disabled}
           className={cn(
-            tab.id === activeId
-              ? `border-brand-dark text-primary border-b-2 font-semibold ${activeTabClassName}`
-              : "text-slate-500 hover:text-slate-700",
-            "flex h-full items-center px-3 text-sm font-medium"
+            "flex h-full items-center px-3 text-sm font-medium",
+            disabled
+              ? "cursor-not-allowed text-slate-400"
+              : tab.id === activeId
+                ? `border-brand-dark text-primary border-b-2 font-semibold ${activeTabClassName}`
+                : "text-slate-500 hover:text-slate-700"
           )}
           aria-current={tab.id === activeId ? "page" : undefined}>
           {tab.icon && <div className="flex h-5 w-5 items-center">{tab.icon}</div>}
@@ -40,7 +44,13 @@ const Nav: React.FC<NavProps> = ({ tabs, activeId, setActiveId, activeTabClassNa
   );
 };
 
-export const TabNav: React.FC<TabNavProps> = ({ tabs, activeId, setActiveId, activeTabClassName }) => {
+export const TabNav: React.FC<TabNavProps> = ({
+  tabs,
+  activeId,
+  setActiveId,
+  activeTabClassName,
+  disabled,
+}) => {
   return (
     <div className={cn("flex h-14 w-full items-center justify-center rounded-t-md")}>
       <Nav
@@ -48,6 +58,7 @@ export const TabNav: React.FC<TabNavProps> = ({ tabs, activeId, setActiveId, act
         activeId={activeId}
         setActiveId={setActiveId}
         activeTabClassName={activeTabClassName}
+        disabled={disabled}
       />
     </div>
   );
