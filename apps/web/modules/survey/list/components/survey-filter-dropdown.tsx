244b5fb1ffde29298bc6712efd134811993bbd46
Dhruwang <dhruwangjariwala18@gmail.com>
2025-07-21 16:46:05 +0530

    tweaks

--- apps/web/modules/survey/list/components/survey-filter-dropdown.tsx
+++ apps/web/modules/survey/list/components/survey-filter-dropdown.tsx
@@ -7,7 +7,6 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/modules/ui/components/dropdown-menu";
-import { useTranslate } from "@tolgee/react";
 import { ChevronDownIcon } from "lucide-react";
 import { TFilterOption } from "@formbricks/types/surveys/types";
 
@@ -30,7 +29,6 @@ export const SurveyFilterDropdown = ({
   isOpen,
   toggleDropdown,
 }: SurveyFilterDropdownProps) => {
-  const { t } = useTranslate();
   const triggerClasses = `surveyFilterDropdown min-w-auto h-8 rounded-md border border-slate-700 sm:px-2 cursor-pointer outline-none 
     ${selectedOptions.length > 0 ? "bg-slate-900 text-white" : "hover:bg-slate-900"}`;
 
@@ -56,7 +54,7 @@ export const SurveyFilterDropdown = ({
                 checked={selectedOptions.includes(option.value)}
                 className={`bg-white ${selectedOptions.includes(option.value) ? "bg-brand-dark border-none" : ""}`}
               />
-              <p className="font-normal text-white">{t(option.label)}</p>
+              <p className="font-normal text-white">{option.label}</p>
             </div>
           </DropdownMenuItem>
         ))}
