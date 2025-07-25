6d916bfb01faddce15a1d2856dbad58d705048ca
Jakob Schott <jakob@formbricks.com>
2025-07-25 13:33:35 +0200

    made consent non-sticky

--- packages/surveys/src/components/questions/consent-question.tsx
+++ packages/surveys/src/components/questions/consent-question.tsx
@@ -80,44 +80,42 @@ export function ConsentQuestion({
             htmlString={getLocalizedValue(question.html, languageCode) || ""}
             questionId={question.id}
           />
-          <div className="fb-bg-survey-bg fb-sticky -fb-bottom-2 fb-z-10 fb-w-full fb-px-1 fb-py-1">
-            <label
-              ref={consentRef}
-              dir="auto"
-              tabIndex={isCurrent ? 0 : -1}
-              id={`${question.id}-label`}
-              onKeyDown={(e) => {
-                // Accessibility: if spacebar was pressed pass this down to the input
-                if (e.key === " ") {
-                  e.preventDefault();
-                  document.getElementById(question.id)?.click();
-                  document.getElementById(`${question.id}-label`)?.focus();
+          <label
+            ref={consentRef}
+            dir="auto"
+            tabIndex={isCurrent ? 0 : -1}
+            id={`${question.id}-label`}
+            onKeyDown={(e) => {
+              // Accessibility: if spacebar was pressed pass this down to the input
+              if (e.key === " ") {
+                e.preventDefault();
+                document.getElementById(question.id)?.click();
+                document.getElementById(`${question.id}-label`)?.focus();
+              }
+            }}
+            className="fb-border-border fb-bg-input-bg fb-text-heading hover:fb-bg-input-bg-selected focus:fb-bg-input-bg-selected focus:fb-ring-brand fb-rounded-custom fb-relative fb-z-10 fb-my-2 fb-flex fb-w-full fb-cursor-pointer fb-items-center fb-border fb-p-4 fb-text-sm focus:fb-outline-none focus:fb-ring-2 focus:fb-ring-offset-2">
+            <input
+              tabIndex={-1}
+              type="checkbox"
+              id={question.id}
+              name={question.id}
+              value={getLocalizedValue(question.label, languageCode)}
+              onChange={(e) => {
+                if (e.target instanceof HTMLInputElement && e.target.checked) {
+                  onChange({ [question.id]: "accepted" });
+                } else {
+                  onChange({ [question.id]: "" });
                 }
               }}
-              className="fb-border-border fb-bg-input-bg fb-text-heading hover:fb-bg-input-bg-selected focus:fb-bg-input-bg-selected focus:fb-ring-brand fb-rounded-custom fb-relative fb-z-10 fb-my-2 fb-flex fb-w-full fb-cursor-pointer fb-items-center fb-border fb-p-4 fb-text-sm focus:fb-outline-none focus:fb-ring-2 focus:fb-ring-offset-2">
-              <input
-                tabIndex={-1}
-                type="checkbox"
-                id={question.id}
-                name={question.id}
-                value={getLocalizedValue(question.label, languageCode)}
-                onChange={(e) => {
-                  if (e.target instanceof HTMLInputElement && e.target.checked) {
-                    onChange({ [question.id]: "accepted" });
-                  } else {
-                    onChange({ [question.id]: "" });
-                  }
-                }}
-                checked={value === "accepted"}
-                className="fb-border-brand fb-text-brand fb-h-4 fb-w-4 fb-border focus:fb-ring-0 focus:fb-ring-offset-0"
-                aria-labelledby={`${question.id}-label`}
-                required={question.required}
-              />
-              <span id={`${question.id}-label`} className="fb-ml-3 fb-mr-3 fb-font-medium">
-                {getLocalizedValue(question.label, languageCode)}
-              </span>
-            </label>
-          </div>
+              checked={value === "accepted"}
+              className="fb-border-brand fb-text-brand fb-h-4 fb-w-4 fb-border focus:fb-ring-0 focus:fb-ring-offset-0"
+              aria-labelledby={`${question.id}-label`}
+              required={question.required}
+            />
+            <span id={`${question.id}-label`} className="fb-ml-3 fb-mr-3 fb-font-medium">
+              {getLocalizedValue(question.label, languageCode)}
+            </span>
+          </label>
           <div className="fb-flex fb-flex-row-reverse fb-w-full fb-justify-between fb-px-6 fb-pt-4">
             <SubmitButton
               tabIndex={isCurrent ? 0 : -1}
