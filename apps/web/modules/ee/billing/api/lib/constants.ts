a27a2a67c8e108cf70617e3ed6078140d1cd28ff
Harsh Bhat <90265455+harshsbhat@users.noreply.github.com>
2025-08-11 17:43:43 +0530

    chore: Change pricing form link (#6394)

--- apps/web/modules/ee/billing/api/lib/constants.ts
+++ apps/web/modules/ee/billing/api/lib/constants.ts
@@ -77,7 +77,7 @@ export const getCloudPricingData = (t: TFnType): { plans: TPricingPlan[] } => {
       t("environments.settings.billing.uptime_sla_99"),
       t("environments.settings.billing.premium_support_with_slas"),
     ],
-    href: "https://app.formbricks.com/s/cm7k8esy20001jp030fh8a9o5?source=billingView&delivery=cloud",
+    href: "https://formbricks.com/custom-plan?source=billingView",
   };
 
   return {
