1604fd2d3e9a7c5264ef65fb01bbff0e79dc36f7
Piyush Gupta <piyushguptaa2z123@gmail.com>
2025-08-29 13:52:17 +0530

    feat: added quota evaluation on update repsonse

---
+++ apps/web/app/api/v1/client/[environmentId]/responses/[responseId]/lib/response.ts
@@ -0,0 +1,40 @@
+import { TResponseWithQuotas } from "@/app/api/v1/client/[environmentId]/responses/lib/response";
+import { updateResponse } from "@/lib/response/service";
+import { getSurvey } from "@/lib/survey/service";
+import { getQuotas } from "@/modules/ee/quotas/lib/quotas";
+import { evaluateQuotas, handleQuotas } from "@/modules/ee/quotas/lib/utils";
+import { logger } from "@formbricks/logger";
+import { TResponseUpdateInput } from "@formbricks/types/responses";
+
+export const updateResponseWithQuotaEvaluation = async (
+  responseId: string,
+  responseInput: TResponseUpdateInput
+): Promise<TResponseWithQuotas> => {
+  const response = await updateResponse(responseId, responseInput);
+
+  try {
+    const [survey, quotas] = await Promise.all([getSurvey(response.surveyId), getQuotas(response.surveyId)]);
+
+    if (!survey || !quotas || quotas.length === 0) {
+      return response;
+    }
+
+    const result = evaluateQuotas(
+      survey,
+      response.data,
+      response.variables || {},
+      quotas,
+      response.language || "default"
+    );
+
+    const quotaFull = await handleQuotas(response.surveyId, response.id, result);
+
+    return {
+      ...response,
+      ...(quotaFull && { quotaFull }),
+    };
+  } catch (error) {
+    logger.error({ error, responseId: response.id }, "Error evaluating quotas for response update");
+    return response;
+  }
+};
