2580ba8d97485aa36a86d812a83d345e57cb0d63
Piyush Gupta <piyushguptaa2z123@gmail.com>
2025-09-24 09:59:24 +0530

    feat: lingodotdev setup

---
+++ apps/web/lingodotdev/config.ts
@@ -0,0 +1,24 @@
+import i18n from "i18next";
+import ICU from "i18next-icu";
+import resourcesToBackend from "i18next-resources-to-backend";
+import { initReactI18next } from "react-i18next";
+import { DEFAULT_LANGUAGE } from "@/lingodotdev/shared";
+
+const isServer = typeof window === "undefined";
+
+if (!isServer) {
+  i18n
+    .use(ICU)
+    .use(initReactI18next)
+    .use(
+      resourcesToBackend((language: string) => {
+        return import(`../locales/${language}.json`);
+      })
+    )
+    .init({
+      fallbackLng: DEFAULT_LANGUAGE,
+      interpolation: { escapeValue: false },
+    });
+}
+
+export default i18n;
---
+++ apps/web/lingodotdev/server.ts
@@ -0,0 +1,31 @@
+import { createInstance } from "i18next";
+import ICU from "i18next-icu";
+import resourcesToBackend from "i18next-resources-to-backend";
+import { initReactI18next } from "react-i18next/initReactI18next";
+import { DEFAULT_LOCALE } from "@/lib/constants";
+import { getLocale } from "@/lingodotdev/language";
+import { DEFAULT_LANGUAGE } from "@/lingodotdev/shared";
+
+const initI18next = async (lng: string) => {
+  const i18nInstance = createInstance();
+  await i18nInstance
+    .use(ICU)
+    .use(initReactI18next)
+    .use(resourcesToBackend((language: string) => import(`../locales/${language}.json`)))
+    .init({
+      lng,
+      fallbackLng: DEFAULT_LANGUAGE,
+      interpolation: {
+        escapeValue: false,
+      },
+    });
+  return i18nInstance;
+};
+
+export async function getTranslate() {
+  const locale = await getLocale();
+  const lng = locale || DEFAULT_LOCALE;
+
+  const i18nextInstance = await initI18next(lng);
+  return i18nextInstance.getFixedT(lng);
+}
