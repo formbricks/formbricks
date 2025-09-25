d41bc7a6169c8ac278acc56cbc7997d3c3e75377
Piyush Gupta <piyushguptaa2z123@gmail.com>
2025-09-25 13:07:34 +0530

    refactor: add server-side check for i18n initialization to prevent multiple initializations

--- apps/web/lingodotdev/config.ts
+++ apps/web/lingodotdev/config.ts
@@ -4,17 +4,25 @@ import resourcesToBackend from "i18next-resources-to-backend";
 import { initReactI18next } from "react-i18next";
 import { DEFAULT_LANGUAGE } from "@/lingodotdev/shared";
 
-i18n
-  .use(ICU)
-  .use(initReactI18next)
-  .use(
-    resourcesToBackend((language: string) => {
-      return import(`../locales/${language}.json`);
-    })
-  )
-  .init({
-    fallbackLng: DEFAULT_LANGUAGE,
-    interpolation: { escapeValue: false },
-  });
+let isInit = false;
+
+const isServer = typeof globalThis.window === "undefined";
+
+if (!isServer && !isInit) {
+  i18n
+    .use(ICU)
+    .use(initReactI18next)
+    .use(
+      resourcesToBackend((language: string) => {
+        return import(`../locales/${language}.json`);
+      })
+    )
+    .init({
+      fallbackLng: DEFAULT_LANGUAGE,
+      interpolation: { escapeValue: false },
+    });
+
+  isInit = true;
+}
 
 export default i18n;
