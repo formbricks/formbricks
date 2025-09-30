4d5adc03736d0ab8ebe42d799fa81791634327de
Dhruwang <dhruwangjariwala18@gmail.com>
2025-09-30 17:49:33 +0530

    empty commit

--- apps/web/modules/ui/components/breadcrumb/index.tsx
+++ apps/web/modules/ui/components/breadcrumb/index.tsx
@@ -83,6 +83,7 @@ const BreadcrumbEllipsis = ({ className, ...props }: React.ComponentProps<"span"
     <span className="sr-only">More</span>
   </span>
 );
+
 BreadcrumbEllipsis.displayName = "BreadcrumbElipssis";
 
 export {
