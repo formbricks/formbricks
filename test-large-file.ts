0d70b2055332793382300fcf188952c4cfeb9f35
Victor Santos <victor@formbricks.com>
2025-10-09 17:59:08 -0300

    Add large test file to trigger PR size warning

---
+++ test-large-file.ts
@@ -0,0 +1,327 @@
+// Large dummy file for testing PR size warnings
+// This file contains repetitive code to simulate a large PR
+
+export interface DummyUser {
+  id: string;
+  name: string;
+  email: string;
+  age: number;
+  address: string;
+  city: string;
+  country: string;
+  phone: string;
+}
+
+export class DummyService1 {
+  constructor() {}
+  
+  method1() { return 'dummy1'; }
+  method2() { return 'dummy2'; }
+  method3() { return 'dummy3'; }
+  method4() { return 'dummy4'; }
+  method5() { return 'dummy5'; }
+  method6() { return 'dummy6'; }
+  method7() { return 'dummy7'; }
+  method8() { return 'dummy8'; }
+  method9() { return 'dummy9'; }
+  method10() { return 'dummy10'; }
+}
+
+export class DummyService2 {
+  constructor() {}
+  
+  method1() { return 'dummy1'; }
+  method2() { return 'dummy2'; }
+  method3() { return 'dummy3'; }
+  method4() { return 'dummy4'; }
+  method5() { return 'dummy5'; }
+  method6() { return 'dummy6'; }
+  method7() { return 'dummy7'; }
+  method8() { return 'dummy8'; }
+  method9() { return 'dummy9'; }
+  method10() { return 'dummy10'; }
+}
+
+export class DummyService3 {
+  constructor() {}
+  
+  method1() { return 'dummy1'; }
+  method2() { return 'dummy2'; }
+  method3() { return 'dummy3'; }
+  method4() { return 'dummy4'; }
+  method5() { return 'dummy5'; }
+  method6() { return 'dummy6'; }
+  method7() { return 'dummy7'; }
+  method8() { return 'dummy8'; }
+  method9() { return 'dummy9'; }
+  method10() { return 'dummy10'; }
+}
+
+export class DummyService4 {
+  constructor() {}
+  
+  method1() { return 'dummy1'; }
+  method2() { return 'dummy2'; }
+  method3() { return 'dummy3'; }
+  method4() { return 'dummy4'; }
+  method5() { return 'dummy5'; }
+  method6() { return 'dummy6'; }
+  method7() { return 'dummy7'; }
+  method8() { return 'dummy8'; }
+  method9() { return 'dummy9'; }
+  method10() { return 'dummy10'; }
+}
+
+export class DummyService5 {
+  constructor() {}
+  
+  method1() { return 'dummy1'; }
+  method2() { return 'dummy2'; }
+  method3() { return 'dummy3'; }
+  method4() { return 'dummy4'; }
+  method5() { return 'dummy5'; }
+  method6() { return 'dummy6'; }
+  method7() { return 'dummy7'; }
+  method8() { return 'dummy8'; }
+  method9() { return 'dummy9'; }
+  method10() { return 'dummy10'; }
+}
+
+export class DummyService6 {
+  constructor() {}
+  
+  method1() { return 'dummy1'; }
+  method2() { return 'dummy2'; }
+  method3() { return 'dummy3'; }
+  method4() { return 'dummy4'; }
+  method5() { return 'dummy5'; }
+  method6() { return 'dummy6'; }
+  method7() { return 'dummy7'; }
+  method8() { return 'dummy8'; }
+  method9() { return 'dummy9'; }
+  method10() { return 'dummy10'; }
+}
+
+export class DummyService7 {
+  constructor() {}
+  
+  method1() { return 'dummy1'; }
+  method2() { return 'dummy2'; }
+  method3() { return 'dummy3'; }
+  method4() { return 'dummy4'; }
+  method5() { return 'dummy5'; }
+  method6() { return 'dummy6'; }
+  method7() { return 'dummy7'; }
+  method8() { return 'dummy8'; }
+  method9() { return 'dummy9'; }
+  method10() { return 'dummy10'; }
+}
+
+export class DummyService8 {
+  constructor() {}
+  
+  method1() { return 'dummy1'; }
+  method2() { return 'dummy2'; }
+  method3() { return 'dummy3'; }
+  method4() { return 'dummy4'; }
+  method5() { return 'dummy5'; }
+  method6() { return 'dummy6'; }
+  method7() { return 'dummy7'; }
+  method8() { return 'dummy8'; }
+  method9() { return 'dummy9'; }
+  method10() { return 'dummy10'; }
+}
+
+export class DummyService9 {
+  constructor() {}
+  
+  method1() { return 'dummy1'; }
+  method2() { return 'dummy2'; }
+  method3() { return 'dummy3'; }
+  method4() { return 'dummy4'; }
+  method5() { return 'dummy5'; }
+  method6() { return 'dummy6'; }
+  method7() { return 'dummy7'; }
+  method8() { return 'dummy8'; }
+  method9() { return 'dummy9'; }
+  method10() { return 'dummy10'; }
+}
+
+export class DummyService10 {
+  constructor() {}
+  
+  method1() { return 'dummy1'; }
+  method2() { return 'dummy2'; }
+  method3() { return 'dummy3'; }
+  method4() { return 'dummy4'; }
+  method5() { return 'dummy5'; }
+  method6() { return 'dummy6'; }
+  method7() { return 'dummy7'; }
+  method8() { return 'dummy8'; }
+  method9() { return 'dummy9'; }
+  method10() { return 'dummy10'; }
+}
+
+export class DummyService11 {
+  constructor() {}
+  
+  method1() { return 'dummy1'; }
+  method2() { return 'dummy2'; }
+  method3() { return 'dummy3'; }
+  method4() { return 'dummy4'; }
+  method5() { return 'dummy5'; }
+  method6() { return 'dummy6'; }
+  method7() { return 'dummy7'; }
+  method8() { return 'dummy8'; }
+  method9() { return 'dummy9'; }
+  method10() { return 'dummy10'; }
+}
+
+export class DummyService12 {
+  constructor() {}
+  
+  method1() { return 'dummy1'; }
+  method2() { return 'dummy2'; }
+  method3() { return 'dummy3'; }
+  method4() { return 'dummy4'; }
+  method5() { return 'dummy5'; }
+  method6() { return 'dummy6'; }
+  method7() { return 'dummy7'; }
+  method8() { return 'dummy8'; }
+  method9() { return 'dummy9'; }
+  method10() { return 'dummy10'; }
+}
+
+export class DummyService13 {
+  constructor() {}
+  
+  method1() { return 'dummy1'; }
+  method2() { return 'dummy2'; }
+  method3() { return 'dummy3'; }
+  method4() { return 'dummy4'; }
+  method5() { return 'dummy5'; }
+  method6() { return 'dummy6'; }
+  method7() { return 'dummy7'; }
+  method8() { return 'dummy8'; }
+  method9() { return 'dummy9'; }
+  method10() { return 'dummy10'; }
+}
+
+export class DummyService14 {
+  constructor() {}
+  
+  method1() { return 'dummy1'; }
+  method2() { return 'dummy2'; }
+  method3() { return 'dummy3'; }
+  method4() { return 'dummy4'; }
+  method5() { return 'dummy5'; }
+  method6() { return 'dummy6'; }
+  method7() { return 'dummy7'; }
+  method8() { return 'dummy8'; }
+  method9() { return 'dummy9'; }
+  method10() { return 'dummy10'; }
+}
+
+export class DummyService15 {
+  constructor() {}
+  
+  method1() { return 'dummy1'; }
+  method2() { return 'dummy2'; }
+  method3() { return 'dummy3'; }
+  method4() { return 'dummy4'; }
+  method5() { return 'dummy5'; }
+  method6() { return 'dummy6'; }
+  method7() { return 'dummy7'; }
+  method8() { return 'dummy8'; }
+  method9() { return 'dummy9'; }
+  method10() { return 'dummy10'; }
+}
+
+export class DummyService16 {
+  constructor() {}
+  
+  method1() { return 'dummy1'; }
+  method2() { return 'dummy2'; }
+  method3() { return 'dummy3'; }
+  method4() { return 'dummy4'; }
+  method5() { return 'dummy5'; }
+  method6() { return 'dummy6'; }
+  method7() { return 'dummy7'; }
+  method8() { return 'dummy8'; }
+  method9() { return 'dummy9'; }
+  method10() { return 'dummy10'; }
+}
+
+export class DummyService17 {
+  constructor() {}
+  
+  method1() { return 'dummy1'; }
+  method2() { return 'dummy2'; }
+  method3() { return 'dummy3'; }
+  method4() { return 'dummy4'; }
+  method5() { return 'dummy5'; }
+  method6() { return 'dummy6'; }
+  method7() { return 'dummy7'; }
+  method8() { return 'dummy8'; }
+  method9() { return 'dummy9'; }
+  method10() { return 'dummy10'; }
+}
+
+export class DummyService18 {
+  constructor() {}
+  
+  method1() { return 'dummy1'; }
+  method2() { return 'dummy2'; }
+  method3() { return 'dummy3'; }
+  method4() { return 'dummy4'; }
+  method5() { return 'dummy5'; }
+  method6() { return 'dummy6'; }
+  method7() { return 'dummy7'; }
+  method8() { return 'dummy8'; }
+  method9() { return 'dummy9'; }
+  method10() { return 'dummy10'; }
+}
+
+export class DummyService19 {
+  constructor() {}
+  
+  method1() { return 'dummy1'; }
+  method2() { return 'dummy2'; }
+  method3() { return 'dummy3'; }
+  method4() { return 'dummy4'; }
+  method5() { return 'dummy5'; }
+  method6() { return 'dummy6'; }
+  method7() { return 'dummy7'; }
+  method8() { return 'dummy8'; }
+  method9() { return 'dummy9'; }
+  method10() { return 'dummy10'; }
+}
+
+export class DummyService20 {
+  constructor() {}
+  
+  method1() { return 'dummy1'; }
+  method2() { return 'dummy2'; }
+  method3() { return 'dummy3'; }
+  method4() { return 'dummy4'; }
+  method5() { return 'dummy5'; }
+  method6() { return 'dummy6'; }
+  method7() { return 'dummy7'; }
+  method8() { return 'dummy8'; }
+  method9() { return 'dummy9'; }
+  method10() { return 'dummy10'; }
+}
+
+export const dummyConstants = {
+  VALUE_1: 'value1',
+  VALUE_2: 'value2',
+  VALUE_3: 'value3',
+  VALUE_4: 'value4',
+  VALUE_5: 'value5',
+  VALUE_6: 'value6',
+  VALUE_7: 'value7',
+  VALUE_8: 'value8',
+  VALUE_9: 'value9',
+  VALUE_10: 'value10',
+};
+
