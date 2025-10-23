95bbe24109e73ee6859f19adab20330c5974316b
Johannes <johannes@formbricks.com>
2025-10-23 19:34:03 +0200

    remove not needed migration

--- packages/database/migration/20251120000000_rename_enterprise_to_custom/migration.sql
+++
@@ -1,8 +0,0 @@
--- Rename billing plan from 'enterprise' to 'custom' for all existing organizations
-UPDATE "Organization"
-SET billing = jsonb_set(
-  billing,
-  '{plan}',
-  '"custom"'
-)
-WHERE billing->>'plan' = 'enterprise';
