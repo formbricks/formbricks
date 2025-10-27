5b44896683a7303bc229d7c9248f7a7e02b489a0
Johannes <johannes@formbricks.com>
2025-10-27 13:59:28 +0100

    preempt merge conflicht with Stripe PR

--- apps/web/modules/survey/follow-ups/lib/utils.ts
+++ apps/web/modules/survey/follow-ups/lib/utils.ts
@@ -4,6 +4,6 @@ import { IS_FORMBRICKS_CLOUD, PROJECT_FEATURE_KEYS } from "@/lib/constants";
 export const getSurveyFollowUpsPermission = async (
   billingPlan: Organization["billing"]["plan"]
 ): Promise<boolean> => {
-  if (IS_FORMBRICKS_CLOUD) return billingPlan === PROJECT_FEATURE_KEYS.ENTERPRISE;
+  if (IS_FORMBRICKS_CLOUD) return billingPlan === PROJECT_FEATURE_KEYS.CUSTOM;
   return true;
 };
