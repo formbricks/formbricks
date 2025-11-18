718a199d5bdc54f9d33c844da721e97387a732d8
Johannes <72809645+jobenjada@users.noreply.github.com>
2025-11-17 21:37:23 -0800

    feat: add Personal Link generation UI (#6819)
    Co-authored-by: Dhruwang <dhruwangjariwala18@gmail.com>

--- apps/web/modules/ee/contacts/[contactId]/page.tsx
+++ apps/web/modules/ee/contacts/[contactId]/page.tsx
@@ -1,9 +1,10 @@
 import { getTagsByEnvironmentId } from "@/lib/tag/service";
 import { getTranslate } from "@/lingodotdev/server";
 import { AttributesSection } from "@/modules/ee/contacts/[contactId]/components/attributes-section";
-import { DeleteContactButton } from "@/modules/ee/contacts/[contactId]/components/delete-contact-button";
+import { ContactControlBar } from "@/modules/ee/contacts/[contactId]/components/contact-control-bar";
 import { getContactAttributes } from "@/modules/ee/contacts/lib/contact-attributes";
 import { getContact } from "@/modules/ee/contacts/lib/contacts";
+import { getPublishedLinkSurveys } from "@/modules/ee/contacts/lib/surveys";
 import { getContactIdentifier } from "@/modules/ee/contacts/lib/utils";
 import { getIsQuotasEnabled } from "@/modules/ee/license-check/lib/utils";
 import { getEnvironmentAuth } from "@/modules/environments/lib/utils";
@@ -19,10 +20,11 @@ export const SingleContactPage = async (props: {
 
   const { environment, isReadOnly, organization } = await getEnvironmentAuth(params.environmentId);
 
-  const [environmentTags, contact, contactAttributes] = await Promise.all([
+  const [environmentTags, contact, contactAttributes, publishedLinkSurveys] = await Promise.all([
     getTagsByEnvironmentId(params.environmentId),
     getContact(params.contactId),
     getContactAttributes(params.contactId),
+    getPublishedLinkSurveys(params.environmentId),
   ]);
 
   if (!contact) {
@@ -31,20 +33,21 @@ export const SingleContactPage = async (props: {
 
   const isQuotasAllowed = await getIsQuotasEnabled(organization.billing.plan);
 
-  const getDeletePersonButton = () => {
+  const getContactControlBar = () => {
     return (
-      <DeleteContactButton
+      <ContactControlBar
         environmentId={environment.id}
         contactId={params.contactId}
         isReadOnly={isReadOnly}
         isQuotasAllowed={isQuotasAllowed}
+        publishedLinkSurveys={publishedLinkSurveys}
       />
     );
   };
 
   return (
     <PageContentWrapper>
-      <PageHeader pageTitle={getContactIdentifier(contactAttributes)} cta={getDeletePersonButton()} />
+      <PageHeader pageTitle={getContactIdentifier(contactAttributes)} cta={getContactControlBar()} />
       <section className="pb-24 pt-6">
         <div className="grid grid-cols-4 gap-x-8">
           <AttributesSection contactId={params.contactId} />
