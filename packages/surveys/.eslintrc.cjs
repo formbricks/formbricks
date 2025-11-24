a2897a242c5a3def2dbf689f42c183356993a1a1
Dhruwang <dhruwangjariwala18@gmail.com>
2025-11-24 16:29:39 +0530

    storybook setup for surveys package

--- packages/surveys/.eslintrc.cjs
+++ packages/surveys/.eslintrc.cjs
@@ -1,4 +1,7 @@
 module.exports = {
-  extends: ["@formbricks/eslint-config/legacy-react.js"],
+  extends: [
+    "@formbricks/eslint-config/legacy-react.js",
+    "plugin:storybook/recommended"
+  ],
   parser: "@typescript-eslint/parser",
 };
--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,1 +1,36 @@
-export default ["packages/*/vite.config.{ts,mts}", "apps/**/vite.config.{ts,mts}"];
+import { storybookTest } from "@storybook/addon-vitest/vitest-plugin";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import { defineWorkspace } from "vitest/config";
+
+const dirname = typeof __dirname !== "undefined" ? __dirname : path.dirname(fileURLToPath(import.meta.url));
+
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
+export default [
+  "packages/*/vite.config.{ts,mts}",
+  "apps/**/vite.config.{ts,mts}",
+  {
+    extends: "packages/surveys/vite.config.mts",
+    plugins: [
+      // The plugin will run tests for the stories defined in your Storybook config
+      // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+      storybookTest({
+        configDir: path.join(dirname, ".storybook"),
+      }),
+    ],
+    test: {
+      name: "storybook",
+      browser: {
+        enabled: true,
+        headless: true,
+        provider: "playwright",
+        instances: [
+          {
+            browser: "chromium",
+          },
+        ],
+      },
+      setupFiles: ["packages/surveys/.storybook/vitest.setup.ts"],
+    },
+  },
+];
