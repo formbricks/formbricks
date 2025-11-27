2698817adb3008b03494a583174e263a1ee780b8
Dhruwang Jariwala <67850763+Dhruwang@users.noreply.github.com>
2025-11-28 01:40:03 +0530

    fix: language select UI (#6890)
    Co-authored-by: Johannes <johannes@formbricks.com>

--- apps/web/modules/analysis/components/ShareSurveyLink/components/LanguageDropdown.tsx
+++ apps/web/modules/analysis/components/ShareSurveyLink/components/LanguageDropdown.tsx
@@ -15,18 +15,16 @@ interface LanguageDropdownProps {
 
 export const LanguageDropdown = ({ survey, setLanguage, locale }: LanguageDropdownProps) => {
   const [showLanguageSelect, setShowLanguageSelect] = useState(false);
-  const languageDropdownRef = useRef(null);
+  const containerRef = useRef(null);
   const enabledLanguages = getEnabledLanguages(survey.languages ?? []);
 
-  useClickOutside(languageDropdownRef, () => setShowLanguageSelect(false));
+  useClickOutside(containerRef, () => setShowLanguageSelect(false));
 
   return (
     enabledLanguages.length > 1 && (
-      <div className="relative">
+      <div className="relative" ref={containerRef}>
         {showLanguageSelect && (
-          <div
-            className="absolute top-12 z-30 w-fit rounded-lg border bg-slate-900 p-1 text-sm text-white"
-            ref={languageDropdownRef}>
+          <div className="absolute top-12 z-30 max-h-64 max-w-48 overflow-auto rounded-lg border bg-slate-900 p-1 text-sm text-white">
             {enabledLanguages.map((surveyLanguage) => (
               <button
                 key={surveyLanguage.language.code}
