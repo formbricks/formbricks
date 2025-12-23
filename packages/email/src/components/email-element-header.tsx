ae6d1ac1338e67822936b14edf2b0fc409503600
Dhruwang Jariwala <67850763+Dhruwang@users.noreply.github.com>
2025-12-23 15:26:53 +0530

    chore: improve wording in email text (Duplicate of #7003) (#7025)
    Co-authored-by: Balázs Úr <balazs@urbalazs.hu>
    Co-authored-by: Johannes <johannes@formbricks.com>

--- packages/email/src/components/email-element-header.tsx
+++ packages/email/src/components/email-element-header.tsx
@@ -10,11 +10,11 @@ interface ElementHeaderProps {
 export function ElementHeader({ headline, subheader, className }: ElementHeaderProps): React.JSX.Element {
   return (
     <>
-      <Container className={cn("text-question-color m-0 block text-base font-semibold leading-6", className)}>
+      <Container className={cn("text-question-color m-0 block text-base leading-6 font-semibold", className)}>
         <div dangerouslySetInnerHTML={{ __html: headline }} />
       </Container>
       {subheader && (
-        <Container className="text-question-color m-0 mt-2 block p-0 text-sm font-normal leading-6">
+        <Container className="text-question-color m-0 mt-2 block p-0 text-sm leading-6 font-normal">
           <div dangerouslySetInnerHTML={{ __html: subheader }} />
         </Container>
       )}
