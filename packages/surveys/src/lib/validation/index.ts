63e7759682c73b7e26d6bacb6d71f44eadf3286e
Dhruwang <dhruwangjariwala18@gmail.com>
2026-01-21 16:44:12 +0530

    fix: docker build

--- packages/surveys/src/lib/validation/index.ts
+++ packages/surveys/src/lib/validation/index.ts
@@ -1,8 +1,1 @@
-export { validateElementResponse, validateBlockResponses, getFirstErrorMessage } from "./evaluator";
-export { validators } from "./validators";
-export type {
-  TValidatorCheckResult,
-  TValidationErrorMap,
-  TValidationResult,
-  TValidationError,
-} from "@formbricks/types/surveys/validation-rules";
+export { validateBlockResponses } from "./evaluator";
