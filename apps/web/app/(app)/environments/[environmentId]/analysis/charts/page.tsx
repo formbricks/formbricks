1157ff7150b533b5b00a39b76235d1c2ff6edba7
Dhruwang <dhruwangjariwala18@gmail.com>
2026-02-25 14:39:08 +0530

    fix sonar issue

--- apps/web/app/(app)/environments/[environmentId]/analysis/charts/page.tsx
+++ apps/web/app/(app)/environments/[environmentId]/analysis/charts/page.tsx
@@ -1,6 +1,8 @@
 import { ChartsListPage } from "@/modules/ee/analysis/charts/components/charts-list-page";
 
-export default async function ChartsPage({ params }: { params: Promise<{ environmentId: string }> }) {
+export default async function ChartsPage({
+  params,
+}: Readonly<{ params: Promise<{ environmentId: string }> }>) {
   const { environmentId } = await params;
   return <ChartsListPage environmentId={environmentId} />;
 }
