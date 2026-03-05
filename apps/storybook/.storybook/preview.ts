1bc30661498653a3e7a8417972bd6d69e1688437
Matti Nannt <matti@formbricks.com>
2026-03-05 08:46:08 +0100

    chore: remove unused npm dependencies

--- apps/storybook/.storybook/preview.ts
+++ apps/storybook/.storybook/preview.ts
@@ -1,6 +1,5 @@
 import type { Preview } from "@storybook/react-vite";
 import React from "react";
-import "../../../packages/survey-ui/src/styles/globals.css";
 
 const preview: Preview = {
   parameters: {
