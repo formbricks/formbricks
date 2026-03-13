21da0e1b39693a7731bacb1c904415f04065ef5f
Johannes <johannes@formbricks.com>
2026-03-13 17:37:25 +0100

    refactor: rename 'start_free_trial' to 'upgrade_plan' across various components and localization files

--- apps/web/modules/ee/teams/team-list/components/teams-view.tsx
+++ apps/web/modules/ee/teams/team-list/components/teams-view.tsx
@@ -37,7 +37,7 @@ export const TeamsView = async ({
 
   const buttons: [ModalButton, ModalButton] = [
     {
-      text: IS_FORMBRICKS_CLOUD ? t("common.start_free_trial") : t("common.request_trial_license"),
+      text: IS_FORMBRICKS_CLOUD ? t("common.upgrade_plan") : t("common.request_trial_license"),
       href: IS_FORMBRICKS_CLOUD
         ? `/environments/${environmentId}/settings/billing`
         : "https://formbricks.com/docs/self-hosting/license#30-day-trial-license-request",
