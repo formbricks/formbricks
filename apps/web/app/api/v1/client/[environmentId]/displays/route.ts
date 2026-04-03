579ec8f9bf7ede80bc9d6b112484a7f72b8cd71d
Dhruwang <dhruwangjariwala18@gmail.com>
2026-04-03 12:11:53 +0530

    refactor: update client display API to use workspaceId instead of environmentId
    - Changed the import from getOrganizationIdFromEnvironmentId to getOrganizationIdFromWorkspaceId in the display route.
    - Updated the POST handler to retrieve organizationId using workspaceId.
    - Added workspaceId to ZDisplayCreateInput type definition.

--- apps/web/app/api/v1/client/[environmentId]/displays/route.ts
+++ apps/web/app/api/v1/client/[environmentId]/displays/route.ts
@@ -4,7 +4,7 @@ import { ResourceNotFoundError } from "@formbricks/types/errors";
 import { responses } from "@/app/lib/api/response";
 import { transformErrorToDetails } from "@/app/lib/api/validator";
 import { THandlerParams, withV1ApiWrapper } from "@/app/lib/api/with-api-logging";
-import { getOrganizationIdFromEnvironmentId } from "@/lib/utils/helper";
+import { getOrganizationIdFromWorkspaceId } from "@/lib/utils/helper";
 import { resolveClientApiIds } from "@/lib/utils/resolve-client-id";
 import { getIsContactsEnabled } from "@/modules/ee/license-check/lib/utils";
 import { createDisplay } from "./lib/display";
@@ -50,7 +50,7 @@ export const POST = withV1ApiWrapper({
     }
 
     if (inputValidation.data.userId) {
-      const organizationId = await getOrganizationIdFromEnvironmentId(environmentId);
+      const organizationId = await getOrganizationIdFromWorkspaceId(workspaceId);
       const isContactsEnabled = await getIsContactsEnabled(organizationId);
       if (!isContactsEnabled) {
         return {
