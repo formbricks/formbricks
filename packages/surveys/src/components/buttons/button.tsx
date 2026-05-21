fb4bf7ca6d837a2229bae78e37bd1a6cce47cd71
Javi Aguilar <122741+itsjavi@users.noreply.github.com>
2026-05-21 16:28:31 +0200

    fix: back button not readable in dark backgrounds - fixes #7922

---
+++ packages/surveys/src/components/buttons/button.tsx
@@ -0,0 +1,49 @@
+import { ButtonHTMLAttributes, CSSProperties } from "preact";
+import { cn } from "@/lib/utils";
+
+export type ButtonProps = {
+  variant: "primary" | "secondary";
+} & ButtonHTMLAttributes<HTMLButtonElement>;
+
+export function Button({ variant, children, ...rest }: Readonly<ButtonProps>) {
+  const commonStyles: CSSProperties = {
+    borderRadius: "var(--fb-button-border-radius)",
+    height: "var(--fb-button-height)",
+    fontSize: "var(--fb-button-font-size)",
+    fontWeight: "var(--fb-button-font-weight)",
+    paddingLeft: "var(--fb-button-padding-x)",
+    paddingRight: "var(--fb-button-padding-x)",
+    paddingTop: "var(--fb-button-padding-y)",
+    paddingBottom: "var(--fb-button-padding-y)",
+  };
+
+  const variantStyles: Record<ButtonProps["variant"], CSSProperties> = {
+    primary: { backgroundColor: "var(--fb-button-bg-color)", color: "var(--fb-button-text-color)" },
+    secondary: {
+      // secondary uses primary's bg color as text color and vice-versa
+      backgroundColor: "var(--fb-button-text-color) !important",
+      color: "var(--fb-button-bg-color) !important",
+      borderColor: "var(--fb-button-text-color) !important",
+    },
+  };
+
+  return (
+    <button
+      dir="auto"
+      type="button"
+      className={cn([
+        "border-submit-button-border",
+        "mb-1 flex items-center justify-center border leading-4",
+        "focus:ring-focus shadow-xs focus:ring-2 focus:ring-offset-2 focus:outline-hidden",
+        "enabled:hover:opacity-90 disabled:cursor-not-allowed disabled:opacity-60",
+        `button-custom button-${variant}-custom`,
+      ])}
+      style={{
+        ...commonStyles,
+        ...variantStyles[variant],
+      }}
+      {...rest}>
+      {children}
+    </button>
+  );
+}
