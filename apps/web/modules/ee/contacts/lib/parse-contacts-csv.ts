b265a878453a19eb8a282b920c49af0bc34caafb
pandeymangg <anshuman.pandey9999@gmail.com>
2026-05-28 11:08:13 +0530

    bom true

--- apps/web/modules/ee/contacts/lib/parse-contacts-csv.ts
+++ apps/web/modules/ee/contacts/lib/parse-contacts-csv.ts
@@ -2,6 +2,7 @@ import { parse } from "csv-parse/sync";
 
 export const parseContactsCSV = (csv: string): unknown[] => {
   return parse(csv, {
+    bom: true,
     columns: true,
     skip_empty_lines: true,
     delimiter: [",", ";", "\t"],
