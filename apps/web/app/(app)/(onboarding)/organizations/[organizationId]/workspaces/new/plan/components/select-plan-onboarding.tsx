3cab9df24a01afacea629b5c6bfb1f3d5c762d85
Harsh Bhat <harsh121102@gmail.com>
2026-08-11 17:44:00 +0530

    chore: clean up experiment code

--- apps/web/app/(app)/(onboarding)/organizations/[organizationId]/workspaces/new/plan/components/select-plan-onboarding.tsx
+++ apps/web/app/(app)/(onboarding)/organizations/[organizationId]/workspaces/new/plan/components/select-plan-onboarding.tsx
@@ -2,25 +2,14 @@ import { SelectPlanCard } from "@/modules/ee/billing/components/select-plan-card
 
 interface SelectPlanOnboardingProps {
   organizationId: string;
-  featureVariant: "control" | "variant_b";
-  ctaVariant: "control" | "variant_b" | "variant_c" | "variant_d";
 }
 
-export const SelectPlanOnboarding = ({
-  organizationId,
-  featureVariant,
-  ctaVariant,
-}: Readonly<SelectPlanOnboardingProps>) => {
+export const SelectPlanOnboarding = ({ organizationId }: Readonly<SelectPlanOnboardingProps>) => {
   const nextUrl = `/organizations/${organizationId}/workspaces/new/survey`;
 
   return (
     <div className="flex min-h-full min-w-full flex-col items-center justify-center">
-      <SelectPlanCard
-        nextUrl={nextUrl}
-        organizationId={organizationId}
-        featureVariant={featureVariant}
-        ctaVariant={ctaVariant}
-      />
+      <SelectPlanCard nextUrl={nextUrl} organizationId={organizationId} />
     </div>
   );
 };
